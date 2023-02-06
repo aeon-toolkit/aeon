797ea1085e745858d5e6da20dfbb41d47707847c
Martin Walter <mf-walter@web.de>
2023-02-07 00:24:26 +0100

    Move PCATransformer to series transformers

--- sktime/transformations/series/tests/test_pca.py
+++ sktime/transformations/series/tests/test_pca.py
@@ -12,4 +12,8 @@ def test_pca():
     X = _make_series(n_columns=3, return_numpy=False)
     transformer = PCATransformer(n_components=2)
     Xt = transformer.fit_transform(X)
+    # test that the shape is correct
     assert Xt.shape == (X.shape[0], 2)
+    # test that the column names are correct
+    assert "PC_0" in Xt.columns
+    assert "PC_1" in Xt.columns
