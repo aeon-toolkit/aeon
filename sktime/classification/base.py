34c6af7f79d919f9c249467015cfc9ecdb584c51
Tony Bagnall <ajb@uea.ac.uk>
2023-02-11 12:00:33 +0000

    add _estimator_type to docstring

--- sktime/classification/base.py
+++ sktime/classification/base.py
@@ -77,7 +77,6 @@ class BaseClassifier(BaseEstimator, ABC):
         self.X_metadata_ = []  # metadata/properties of X seen in fit
         self.class_dictionary_ = {}
         self._threads_to_use = 1
-
         # required for compatability with some sklearn interfaces
         # i.e. CalibratedClassifierCV
         self._estimator_type = "classifier"
