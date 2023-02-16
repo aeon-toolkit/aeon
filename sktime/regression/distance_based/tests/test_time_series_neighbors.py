69f330b2121ea473dd8f1d2588259dc462075111
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-02-16 16:58:31 +0000

    Hard codes unit test results for kNN regressor

--- sktime/regression/distance_based/tests/test_time_series_neighbors.py
+++ sktime/regression/distance_based/tests/test_time_series_neighbors.py
@@ -13,16 +13,14 @@ def test_knn_neighbors():
     from sklearn.datasets import make_regression
     from sklearn.model_selection import train_test_split
 
-    X, y = make_regression(n_samples=100, n_features=32, random_state=42)
+    X, y = make_regression(n_samples=9, n_features=3, random_state=42)
     X = X.reshape((X.shape[0], 1, X.shape[1]))
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
-    )
+    X_train, X_test, y_train, _ = train_test_split(X, y, test_size=3, random_state=42)
 
     model = KNeighborsTimeSeriesRegressor(n_neighbors=5, weights="distance")
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
+    y_pred_expected = np.array([-216.06541863, -4.54133078, -324.7624233])
 
-    assert isinstance(y_pred, np.ndarray)
-    assert y_pred.shape == y_test.shape
+    assert np.abs(y_pred - y_pred_expected).max() < 1e-6
