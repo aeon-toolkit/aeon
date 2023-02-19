dbe0b92a4fbd2712821f8e4e0f6391b522ed71f6
Martin Walter <mf-walter@web.de>
2023-02-19 13:40:24 +0100

    linting

--- sktime/transformations/series/tests/test_feature_selection.py
+++ sktime/transformations/series/tests/test_feature_selection.py
@@ -58,7 +58,6 @@ def test_feature_selection(method, n_columns, random_state):
             transformer_f1.feature_importances_ != transformer_f2.feature_importances_
         )
 
-
     if method == "random":
         if n_columns is None:
             n_columns = int(math.ceil(X_train.shape[1] / 2))
