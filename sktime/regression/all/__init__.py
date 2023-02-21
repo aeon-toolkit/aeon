3c631b2b818caac0a25353062f996178c9a4a89e
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-02-21 17:59:14 +0000

    missed

--- sktime/regression/all/__init__.py
+++ sktime/regression/all/__init__.py
@@ -6,12 +6,10 @@ __author__ = ["mloning"]
 __all__ = [
     "np",
     "pd",
-    "ComposableTimeSeriesForestRegressor",
     "TimeSeriesForestRegressor",
 ]
 
 import numpy as np
 import pandas as pd
 
-from sktime.regression.compose import ComposableTimeSeriesForestRegressor
 from sktime.regression.interval_based import TimeSeriesForestRegressor
