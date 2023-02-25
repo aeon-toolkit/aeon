5975dcc2d6898113f8b9adc7412d3f2d0b3efb09
Tony Bagnall <ajb@uea.ac.uk>
2023-02-25 16:05:34 +0000

    more notebook dataframes

--- sktime/datasets/_data_io.py
+++ sktime/datasets/_data_io.py
@@ -398,22 +398,11 @@ def load_from_tsfile(
     return_y : boolean, default True
        whether to return the y variable, if it is present.
     return_data_type : str, optional, default = "nested_univ"
-        memory data format specification to return X in.
-        str can be any other supported Panel mtype
-            for list of mtypes, see datatypes.SCITYPE_REGISTER
-            for specifications, see examples/AA_datatypes_and_datasets.ipynb
-        commonly used specifications:
-            "nested_univ: nested pd.DataFrame, pd.Series in cells
-            "numpy3D"/"numpy3d"/"np3D": 3D np.ndarray (instance, variable, time index)
-            "numpy2d"/"np2d"/"numpyflat": 2D np.ndarray (instance, time index)
-            "pd-multiindex": pd.DataFrame with 2-level (instance, time) MultiIndex
-        Exception is raised if the data cannot be stored in the requested type.
+        options are "numpy3d" for equal length or "numpy2d" for univariate
 
     Returns
     -------
-    X : sktime compatible in-memory container of mtype return_data_type
-        for list of mtypes, see datatypes.SCITYPE_REGISTER
-        for specifications, see examples/AA_datatypes_and_datasets.ipynb
+    X : sktime compatible in-memory container of return_data_type
     y : returned only if return_y=True, np.ndarray
 
     Raises
