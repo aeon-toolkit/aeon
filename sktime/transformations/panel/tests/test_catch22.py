a4f0449d0708c658b4b0d5663b0e154b9c211cf7
Tony Bagnall <ajb@uea.ac.uk>
2023-02-26 17:40:12 +0000

    test_tsfresh

--- sktime/transformations/panel/tests/test_catch22.py
+++ sktime/transformations/panel/tests/test_catch22.py
@@ -44,14 +44,14 @@ def test_catch22_wrapper_on_basic_motions():
 
     # fit Catch22Wrapper and assert transformed data is the same
     c22 = Catch22Wrapper(replace_nans=True)
-    data = c22.fit_transform(X_train.iloc[indices])
+    data = c22.fit_transform(X_train[indices])
     testing.assert_array_almost_equal(
         data, catch22wrapper_basic_motions_data, decimal=4
     )
 
     # fit Catch22Wrapper with select features and assert transformed data is the same
     c22 = Catch22Wrapper(replace_nans=True, features=feature_names)
-    data = c22.fit_transform(X_train.iloc[indices])
+    data = c22.fit_transform(X_train[indices])
     testing.assert_array_almost_equal(
         data,
         catch22wrapper_basic_motions_data[
