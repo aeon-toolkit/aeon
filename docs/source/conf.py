32230a7760af2cfff43f16f7415b61c26e41a512
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-02-27 17:33:06 +0000

    furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -190,7 +190,7 @@ def linkcode_resolve(domain, info):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
