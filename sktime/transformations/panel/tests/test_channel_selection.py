f7e226acd6286e676bfdf04a90a693a19aba84e4
Tony Bagnall <ajb@uea.ac.uk>
2023-02-28 19:09:00 +0000

    [ENH] make single problem loaders for equal length problems return numpy arrays (#109)
    Changes the `return_type` from `None` to `numpy3d` and removes the very long comments describing possible data structures in unnecessary detail. Part of #42

--- sktime/transformations/panel/tests/test_channel_selection.py
+++ sktime/transformations/panel/tests/test_channel_selection.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """Channel selection test code."""
-from sktime.datasets import load_basic_motions
 from sktime.transformations.panel.channel_selection import ElbowClassPairwise
+from sktime.utils._testing.panel import make_classification_problem
 
 
 def test_cs_basic_motions():
-    """Test channel selection on basic motions dataset."""
-    X, y = load_basic_motions(split="train", return_X_y=True)
+    """Test channel selection on random nested data frame."""
+    X, y = make_classification_problem(n_instances=10, n_timepoints=10, n_columns=3)
 
     ecp = ElbowClassPairwise()
 
@@ -14,13 +14,15 @@ def test_cs_basic_motions():
 
     # transform the training data
 
-    ecp.transform(X, y)
+    trans_X = ecp.transform(X, y)
 
     # test shape pf transformed data should be (n_samples, n_channels_selected)
-    assert ecp.transform(X, y).shape == (X.shape[0], len(ecp.channels_selected_))
+    assert trans_X.shape == (X.shape[0], len(ecp.channels_selected_))
 
     # test shape of transformed data should be (n_samples, n_channels_selected)
 
-    X_test, y_test = load_basic_motions(split="test", return_X_y=True)
+    X_test, y_test = make_classification_problem(
+        n_instances=10, n_timepoints=10, n_columns=3
+    )
 
     assert ecp.transform(X_test).shape == (X_test.shape[0], len(ecp.channels_selected_))
