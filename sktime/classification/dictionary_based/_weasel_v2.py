601094160853841c013405fe880122c4108d91c2
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2023-03-06 19:47:03 +0100

    remove norm-options from constructor

--- sktime/classification/dictionary_based/_weasel_v2.py
+++ sktime/classification/dictionary_based/_weasel_v2.py
@@ -43,11 +43,6 @@ class WEASEL_V2(BaseClassifier):
     ----------
     min_window : int, default=4,
         Minimal length of the subsequences to compute words from.
-    norm_options : array of bool, default=[False],
-        If the array contains True, words are computed over mean-normed TS
-        If the array contains False, words are computed over raw TS
-        If both are set, words are computed for both.
-        A value will be randomly chosen for each parameter-configuration.
     word_lengths : array of int, default=[7, 8],
         Length of the words to compute. A value will be randomly chosen for each
         parameter-configuration.
@@ -116,7 +111,6 @@ class WEASEL_V2(BaseClassifier):
     def __init__(
         self,
         min_window=4,
-        norm_options=(False),
         word_lengths=(7, 8),
         use_first_differences=(True, False),
         feature_selection="chi2_top_k",
@@ -134,7 +128,13 @@ class WEASEL_V2(BaseClassifier):
         self.lower_bounding = True
         self.remove_repeat_words = False
 
-        self.norm_options = norm_options
+        #     norm_options : array of bool, default=[False],
+        #         If the array contains True, words are computed over mean-normed TS
+        #         If the array contains False, words are computed over raw TS
+        #         If both are set, words are computed for both.
+        #         A value will be randomly chosen for each parameter-configuration.
+        self.norm_options = [False]
+
         self.word_lengths = word_lengths
 
         self.random_state = random_state
