32e4dfceb4123df43bec2e38e949e8c9fd94b94a
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2023-03-08 12:46:11 +0000

    run tests

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -17,16 +17,6 @@ EXCLUDE_ESTIMATORS = [
     "SFA",
     # PlateauFinder seems to be broken, see #2259
     "PlateauFinder",
-    # below are removed due to mac failures we don't fully understand, see #3103
-    "HIVECOTEV1",
-    "HIVECOTEV2",
-    "RandomIntervalSpectralEnsemble",
-    "RandomInvervals",
-    "RandomIntervalSegmenter",
-    "RandomIntervalFeatureExtractor",
-    "RandomIntervalClassifier",
-    "MiniRocket",
-    "MatrixProfileTransformer",
     # tapnet based estimators fail stochastically for unknown reasons, see #3525
     "TapNetRegressor",
     "TapNetClassifier",
