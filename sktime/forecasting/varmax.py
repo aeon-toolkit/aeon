0aa0b6b7b9a5f57a9921e1909bf7da2ba9f7595b
Martin Walter <mf-walter@web.de>
2023-03-10 00:10:56 +0100

    test VARMAX with is_integer_dtype

--- sktime/forecasting/varmax.py
+++ sktime/forecasting/varmax.py
@@ -5,7 +5,7 @@ __author__ = ["KatieBuc"]
 
 import warnings
 
-import pandas as pd
+from pandas.api.types import is_integer_dtype
 
 from sktime.forecasting.base.adapters import _StatsModelsAdapter
 
@@ -371,19 +371,20 @@ class VARMAX(_StatsModelsAdapter):
         # given with an integer index beginning at `start`...
         # but only when out-of-sample forecasting, i.e. when forecasting horizon is
         # greater than zero
-        if pd.__version__ < "2.0.0":
-            if (type(self._y.index) == pd.core.indexes.numeric.Int64Index) & (
-                any(fh.to_relative(self.cutoff) > 0)
-            ):
-                y_pred.index = y_pred.index + self._y.index[0]
-        else:
-            from pandas.api.types import is_any_real_numeric_dtype
+        # if pd.__version__ < "2.0.0":
+        #     if (type(self._y.index) == pd.core.indexes.numeric.Int64Index) & (
+        #         any(fh.to_relative(self.cutoff) > 0)
+        #     ):
+        #         y_pred.index = y_pred.index + self._y.index[0]
+        # else:
+        #     from pandas.api.types import is_any_real_numeric_dtype
 
-            if is_any_real_numeric_dtype(self._y.index) & any(
-                fh.to_relative(self.cutoff) > 0
-            ):
-                y_pred.index = y_pred.index + self._y.index[0]
-
+        #     if is_any_real_numeric_dtype(self._y.index) & any(
+        #         fh.to_relative(self.cutoff) > 0
+        #     ):
+        #         y_pred.index = y_pred.index + self._y.index[0]
+        if is_integer_dtype(self._y.index) & any(fh.to_relative(self.cutoff) > 0):
+            y_pred.index = y_pred.index + self._y.index[0]
         return y_pred.loc[fh.to_absolute(self.cutoff).to_pandas()]
 
     @classmethod
