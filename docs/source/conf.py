7558505de9949a673345679dd7af367fdd98f693
Martin Walter <mf-walter@web.de>
2023-03-21 16:03:17 +0100

    [MNT] Fix disclaimer

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ from importlib import import_module
 import aeon
 
 rst_prolog = """.. attention::
-    DISCLAIMER: This is a fork of the aeon repository. Work is ongoing to change links
+    DISCLAIMER: This is a fork of the sktime repository. Work is ongoing to change links
     and documentation to reflect this. We do not recommend using this repository in any
     production setting, but welcome any contributors willing to help us update the
     project.
