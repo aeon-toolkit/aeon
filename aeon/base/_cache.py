de00cae56c9112eaecd3dde87f079a631b2e2884
Martin Walter <mf-walter@web.de>
2023-03-26 12:32:44 +0200

    Store cache in config

--- aeon/base/_cache.py
+++ aeon/base/_cache.py
@@ -4,23 +4,28 @@ from functools import wraps
 import joblib
 from cachetools import LRUCache
 
-from aeon import get_config
+from aeon import get_config, set_config
 
 
 def cache(func):
     """Decorate functions to cache return."""
     if get_config()["cache"]:
-        cache = LRUCache(maxsize=1000)
 
         @wraps(func)
         def cached_func(*args, **kwargs):
             # Convert unhashable objects to hashable ones
             key = joblib.hash((args, kwargs))
-            if key in cache:
-                return cache[key]
+            if get_config()["_cache"] is None:
+                _cache = LRUCache(maxsize=5)
+            else:
+                _cache = get_config()["_cache"]
+            if key in _cache:
+                return _cache[key]
             else:
                 result = func(*args, **kwargs)
-                cache[key] = result
+                _cache[key] = result
+                set_config(_cache=_cache)
+
                 return result
 
         return cached_func
