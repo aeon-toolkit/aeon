f3bb85f9e35c95e7aa15aa406e8104a1af4602f8
Tony Bagnall <ajb@uea.ac.uk>
2023-03-29 18:30:11 +0100

    adapt tests to numpy

--- aeon/transformations/panel/tests/test_slope_transformer.py
+++ aeon/transformations/panel/tests/test_slope_transformer.py
@@ -2,11 +2,9 @@
 import math
 
 import numpy as np
-import pandas as pd
 import pytest
 
 from aeon.transformations.panel.slope import SlopeTransformer
-from aeon.utils._testing.panel import _make_nested_from_array
 
 
 # Check that exception is raised for bad num levels.
@@ -30,7 +28,7 @@ def test_output_of_transformer():
     s = SlopeTransformer(n_intervals=2).fit(X)
     res = s.transform(X)
     orig = np.array([[[(5 + math.sqrt(41)) / 4, (1 + math.sqrt(101)) / -10]]])
-    np.testing.assert_almost_equal(res, orig)
+    np.testing.assert_array_almost_equal(res, orig, decimal=2)
     X = np.array([[[-5, 2.5, 1, 3, 10, -1.5, 6, 12, -3, 0.2]]])
     res = s.fit_transform(X)
     orig = np.array(
@@ -43,20 +41,19 @@ def test_output_of_transformer():
             ]
         ]
     )
-    np.testing.assert_almost_equal(res, orig)
+    np.testing.assert_array_almost_equal(res, orig, decimal=2)
 
 
 @pytest.mark.parametrize("num_intervals,corr_series_length", [(2, 2), (5, 5), (8, 8)])
 def test_output_dimensions(num_intervals, corr_series_length):
-    X = _make_nested_from_array(np.ones(13), n_instances=10, n_columns=1)
-
-    s = SlopeTransformer(n_intervals=num_intervals).fit(X)
-    res = s.transform(X)
+    X = np.ones(shape=(10, 1, 10))
+    s = SlopeTransformer(n_intervals=num_intervals)
+    res = s.fit_transform(X)
 
     # get the dimension of the generated dataframe.
-    act_time_series_length = res.iloc[0, 0].shape[0]
     num_rows = res.shape[0]
     num_cols = res.shape[1]
+    act_time_series_length = res.shape[2]
 
     assert act_time_series_length == corr_series_length
     assert num_rows == 10
@@ -75,29 +72,4 @@ def test_slope_performs_correcly_along_each_dim():
             [[(5 + math.sqrt(41)) / 4, (1 + math.sqrt(101)) / -10]],
         ]
     )
-    assert np.testing.assert_almost_equal(res, orig)
-
-
-def convert_list_to_dataframe(list_to_convert):
-    # Convert this into a panda's data frame
-    df = pd.DataFrame()
-    for i in range(len(list_to_convert)):
-        inst = list_to_convert[i]
-        data = []
-        data.append(pd.Series(inst))
-        df[i] = data
-
-    return df
-
-
-def check_if_dataframes_are_equal(df1, df2):
-    """
-    for some reason, this is how you check that two dataframes are equal.
-    """
-    from pandas.testing import assert_frame_equal
-
-    try:
-        assert_frame_equal(df1, df2)
-        return True
-    except AssertionError:
-        return False
+    np.testing.assert_array_almost_equal(res, orig, decimal=2)
