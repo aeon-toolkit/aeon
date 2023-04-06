f80a6898f90dd5711d9e9d9e3b8c9a18f86c81b6
Tony Bagnall <ajb@uea.ac.uk>
2023-04-06 09:32:03 +0100

    sliding window transformer

--- aeon/transformations/panel/segment.py
+++ aeon/transformations/panel/segment.py
@@ -48,7 +48,7 @@ class IntervalSegmenter(BaseTransformer):
         self.intervals = intervals
         self._time_index = []
         self.input_shape_ = ()
-        super(IntervalSegmenter, self).__init__()
+        super(IntervalSegmenter, self).__init__(_output_convert=False)
 
     def _fit(self, X, y=None):
         """
@@ -408,7 +408,7 @@ class SlidingWindowSegmenter(BaseTransformer):
 
     def __init__(self, window_length=5):
         self.window_length = window_length
-        super(SlidingWindowSegmenter, self).__init__()
+        super(SlidingWindowSegmenter, self).__init__(_output_convert=False)
 
     def _transform(self, X, y=None):
         """Transform time series.
