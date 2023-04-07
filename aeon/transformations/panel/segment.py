0e469ca65e5ab57b9f97a59cb035ec0ccee8ff3a
Tony Bagnall <ajb@uea.ac.uk>
2023-04-07 10:20:57 +0100

    exclude SlidingWindowTransformer from test output

--- aeon/transformations/panel/segment.py
+++ aeon/transformations/panel/segment.py
@@ -408,10 +408,9 @@ class SlidingWindowSegmenter(BaseTransformer):
         "scitype:transform-input": "Series",
         # what is the scitype of X: Series, or Panel
         "scitype:transform-output": "Series",
-        # what scitype is returned: Primitives, Series, Panel
-        "scitype:instancewise": False,  # is this an instance-wise transform?
-        "X_inner_mtype": "numpy3D",  # which mtypes do _fit/_predict support for X?
-        "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
+        "scitype:instancewise": False,
+        "X_inner_mtype": "numpy3D",
+        "y_inner_mtype": "None",
     }
 
     def __init__(self, window_length=5):
