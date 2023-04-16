e160dae933f2f17dfe8e9dedf3fc691828e79d1a
Tony Bagnall <ajb@uea.ac.uk>
2023-04-16 17:49:52 +0100

    [ENH] remove more mentions of nested_univ (#295)
    * remove nested_univ from column concatenator testing
    
    * switch tsfresh to use unit_test
    
    * remove mentions of nested_univ
    
    * revert colum transformer change
    
    * revert setup.cfg

--- aeon/transformations/panel/tests/test_truncate.py
+++ aeon/transformations/panel/tests/test_truncate.py
@@ -3,36 +3,32 @@
 
 from aeon.datasets import load_basic_motions
 from aeon.datatypes import get_examples
-from aeon.datatypes._panel._convert import from_nested_to_2d_array
 from aeon.transformations.panel.truncation import TruncationTransformer
 
 
 def test_truncation_transformer():
     """Test truncation to the shortest series length."""
     # load data
-    X_train, y_train = load_basic_motions(split="train", return_type="nested_univ")
-
+    X_train, y_train = load_basic_motions(split="train")
+    n_cases, n_channels, _ = X_train.shape
     truncated_transformer = TruncationTransformer(5)
     Xt = truncated_transformer.fit_transform(X_train)
 
     # when we tabularize the data it has 6 dimensions
     # and we've truncated them all to 5 long.
-    data = from_nested_to_2d_array(Xt)
-    assert len(data.columns) == 5 * 6
+    assert Xt.shape == (n_cases, n_channels, 5)
 
 
 def test_truncation_paramterised_transformer():
     """Test truncation to the a user defined length."""
     # load data
-    X_train, y_train = load_basic_motions(split="train", return_type="nested_univ")
+    X_train, y_train = load_basic_motions(split="train")
+    n_cases, n_channels, _ = X_train.shape
 
     truncated_transformer = TruncationTransformer(2, 10)
     Xt = truncated_transformer.fit_transform(X_train)
 
-    # when we tabularize the data it has 6 dimensions
-    # and we've truncated them all to (10-2) long.
-    data = from_nested_to_2d_array(Xt)
-    assert len(data.columns) == 8 * 6
+    assert Xt.shape == (n_cases, n_channels, 8)
 
 
 def test_truncation_pd_multiindex():
