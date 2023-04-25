dab720901bfe7bf79e542888895df043932271c0
Rafael Ayllón Gavilán <37343008+RafaAyGar@users.noreply.github.com>
2023-04-25 10:26:18 +0200

    Update __init__.py

--- aeon/classification/ordinal_classification/__init__.py
+++ aeon/classification/ordinal_classification/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 """Ordinal time series classifiers."""
-__all__ = ["OrdinalTemporalDictionaryEnsemble", "IndividualOrdinalTDE"]
+__all__ = [
+  "OrdinalTemporalDictionaryEnsemble",
+  "IndividualOrdinalTDE",
+  "histogram_intersection",
+]
 
-from aeon.classification.ordinal_classification._ordinal_tde import OrdinalTemporalDictionaryEnsemble
-from aeon.classification.ordinal_classification._ordinal_tde import IndividualOrdinalTDE
+from aeon.classification.ordinal_classification._ordinal_tde import (
+    IndividualOrdinalTDE,
+    OrdinalTemporalDictionaryEnsemble,
+    histogram_intersection,
+)
