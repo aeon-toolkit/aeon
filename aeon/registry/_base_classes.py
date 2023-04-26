56de2837338f6eb868d19d6e8985846953ed7e33
Tony Bagnall <ajb@uea.ac.uk>
2023-04-26 20:48:00 +0100

    [ENH] refactor param_est to live in forecasting module (#330)
    * refactor param_est to forecasting
    
    * base_classes registry
    
    * fix examples in param_est refactor
    
    * fix imports in examples in param_est refactor

--- aeon/registry/_base_classes.py
+++ aeon/registry/_base_classes.py
@@ -40,8 +40,8 @@ from aeon.classification.early_classification import BaseEarlyClassifier
 from aeon.clustering.base import BaseClusterer
 from aeon.forecasting.base import BaseForecaster
 from aeon.forecasting.model_selection._split import BaseSplitter
+from aeon.forecasting.param_est.base import BaseParamFitter
 from aeon.networks.base import BaseDeepNetwork
-from aeon.param_est.base import BaseParamFitter
 from aeon.performance_metrics.base import BaseMetric
 from aeon.regression.base import BaseRegressor
 from aeon.transformations.base import BaseTransformer
