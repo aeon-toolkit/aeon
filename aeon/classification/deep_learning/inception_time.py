9145569cddd42dc77b190f7add1dde61b1a86cf5
Tony Bagnall <ajb@uea.ac.uk>
2023-04-26 22:12:43 +0100

    [ENH] Removes nested dataframes from shape dtw (#329)
    * shape_dtw without dataframes
    
    * docstrings in signatures
    
    * docstrings in inception time
    
    * docstrings in muse
    
    * docstrings in shape dtw
    
    * docstrings in dummy
    
    * docstrings in channel_ensemble
    
    * remove unused import
    
    * remove mentions of nested
    
    * correct Inception time doc strings
    
    * refert shapeDTW

--- aeon/classification/deep_learning/inception_time.py
+++ aeon/classification/deep_learning/inception_time.py
@@ -539,20 +539,8 @@ class IndividualInceptionClassifier(BaseDeepClassifier):
         X : array-like of shape = (n_instances, n_channels, series_length)
             The training input samples. If a 2D array-like is passed,
             n_channels is assumed to be 1.
-        y : array-like, shape = [n_instances]
+        y : array-like, shape = (n_instances)
             The training data class labels.
-        input_checks : boolean
-            whether to check the X and y parameters
-        validation_X : a nested pd.Dataframe, or array-like of shape =
-        (n_instances, series_length, n_channels)
-            The validation samples. If a 2D array-like is passed,
-            n_channels is assumed to be 1.
-            Unless strictly defined by the user via callbacks (such as
-            EarlyStopping), the presence or state of the validation
-            data does not alter training in any way. Predictions at each epoch
-            are stored in the model's fit history.
-        validation_y : array-like, shape = [n_instances]
-            The validation class labels.
 
         Returns
         -------
