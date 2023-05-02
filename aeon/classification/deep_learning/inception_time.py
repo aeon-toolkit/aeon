d82b5fda3b1271f3c440a37adf574be22af83b72
hadifawaz1999 <hadifawaz2291999@gmail.com>
2023-05-02 17:53:05 +0200

    [ENH] made file name generation more optimal

--- aeon/classification/deep_learning/inception_time.py
+++ aeon/classification/deep_learning/inception_time.py
@@ -568,9 +568,7 @@ class IndividualInceptionClassifier(BaseDeepClassifier):
         if self.verbose:
             self.training_model_.summary()
 
-        self.file_name_ = "".join(
-            [str(i) for i in np.random.choice(a=np.arange(100), replace=True, size=20)]
-        )
+        self.file_name_ = str(np.random.randint(low=0, high=1e12, size=1)[0])
 
         self.callbacks_ = (
             [
