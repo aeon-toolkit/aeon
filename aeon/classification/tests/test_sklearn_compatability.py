9254f4871a2c0407ab9069fea18c344442a3aa02
Tony Bagnall <ajb@uea.ac.uk>
2023-05-03 17:02:13 +0100

    random collection in sklearn tests

--- aeon/classification/tests/test_sklearn_compatability.py
+++ aeon/classification/tests/test_sklearn_compatability.py
@@ -36,6 +36,7 @@ from sklearn.pipeline import Pipeline
 
 from aeon.classification.interval_based import CanonicalIntervalForest
 from aeon.transformations.panel.interpolate import TSInterpolator
+from aeon.utils._testing.collection import make_3d_test_data
 
 # StratifiedGroupKFold(n_splits=2), removed because it is not available in sklearn 0.24
 CROSS_VALIDATION_METHODS = [
@@ -82,8 +83,7 @@ def test_sklearn_cross_validation():
     """Test sklearn cross-validation works with aeon time series data and
     classifiers."""
     clf = CanonicalIntervalForest.create_test_instance()
-    X = np.ones((10, 2, 30))
-    y = np.array([1, 1, 1, 1, 1, 2, 2, 2, 2, 2])
+    X, y = make_3d_test_data(n_samples=20, n_channels=2, series_length=30)
     scores = cross_val_score(clf, X, y=y, cv=KFold(n_splits=2))
     assert isinstance(scores, np.ndarray)
 
@@ -91,8 +91,7 @@ def test_sklearn_cross_validation():
 @pytest.mark.parametrize("cross_validation_method", CROSS_VALIDATION_METHODS)
 def test_sklearn_cross_validation_iterators(cross_validation_method):
     """Test if sklearn cross-validation iterators can handle aeon time series data."""
-    X = np.ones((20, 2, 30))
-    y = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+    X, y = make_3d_test_data(n_samples=20, n_channels=2, series_length=30)
     groups = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
 
     for train, test in cross_validation_method.split(X=X, y=y, groups=groups):
@@ -104,8 +103,7 @@ def test_sklearn_parameter_tuning(parameter_tuning_method):
     """Test if sklearn parameter tuners can handle aeon data and classifiers."""
     clf = CanonicalIntervalForest.create_test_instance()
     param_grid = {"n_intervals": [2, 3], "att_subsample_size": [2, 3]}
-    X = np.ones((20, 2, 30))
-    y = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+    X, y = make_3d_test_data(n_samples=20, n_channels=2, series_length=30)
 
     parameter_tuning_method = parameter_tuning_method(
         clf, param_grid, cv=KFold(n_splits=3)
@@ -117,8 +115,7 @@ def test_sklearn_parameter_tuning(parameter_tuning_method):
 @pytest.mark.parametrize("composite_classifier", COMPOSITE_ESTIMATORS)
 def test_sklearn_composite_classifiers(composite_classifier):
     """Test if sklearn composite classifiers can handle aeon data and classifiers."""
-    X = np.ones((20, 2, 30))
-    y = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+    X, y = make_3d_test_data(n_samples=20, n_channels=2, series_length=30)
     composite_classifier.fit(X, y)
     preds = composite_classifier.predict(X=X)
     assert isinstance(preds, np.ndarray)
