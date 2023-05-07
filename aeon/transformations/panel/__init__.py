8d4d5ddddb1379ab3e54ab02746a565f0a8e92a5
Tony Bagnall <ajb@uea.ac.uk>
2023-05-07 19:29:49 +0100

    [ENH] TruncationTransformer, PaddingTransformer and TSFresh internal type to np-list (#364)
    * make dummy classifier accept np-list
    
    * revert df-list
    
    * change some weirdness in datatypes
    
    * draft converter
    
    * np-list changes
    
    * revert setup.cfg
    
    * loads of pointless converters
    
    * more pointless converters
    
    * data loading
    
    * load to list numpy
    
    * loading into lists instead of nested_univ
    
    * converter bugs
    
    * load to list
    
    * separate writers and loaders
    
    * refactor data_io. split readers and writers
    
    * refactor data_io. split readers and writers
    
    * adapt channel_selection.ipynb
    
    * adapt soon to go datasets
    
    * reconfiger interpolate example
    
    * loading_data rework
    
    * add converter for np-list to df-list
    
    * add 3d to long converter
    
    * change Catch22Wrapper to return numpy
    
    * reformat writers and loaders
    
    * loaders
    
    * tests for arff and tsv loaders
    
    * redesign truncator
    
    * fix conversion 3D -> 2D
    
    * add numpy3D -> list numpy converter
    
    * allow 3D numpy input without conversion
    
    * switch to list comprehension because chatGPT told me to :)
    
    * remove tsfresh test that relies on nested_univ
    
    * switch from file loader to generator
    
    * file deletion
    
    * file deletion
    
    * file deletion
    
    * fix test params
    
    * fix test params
    
    * pad
    
    * data loaders
    
    * set column names in compose test
    
    * set column names in compose test
    
    * temporarily skip tsfresh test
    
    * typos
    
    * revert setup (yet again!)
    
    * pad fix
    
    * Pad transformer rework
    
    * tests
    
    * docs update
    
    * add dummy to test
    
    * refactor stupid tests
    
    * refactor stupid tests

--- aeon/transformations/panel/__init__.py
+++ aeon/transformations/panel/__init__.py
@@ -39,7 +39,7 @@ from aeon.transformations.panel.dwt import DWTTransformer
 from aeon.transformations.panel.hog1d import HOG1DTransformer
 from aeon.transformations.panel.interpolate import TSInterpolator
 from aeon.transformations.panel.matrix_profile import MatrixProfile
-from aeon.transformations.panel.padder import PaddingTransformer
+from aeon.transformations.panel.pad import PaddingTransformer
 from aeon.transformations.panel.random_intervals import RandomIntervals
 from aeon.transformations.panel.reduce import Tabularizer, TimeBinner
 from aeon.transformations.panel.segment import (
@@ -53,4 +53,4 @@ from aeon.transformations.panel.shapelet_transform import (
 )
 from aeon.transformations.panel.slope import SlopeTransformer
 from aeon.transformations.panel.supervised_intervals import SupervisedIntervals
-from aeon.transformations.panel.truncation import TruncationTransformer
+from aeon.transformations.panel.truncate import TruncationTransformer
