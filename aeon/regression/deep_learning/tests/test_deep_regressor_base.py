0755f30ce26088dd101f8b5bc2c8af8597f14e0e
hadifawaz1999 <hadifawaz2291999@gmail.com>
2023-05-19 13:15:54 +0200

    [ENH] refactored test to do something

--- aeon/regression/deep_learning/tests/test_deep_regressor_base.py
+++ aeon/regression/deep_learning/tests/test_deep_regressor_base.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Unit tests for regressors deep learning base class functionality."""
 
+import os
+
 import pytest
 
 from aeon.regression.deep_learning.base import BaseDeepRegressor
@@ -9,16 +11,37 @@ from aeon.utils.validation._dependencies import _check_soft_dependencies
 __author__ = ["achieveordie", "hadifawaz1999"]
 
 
-class _DummyDeepRegressorEmpty(BaseDeepRegressor):
+class _DummyDeepRegressor(BaseDeepRegressor):
     """Dummy Deep Regressor for testing empty base deep class save utilities."""
 
     def __init__(self):
-        super(_DummyDeepRegressorEmpty, self).__init__()
+        super(_DummyDeepRegressor, self).__init__()
 
     def build_model(self, input_shape, n_classes, **kwargs):
-        return None
+        import tensorflow as tf
+
+        input_layer = tf.keras.layers.Input(input_shape)
+        output_layer = tf.keras.layers.Dense(units=n_classes)(input_layer)
+
+        model = tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+        model.compile()
+
+        return model
 
     def _fit(self, X, y):
+        X = X.transpose(0, 2, 1)
+
+        self.input_shape_ = X.shape[1:]
+        self.model_ = self.build_model(self.input_shape_)
+
+        self.history = self.model_.fit(
+            X,
+            y,
+            batch_size=16,
+            epochs=2,
+        )
+
         return self
 
 
@@ -29,8 +52,20 @@ class _DummyDeepRegressorEmpty(BaseDeepRegressor):
 def test_dummy_deep_regressor():
     import numpy as np
 
-    dumpy_deep_clf = _DummyDeepRegressorEmpty()
-    dumpy_deep_clf.build_model(input_shape=(10, 1), n_classes=2)
-    dumpy_deep_clf.fit(
-        X=np.random.normal(size=(10, 1, 10)), y=np.random.normal(size=(10,))
+    # create a dummy regressor
+    dummy_deep_rg = _DummyDeepRegressor()
+
+    # test fit function on random data
+    dummy_deep_rg.fit(
+        X=np.random.normal(size=(10, 100)), y=np.random.normal(size=(10,))
     )
+
+    # test save last model to file than delete it
+
+    dummy_deep_rg.save_last_model_to_file()
+
+    os.remove("./last_model.hdf5")
+
+    # test summary of model
+
+    dummy_deep_rg.summary()
