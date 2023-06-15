8f99ecebfad17ea3479499591442d478e14acd36
Tony Bagnall <ajb@uea.ac.uk>
2023-06-15 13:29:29 +0100

    [ENH] Data downloaders for classification/regression and forecasting (#478)
    * results getter
    
    * results loaders
    
    * results loaders
    
    * adapt for possible other clique finding
    
    * revert setup.cfg
    
    * available list
    
    * start plotting tools
    
    * change example
    
    * add aliasing function for classifier names
    
    * example results files
    
    * notebook
    
    * exclude benchmarking/example_results from init check
    
    * exclude from init
    
    * notebooks
    
    * violin plot
    
    * plot boxplot
    
    * allow nans and 2D return
    
    * include load_TSC
    
    * revise tests
    
    * adapt data loaders for return type argument
    
    * data notebooks
    
    * write regression
    
    * np.float -> float
    
    * revert notebooks to match main
    
    * loading data notebook
    
    * revert setup.cfg
    
    * remove notebook
    
    * load_UEA change
    
    * change arff
    
    * arff loader to numpy
    
    * refactor load_tsc
    
    * sort column names
    
    * remove setup in datasets
    
    * revert setup.cfg
    
    * load_from_tsf_file
    
    * refactor load_arrf to load_from_arff_file
    
    * add doctest skip
    
    * remove head()
    
    * remove head()
    
    * attempt to fix mlflow
    
    * try fix mlflow
    
    * attempt to fix mlflow
    
    * revert mlflow notebook changes and pin version
    
    * monash forecasting loader
    
    * add regression list
    
    * add tsf list
    
    * forecasting downloader
    
    * regression downloader
    
    * regression downloader
    
    * tidy docstrings
    
    * add notebook
    
    * add references
    
    * excluded notebook list
    
    * remove placeholder functions
    
    * change default tsc to not return meta
    
    * fix Skip
    
    * fix Skip
    
    * comment out get regressors
    
    * remove random ghost file
    
    * remove external tests
    
    * docstrings
    
    * case fix
    
    * results loading tests
    
    * results loading tests
    
    * results loading tests
    
    * results loading tests
    
    * fix notebook
    
    * refactor local directories
    
    * Update FreshPRINCE_TESTFOLDS.csv
    
    * refactor local directories
    
    * refactor local directories
    
    * fix notebook
    
    * revert setup.cfg
    
    * fix import in notebook
    
    * remove not on end of init file
    
    * fix notebook
    
    * fix examples
    
    * Update FreshPRINCE_TESTFOLDS.csv
    
    * Update FreshPRINCE_TESTFOLDS.csv
    
    * Create DTW_GUI_warped.gif
    
    * fix tsc_dataset_lists
    
    ---------
    
    Co-authored-by: MatthewMiddlehurst <m.middlehurst@uea.ac.uk>

--- build_tools/run_examples.sh
+++ build_tools/run_examples.sh
@@ -5,8 +5,17 @@ set -euxo pipefail
 
 CMD="jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=600"
 
+excluded=("examples/datasets/benchmarking_data.ipynb")
+
+# Loop over all notebooks in the examples directory.
 find "examples/" -name "*.ipynb" -print0 |
-  while IFS= read -r -d '' notebook; do
-    echo "Running: $notebook"
-    $CMD "$notebook"
+  while IFS= read -r -d "" notebook; do
+    # Skip notebooks in the excluded list.
+    if printf "%s\0" "${excluded[@]}" | grep -Fxqz -- "$notebook"; then
+      echo "Skipping: $notebook"
+    # Run the notebook.
+    else
+      echo "Running: $notebook"
+      $CMD "$notebook"
+    fi
   done
