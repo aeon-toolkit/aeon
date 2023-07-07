e6438224b7677d5cbd55d6b673f5442e5e83cf18
Tony Bagnall <ajb@uea.ac.uk>
2023-07-07 14:57:50 +0100

    move _check_internal up a level

--- aeon/regression/tests/test_base.py
+++ aeon/regression/tests/test_base.py
@@ -1,2 +1,56 @@
 # -*- coding: utf-8 -*-
 """Unit tests for regression base class functionality."""
+import numpy as np
+import pytest
+
+from aeon.regression.base import BaseRegressor
+
+
+class _DummyRegressor(BaseRegressor):
+    """Dummy regressor for testing base class fit/predict."""
+
+    def _fit(self, X, y):
+        """Fit dummy."""
+        return self
+
+    def _predict(self, X):
+        """Predict dummy."""
+        return self
+
+
+multivariate_message = r"multivariate series"
+missing_message = r"missing values"
+unequal_message = r"unequal length series"
+incorrect_X_data_structure = r"must be a np.array or a pd.Series"
+incorrect_y_data_structure = r"must be 1-dimensional"
+
+
+def test_base_regressor_fit():
+    """Test function for the BaseRegressor class fit.
+
+    Test fit. It should:
+    1. Work with 2D, 3D and DataFrame for X and nparray for y.
+    2. Calculate the number of classes and record the fit time.
+    3. have self.n_jobs set or throw  an exception if the classifier can
+    multithread.
+    4. Set the class dictionary correctly.
+    5. Set is_fitted after a call to _fit.
+    6. Return self.
+    """
+    dummy = _DummyRegressor()
+    cases = 5
+    length = 10
+    test_X1 = np.random.uniform(-1, 1, size=(cases, length))
+    test_X2 = np.random.uniform(-1, 1, size=(cases, 2, length))
+    test_y1 = np.random.random(cases)
+    result = dummy.fit(test_X1, test_y1)
+    assert result is dummy
+    with pytest.raises(ValueError, match=multivariate_message):
+        result = dummy.fit(test_X2, test_y1)
+    assert result is dummy
+    # Raise a specific error if y is in a 2D matrix (1,cases)
+    test_y2 = np.array([test_y1])
+    # What if y is in a 2D matrix (cases,1)?
+    test_y2 = np.array([test_y1]).transpose()
+    with pytest.raises(ValueError, match=incorrect_y_data_structure):
+        result = dummy.fit(test_X1, test_y2)
