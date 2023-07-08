ad8754d7824fcf46f217b28e1389ac8fda0a1902
Tony Bagnall <ajb@uea.ac.uk>
2023-07-08 08:54:25 +0100

    add capabilities testing for base regressor

--- aeon/regression/tests/test_base.py
+++ aeon/regression/tests/test_base.py
@@ -20,6 +20,25 @@ class _DummyRegressor(BaseRegressor):
         return self
 
 
+class _DummyHandlesAllInput(BaseRegressor):
+    """Dummy regress for testing base class fit/predict."""
+
+    _tags = {
+        "capability:multivariate": True,
+        "capability:unequal_length": True,
+        "capability:missing_values": True,
+        "X_inner_mtype": ["np-list", "numpy3D"],
+    }
+
+    def _fit(self, X, y):
+        """Fit dummy."""
+        return self
+
+    def _predict(self, X):
+        """Predict dummy."""
+        return self
+
+
 multivariate_message = r"multivariate series"
 missing_message = r"missing values"
 unequal_message = r"unequal length series"
@@ -31,7 +50,7 @@ def test_base_regressor_fit():
     """Test function for the BaseRegressor class fit.
 
     Test fit. It should:
-    1. Work with 2D, 3D and DataFrame for X and nparray for y.
+    1. Work with 2D, 3D and list of np.ndarray for X and np.ndarray for y.
     2. Calculate the number of classes and record the fit time.
     3. have self.n_jobs set or throw  an exception if the classifier can
     multithread.
@@ -56,6 +75,30 @@ def test_base_regressor_fit():
     test_y2 = np.array([test_y1]).transpose()
     with pytest.raises(ValueError, match=incorrect_y_data_structure):
         result = dummy.fit(test_X1, test_y2)
+
+
+@pytest.mark.parametrize("missing", [True, False])
+@pytest.mark.parametrize("multivariate", [True, False])
+@pytest.mark.parametrize("unequal", [True, False])
+def test_check_capabilities(missing, multivariate, unequal):
+    """Test the checking of capabilities."""
+    handles_none = _DummyRegressor()
+
+    # checks that errors are raised
+    if missing:
+        with pytest.raises(ValueError, match=missing_message):
+            handles_none._check_capabilities(missing, multivariate, unequal)
+    if multivariate:
+        with pytest.raises(ValueError, match=multivariate_message):
+            handles_none._check_capabilities(missing, multivariate, unequal)
+    if unequal:
+        with pytest.raises(ValueError, match=unequal_message):
+            handles_none._check_capabilities(missing, multivariate, unequal)
+    if not missing and not multivariate and not unequal:
+        handles_none._check_capabilities(missing, multivariate, unequal)
+
+    handles_all = _DummyHandlesAllInput()
+    handles_all._check_capabilities(missing, multivariate, unequal)
 
 
 def test_score():
