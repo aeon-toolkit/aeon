a2ecfdfa709f36adc410c56151d100f891b54af7
Tony Bagnall <ajb@uea.ac.uk>
2023-07-08 09:25:15 +0100

    Revert "add inception time to init"
    This reverts commit d8fbc49512e225fd5a35ec2b3ad74130ca139310.

--- aeon/regression/deep_learning/__init__.py
+++ aeon/regression/deep_learning/__init__.py
@@ -3,9 +3,7 @@
 __all__ = [
     "CNNRegressor",
     "TapNetRegressor",
-    "InceptionTimeRegressor",
 ]
 
 from aeon.regression.deep_learning.cnn import CNNRegressor
-from aeon.regression.deep_learning.inception_time import InceptionTimeRegressor
 from aeon.regression.deep_learning.tapnet import TapNetRegressor
