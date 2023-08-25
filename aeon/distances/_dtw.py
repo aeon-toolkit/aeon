a1ad13eb1413705dc2ba5e7eaf3f68e9d9eb22e4
Tony Bagnall <ajb@uea.ac.uk>
2023-08-25 10:02:14 +0100

    dtw docstring

--- aeon/distances/_dtw.py
+++ aeon/distances/_dtw.py
@@ -59,13 +59,13 @@ def dtw_distance(x: np.ndarray, y: np.ndarray, window: float = None) -> float:
 
     Parameters
     ----------
-    x : np.ndarray of shape ``(n_timepoints,)`` or ``(n_channels, n_timepoints)``
-        First time series either univariate length ``n_timepoints`` or multivariate with
-        ``n_channels`` channels and length ``n_timepoints``.
-    y : np.ndarray, of shape ``(m_timepoints,)`` or ``(m_channels, m_timepoints)``
-        Second time series either univariate length ``n_timepoints`` or multivariate
-        with ``n_channels`` channels and length ``n_timepoints``.
-    window : float, default=None
+    x : np.ndarray
+        First time series either univariate, shape ``(n_timepoints,)``, or
+        multivariate, shape ``(n_channels, n_timepoints)``.
+    y : np.ndarray
+        Second time series either univariate, shape ``(n_timepoints,)``, or
+        multivariate, shape ``(n_channels, n_timepoints)``.
+    window : float or None, default=None
         The window to use for the bounding matrix. If None, no bounding matrix
         is used. window is a percentage deviation, so if ``window = 0.1`` then
         10% of the series length is the max warping allowed.
