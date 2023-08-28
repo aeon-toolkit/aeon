30fa21e21ddba0cd06452795594787a2384621e2
Tony Bagnall <ajb@uea.ac.uk>
2023-08-28 17:34:51 +0100

    invalid file path test

--- aeon/datasets/tests/test_data_loaders.py
+++ aeon/datasets/tests/test_data_loaders.py
@@ -473,8 +473,8 @@ def test_load_forecasting():
     df, meta = load_forecasting("m1_yearly_dataset")
     assert meta == expected_metadata
     assert df.shape == (181, 3)
-    with pytest.raises((OSError, ValueError)):
-        X, y, meta = load_forecasting("m1_yearly_dataset", extract_path="?>S")
+    with pytest.raises((OSError)):
+        X, y, meta = load_forecasting("m1_yearly_dataset", extract_path="c:/j  +.+?!")
     data_path = os.path.join(
         os.path.dirname(aeon.__file__),
         "datasets/data/UnitTest/",
@@ -501,8 +501,8 @@ def test_load_regression():
     assert isinstance(y, np.ndarray)
     assert X.shape == (201, 1, 84)
     assert y.shape == (201,)
-    with pytest.raises((OSError, ValueError)):
-        X, y, meta = load_regression("Covid3Month", extract_path="?>S")
+    with pytest.raises((OSError)):
+        X, y, meta = load_regression("Covid3Month", extract_path="'cc:/^- j+.+?! :-)")
     data_path = os.path.join(
         os.path.dirname(aeon.__file__),
         "datasets/data/UnitTest/",
@@ -529,8 +529,8 @@ def test_load_classification():
     assert isinstance(y, np.ndarray)
     assert X.shape == (42, 1, 24)
     assert y.shape == (42,)
-    with pytest.raises((OSError, ValueError)):
-        X, y, meta = load_classification("UnitTest", extract_path="?>S")
+    with pytest.raises((OSError)):
+        X, y, meta = load_classification("UnitTest", extract_path="'cc:/^- =#$$j+ :-)")
     data_path = os.path.join(
         os.path.dirname(aeon.__file__),
         "datasets/data/UnitTest/",
