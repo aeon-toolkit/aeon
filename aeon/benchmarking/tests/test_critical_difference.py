d00c23e3b474706c7f62494d4f176ed936b8c059
Tony Bagnall <ajb@uea.ac.uk>
2023-09-03 11:18:42 +0100

    set up tests for critical difference

---
+++ aeon/benchmarking/tests/test_critical_difference.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# copyright: aeon developers, BSD-3-Clause License (see LICENSE file)
+"""Tests for the Critical Difference maker."""
+# from aeon.benchmarking._critical_difference import (
+#     _check_friedman,
+#     build_cliques,
+#     nemenyi_cliques,
+#     plot_critical_difference,
+#     wilcoxon_holm_cliques,
+# )
+
+
+def test__check_friedman():
+    pass
+
+
+def test_nemenyi_cliques():
+    pass
+
+
+def test_wilcoxon_holm_cliques():
+    pass
+
+
+def test_build_cliques():
+    pass
+
+
+def test_plot_critical_difference():
+    pass
