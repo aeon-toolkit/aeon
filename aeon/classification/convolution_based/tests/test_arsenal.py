0334fc00b430deb061e9b421c7308daa46133fc1
Tony Bagnall <ajb@uea.ac.uk>
2023-09-05 21:46:09 +0100

    ROCKET tests

--- aeon/classification/convolution_based/tests/test_arsenal.py
+++ aeon/classification/convolution_based/tests/test_arsenal.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """Arsenal test code."""
 from aeon.classification.convolution_based import Arsenal
-from aeon.datasets import load_unit_test
+from aeon.utils._testing.collection import make_2d_test_data, make_3d_test_data
 
 
 def test_contracted_arsenal():
     """Test of contracted Arsenal on unit test data."""
     # load unit test data
-    X_train, y_train = load_unit_test(split="train")
-
+    X_train, y_train = make_2d_test_data()
     # train contracted Arsenal
     arsenal = Arsenal(
         time_limit_in_minutes=0.25,
@@ -17,5 +16,7 @@ def test_contracted_arsenal():
         random_state=0,
     )
     arsenal.fit(X_train, y_train)
-
+    assert len(arsenal.estimators_) > 1
+    X_train, y_train = make_3d_test_data()
+    arsenal.fit(X_train, y_train)
     assert len(arsenal.estimators_) > 1
