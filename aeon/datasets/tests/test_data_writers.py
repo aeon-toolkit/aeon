501c4bf7a9956d8c2319f61433bb00f7aebfaae1
hedeershowk <hedeer@elshowk.com>
2023-09-12 22:23:48 -0400

    use tempfile for temporary directories

--- aeon/datasets/tests/test_data_writers.py
+++ aeon/datasets/tests/test_data_writers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-import shutil
+import tempfile
 
 import numpy as np
 import pandas as pd
@@ -26,20 +26,20 @@ def test_write_to_tsfile_equal_length(regression, problem_name):
     then deletes the files.
     """
     X, y = make_3d_test_data(regression_target=regression)
-    write_to_tsfile(
-        X=X, path="./Temp/", y=y, problem_name=problem_name, regression=regression
-    )
-    load_path = "./Temp/" + problem_name
-    newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
-    assert isinstance(newX, np.ndarray)
-    assert X.shape == newX.shape
-    assert X[0][0][0] == newX[0][0][0]
-    if not regression:
-        y = y.astype(str)
-        np.testing.assert_array_equal(y, newy)
-    else:
-        np.testing.assert_array_almost_equal(y, newy)
-    shutil.rmtree("./Temp")
+    with tempfile.TemporaryDirectory() as tmp:
+        write_to_tsfile(
+            X=X, path=tmp, y=y, problem_name=problem_name, regression=regression
+        )
+        load_path = os.path.join(tmp, problem_name)
+        newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
+        assert isinstance(newX, np.ndarray)
+        assert X.shape == newX.shape
+        assert X[0][0][0] == newX[0][0][0]
+        if not regression:
+            y = y.astype(str)
+            np.testing.assert_array_equal(y, newy)
+        else:
+            np.testing.assert_array_almost_equal(y, newy)
 
 
 @pytest.mark.parametrize("problem_name", ["Testy", "Testy2.ts"])
@@ -50,15 +50,15 @@ def test_write_regression_to_tsfile_equal_length(problem_name):
     writes locally, reloads, then compares all class labels. It then delete the files.
     """
     X, y = make_3d_test_data(regression_target=True)
-    write_to_tsfile(X=X, path="./Temp/", y=y, problem_name=problem_name)
-    load_path = "./Temp/" + problem_name
-    newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
-    assert isinstance(newX, np.ndarray)
-    assert X.shape == newX.shape
-    assert X[0][0][0] == newX[0][0][0]
-    y = y.astype(str)
-    assert np.array_equal(y, newy)
-    shutil.rmtree("./Temp")
+    with tempfile.TemporaryDirectory() as tmp:
+        write_to_tsfile(X=X, path=tmp, y=y, problem_name=problem_name)
+        load_path = os.path.join(tmp, problem_name)
+        newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
+        assert isinstance(newX, np.ndarray)
+        assert X.shape == newX.shape
+        assert X[0][0][0] == newX[0][0][0]
+        y = y.astype(str)
+        assert np.array_equal(y, newy)
 
 
 @pytest.mark.parametrize("problem_name", ["Testy", "Testy2.ts"])
@@ -69,15 +69,15 @@ def test_write_to_tsfile_unequal_length(problem_name):
     writes locally, reloads, then compares all class labels. It then delete the files.
     """
     X, y = make_unequal_length_test_data()
-    write_to_tsfile(X=X, path="./Temp/", y=y, problem_name=problem_name)
-    load_path = "./Temp/" + problem_name
-    newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
-    assert isinstance(newX, list)
-    assert len(X) == len(newX)
-    assert X[0][0][0] == newX[0][0][0]
-    y = y.astype(str)
-    assert np.array_equal(y, newy)
-    shutil.rmtree("./Temp")
+    with tempfile.TemporaryDirectory() as tmp:
+        write_to_tsfile(X=X, path=tmp, y=y, problem_name=problem_name)
+        load_path = os.path.join(tmp, problem_name)
+        newX, newy = load_from_tsfile(full_file_path_and_name=load_path)
+        assert isinstance(newX, list)
+        assert len(X) == len(newX)
+        assert X[0][0][0] == newX[0][0][0]
+        y = y.astype(str)
+        assert np.array_equal(y, newy)
 
 
 def test_write_dataframe_to_ts():
@@ -85,45 +85,18 @@ def test_write_dataframe_to_ts():
     # load an example dataset
     problem_name = "Testy.ts"
     X, y = make_nested_dataframe_data()
-    # output the dataframe in a ts file
-    temp_path = os.path.join(os.path.dirname(__file__), "Temp/")
-    _write_dataframe_to_tsfile(
-        X=X,
-        path=temp_path,
-        y=y,
-        problem_name=problem_name,
-    )
-    # load data back from the ts file into dataframe
-    tsfile_path = os.path.join(temp_path, problem_name)
-    newX, newy = load_from_tsfile_to_dataframe(tsfile_path)
-    # check if the dataframes are the same
-    pd.testing.assert_frame_equal(newX, X)
-    y2 = pd.Series(y)
-    pd.testing.assert_series_equal(y, y2)
-    shutil.rmtree("./Temp/")
-
-
-def test_write_to_tsfile_with_dataframe():
-    """
-    Tests whether a dataframe can be written to a .ts file with
-    write_to_tsfile()
-    """
-    # load an example dataset
-    problem_name = "Testy.ts"
-    X, y = make_nested_dataframe_data()
-    # output the dataframe in a ts file
-    temp_path = os.path.join(os.path.dirname(__file__), "Temp/")
-    write_to_tsfile(
-        X=X,
-        path=temp_path,
-        y=y,
-        problem_name=problem_name,
-    )
-    # load data back from the ts file into dataframe
-    tsfile_path = os.path.join(temp_path, problem_name)
-    newX, newy = load_from_tsfile_to_dataframe(tsfile_path)
-    # check if the dataframes are the same
-    pd.testing.assert_frame_equal(newX, X)
-    y2 = pd.Series(y)
-    pd.testing.assert_series_equal(y, y2)
-    shutil.rmtree("./Temp/")
+    with tempfile.TemporaryDirectory() as tmp:
+        # output the dataframe in a ts file
+        _write_dataframe_to_tsfile(
+            X=X,
+            path=tmp,
+            y=y,
+            problem_name=problem_name,
+        )
+        # load data back from the ts file into dataframe
+        load_path = os.path.join(tmp, problem_name)
+        newX, newy = load_from_tsfile_to_dataframe(load_path)
+        # check if the dataframes are the same
+        pd.testing.assert_frame_equal(newX, X)
+        y2 = pd.Series(y)
+        pd.testing.assert_series_equal(y, y2)
