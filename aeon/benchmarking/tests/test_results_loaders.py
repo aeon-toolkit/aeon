4871d09bf061d074d51bc0ce0b6f5d1fb44684e0
David Guijo-Rubio <dguijo@uco.es>
2023-09-23 09:46:02 +0200

    typo results_loader test

--- aeon/benchmarking/tests/test_results_loaders.py
+++ aeon/benchmarking/tests/test_results_loaders.py
@@ -15,7 +15,7 @@ from aeon.benchmarking.results_loaders import (
 cls = ["HC2", "FreshPRINCE", "InceptionT"]
 data = ["Chinatown", "Tools"]
 test_path = MODULE = os.path.dirname(__file__)
-data_path = os.path.join(test_path,"../example_results/")
+data_path = os.path.join(test_path, "../example_results/")
 
 
 def test_get_estimator_results():
