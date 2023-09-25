f6b2dda15a48890ee46a9670b285f0ebbdad5b42
Stijn Rotman <s.j.rotman@tilburguniversity.edu>
2023-09-25 11:00:27 +0200

    Add test for transformations._split

---
+++ aeon/transformations/tests/test_split.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pytest
+
+from aeon.transformations._split import SplitsTimeSeries
+
+testdata = [
+    (np.arange(10), 2, [np.array([0, 1, 2, 3, 4]), np.array([5, 6, 7, 8, 9])]),
+    (np.arange(10), 3, [np.array([0, 1, 2]), np.array([3, 4, 5]), np.array([6, 7, 8, 9])]),
+]
+
+
+@pytest.mark.parametrize('X,n_intervals,expected', testdata)
+def test_split_(X, n_intervals, expected):
+    splitter = SplitsTimeSeries()
+    splitter.n_intervals = n_intervals
+    res = splitter._split(X)
+
+    assert len(res) == n_intervals
+    for x, y in zip(res, expected):
+        np.testing.assert_array_equal(x, y)
+
