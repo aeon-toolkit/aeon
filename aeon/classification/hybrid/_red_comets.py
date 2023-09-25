f7a2af84f8bfe3fa27d7a9c345e7968f599f5198
Luca Bennett <zy18811@bristol.ac.uk>
2023-09-25 23:54:08 +0100

    Passes all estimator tests!

--- aeon/classification/hybrid/_red_comets.py
+++ aeon/classification/hybrid/_red_comets.py
@@ -10,13 +10,13 @@ classifier.
 __author__ = ["zy18811"]
 __all__ = ["REDCOMETS"]
 
-import numpy as np
-
 from collections import Counter
 
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.utils import check_random_state
 from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import NearestNeighbors
+from sklearn.utils import check_random_state
 
 from aeon.classification.base import BaseClassifier
 from aeon.transformations.collection.dictionary_based import SAX, SFA
@@ -82,19 +82,21 @@ class REDCOMETS(BaseClassifier):
         random_state=None,
         n_jobs=1,
     ):
-        _check_soft_dependencies("imbalanced-learn",
-                                 package_import_alias={"imbalanced-learn": "imblearn"},
-                                 severity="error",
-                                 obj=self)
+        _check_soft_dependencies(
+            "imbalanced-learn",
+            package_import_alias={"imbalanced-learn": "imblearn"},
+            severity="error",
+            obj=self,
+        )
 
         self.variant = variant
         self.perc_length = perc_length
         self.n_trees = n_trees
 
         self.random_state = random_state
-        self.rng = check_random_state(random_state)
-
         self.n_jobs = n_jobs
+
+        self._n_channels = 1
 
         self.sfa_clfs = []
         self.sfa_transforms = []
@@ -119,13 +121,11 @@ class REDCOMETS(BaseClassifier):
         self :
             Reference to self.
         """
-
         if (n_channels := X.shape[1]) == 1:  # Univariate
             self._fit_univariate(np.squeeze(X), y)
         else:  # Multivariate
-
             if self.variant in [1, 2, 3]:  # Concatenate dimensions
-                self.perc_length /= n_channels
+                self._n_channels = n_channels
                 X_concat = X.reshape(*X.shape[:-2], -1)
                 self._fit_univariate(X_concat, y)
 
@@ -147,10 +147,10 @@ class REDCOMETS(BaseClassifier):
         self :
             Reference to self.
         """
-
         from imblearn.over_sampling import SMOTE, RandomOverSampler
 
-        n_lenses = 2*int(self.perc_length*X.shape[1]//100)
+        perc_length = self.perc_length / self._n_channels
+        n_lenses = 2 * int(perc_length * X.shape[1] // 100)
 
         min_neighbours = min(Counter(y).items(), key=lambda k: k[1])[1]
         max_neighbours = max(Counter(y).items(), key=lambda k: k[1])[1]
@@ -160,57 +160,73 @@ class REDCOMETS(BaseClassifier):
             y_smote = y
 
         else:
-
             if min_neighbours > 5:
                 min_neighbours = 6
             try:
-                X_smote, y_smote = SMOTE(sampling_strategy="all",
-                                         k_neighbors=min_neighbours-1,
-                                         random_state=self.rng,
-                                         n_jobs=self.n_jobs
-                                         ).fit_resample(X, y)
+                X_smote, y_smote = SMOTE(
+                    sampling_strategy="all",
+                    k_neighbors=NearestNeighbors(
+                        n_neighbors=min_neighbours - 1, n_jobs=self.n_jobs
+                    ),
+                    random_state=self.random_state,
+                ).fit_resample(X, y)
+
             except ValueError:
-                X_smote, y_smote = RandomOverSampler(sampling_strategy='all',
-                                                     random_state=self.rng
-                                                     ).fit_resample(X, y)
+                X_smote, y_smote = RandomOverSampler(
+                    sampling_strategy="all", random_state=self.random_state
+                ).fit_resample(X, y)
 
         sax_lenses, sfa_lenses = np.split(self._get_random_lenses(X_smote, n_lenses), 2)
 
         cv = np.min([5, len(y_smote) // len(list(set(y_smote)))])
 
-        self.sfa_transforms = [SFA(word_length=w, alphabet_size=a,
-                                   window_size=X_smote.shape[1],
-                                   binning_method='equi-width',
-                                   save_words=True, n_jobs=self.n_jobs,
-                                   random_state=self.rng)
-                               for w, a in sfa_lenses]
+        self.sfa_transforms = [
+            SFA(
+                word_length=w,
+                alphabet_size=a,
+                window_size=X_smote.shape[1],
+                binning_method="equi-width",
+                save_words=True,
+                n_jobs=self.n_jobs,
+                random_state=self.random_state,
+            )
+            for w, a in sfa_lenses
+        ]
 
         for sfa in self.sfa_transforms:
             sfa.fit_transform(X_smote, y_smote)
-            X_sfa = np.array(list(map(lambda word: sfa.word_list(int(word[0])),
-                                      sfa.words)))
+            X_sfa = np.array(
+                list(map(lambda word: sfa.word_list(int(word[0])), sfa.words))
+            )
 
-            rf = RandomForestClassifier(n_estimators=self.n_trees,
-                                        random_state=self.rng,
-                                        n_jobs=self.n_jobs)
+            rf = RandomForestClassifier(
+                n_estimators=self.n_trees,
+                random_state=self.random_state,
+                n_jobs=self.n_jobs,
+            )
             rf.fit(X_sfa, y_smote)
-            weight = cross_val_score(rf, X_sfa, y_smote, cv=cv,
-                                     n_jobs=self.n_jobs).mean()
+            weight = cross_val_score(
+                rf, X_sfa, y_smote, cv=cv, n_jobs=self.n_jobs
+            ).mean()
 
             self.sfa_clfs.append((rf, weight))
 
-        self.sax_transforms = [SAX(n_segments=w, alphabet_size=a)
-                               for w, a in sax_lenses]
+        self.sax_transforms = [
+            SAX(n_segments=w, alphabet_size=a) for w, a in sax_lenses
+        ]
 
         for sax in self.sax_transforms:
             X_sax = np.squeeze(sax.fit_transform(X_smote))
 
-            rf = RandomForestClassifier(n_estimators=self.n_trees,
-                                        random_state=self.rng,
-                                        n_jobs=self.n_jobs)
+            rf = RandomForestClassifier(
+                n_estimators=self.n_trees,
+                random_state=self.random_state,
+                n_jobs=self.n_jobs,
+            )
             rf.fit(X_sax, y_smote)
-            weight = cross_val_score(rf, X_sax, y_smote, cv=cv,
-                                     n_jobs=self.n_jobs).mean()
+            weight = cross_val_score(
+                rf, X_sax, y_smote, cv=cv, n_jobs=self.n_jobs
+            ).mean()
 
             self.sax_clfs.append((rf, weight))
 
@@ -227,8 +243,9 @@ class REDCOMETS(BaseClassifier):
         y : array-like, shape = [n_instances]
             Predicted class labels.
         """
-
-        return [self.classes_[i] for i in self._predict_proba(X).argmax(axis=1)]
+        return np.array(
+            [self.classes_[i] for i in self._predict_proba(X).argmax(axis=1)]
+        )
 
     def _predict_proba(self, X) -> np.ndarray:
         """Predicts labels probabilities for sequences in X.
@@ -243,11 +260,9 @@ class REDCOMETS(BaseClassifier):
         y : array-like, shape = [n_instances, n_classes_]
             Predicted probabilities using the ordering in classes_.
         """
-
         if X.shape[1] == 1:  # Univariate
             return self._predict_proba_unvivariate(np.squeeze(X))
         else:  # Multivariate
-
             if self.variant in [1, 2, 3]:  # Concatenate dimensions
                 X_concat = X.reshape(*X.shape[:-2], -1)
                 return self._predict_proba_unvivariate(X_concat)
@@ -265,15 +280,14 @@ class REDCOMETS(BaseClassifier):
         y : array-like, shape = [n_instances, n_classes_]
             Predicted probabilities using the ordering in classes_.
         """
-
         pred_mat = np.zeros((X.shape[0], self.n_classes_))
 
         placeholder_y = np.zeros(X.shape[0])
         for sfa, (rf, weight) in zip(self.sfa_transforms, self.sfa_clfs):
-
             sfa.fit_transform(X, placeholder_y)
-            X_sfa = np.array(list(map(lambda word: sfa.word_list(int(word[0])),
-                                      sfa.words)))
+            X_sfa = np.array(
+                list(map(lambda word: sfa.word_list(int(word[0])), sfa.words))
+            )
 
             pred_mat += rf.predict_proba(X_sfa) * weight
 
@@ -281,6 +295,8 @@ class REDCOMETS(BaseClassifier):
             X_sax = np.squeeze(sax.fit_transform(X))
 
             pred_mat += rf.predict_proba(X_sax) * weight
+
+        pred_mat /= np.sum(pred_mat, axis=1).reshape(-1, 1)  # Rescales rows to sum to 1
 
         return pred_mat
 
@@ -293,29 +309,36 @@ class REDCOMETS(BaseClassifier):
             The training data shape = (n_instances, n_channels, n_timepoints).
         n_lenses : int
             Number of lenses to select.
+
         Returns
         -------
         lenses : array-like, shape = [n_lenses, 2]
             Selected lenses.
         """
-
         maxCoof = 130
 
-        if (X.shape[1] < maxCoof):
+        if X.shape[1] < maxCoof:
             maxCoof = X.shape[1] - 1
-        if (X.shape[1] < 100):
+        if X.shape[1] < 100:
             n_segments = list(range(5, maxCoof, 5))
         else:
             n_segments = list(range(10, maxCoof, 10))
 
         maxBin = 26
-        if (X.shape[1] < maxBin):
+        if X.shape[1] < maxBin:
             maxBin = X.shape[1] - 2
-        if (X.shape[0] < maxBin):
+        if X.shape[0] < maxBin:
             maxBin = X.shape[0] - 2
 
         alphas = range(3, maxBin)
 
-        lenses = np.array(list(zip(self.rng.choice(n_segments, size=n_lenses),
-                                   self.rng.choice(alphas, size=n_lenses))))
+        rng = check_random_state(self.random_state)
+        lenses = np.array(
+            list(
+                zip(
+                    rng.choice(n_segments, size=n_lenses),
+                    rng.choice(alphas, size=n_lenses),
+                )
+            )
+        )
         return lenses
