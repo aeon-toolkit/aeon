9ebcf3600e3cebf589332a75165767e88a3d1b9d
Stijn Rotman <s.j.rotman@tilburguniversity.edu>
2023-09-25 11:21:59 +0200

    Improve code style.

--- aeon/transformations/tests/test_split.py
+++ aeon/transformations/tests/test_split.py
@@ -3,9 +3,10 @@ import pytest
 
 from aeon.transformations._split import SplitsTimeSeries
 
+X = np.arange(10)
 testdata = [
-    (np.arange(10), 2, [np.array([0, 1, 2, 3, 4]), np.array([5, 6, 7, 8, 9])]),
-    (np.arange(10), 3, [np.array([0, 1, 2]), np.array([3, 4, 5]), np.array([6, 7, 8, 9])]),
+    (X, 2, [np.array([0, 1, 2, 3, 4]), np.array([5, 6, 7, 8, 9])]),
+    (X, 3, [np.array([0, 1, 2]), np.array([3, 4, 5]), np.array([6, 7, 8, 9])]),
 ]
 
 
@@ -18,4 +19,3 @@ def test_split_(X, n_intervals, expected):
     assert len(res) == n_intervals
     for x, y in zip(res, expected):
         np.testing.assert_array_equal(x, y)
-
