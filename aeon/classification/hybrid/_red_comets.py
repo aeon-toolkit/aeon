d58654c7c6eaba13da56a33b7059cfcbc5c8fa1b
Luca Bennett <zy18811@bristol.ac.uk>
2023-09-26 22:33:35 +0100

    get_test_params() defined

--- aeon/classification/hybrid/_red_comets.py
+++ aeon/classification/hybrid/_red_comets.py
@@ -544,3 +544,26 @@ class REDCOMETS(BaseClassifier):
             )
         )
         return lenses
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        dict
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`.
+        """
+        return {
+            "variant": 1,
+            "n_trees": 3,
+        }
