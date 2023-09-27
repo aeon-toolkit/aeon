ec72dc758db3cb58326040b5cdd6f6735d6a3f7f
Luca Bennett <74973887+zy18811@users.noreply.github.com>
2023-09-27 14:23:18 +0100

    Revert _sfa.py

--- aeon/transformations/collection/dictionary_based/_sfa.py
+++ aeon/transformations/collection/dictionary_based/_sfa.py
@@ -1016,10 +1016,6 @@ class SFA(BaseCollectionTransformer):
         word_bits = self.word_bits + self.level_bits
         shift = word_bits - self.letter_bits
         
-        if word > np.iinfo(np.int64).max:
-            shift = int(shift)
-
-
         for _ in range(self.word_length, 0, -1):
             letters.append(word >> shift & self.letter_max)
             shift -= self.letter_bits
