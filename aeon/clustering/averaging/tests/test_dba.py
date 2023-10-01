2329332de08be8bd6412782c0ea6f9c81da3613a
chrisholder <chrisholder987@hotmail.com>
2023-10-01 20:42:10 +0100

    [MNT] Kmeans refactor (#762)
    * begun kmeans refactor
    
    * continue
    
    * first pass of refactor
    
    * precommit
    
    * examples
    
    * removed metric directory
    
    * removed metric directory
    
    * precommit
    
    * fixed bad imports
    
    * fixed bad imports
    
    * fixed plt and example notebook
    
    * removed partition from api
    
    * updated tests
    
    * setup
    
    * fixed kmeans++ bug
    
    * setup
    
    * added to init
    
    * precommit
    
    * fixed notebooks
    
    * setup

--- aeon/clustering/averaging/tests/test_dba.py
+++ aeon/clustering/averaging/tests/test_dba.py
@@ -2,7 +2,7 @@
 """Tests for DBA."""
 import numpy as np
 
-from aeon.clustering.metrics.averaging import elastic_barycenter_average
+from aeon.clustering.averaging import elastic_barycenter_average
 from aeon.distances.tests._utils import create_test_distance_numpy
 
 expected_dba = np.array(
@@ -159,7 +159,7 @@ def test_elastic_dba_variations():
         X_train = create_test_distance_numpy(10, 10, 10)
 
         average_ts = elastic_barycenter_average(
-            X_train, metric=dist, window=0.2, independent=False
+            X_train, distance=dist, window=0.2, independent=False
         )
 
         assert isinstance(average_ts, np.ndarray)
