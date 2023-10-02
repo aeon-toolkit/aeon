a64e29bc648fdb166cfc719362c46d35dd1eba70
Tony Bagnall <ajb@uea.ac.uk>
2023-10-02 16:49:45 +0100

    add similarity base to register

--- aeon/registry/_base_classes.py
+++ aeon/registry/_base_classes.py
@@ -43,6 +43,7 @@ from aeon.forecasting.model_selection._split import BaseSplitter
 from aeon.networks.base import BaseDeepNetwork
 from aeon.performance_metrics.base import BaseMetric
 from aeon.regression.base import BaseRegressor
+from aeon.similarity_search.base import BaseSimiliaritySearch
 from aeon.transformations.base import BaseTransformer
 from aeon.transformations.collection import BaseCollectionTransformer
 
@@ -64,6 +65,7 @@ BASE_CLASS_REGISTER = [
         BaseCollectionTransformer,
         "time series collection transformer",
     ),
+    ("similarity_search", BaseSimiliaritySearch, "similarity search"),
 ]
 
 
