5d796714abc8d3db2c1c80dbd991b9c23d528433
Tony Bagnall <ajb@uea.ac.uk>
2023-10-20 16:52:38 +0100

    purge numpyflat

--- aeon/utils/validation/tests/test_collection.py
+++ aeon/utils/validation/tests/test_collection.py
@@ -7,20 +7,20 @@ from aeon.datasets import make_example_multi_index_dataframe
 from aeon.utils._testing.tests.test_collection import make_nested_dataframe_data
 from aeon.utils.validation._convert_collection import (
     _equal_length,
-    _from_nested_univ_to_numpyflat,
+    _from_nested_univ_to_numpy2d,
     _from_nested_univ_to_pd_multiindex,
+    _from_numpy2d_to_df_list,
+    _from_numpy2d_to_nested_univ,
+    _from_numpy2d_to_np_list,
+    _from_numpy2d_to_numpy3d,
+    _from_numpy2d_to_pd_multiindex,
+    _from_numpy2d_to_pd_wide,
     _from_numpy3d_to_df_list,
     _from_numpy3d_to_nested_univ,
     _from_numpy3d_to_np_list,
-    _from_numpy3d_to_numpyflat,
+    _from_numpy3d_to_numpy2d,
     _from_numpy3d_to_pd_multiindex,
     _from_numpy3d_to_pd_wide,
-    _from_numpyflat_to_df_list,
-    _from_numpyflat_to_nested_univ,
-    _from_numpyflat_to_np_list,
-    _from_numpyflat_to_numpy3d,
-    _from_numpyflat_to_pd_multiindex,
-    _from_numpyflat_to_pd_wide,
     _is_nested_univ_dataframe,
     _is_pd_wide,
     _nested_univ_is_equal,
@@ -240,7 +240,7 @@ NUMPY3D = [
     _from_numpy3d_to_np_list,
     _from_numpy3d_to_df_list,
     _from_numpy3d_to_pd_wide,
-    _from_numpy3d_to_numpyflat,
+    _from_numpy3d_to_numpy2d,
     _from_numpy3d_to_nested_univ,
     _from_numpy3d_to_pd_multiindex,
 ]
@@ -254,12 +254,12 @@ def test_numpy3D_error(function):
 
 
 NUMPY2D = [
-    _from_numpyflat_to_numpy3d,
-    _from_numpyflat_to_np_list,
-    _from_numpyflat_to_df_list,
-    _from_numpyflat_to_pd_wide,
-    _from_numpyflat_to_nested_univ,
-    _from_numpyflat_to_pd_multiindex,
+    _from_numpy2d_to_numpy3d,
+    _from_numpy2d_to_np_list,
+    _from_numpy2d_to_df_list,
+    _from_numpy2d_to_pd_wide,
+    _from_numpy2d_to_nested_univ,
+    _from_numpy2d_to_pd_multiindex,
 ]
 
 
@@ -306,14 +306,14 @@ def test_from_nested():
     }
     X = pd.DataFrame(data)
     with pytest.raises(
-        TypeError, match="Cannot convert unequal length series to " "numpyflat"
+        TypeError, match="Cannot convert unequal length series to numpy2D"
     ):
-        _from_nested_univ_to_numpyflat(X)
+        _from_nested_univ_to_numpy2d(X)
     X, _ = make_nested_dataframe_data(n_cases=10, n_channels=1, n_timepoints=20)
-    result = _from_nested_univ_to_numpyflat(X)
+    result = _from_nested_univ_to_numpy2d(X)
     assert result.shape == (10, 20)
     X, _ = make_nested_dataframe_data(n_cases=10, n_channels=2, n_timepoints=20)
     with pytest.raises(
-        TypeError, match="Cannot convert multivariate nested into " "numpyflat"
+        TypeError, match="Cannot convert multivariate nested into numpy2D"
     ):
-        _from_nested_univ_to_numpyflat(X)
+        _from_nested_univ_to_numpy2d(X)
