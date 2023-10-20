f04b4362a0c9ba98cc60a61c2ba10ebd112476b2
Tony Bagnall <ajb@uea.ac.uk>
2023-10-20 17:48:17 +0100

    some testing

--- aeon/utils/validation/_convert_collection.py
+++ aeon/utils/validation/_convert_collection.py
@@ -48,12 +48,11 @@ def _is_nested_univ_dataframe(X):
 
 def _is_pd_wide(X):
     """Check whether the input nested DataFrame is "pd-wide" type."""
-    # only test is if all values are float. This from chatgpt seems stupid
+    # Test if all float values
     if isinstance(X, pd.DataFrame) and not isinstance(X.index, pd.MultiIndex):
         if _is_nested_univ_dataframe(X):
             return False
-        float_cols = X.select_dtypes(include=[float]).columns
-        for col in float_cols:
+        for col in X.columns:
             if not np.issubdtype(X[col].dtype, np.floating):
                 return False
         return True
