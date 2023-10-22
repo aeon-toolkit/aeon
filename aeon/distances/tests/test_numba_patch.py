7f5fa4f690ec9f8aa936d73f17ca44fb34ef18f4
chrisholder <chrisholder987@hotmail.com>
2023-10-22 21:16:15 +0100

    added prints

--- aeon/distances/tests/test_numba_patch.py
+++ aeon/distances/tests/test_numba_patch.py
@@ -1,4 +1,3 @@
-import logging
 import pickle
 
 import numba.core.caching
@@ -10,6 +9,7 @@ from aeon.distances._distance import DISTANCES
 
 # Step 1: Define a new function with the same signature as the method you're replacing.
 def custom_load_index(self):
+    print("HEHREH")  # noqa: T001, T201
     try:
         with open(self._index_path, "rb") as f:
             version = pickle.load(f)
@@ -23,13 +23,13 @@ def custom_load_index(self):
         return {}
     stamp, overloads = pickle.loads(data)
     if stamp != self._source_stamp:
-        logging.info("Stamp not equal")  # noqa: T001, T201
-        logging.info(f"Stamp: {stamp}")  # noqa: T001, T201
-        logging.info(f"Source stamp: {self._source_stamp}")  # noqa: T001, T201
+        print("Stamp not equal")  # noqa: T001, T201
+        print(f"Stamp: {stamp}")  # noqa: T001, T201
+        print(f"Source stamp: {self._source_stamp}")  # noqa: T001, T201
 
     original_result = original_load_index(self)  # Calling the original method
     data = f"Cache loaded with the following data: {original_result}"
-    logging.info(data)  # noqa: T001, T201
+    print(data)  # noqa: T001, T201
     return original_result
 
 
@@ -40,7 +40,7 @@ numba.core.caching.IndexDataCacheFile._load_index = custom_load_index
 
 @pytest.mark.parametrize("dist", DISTANCES)
 def test_numba_cache(dist):
+    print("CALLED")  # noqa: T001, T201
     x = np.array([[1, 2, 3], [4, 5, 6]])
     y = np.array([[1, 2, 3], [4, 5, 6]])
     dist["distance"](x, y)
-    assert True
