cbda338062e4eae29c01b428790e8f26cb742f2d
chrisholder <chrisholder987@hotmail.com>
2023-10-22 20:35:32 +0100

    added numba patch

---
+++ aeon/distances/tests/test_numba_patch.py
@@ -0,0 +1,42 @@
+import pickle
+
+import numba.core.caching
+
+from aeon.distances import squared_distance
+from aeon.distances.tests._utils import create_test_distance_numpy
+
+
+# Step 1: Define a new function with the same signature as the method you're replacing.
+def custom_load_index(self):
+    try:
+        with open(self._index_path, "rb") as f:
+            version = pickle.load(f)
+            data = f.read()
+    except FileNotFoundError:
+        # Index doesn't exist yet?
+        return {}
+    if version != self._version:
+        # This is another version.  Avoid trying to unpickling the
+        # rest of the stream, as that may fail.
+        return {}
+    stamp, overloads = pickle.loads(data)
+    if stamp != self._source_stamp:
+        print("Stamp not equal")  # noqa: T001, T201
+        print(f"Stamp: {stamp}")  # noqa: T001, T201
+        print(f"Source stamp: {self._source_stamp}")  # noqa: T001, T201
+
+    original_result = original_load_index(self)  # Calling the original method
+    data = f"Cache loaded with the following data: {original_result}"
+    print(data)  # noqa: T001, T201
+    return original_result
+
+
+original_load_index = numba.core.caching.IndexDataCacheFile._load_index
+
+numba.core.caching.IndexDataCacheFile._load_index = custom_load_index
+
+
+def test_numba_cache():
+    x = create_test_distance_numpy(10, 10)
+    y = create_test_distance_numpy(10, 10, random_state=2)
+    squared_distance(x, y)
