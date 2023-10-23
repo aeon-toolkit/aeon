bf1c1a8be585ed71254cb4e6fa260cf7a2f5eaab
chrisholder <chrisholder987@hotmail.com>
2023-10-23 02:04:02 +0100

    trying something

--- aeon/distances/tests/test_numba_patch.py
+++ aeon/distances/tests/test_numba_patch.py
@@ -21,21 +21,34 @@ def custom_load_index(self):
         # rest of the stream, as that may fail.
         return {}
     stamp, overloads = pickle.loads(data)
-    if stamp != self._source_stamp:
-        print("-----------------------------")  # noqa: T001, T201
-        print("Stamp not equal")  # noqa: T001, T201
-        print(f"Stamp: {stamp}")  # noqa: T001, T201
-        print(f"Source stamp: {self._source_stamp}")  # noqa: T001, T201
-        print("-----------------------------")  # noqa: T001, T201
-    else:
-        print("+++++++++++++++++++++++++++++")  # noqa: T001, T201
-        print("Stamp equal")  # noqa: T001, T201
-        print("+++++++++++++++++++++++++++++")  # noqa: T001, T201
-
-    original_result = original_load_index(self)  # Calling the original method
-    data = f"Cache loaded with the following data: {original_result}"
-    print(data)  # noqa: T001, T201
-    return original_result
+    return overloads
+    # try:
+    #     with open(self._index_path, "rb") as f:
+    #         version = pickle.load(f)
+    #         data = f.read()
+    # except FileNotFoundError:
+    #     # Index doesn't exist yet?
+    #     return {}
+    # if version != self._version:
+    #     # This is another version.  Avoid trying to unpickling the
+    #     # rest of the stream, as that may fail.
+    #     return {}
+    # stamp, overloads = pickle.loads(data)
+    # if stamp != self._source_stamp:
+    #     print("-----------------------------")  # noqa: T001, T201
+    #     print("Stamp not equal")  # noqa: T001, T201
+    #     print(f"Stamp: {stamp}")  # noqa: T001, T201
+    #     print(f"Source stamp: {self._source_stamp}")  # noqa: T001, T201
+    #     print("-----------------------------")  # noqa: T001, T201
+    # else:
+    #     print("+++++++++++++++++++++++++++++")  # noqa: T001, T201
+    #     print("Stamp equal")  # noqa: T001, T201
+    #     print("+++++++++++++++++++++++++++++")  # noqa: T001, T201
+    #
+    # original_result = original_load_index(self)  # Calling the original method
+    # data = f"Cache loaded with the following data: {original_result}"
+    # print(data)  # noqa: T001, T201
+    # return original_result
 
 
 original_load_index = numba.core.caching.IndexDataCacheFile._load_index
