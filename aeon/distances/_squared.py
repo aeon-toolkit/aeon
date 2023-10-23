d62537ab9f0ea8e769c6f507ac6704d997d3f278
chrisholder <chrisholder987@hotmail.com>
2023-10-24 00:48:32 +0100

    changed file

--- aeon/distances/_squared.py
+++ aeon/distances/_squared.py
@@ -5,6 +5,8 @@ from numba import njit
 
 from aeon.distances._utils import reshape_pairwise_to_multiple
 
+# Test change
+
 
 @njit(cache=True, fastmath=True)
 def squared_distance(x: np.ndarray, y: np.ndarray) -> float:
