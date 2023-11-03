4dcceab594c4d48b91306cf6cee696d50a6ebc65
Tony Bagnall <ajb@uea.ac.uk>
2023-11-03 16:14:06 +0000

    remove test of invalid dir

--- aeon/datasets/tests/test_data_writers.py
+++ aeon/datasets/tests/test_data_writers.py
@@ -166,8 +166,6 @@ def test__write_header():
             ValueError, match="Cannot have class_labels true for a regression problem"
         ):
             _write_header(tmp, problem_name, class_labels=True, regression=True)
-    with pytest.raises(ValueError, match="Error trying to access"):
-        _write_header(path="<invalid:/\0|*?>", problem_name=problem_name)
     _write_header(
         tmp,
         problem_name,
