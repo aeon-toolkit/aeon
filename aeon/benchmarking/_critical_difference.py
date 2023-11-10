1f46462e0d166e29eb598699dcf8e582cee12948
David Guijo-Rubio <dguijo@uco.es>
2023-11-10 14:07:29 +0100

    Correction methods

--- aeon/benchmarking/_critical_difference.py
+++ aeon/benchmarking/_critical_difference.py
@@ -5,6 +5,8 @@ __author__ = ["SveaMeyer13", "dguijo"]
 import math
 
 import numpy as np
+
+# import pandas as pd
 from scipy.stats import distributions, find_repeats, rankdata, wilcoxon
 
 from aeon.benchmarking.utils import get_qalpha
@@ -57,9 +59,8 @@ def _check_friedman(ranks):
     return chisq, p_value
 
 
-def nemenyi_cliques(n_estimators, n_datasets, ordered_avg_ranks, alpha):
+def nemenyi_test(n_estimators, n_datasets, ordered_avg_ranks, alpha):
     """Find cliques using post hoc Nemenyi test."""
-    # Get critical value, there is an exact way now
     qalpha = get_qalpha(alpha)
     # calculate critical difference with Nemenyi
     cd = qalpha[n_estimators] * np.sqrt(
@@ -72,13 +73,29 @@ def nemenyi_cliques(n_estimators, n_datasets, ordered_avg_ranks, alpha):
     cliques[cliques < 0] = np.inf
     cliques = cliques < cd
 
-    cliques = build_cliques(cliques)
+    cliques = _build_cliques(cliques)
 
     return cliques
 
 
-def wilcoxon_holm_cliques(results, alpha):
-    """Find cliques using Wilcoxon and post hoc Holm test."""
+def wilcoxon_test(results, adjusted_alpha):
+    """
+    Perform Wilcoxon test.
+
+    Parameters
+    ----------
+    results: np.array
+      results of strategies on datasets
+    adjusted_alpha: float
+        alpha level adjusted for multiple testing
+
+    Returns
+    -------
+    cliques: list of lists
+        statistically similar cliques
+    p_values: np.array
+        p-values of Wilcoxon test
+    """
     n_estimators = results.shape[1]
 
     p_values = np.eye(n_estimators)
@@ -89,34 +106,36 @@ def wilcoxon_holm_cliques(results, alpha):
                 results[:, i], results[:, j], zero_method="wilcox"
             )[1]
 
-    # correct alpha with holm
-    new_alpha = float(alpha / (n_estimators - 1))
-
-    cliques = build_cliques(p_values > new_alpha)
+    cliques = _build_cliques(p_values > adjusted_alpha)
 
     return cliques, p_values
 
 
-def build_cliques(same):
-    """Build cliques."""
-    n_estimators = same.shape[1]
+def _build_cliques(pairwise_matrix):
+    """
+    Build cliques from pairwise comparison matrix.
 
-    for i in range(n_estimators - 1, 1, -1):
-        if np.sum(same[i, :]) > 1:
-            true_values_i = np.where(same[i, :] == 1)[0]
-            first_true_i = true_values_i[0]
-            last_true_i = true_values_i[-1]
-            for j in range(i - 1, 0, -1):
-                true_values_j = np.where(same[j, :] == 1)[0]
-                first_true_j = true_values_j[0]
-                last_true_j = true_values_j[-1]
-                if np.all(same[i, :] == same[j, :]) or (
-                    first_true_i >= first_true_j and last_true_i <= last_true_j
-                ):
-                    same[i, :] = 0
+    Parameters
+    ----------
+    pairwise_matrix: np.array
+        pairwise comparison matrix
 
-    n = np.sum(same, 1)
-    cliques = same[n > 1, :]
+    Returns
+    -------
+    cliques: list of lists
+        statistically similar cliques
+    """
+    n = np.sum(pairwise_matrix, 1)
+    possible_cliques = pairwise_matrix[n > 1, :]
+
+    for i in range(possible_cliques.shape[0] - 1, 1, -1):
+        for j in range(i - 1, 0, -1):
+            if np.all(possible_cliques[j, possible_cliques[i, :]]):
+                possible_cliques[i, :] = 0
+                break
+
+    n = np.sum(possible_cliques, 1)
+    cliques = possible_cliques[n > 1, :]
 
     return cliques
 
@@ -126,26 +145,37 @@ def plot_critical_difference(
     labels,
     highlight=None,
     errors=False,
-    clique_method="holm",
+    test="wilconxon",
+    correction="holm",
     alpha=0.05,
     width=6,
     textspace=1.5,
     reverse=True,
 ):
     """
-    Draw critical difference diagram.
+    Find cliques using Wilcoxon and post hoc Holm test.
 
-    Step 1 & 2: Calculate average ranks from data
-    Step 3: Use Friedman test to check whether
-    the strategy significantly affects the classification performance
-    Step 4: Compute critical differences using Nemenyi post-hoc test.
-    (How much should the average rank of two strategies differ to be
-     statistically significant)
-    Step 5: Compute statistically similar cliques of strategies
-    Step 6: Draw the diagram
+    Computes groups of estimators (cliques) within which there is no significant
+    difference. The algorithm assumes that the estimators (named in labels) are
+    sorted by average rank, so that labels[0] is the "best" estimator in terms of
+    lowest average rank.
 
-    See Janez Demsar, Statistical Comparisons of Classifiers over
-    Multiple Data Sets, 7(Jan):1--30, 2006.
+    This algorithm first forms a clique for each estimator set as control, then merges
+    dominated cliques.
+
+    Suppose we have four estimators, A, B, C and D sorted by average rank. Starting
+    from A, we test the null hypothesis that average ranks are equal against the
+    alternative hypothesis that the average rank of A is less than that of B. If we
+    reject the null hypothesis then we stop, and A is not in a clique. If we cannot
+    reject the null, we test A vs C, continuing until we reject the null or we have
+    tested all estimators.
+
+    Suppose we find B is significantly worse that A, but that on the next iteration we
+    find no difference between B and C, nor any difference between B and D. We have
+    formed one clique, [B, C, D]. On the third iteration, we also find not difference
+    between C and D and thus form a second clique, [C, D]. We have found two cliques,
+    but [C,D] is contained in [B, C, D] and is thus redundant. In this case we would
+    return a single clique, [ B, C, D].
 
     Parts of the code are copied and adapted from here:
     https://github.com/hfawaz/cd-diagram
@@ -161,8 +191,10 @@ def plot_critical_difference(
             be the same as scores
         errors : bool, default = False
             indicates whether scores are passed as errors (default) or accuracies
-        clique_method : string, default = "holm"
-            clique forming method, to include "nemenyi" and "holm"
+        test : string, default = "wilcoxon"
+            test method, to include "nemenyi" and "wilcoxon"
+        correction: string, default = "holm"
+            correction method, to include "bonferroni", "holm" and None
         alpha : float default = 0.05
              Alpha level for statistical tests currently supported: 0.1, 0.05 or 0.01)
         width : int, default = 6
@@ -186,7 +218,7 @@ def plot_critical_difference(
     >>> plot = plot_critical_difference(results[0], methods, alpha=0.1)\
         # doctest: +SKIP
     >>> plot.show()  # doctest: +SKIP
-    >>> plot.savefig("scatterplot.pdf", bbox_inches="tight")  # doctest: +SKIP
+    >>> plot.savefig("cd.pdf", bbox_inches="tight")  # doctest: +SKIP
     """
     _check_soft_dependencies("matplotlib")
 
@@ -230,19 +262,35 @@ def plot_critical_difference(
     _, p_value_friedman = _check_friedman(ranks)
     # Step 4: If Friedman test is significant find cliques
     if p_value_friedman < alpha:
-        if clique_method == "nemenyi":
-            cliques = nemenyi_cliques(
-                n_estimators, n_datasets, ordered_avg_ranks, alpha
-            )
-        elif clique_method == "holm":
-            cliques, p_values = wilcoxon_holm_cliques(ordered_scores, alpha)
+        if test == "nemenyi":
+            cliques = nemenyi_test(n_estimators, n_datasets, ordered_avg_ranks, alpha)
+        elif test == "wilcoxon":
+            if correction == "bonferroni":
+                adjusted_alpha = alpha / (n_estimators * (n_estimators - 1) / 2)
+            elif correction == "holm":
+                adjusted_alpha = alpha / (n_estimators - 1)
+            elif correction is None:
+                adjusted_alpha = alpha
+            else:
+                raise ValueError("correction available are None, bonferroni and holm.")
+            cliques, p_values = wilcoxon_test(ordered_scores, adjusted_alpha)
         else:
-            raise ValueError(
-                "clique methods available are only nemenyi, bonferroni and holm."
-            )
+            raise ValueError("tests available are only nemenyi and wilcoxon.")
     # If Friedman test is not significant everything has to be one clique
     else:
         cliques = [[1] * n_estimators]
+
+    # pd.DataFrame(p_values, index=ordered_labels, columns=ordered_labels).round(
+    #     6
+    # ).to_csv("./p_values.csv")
+
+    # print((alpha / (n_estimators - 1)))
+
+    # pd.DataFrame(
+    #     p_values > (alpha / (n_estimators - 1)),
+    #     index=ordered_labels,
+    #     columns=ordered_labels,
+    # ).to_csv("./same.csv")
 
     # Step 6 create the diagram:
     # check from where to where the axis has to go
