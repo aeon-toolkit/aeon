0ee47515c5712233e9a3c5fbef115207d21ab54a
Tony Bagnall <ajb@uea.ac.uk>
2023-11-27 15:32:41 +0000

    import Lite

--- aeon/networks/__init__.py
+++ aeon/networks/__init__.py
@@ -9,12 +9,14 @@ __all__ = [
     "ResNetNetwork",
     "TapNetNetwork",
     "AEFCNNetwork",
+    "LITENetwork",
 ]
 from aeon.networks._ae_fcn import AEFCNNetwork
 from aeon.networks._cnn import CNNNetwork
 from aeon.networks._encoder import EncoderNetwork
 from aeon.networks._fcn import FCNNetwork
 from aeon.networks._inception import InceptionNetwork
+from aeon.networks._lite import LITENetwork
 from aeon.networks._mlp import MLPNetwork
 from aeon.networks._resnet import ResNetNetwork
 from aeon.networks._tapnet import TapNetNetwork
