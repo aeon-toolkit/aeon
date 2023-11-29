1e8d587dfee545133d1110e1c2687c9665422199
Tony Bagnall <ajb@uea.ac.uk>
2023-11-29 12:21:35 +0000

    conversion pipeline

--- aeon/classification/compose/_pipeline.py
+++ aeon/classification/compose/_pipeline.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from aeon.base import _HeterogenousMetaEstimator
 from aeon.classification.base import BaseClassifier
-from aeon.datatypes import convert_to
 from aeon.transformations.base import BaseTransformer
 from aeon.transformations.compose import TransformerPipeline
 from aeon.utils.sklearn import is_sklearn_classifier
@@ -444,27 +443,14 @@ class SklearnClassifierPipeline(_HeterogenousMetaEstimator, BaseClassifier):
             return NotImplemented
 
     def _convert_X_to_sklearn(self, X):
-        """Convert a Table or Panel X to 2D numpy required by sklearn."""
-        if isinstance(X, np.ndarray):
-            if X.ndim == 2:
-                return X
-            elif X.ndim == 3:
-                return np.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]))
+        """Convert X to 2D numpy required by sklearn."""
+        from aeon.utils.validation.collection import convert_collection
 
-        output_type = self.transformers_.get_tag("output_data_type")
-        # if output_type is Primitives, output is Table, convert to 2D numpy array
-        if output_type == "Primitives":
-            Xt = convert_to(X, to_type="numpy2D", as_scitype="Table")
-        # if output_type is Series, output is Panel, convert to 2D numpy array
-        elif output_type == "Series":
-            Xt = convert_to(X, to_type="numpy2D", as_scitype="Panel")
-        else:
-            raise TypeError(
-                f"unexpected X output type "
-                f'in tag "output_data_type", found "{output_type}", '
-                'expected one of "Primitives" or "Series"'
-            )
-
+        Xt = convert_collection(X, "numpy3D")
+        if Xt.ndim == 2:
+            return X
+        elif Xt.ndim == 3:
+            return np.reshape(Xt, (Xt.shape[0], Xt.shape[1] * Xt.shape[2]))
         return Xt
 
     def _fit(self, X, y):
