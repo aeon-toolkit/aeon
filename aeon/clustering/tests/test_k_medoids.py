9cb4aff3467fe82a0a7336855b7d4ed7d48ae45e
Tony Bagnall <ajb@uea.ac.uk>
2023-12-04 16:43:03 +0000

    [ENH] Make clustering files private (#943)
    * refactor clustering files
    
    * notebooks

--- aeon/clustering/tests/test_k_medoids.py
+++ aeon/clustering/tests/test_k_medoids.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import metrics
 from sklearn.utils import check_random_state
 
-from aeon.clustering.k_medoids import TimeSeriesKMedoids
+from aeon.clustering._k_medoids import TimeSeriesKMedoids
 from aeon.datasets import load_basic_motions, load_gunpoint
 from aeon.distances import euclidean_distance
 
