a2d62ce6aeaa657d1c8f21157d8a29089b2e1d9c
Tony Bagnall <ajb@uea.ac.uk>
2023-12-18 19:02:21 +0000

    segmenter interface tests

--- aeon/segmentation/tests/test_all_segmenters.py
+++ aeon/segmentation/tests/test_all_segmenters.py
@@ -1,9 +1,24 @@
 """Tests for all segmenters."""
+import pytest
 
-# 1. Test they implement predict
+from aeon.registry import all_estimators
 
-# 2. Test that fit_is_empty is correctly set
+ALL_SEGMENTERS = all_estimators(
+    estimator_types="segmenter",
+    return_names=False,
+)
 
-# 3. Test that multivariate capability is correctly set
 
-# 4. Test axis is correctly set.
+@pytest.mark.parametrize("segmenter", ALL_SEGMENTERS)
+def test_segmenter_interface(segmenter):
+    """Test does not override final methods."""
+    assert "fit" not in segmenter.__dict__
+    assert "predict" not in segmenter.__dict__
+    # Test that all segmenters implement abstract predict.
+    assert "_predict" in segmenter.__dict__
+    # Test that fit_is_empty is correctly set
+    fit_is_empty = segmenter.get_class_tag(tag_name="fit_is_empty")
+    assert not fit_is_empty == "_fit" not in segmenter.__dict__
+    # Test that capability:multivariate is correctly set
+    # multivariate = segmenter.get_class_tag(tag_name="capability:multivariate")
+    # Test axis is correctly set.
