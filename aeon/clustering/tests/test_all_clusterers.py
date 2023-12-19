b9d594a25ac1d73fe5934e5fb2d9dfcabe606b4c
Tony Bagnall <ajb@uea.ac.uk>
2023-12-19 17:49:39 +0000

    standardise regression, classification and clustering tests

--- aeon/clustering/tests/test_all_clusterers.py
+++ aeon/clustering/tests/test_all_clusterers.py
@@ -9,14 +9,7 @@ ALL_CLUSTERERS = all_estimators("clusterer", return_names=False)
 
 
 @pytest.mark.parametrize("clst", ALL_CLUSTERERS)
-def test_interface_compliance(clst):
-    """Test does not override final methods."""
-    assert "fit" not in clst.__dict__
-    assert "predict" not in clst.__dict__
-
-
-@pytest.mark.parametrize("clst", ALL_CLUSTERERS)
-def test_capability_tags(clst):
+def test_classifier_tags_consistent(clst):
     """Test all estimators capability tags reflect their capabilities."""
     if not soft_deps_installed(clst):
         return
@@ -29,13 +22,18 @@ def test_capability_tags(clst):
             assert internal_types in valid_types
         else:  # must be a list
             assert bool(set(internal_types) & valid_types)
-
     # Test can actually fit/predict with multivariate if tag is set
     multivariate = clst.get_class_tag("capability:multivariate")
-    X = np.random.random((10, 2, 20))
-    inst = clst.create_test_instance(parameter_set="default")
-
     if multivariate:
+        X = np.random.random((10, 2, 20))
+        inst = clst.create_test_instance(parameter_set="default")
         inst.fit(X)
         inst.predict(X)
         inst.predict_proba(X)
+
+
+@pytest.mark.parametrize("clst", ALL_CLUSTERERS)
+def test_does_not_override_final_methods(clst):
+    """Test does not override final methods."""
+    assert "fit" not in clst.__dict__
+    assert "predict" not in clst.__dict__
