9fb62ac2eafa724ad64fa01f42f0cc35749f729c
Tony Bagnall <ajb@uea.ac.uk>
2024-01-13 18:19:45 +0000

    add mock classifier

--- aeon/testing/mock_estimators/__init__.py
+++ aeon/testing/mock_estimators/__init__.py
@@ -6,9 +6,17 @@ __all__ = [
     "MockForecaster",
     "MockUnivariateForecasterLogger",
     "make_mock_estimator",
+    "MockClassifier",
+    "MockClassifierPredictProba",
+    "MockClassifierFullTags",
 ]
 
-from aeon.utils.estimators._mock_forecasters import (
+from aeon.testing.mock_estimators._mock_classifiers import (
+    MockClassifier,
+    MockClassifierFullTags,
+    MockClassifierPredictProba,
+)
+from aeon.testing.mock_estimators._mock_forecasters import (
     MockForecaster,
     MockUnivariateForecasterLogger,
     make_mock_estimator,
