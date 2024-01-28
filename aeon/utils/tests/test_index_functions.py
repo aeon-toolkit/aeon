454ecfcce59340412e6b58adc3c3847121a862f0
Tony Bagnall <ajb@uea.ac.uk>
2024-01-28 15:02:49 +0000

    move test index out of datatypes

--- aeon/utils/tests/test_index_functions.py
+++ aeon/utils/tests/test_index_functions.py
@@ -1,14 +1,10 @@
 """Testing index functions."""
 
-__author__ = ["fkiraly"]
-
 import numpy as np
 import pandas as pd
 import pytest
 from pandas.api.types import is_integer_dtype
 
-from aeon.datatypes._check import check_is_mtype
-from aeon.datatypes._examples import get_examples
 from aeon.testing.utils.data_gen import _make_hierarchical
 from aeon.utils.index_functions import (
     _get_cutoff_from_index,
@@ -18,68 +14,47 @@ from aeon.utils.index_functions import (
     get_window,
 )
 
-SCITYPE_MTYPE_PAIRS = [
-    ("Series", "pd.Series"),
-    ("Series", "pd.DataFrame"),
-    ("Series", "np.ndarray"),
-    ("Panel", "pd-multiindex"),
-    ("Panel", "numpy3D"),
-    ("Panel", "nested_univ"),
-    ("Hierarchical", "pd_multiindex_hier"),
+cols = ["instances", "timepoints"] + [f"var_{i}" for i in range(2)]
+
+Xlist = [
+    pd.DataFrame([[0, 0, 1, 4], [0, 1, 2, 5], [0, 2, 3, 6]], columns=cols),
+    pd.DataFrame([[1, 0, 1, 4], [1, 1, 2, 55], [1, 2, 3, 6]], columns=cols),
+    pd.DataFrame([[2, 0, 1, 42], [2, 1, 2, 5], [2, 2, 3, 6]], columns=cols),
 ]
-multi_index = pd.MultiIndex.from_tuples(
-    [
-        ("0/0", 1),
-        ("0/1", 2),
-        ("0/2", 3),
-        ("1/0", 1),
-        ("1/1", 2),
-        ("1/2", 3),
-        ("2/0", 1),
-        ("2/1", 2),
-        ("2/2", 3),
-    ]
-)
-data = {"var_0": np.random.rand(9), "var_1": np.random.rand(9)}
+X = pd.concat(Xlist)
+multi_index = X.set_index(["instances", "timepoints"])
 
-X_multi = pd.DataFrame(data, index=multi_index)
-
-multi_index_hier = pd.MultiIndex.from_tuples(
-    [
-        ("a", "0/0", 1),
-        ("a", "0/1", 2),
-        ("a", "0/2", 3),
-        ("a", "1/0", 1),
-        ("a", "1/1", 2),
-        ("a", "1/2", 3),
-        ("a", "2/0", 1),
-        ("a", "2/1", 2),
-        ("a", "2/2", 3),
-        ("b", "0/0", 1),
-        ("b", "0/1", 2),
-        ("b", "0/2", 3),
-        ("b", "1/0", 1),
-        ("b", "1/1", 2),
-        ("b", "1/2", 3),
-        ("b", "2/0", 1),
-        ("b", "2/1", 2),
-        ("b", "2/2", 3),
-    ]
-)
-X_hi = pd.DataFrame(
-    np.random.randint(1, 100, size=(18, 2)),
-    index=multi_index_hier,
-    columns=["var_0", "var_1"],
-)
-X_hi["var_0"] = [1, 2, 3] * 6
+cols = ["foo", "bar", "timepoints"] + [f"var_{i}" for i in range(2)]
+Xlist = [
+    pd.DataFrame(
+        [["a", 0, 0, 1, 4], ["a", 0, 1, 2, 5], ["a", 0, 2, 3, 6]], columns=cols
+    ),
+    pd.DataFrame(
+        [["a", 1, 0, 1, 4], ["a", 1, 1, 2, 55], ["a", 1, 2, 3, 6]], columns=cols
+    ),
+    pd.DataFrame(
+        [["a", 2, 0, 1, 42], ["a", 2, 1, 2, 5], ["a", 2, 2, 3, 6]], columns=cols
+    ),
+    pd.DataFrame(
+        [["b", 0, 0, 1, 4], ["b", 0, 1, 2, 5], ["b", 0, 2, 3, 6]], columns=cols
+    ),
+    pd.DataFrame(
+        [["b", 1, 0, 1, 4], ["b", 1, 1, 2, 55], ["b", 1, 2, 3, 6]], columns=cols
+    ),
+    pd.DataFrame(
+        [["b", 2, 0, 1, 42], ["b", 2, 1, 2, 5], ["b", 2, 2, 3, 6]], columns=cols
+    ),
+]
+X = pd.concat(Xlist)
+multi_index_hier = X.set_index(["foo", "bar", "timepoints"])
 
 EXAMPLE_DATA = {
     "pd.Series": pd.Series(np.random.rand(4)),
     "pd.DataFrame": pd.DataFrame(np.random.rand(4, 2)),
     "np.ndarray": np.random.rand(4, 2),
     "numpy3D": np.random.rand(4, 2, 3),
-    "pd-multiindex": X_multi,
-    "pd-multiindex-hier": X_hi,
+    "pd-multiindex": multi_index,
+    "pd-multiindex-hier": multi_index_hier,
 }
 
 
@@ -101,7 +76,9 @@ def test_get_time_index(datatype):
             assert len(idx) == data.shape[0]
         else:
             assert len(idx) == data.shape[-1]
-
+    if isinstance(data, pd.MultiIndex):
+        assert isinstance(idx, pd.Index)
+        assert (idx == data.get_level_values(-1)).all()
     if datatype in ["pd-multiindex", "pd_multiindex_hier"]:
         exp_idx = data.index.get_level_values(-1).unique()
         assert (idx == exp_idx).all()
@@ -110,14 +87,13 @@ def test_get_time_index(datatype):
 @pytest.mark.parametrize("convert_input", [True, False])
 @pytest.mark.parametrize("reverse_order", [True, False])
 @pytest.mark.parametrize("return_index", [True, False])
-@pytest.mark.parametrize("scitype,mtype", SCITYPE_MTYPE_PAIRS)
-def test_get_cutoff(scitype, mtype, return_index, reverse_order, convert_input):
+@pytest.mark.parametrize("datatype", EXAMPLE_DATA.keys())
+def test_get_cutoff(datatype, return_index, reverse_order, convert_input):
     """Tests that get_cutoff has correct output.
 
     Parameters
     ----------
-    scitype : str - scitype of input
-    mtype : str - mtype of input
+    datatype : str - datatype of input
     return_index : bool - whether index (True) or index element is returned (False)
     reverse_order : bool - whether first (True) or last index (False) is retrieved
     convert_input : bool - whether input is converted (True) or passed through (False)
@@ -127,57 +103,53 @@ def test_get_cutoff(scitype, mtype, return_index, reverse_order, convert_input):
     AssertionError if get_cutoff does not return a length 1 pandas.index
         for any fixture example of given scitype, mtype
     """
-    # retrieve example fixture
-    fixtures = get_examples(mtype=mtype, as_scitype=scitype, return_lossy=False)
+    # retrieve example data structure
+    data = EXAMPLE_DATA[datatype]
 
-    for fixture in fixtures.values():
-        if fixture is None:
-            continue
+    cutoff = get_cutoff(
+        data,
+        return_index=return_index,
+        reverse_order=reverse_order,
+        convert_input=convert_input,
+    )
 
-        cutoff = get_cutoff(
-            fixture,
-            return_index=return_index,
-            reverse_order=reverse_order,
-            convert_input=convert_input,
-        )
+    if return_index:
+        expected_types = pd.Index
+        cutoff_val = cutoff[0]
+    else:
+        expected_types = (int, float, np.int64, pd.Timestamp)
+        cutoff_val = cutoff
 
-        if return_index:
-            expected_types = pd.Index
-            cutoff_val = cutoff[0]
+    msg = (
+        f"incorrect return type of get_cutoff"
+        f"expected {expected_types}, found {type(cutoff)}"
+    )
+
+    assert isinstance(cutoff, expected_types), msg
+
+    if return_index:
+        assert len(cutoff) == 1
+        if isinstance(cutoff_val, (pd.Period, pd.Timestamp)):
+            assert hasattr(cutoff, "freq") and cutoff.freq is not None
+
+    if isinstance(data, np.ndarray):
+        if reverse_order:
+            assert cutoff_val == 0
         else:
-            expected_types = (int, float, np.int64, pd.Timestamp)
-            cutoff_val = cutoff
+            assert cutoff_val > 0
 
-        msg = (
-            f"incorrect return type of get_cutoff"
-            f"expected {expected_types}, found {type(cutoff)}"
-        )
+    if datatype in ["pd.Series", "pd.DataFrame"]:
+        if reverse_order:
+            assert cutoff_val == data.index[0]
+        else:
+            assert cutoff_val == data.index[-1]
 
-        assert isinstance(cutoff, expected_types), msg
-
-        if return_index:
-            assert len(cutoff) == 1
-            if isinstance(cutoff_val, (pd.Period, pd.Timestamp)):
-                assert hasattr(cutoff, "freq") and cutoff.freq is not None
-
-        if isinstance(fixture, np.ndarray):
-            if reverse_order:
-                assert cutoff_val == 0
-            else:
-                assert cutoff_val > 0
-
-        if mtype in ["pd.Series", "pd.DataFrame"]:
-            if reverse_order:
-                assert cutoff_val == fixture.index[0]
-            else:
-                assert cutoff_val == fixture.index[-1]
-
-        if mtype in ["pd-multiindex", "pd_multiindex_hier"]:
-            time_idx = fixture.index.get_level_values(-1)
-            if reverse_order:
-                assert cutoff_val == time_idx.min()
-            else:
-                assert cutoff_val == time_idx.max()
+    if datatype in ["pd-multiindex", "pd_multiindex_hier"]:
+        time_idx = data.index.get_level_values(-1)
+        if reverse_order:
+            assert cutoff_val == time_idx.min()
+        else:
+            assert cutoff_val == time_idx.max()
 
 
 @pytest.mark.parametrize("reverse_order", [True, False])
@@ -249,14 +221,13 @@ def test_get_cutoff_wrong_input(bad_inputs):
 
 
 @pytest.mark.parametrize("window_length, lag", [(2, 0), (None, 0), (4, 1)])
-@pytest.mark.parametrize("scitype,mtype", SCITYPE_MTYPE_PAIRS)
-def test_get_window_output_type(scitype, mtype, window_length, lag):
+@pytest.mark.parametrize("datatype", EXAMPLE_DATA.keys())
+def test_get_window_output_type(datatype, window_length, lag):
     """Tests that get_window runs for all mtypes, and returns output of same mtype.
 
     Parameters
     ----------
-    scitype : str - scitype of input
-    mtype : str - mtype of input
+    datatype : str - datatype of input
     window_length : int, passed to get_window
     lag : int, passed to get_window
 
@@ -265,17 +236,9 @@ def test_get_window_output_type(scitype, mtype, window_length, lag):
     Exception if get_window raises one
     """
     # retrieve example fixture
-    fixture = get_examples(mtype=mtype, as_scitype=scitype, return_lossy=False)[0]
-    X = get_window(fixture, window_length=window_length, lag=lag)
-    valid, err, _ = check_is_mtype(X, mtype=mtype, return_metadata=True)
-
-    msg = (
-        f"get_window should return an output of mtype {mtype} for that type of input, "
-        f"but it returns an output not conformant with that mtype."
-        f"Error from mtype check: {err}"
-    )
-
-    assert valid, msg
+    data = EXAMPLE_DATA[datatype]
+    X = get_window(data, window_length=window_length, lag=lag)
+    assert isinstance(X, type(data))
 
 
 def test_get_window_expected_result():
@@ -314,8 +277,8 @@ def test_get_window_expected_result():
     assert get_window(X_np3d, None, None).shape == (3, 2, 3)
 
 
-@pytest.mark.parametrize("scitype,mtype", SCITYPE_MTYPE_PAIRS)
-def test_get_slice_output_type(scitype, mtype):
+@pytest.mark.parametrize("datatype", EXAMPLE_DATA.keys())
+def test_get_slice_output_type(datatype):
     """Tests that get_slice runs for all mtypes, and returns output of same mtype.
 
     Parameters
@@ -328,17 +291,9 @@ def test_get_slice_output_type(scitype, mtype):
     Exception if get_slice raises one
     """
     # retrieve example fixture
-    fixture = get_examples(mtype=mtype, as_scitype=scitype, return_lossy=False)[0]
-    X = get_slice(fixture)
-    valid, err, _ = check_is_mtype(X, mtype=mtype, return_metadata=True)
-
-    msg = (
-        f"get_slice should return an output of mtype {mtype} for that type of input, "
-        f"but it returns an output not conformant with that mtype."
-        f"Error from mtype check: {err}"
-    )
-
-    assert valid, msg
+    data = EXAMPLE_DATA[datatype]
+    X = get_slice(data)
+    assert isinstance(X, type(data))
 
 
 def test_get_slice_expected_result():
@@ -348,11 +303,11 @@ def test_get_slice_expected_result():
     ------
     Exception if get_slice raises one
     """
-    X_df = get_examples(mtype="pd.DataFrame")[0]
+    X_df = EXAMPLE_DATA["pd.DataFrame"]
     assert len(get_slice(X_df, start=1, end=3)) == 2
 
-    X_s = get_examples(mtype="pd.Series")[0]
+    X_s = EXAMPLE_DATA["pd.Series"]
     assert len(get_slice(X_s, start=1, end=3)) == 2
 
-    X_np = get_examples(mtype="numpy3D")[0]
+    X_np = EXAMPLE_DATA["numpy3D"]
     assert get_slice(X_np, start=1, end=3).shape == (2, 2, 3)
