b0a293ab41347f8dec0d4a218b07a1c742ebad9f
Tony Bagnall <ajb@uea.ac.uk>
2024-01-30 22:17:54 +0000

    test imports

--- aeon/utils/__init__.py
+++ aeon/utils/__init__.py
@@ -1,5 +1,5 @@
 """Utility functionality."""
 
-__all__ = ["get_cutoff", "update_data"]
+__all__ = ["get_cutoff", "update_data", "get_window"]
 
-from aeon.utils.index_functions import get_cutoff, update_data
+from aeon.utils.index_functions import get_cutoff, get_window, update_data
