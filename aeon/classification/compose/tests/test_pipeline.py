33d70cccfb0193549e734b028c711713c83d1cc5
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-02-01 01:00:52 +0000

    pipeline rework

--- aeon/classification/compose/tests/test_pipeline.py
+++ aeon/classification/compose/tests/test_pipeline.py
@@ -3,110 +3,141 @@
 __author__ = ["fkiraly", "TonyBagnall"]
 
 import numpy as np
+import pytest
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 from aeon.classification import DummyClassifier
-from aeon.classification.compose import ClassifierPipeline, SklearnClassifierPipeline
+from aeon.classification.compose import ClassifierPipeline
 from aeon.classification.convolution_based import RocketClassifier
+from aeon.classification.dictionary_based import ContractableBOSS
+from aeon.testing.utils.data_gen import make_3d_test_data
 from aeon.testing.utils.estimator_checks import _assert_array_almost_equal
-from aeon.transformations.collection import PaddingTransformer
+from aeon.transformations.adapt import TabularToSeriesAdaptor
+from aeon.transformations.collection import PaddingTransformer, Tabularizer
+from aeon.transformations.collection.feature_based import SevenNumberSummaryTransformer
 from aeon.transformations.exponent import ExponentTransformer
 from aeon.transformations.impute import Imputer
 
 
-def test_classifier_pipeline():
+@pytest.mark.parametrize(
+    "transformers",
+    [
+        ExponentTransformer(power=2),
+        SevenNumberSummaryTransformer(),
+        TabularToSeriesAdaptor(StandardScaler()),
+        [ExponentTransformer(power=2), Tabularizer(), StandardScaler()],
+        [ExponentTransformer(power=2), SevenNumberSummaryTransformer()],
+        [Tabularizer(), StandardScaler(), SevenNumberSummaryTransformer()],
+        [
+            TabularToSeriesAdaptor(StandardScaler()),
+            ExponentTransformer(power=2),
+            SevenNumberSummaryTransformer(),
+        ],
+    ],
+)
+def test_classifier_pipeline(transformers):
     """Test the classifier pipeline."""
-    RAND_SEED = 42
-    X_train, y_train = make_3d_test_data(
-        n_cases=10, n_timepoints=12, random_state=RAND_SEED
-    )
-    X_test, _ = make_3d_test_data(n_cases=10, n_timepoints=12, random_state=RAND_SEED)
+    X_train, y_train = make_3d_test_data(n_cases=10, n_timepoints=12)
+    X_test, _ = make_3d_test_data(n_cases=10, n_timepoints=12)
 
-    t1 = ExponentTransformer(power=4)
-    t2 = ExponentTransformer(power=0.25)
     c = DummyClassifier()
-    pipeline = ClassifierPipeline(transformers=[t1, t2], classifier=c)
+    pipeline = ClassifierPipeline(transformers=transformers, classifier=c)
+    pipeline.fit(X_train, y_train)
+    c.fit(X_train, y_train)
 
-    y_pred = pipeline.fit(X_train, y_train).predict(X_test)
+    y_pred = pipeline.predict(X_test)
     assert isinstance(y_pred, np.ndarray)
 
+    if not isinstance(transformers, list):
+        transformers = [transformers]
 
-def test_sklearn_classifier_pipeline():
-    """Test auto-adapter for sklearn in mul."""
-    RAND_SEED = 42
-    X_train, y_train = make_3d_test_data(
-        n_cases=10, n_timepoints=12, random_state=RAND_SEED
-    )
-    X_test, _ = make_3d_test_data(n_cases=10, n_timepoints=12, random_state=RAND_SEED)
+    for t in transformers:
+        X_train = t.fit_transform(X_train)
+        X_test = t.transform(X_test)
 
-    t1 = ExponentTransformer(power=2)
-    t2 = StandardScaler()
-    c = RandomForestClassifier(n_estimators=2)
-    pipeline = SklearnClassifierPipeline(transformers=[t1, t2], classifier=c)
+    c.fit(X_train, y_train)
+    _assert_array_almost_equal(y_pred, c.predict(X_test))
 
-    y_pred = pipeline.fit(X_train, y_train).predict(X_test)
+
+@pytest.mark.parametrize(
+    "transformers",
+    [
+        [ExponentTransformer(power=2), Tabularizer()],
+        SevenNumberSummaryTransformer(),
+        [Tabularizer(), StandardScaler()],
+        [ExponentTransformer(power=2), Tabularizer(), StandardScaler()],
+        [ExponentTransformer(power=2), SevenNumberSummaryTransformer()],
+        [Tabularizer(), StandardScaler(), SevenNumberSummaryTransformer()],
+        [
+            TabularToSeriesAdaptor(StandardScaler()),
+            ExponentTransformer(power=2),
+            SevenNumberSummaryTransformer(),
+        ],
+    ],
+)
+def test_sklearn_classifier_pipeline(transformers):
+    """Test classifier pipeline with sklearn estimator."""
+    X_train, y_train = make_3d_test_data(n_cases=10, n_timepoints=12)
+    X_test, _ = make_3d_test_data(n_cases=10, n_timepoints=12)
+
+    c = RandomForestClassifier(n_estimators=2, random_state=0)
+    pipeline = ClassifierPipeline(transformers=transformers, classifier=c)
+    pipeline.fit(X_train, y_train)
+
+    y_pred = pipeline.predict(X_test)
     assert isinstance(y_pred, np.ndarray)
 
+    if not isinstance(transformers, list):
+        transformers = [transformers]
 
-def test_missing_unequal_tag_inference():
-    """Test that ClassifierPipeline infers missing/unequal tags correctly."""
-    t1 = ExponentTransformer(power=4)
+    for t in transformers:
+        X_train = t.fit_transform(X_train)
+        X_test = t.transform(X_test)
+
+    c.fit(X_train, y_train)
+    _assert_array_almost_equal(y_pred, c.predict(X_test))
+
+
+def test_tag_inference():
+    """Test that ClassifierPipeline infers tags correctly."""
+    t1 = SevenNumberSummaryTransformer()
     t2 = PaddingTransformer()
     t3 = Imputer()
-    c1 = RocketClassifier(num_kernels=100)
-    c2 = DummyClassifier()
-
-    p1 = ClassifierPipeline(transformers=[t1, t2], classifier=c1)
-    p2 = ClassifierPipeline(transformers=[t1, t1], classifier=c1)
-    p3 = ClassifierPipeline(transformers=[t3, t1], classifier=c1)
-    p4 = ClassifierPipeline(transformers=[t1, t3], classifier=c1)
+    c1 = DummyClassifier()
+    c2 = RocketClassifier(num_kernels=5)
+    c3 = ContractableBOSS(n_parameter_samples=5, max_ensemble_size=3)
 
     assert c1.get_tag("capability:unequal_length")
+    assert c1.get_tag("capability:missing_values")
+    assert c1.get_tag("capability:multivariate")
     assert not c2.get_tag("capability:unequal_length")
-    assert c3.get_tag("capability:missing_values")
-    assert not c4.get_tag("capability:missing_values")
+    assert not c2.get_tag("capability:missing_values")
+    assert not c3.get_tag("capability:multivariate")
 
-    # assert t1 t1 with dummy has unequal
-    # assert t1 t3 with dummy has missing
-
-
-def test_operator_overload():
-    """Test auto-adapter for sklearn in mul."""
-    RAND_SEED = 42
-    X_train, y_train = make_3d_test_data(
-        n_cases=10, n_timepoints=12, random_state=RAND_SEED
+    assert ClassifierPipeline(transformers=t1, classifier=c1).get_tag(
+        "capability:unequal_length"
     )
-    X_test, _ = make_3d_test_data(n_cases=10, n_timepoints=12, random_state=RAND_SEED)
+    assert ClassifierPipeline(transformers=[t1, t2], classifier=c2).get_tag(
+        "capability:unequal_length"
+    )
+    assert not ClassifierPipeline(transformers=t1, classifier=c2).get_tag(
+        "capability:unequal_length"
+    )
 
-    t1 = ExponentTransformer(power=2)
-    t2 = StandardScaler()
-    c = DummyClassifier()
+    assert ClassifierPipeline(transformers=[t1, t3], classifier=c1).get_tag(
+        "capability:missing_values"
+    )
+    assert ClassifierPipeline(transformers=[t1, t3], classifier=c2).get_tag(
+        "capability:missing_values"
+    )
+    assert not ClassifierPipeline(transformers=t1, classifier=c1).get_tag(
+        "capability:missing_values"
+    )
 
-    t1 = t1 * c
-    t2 = t1 * (t2 * c)
-    t3 = (t1 * t2) * c
-    t4 = t1 * t2 * c
-
-    assert isinstance(t1, ClassifierPipeline)
-    assert isinstance(t2, ClassifierPipeline)
-    assert isinstance(t3, ClassifierPipeline)
-    assert isinstance(t4, ClassifierPipeline)
-
-    # assert single transformer works
-    # assert regular is the same as these three
-
-    y_pred = t12c_1.fit(X, y).predict(X_test)
-
-    _assert_array_almost_equal(y_pred, t12c_2.fit(X_train, y_train).predict(X_test))
-    _assert_array_almost_equal(y_pred, t12c_3.fit(X_train, y_train).predict(X_test))
-
-
-# series-to-primitive transformer sklearn
-# collection transformer
-# collection transformer sklearn
-
-# t1 = ExponentTransformer(power=2)
-# t2 = SummaryTransformer()
-# c = KNeighborsClassifier()
-# params2 = {"transformers": [t1, t2], "classifier": c}
+    assert ClassifierPipeline(transformers=t1, classifier=c1).get_tag(
+        "capability:multivariate"
+    )
+    assert not ClassifierPipeline(transformers=t1, classifier=c3).get_tag(
+        "capability:multivariate"
+    )
