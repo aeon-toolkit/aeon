7cbdac6f7daf6f5a1176820d5c1087a97e93f8cf
xiaopu222 <Xh9n21@soton.ac.uk>
2024-02-01 23:58:22 +0800

    Add ae_resnet for tscl

--- aeon/clustering/deep_learning/__init__.py
+++ aeon/clustering/deep_learning/__init__.py
@@ -1,4 +1,5 @@
 """Deep learning based clusterers."""
-__all__ = ["BaseDeepClusterer", "AEFCNClusterer"]
+__all__ = ["BaseDeepClusterer", "AEFCNClusterer", "ResNetClusterer"]
 from aeon.clustering.deep_learning.ae_fcn import AEFCNClusterer
 from aeon.clustering.deep_learning.base import BaseDeepClusterer
+from aeon.clustering.deep_learning.ae_resnet import ResNetClusterer
