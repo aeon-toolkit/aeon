779afb452c4afaf829917c1b4fce00e46bd8a21b
xiaopu222 <Xh9n21@soton.ac.uk>
2024-02-02 20:21:00 +0800

    resolve init files

--- aeon/networks/__init__.py
+++ aeon/networks/__init__.py
@@ -1,5 +1,4 @@
 """Deep learning networks."""
-
 __all__ = [
     "BaseDeepNetwork",
     "CNNNetwork",
@@ -10,17 +9,15 @@ __all__ = [
     "ResNetNetwork",
     "TapNetNetwork",
     "AEFCNNetwork",
-    "LITENetwork",
     "AEResNetNetwork",
 ]
 from aeon.networks.ae_resnet import AEResNetNetwork
-from aeon.networks._ae_fcn import AEFCNNetwork
-from aeon.networks._cnn import CNNNetwork
-from aeon.networks._encoder import EncoderNetwork
-from aeon.networks._fcn import FCNNetwork
-from aeon.networks._inception import InceptionNetwork
-from aeon.networks._lite import LITENetwork
-from aeon.networks._mlp import MLPNetwork
-from aeon.networks._resnet import ResNetNetwork
-from aeon.networks._tapnet import TapNetNetwork
+from aeon.networks.ae_fcn import AEFCNNetwork
 from aeon.networks.base import BaseDeepNetwork
+from aeon.networks.cnn import CNNNetwork
+from aeon.networks.encoder import EncoderNetwork
+from aeon.networks.fcn import FCNNetwork
+from aeon.networks.inception import InceptionNetwork
+from aeon.networks.mlp import MLPNetwork
+from aeon.networks.resnet import ResNetNetwork
+from aeon.networks.tapnet import TapNetNetwork
