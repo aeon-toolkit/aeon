ba3f4cfc89b86928411ade05cd1f664138b5c867
xiaopu222 <Xh9n21@soton.ac.uk>
2024-02-02 20:14:37 +0800

    resolve init files

--- aeon/clustering/deep_learning/__init__.py
+++ aeon/clustering/deep_learning/__init__.py
@@ -1,5 +1,5 @@
 """Deep learning based clusterers."""
 __all__ = ["BaseDeepClusterer", "AEFCNClusterer", "ResNetClusterer"]
-from aeon.clustering.deep_learning.ae_fcn import AEFCNClusterer
+from aeon.clustering.deep_learning._ae_fcn import AEFCNClusterer
 from aeon.clustering.deep_learning.base import BaseDeepClusterer
 from aeon.clustering.deep_learning.ae_resnet import ResNetClusterer
