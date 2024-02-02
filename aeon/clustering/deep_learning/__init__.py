a78a062d8f4ac0abdf6d70a0dd95e0ee090af27f
xiaopu222 <Xh9n21@soton.ac.uk>
2024-02-02 23:08:41 +0800

    fix bug

--- aeon/clustering/deep_learning/__init__.py
+++ aeon/clustering/deep_learning/__init__.py
@@ -2,5 +2,5 @@
 
 __all__ = ["BaseDeepClusterer", "AEFCNClusterer", "AEResNetClusterer"]
 from aeon.clustering.deep_learning._ae_fcn import AEFCNClusterer
-from aeon.clustering.deep_learning.ae_resnet import AEResNetClusterer
+from aeon.clustering.deep_learning._ae_resnet import AEResNetClusterer
 from aeon.clustering.deep_learning.base import BaseDeepClusterer
