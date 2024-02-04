23ecf7efbb726f333ae3f222834f212ceab279f6
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-02-04 21:24:27 +0000

    dask bound and base object init

--- aeon/networks/base.py
+++ aeon/networks/base.py
@@ -5,10 +5,15 @@ __author__ = ["hadifawaz1999", "Withington", "TonyBagnall"]
 from abc import ABC, abstractmethod
 
 from aeon.base import BaseObject
+from aeon.utils.validation._dependencies import _check_estimator_deps
 
 
 class BaseDeepNetwork(BaseObject, ABC):
     """Abstract base class for deep learning networks."""
+
+    def __init__(self):
+        super().__init__()
+        _check_estimator_deps(self)
 
     _tags = {
         "python_dependencies": "tensorflow",
