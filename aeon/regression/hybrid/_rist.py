b375b7da81eb6018a8334ec303b8dc9625f0760e
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-02-15 13:51:12 +0000

    example

--- aeon/regression/hybrid/_rist.py
+++ aeon/regression/hybrid/_rist.py
@@ -85,8 +85,7 @@ class RISTRegressor(BaseRIST, BaseRegressor):
     >>> from aeon.regression.hybrid import RISTRegressor
     >>> from aeon.testing.utils.data_gen import make_example_3d_numpy
     >>> X, y = make_example_3d_numpy(n_cases=10, n_channels=1, n_timepoints=12,
-    ...                              return_y=True, regression_target=True,
-    ...                              random_state=0)
+    ...                              regression_target=True, random_state=0)
     >>> reg = RISTRegressor(random_state=0)  # doctest: +SKIP
     >>> reg.fit(X, y)  # doctest: +SKIP
     RISTRegressor(...)
