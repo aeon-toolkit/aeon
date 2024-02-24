fa60863e6c0e0ea822af30d9a298af035fe0bdeb
CodeLionX <sebastian.schmidl@t-online.de>
2024-02-24 14:38:34 +0100

    feat: document SBD distance

--- aeon/distances/_sbd.py
+++ aeon/distances/_sbd.py
@@ -6,14 +6,15 @@ from typing import Optional
 
 import numpy as np
 from numba import njit, objmode
-from scipy.signal import correlate, fftconvolve
+from scipy.signal import correlate
 
 from aeon.distances._utils import reshape_pairwise_to_multiple
 
 
 @njit(cache=True, fastmath=True)
 def sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool = True) -> float:
-    r"""Compute the shape-based distance (SBD) between two time series.
+    r"""
+    Compute the shape-based distance (SBD) between two time series.
 
     Shape-based distance (SBD) [1]_ is a normalized version of
     cross-correlation (CC) that is shifting and scaling (if standardization
@@ -25,27 +26,28 @@ def sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool = True) -> floa
     (:math:`x' = \frac{x - \mu}{\sigma}`), then computing the cross-correlation
     between x and y (:math:`CC(\mathbf{x}, \mathbf{y})`), then deviding it by the
     geometric mean of both autocorrelations of the individual sequences to normalize
-    it to :math:`[-1 1]` (coefficient normalization), and finally detecting the
+    it to :math:`[-1, 1]` (coefficient normalization), and finally detecting the
     position with the maximum normalized cross-correlation:
 
-    .. math:
-        SBD(\mathbf{x}, \mathbf{y}) = 1 - max_w( \frac{
+    .. math::
+        SBD(\mathbf{x}, \mathbf{y}) = 1 - max_w\left( \frac{
             CC_w(\mathbf{x}, \mathbf{y})
         }{
-            \sqrt{ \mathbf{x} \cdot \mathbf{x} * \mathbf{y} \cdot \mathbf{y} }
-        })
+            \sqrt{ (\mathbf{x} \cdot \mathbf{x}) * (\mathbf{y} \cdot \mathbf{y}) }
+        }\right)
 
     This distance measure has values between 0 and 2; 0 is perfect similarity.
 
     The computation of the cross-correlation :math:`CC(\mathbf{x}, \mathbf{y})` for
     all values of w requires :math:`O(m^2)` time, where m is the maximum time-series
-    length.
-    We can however use the convolution theorem to our advantage, and use the fast
-    (inverse) fourier transform (FFT) to perform the computation of CC(x, y) in
-    :math:`O(m log(m))`:
+    length. We can however use the convolution theorem to our advantage, and use the
+    fast (inverse) fourier transform (FFT) to perform the computation of
+    :math:`CC(\mathbf{x}, \mathbf{y})` in :math:`O(m \cdot log(m))`:
 
-    .. math:
-        CC(x, y) = \mathcal{F}^{-1}\{\mathcal{F}(\mathbf{x}) * \mathcal{F}(\mathbf{y})}.
+    .. math::
+        CC(x, y) = \mathcal{F}^{-1}\{
+            \mathcal{F}(\mathbf{x}) * \mathcal{F}(\mathbf{y})
+        \}
 
     Parameters
     ----------
@@ -55,9 +57,9 @@ def sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool = True) -> floa
     y : np.ndarray
         Second time series, either univariate, shape ``(n_timepoints,)``, or
         multivariate, shape ``(n_channels, n_timepoints)``.
-    standardize : bool
+    standardize : bool, default=True
         Apply z-score to both input time series for standardization before
-        computing the distance. This make SBD scaling invariant.
+        computing the distance. This make SBD scaling invariant. Default is True.
 
     Returns
     -------
@@ -69,11 +71,16 @@ def sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool = True) -> floa
     ValueError
         If x and y are not 1D or 2D arrays.
 
+    See Also
+    --------
+    :func:`~aeon.distances.sbd_pairwise_distance` : Compute the shape-based
+        distance (SBD) between all pairs of time series.
+
     References
     ----------
     .. [1] Paparrizos, John, and Luis Gravano: Fast and Accurate Time-Series
-    Clustering. ACM Transactions on Database Systems 42, no. 2 (2017):
-    8:1-8:49. https://doi.org/10.1145/3044711.
+           Clustering. ACM Transactions on Database Systems 42, no. 2 (2017):
+           8:1-8:49. https://doi.org/10.1145/3044711.
 
     Examples
     --------
@@ -84,65 +91,133 @@ def sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool = True) -> floa
     >>> dist = sbd_distance(x, y)
     """
     if x.ndim == 1 and y.ndim == 1:
-        return _univariate_sbd_distance(x, y)
+        return _univariate_sbd_distance(x, y, standardize)
     if x.ndim == 2 and y.ndim == 2:
         if x.shape[0] == y.shape[0] == 1:
             _x = x.ravel()
             _y = y.ravel()
-            return _univariate_sbd_distance(_x, _y)
+            return _univariate_sbd_distance(_x, _y, standardize)
         else:
             # independent
             nchannels = min(x.shape[0], y.shape[0])
             distance = 0.0
             for i in range(nchannels):
-                distance += _univariate_sbd_distance(x[i], y[i])
-            return distance
+                distance += _univariate_sbd_distance(x[i], y[i], standardize)
+            return distance / nchannels
 
     raise ValueError("x and y must be 1D or 2D")
 
 
 @njit(cache=True, fastmath=True)
-def sbd_pairwise_distance(x: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
+def sbd_pairwise_distance(
+    x: np.ndarray, y: Optional[np.ndarray] = None, standardize: bool = True
+) -> np.ndarray:
+    """
+    Compute the shape-based distance (SBD) between all pairs of time series.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        A collection of time series instances  of shape ``(n_instances, n_timepoints)``
+        or ``(n_instances, n_channels, n_timepoints)``.
+    y : np.ndarray or None, default=None
+        A single series or a collection of time series of shape ``(m_timepoints,)`` or
+        ``(m_instances, m_timepoints)`` or ``(m_instances, m_channels, m_timepoints)``.
+        If None, then the BSD is calculated between pairwise instances of x.
+    standardize : bool, default=True
+        Apply z-score to both input time series for standardization before
+        computing the distance. This make SBD scaling invariant. Default is True.
+
+    Returns
+    -------
+    np.ndarray (n_instances, n_instances)
+        BSD matrix between the instances of x (and y).
+
+    Raises
+    ------
+    ValueError
+        If x is not 2D or 3D array when only passing x.
+        If x and y are not 1D, 2D or 3D arrays when passing both x and y.
+
+    See Also
+    --------
+    :func:`~aeon.distances.sbd_distance` : Compute the shape-based distance between
+        two time series.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from aeon.distances import sbd_pairwise_distance
+    >>> # Distance between each time series in a collection of time series
+    >>> X = np.array([[[1, 2, 3]],[[4, 5, 6]], [[7, 8, 9]]])
+    >>> sbd_pairwise_distance(X)
+    array([[0. 0. 0.]
+           [0. 0. 0.]
+           [0. 0. 0.]])
+
+    >>> # Distance between two collections of time series
+    >>> X = np.array([[[1, 2, 3]],[[4, 5, 6]], [[7, 8, 9]]])
+    >>> y = np.array([[[11, 12, 13]],[[14, 15, 16]], [[17, 18, 19]]])
+    >>> sbd_pairwise_distance(X, y)
+    array([[0. 0. 0.]
+           [0. 0. 0.]
+           [0. 0. 0.]])
+
+    >>> X = np.array([[[1, 2, 3]],[[4, 5, 6]], [[7, 8, 9]]])
+    >>> y_univariate = np.array([[11, 12, 13],[14, 15, 16], [17, 18, 19]])
+    >>> sbd_pairwise_distance(X, y_univariate)
+    array([[0.],
+           [0.],
+           [0.]])
+    """
     if y is None:
         # To self
         if x.ndim == 3:
-            return _sbd_pairwise_distance_single(x)
+            return _sbd_pairwise_distance_single(x, standardize)
         elif x.ndim == 2:
             _X = x.reshape((x.shape[0], 1, x.shape[1]))
-            return _sbd_pairwise_distance_single(_X)
+            return _sbd_pairwise_distance_single(_X, standardize)
         raise ValueError("X must be 2D or 3D")
 
     _x, _y = reshape_pairwise_to_multiple(x, y)
-    return _sbd_pairwise_distance(_x, _y)
+    return _sbd_pairwise_distance(_x, _y, standardize)
 
 
 @njit(cache=True, fastmath=True)
-def _sbd_pairwise_distance_single(x: np.ndarray) -> np.ndarray:
+def _sbd_pairwise_distance_single(x: np.ndarray, standardize: bool) -> np.ndarray:
     n_instances = x.shape[0]
     distances = np.zeros((n_instances, n_instances))
 
     for i in range(n_instances):
         for j in range(i + 1, n_instances):
-            distances[i, j] = sbd_distance(x[i], x[j])
+            distances[i, j] = sbd_distance(x[i], x[j], standardize)
             distances[j, i] = distances[i, j]
 
     return distances
 
 
 @njit(cache=True, fastmath=True)
-def _sbd_pairwise_distance(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+def _sbd_pairwise_distance(
+    x: np.ndarray, y: np.ndarray, standardize: bool
+) -> np.ndarray:
     n_instances = x.shape[0]
     m_instances = y.shape[0]
     distances = np.zeros((n_instances, m_instances))
 
     for i in range(n_instances):
         for j in range(m_instances):
-            distances[i, j] = sbd_distance(x[i], y[j])
+            distances[i, j] = sbd_distance(x[i], y[j], standardize)
     return distances
 
 
 @njit(cache=True, fastmath=True)
-def _univariate_sbd_distance(x: np.ndarray, y: np.ndarray) -> float:
+def _univariate_sbd_distance(x: np.ndarray, y: np.ndarray, standardize: bool) -> float:
+    x = x.astype(np.float64)
+    y = y.astype(np.float64)
+    if standardize:
+        x = (x - np.mean(x)) / np.std(x)
+        y = (y - np.mean(y)) / np.std(y)
+
     # option 1:
     with objmode(a="float64[:]"):
         a = correlate(x, y, method="fft")
@@ -155,7 +230,23 @@ def _univariate_sbd_distance(x: np.ndarray, y: np.ndarray) -> float:
 
 @njit(cache=True, fastmath=True)
 def _reverse_and_conj(x: np.ndarray) -> np.ndarray:
-    """Reverse array `x` in all dimensions and perform the complex conjugate."""
+    """Reverse array `x` in all dimensions and perform the complex conjugate.
+
+    Implementation in scipy that is not compatible with Numba:
+
+        >>> reverse_mask = (slice(None, None, -1),) * x.ndim
+        >>> x = x[reverse_mask].conj()
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Input array.
+
+    Returns
+    -------
+    np.ndarray
+        Reversed (in all dimensions) and conjugated array.
+    """
     if x.ndim == 1:
         return x[::-1].conj()
     elif x.ndim == 2:
@@ -167,6 +258,3 @@ def _reverse_and_conj(x: np.ndarray) -> np.ndarray:
             "reverse_and_conj ist not implemented for arrays of "
             "dimensionality 4 or larger!"
         )
-    # Implementation in scipy (not compatible with Numba):
-    # reverse = (slice(None, None, -1),) * x.ndim
-    # return x[reverse].conj()
