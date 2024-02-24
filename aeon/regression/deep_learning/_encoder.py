64b49122e25f15ad181e2d5df173befbdd6de2b1
_youteakay <67187480+AnonymousCodes911@users.noreply.github.com>
2024-02-25 01:38:58 +0530

    Update _encoder.py

--- aeon/regression/deep_learning/_encoder.py
+++ aeon/regression/deep_learning/_encoder.py
@@ -204,7 +204,7 @@ class EncoderRegressor(BaseDeepRegressor):
 
         Parameters
         ----------
-        X : np.ndarray of shape = (n_instances (n), n_channels (d), series_length (m))
+        X : np.ndarray of shape = (n_instances, n_channels, n_timepoints)
             The training input samples.
         y : np.ndarray of shape n
             The training data Target Values.
