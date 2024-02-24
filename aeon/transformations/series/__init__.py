8fe8f97c7c277351ff8026f4dfdf00de761e7738
aadya940 <aadya@unify.ai>
2024-02-24 18:06:24 +0530

    Add imports

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -1,6 +1,7 @@
 """Series transformations."""
 
-__all__ = ["BaseSeriesTransformer", "MatrixProfileSeriesTransformer"]
+__all__ = ["BaseSeriesTransformer", "MatrixProfileSeriesTransformer", "SpectogramTransformer"]
 
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
+from aeon.transformations.series._spectogram import SpectogramTransformer
