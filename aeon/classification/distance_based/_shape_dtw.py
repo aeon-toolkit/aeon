f1faf33eac37bc87077b0914a71f06bb3530499c
_youteakay <67187480+AnonymousCodes911@users.noreply.github.com>
2024-02-26 17:41:54 +0530

    updated shapedtw (#1245)
    fixes: #1152
    Reformatted the docstrings as per AEON's conventions.

--- aeon/classification/distance_based/_shape_dtw.py
+++ aeon/classification/distance_based/_shape_dtw.py
@@ -115,9 +115,10 @@ class ShapeDTW(BaseClassifier):
 
         Parameters
         ----------
-        X : 3D np.ndarray of shape = [n_instances, n_channels, series_length]
-            The training data.
-        y : array-like, shape = [n_instances]
+        X : np.ndarray of shape = (n_cases, n_channels, n_timepoints)
+            or list of [n_cases] np.ndarray shape (n_channels, n_timepoints_i)
+            The training input samples.
+        y : array-like, shape = (n_cases)
             The class labels.
 
         Returns
