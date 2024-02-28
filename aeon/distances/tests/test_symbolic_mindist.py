bd09d8375ba959f8707883d0e19e6daa5e526048
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2024-02-28 09:24:28 +0100

    updates

--- aeon/distances/tests/test_symbolic_mindist.py
+++ aeon/distances/tests/test_symbolic_mindist.py
@@ -14,28 +14,19 @@ from aeon.transformations.collection.dictionary_based import SAX, SFAFast
 
 
 def test_sax_mindist():
-    n_segments = 8
+    n_segments = 16
     alphabet_size = 8
 
-    X_train = pd.read_csv(
-        "/Users/bzcschae/workspace/similarity/datasets/classification/CBF/CBF_TRAIN",
-        delim_whitespace=True,
-        header=None,
-    ).values[:, 1:]
+    X_train, _ = load_unit_test("TRAIN")
+    X_test, _ = load_unit_test("TEST")
 
-    X_test = pd.read_csv(
-        "/Users/bzcschae/workspace/similarity/datasets/classification/CBF/CBF_TEST",
-        delim_whitespace=True,
-        header=None,
-    ).values[:, 1:]
+    X_train = zscore(X_train.squeeze(), axis=1)
+    X_test = zscore(X_test.squeeze(), axis=1)
 
     SAX_transform = SAX(n_segments=n_segments, alphabet_size=alphabet_size)
     SAX_train = SAX_transform.fit_transform(X_train).squeeze()
     SAX_test = SAX_transform.transform(X_test).squeeze()
     PAA_train = SAX_transform._get_paa(X_train).squeeze()
-
-    X_train = zscore(X_train, axis=1)
-    X_test = zscore(X_test, axis=1)
 
     tightness_sax = 0.0
     tightness_paa_sax = 0.0
@@ -77,21 +68,13 @@ def test_sax_mindist():
 def test_sfa_mindist():
     n_segments = 16
     alphabet_size = 8
-    X_train = pd.read_csv(
-        "/Users/bzcschae/workspace/similarity/datasets/classification/CBF/CBF_TRAIN",
-        delim_whitespace=True,
-        header=None,
-    ).values[:, 1:]
 
-    X_test = pd.read_csv(
-        "/Users/bzcschae/workspace/similarity/datasets/classification/CBF/CBF_TEST",
-        delim_whitespace=True,
-        header=None,
-    ).values[:, 1:]
+    X_train, _ = load_unit_test("TRAIN")
+    X_test, _ = load_unit_test("TEST")
 
     n = X_train.shape[-1]
-    X_train = zscore(X_train, axis=1)
-    X_test = zscore(X_test, axis=1)
+    X_train = zscore(X_train.squeeze(), axis=1)
+    X_test = zscore(X_test.squeeze(), axis=1)
 
     SFA = SFAFast(
         word_length=n_segments,
@@ -145,43 +128,3 @@ def test_sfa_mindist():
     print("All ok:")
     print("DFT-SFA-Min-Dist Tightness:\t", tightness_dft_sfa / X_train.shape[0])
     print("SFA-Min-Dist Tightness:    \t", tightness_sfa / X_train.shape[0])
-
-
-# def transform_to_word(SFA_transform, X, alphabet_size, n_segments):
-#     SFA_transform.transform(X)
-#     SFA_train_ints = np.squeeze(SFA_transform.words)
-#     return get_chars(SFA_train_ints, n_segments, alphabet_size)
-#
-# def test_unwrap_word_sfa():
-#     # n_segments = 8
-#     alphabet_size = 4
-#
-#     letter_bits = int(np.log2(alphabet_size))
-#
-#     word = np.array([1, 3, 2, 0])
-#     int_word = word_to_int(word, letter_bits)
-#     word_new = get_chars(int_word, word.shape[0], alphabet_size)
-#
-#     print(word_new)
-#
-#
-# def word_to_int(chars, letter_bits):
-#     word = np.int32(0)
-#     for i in range(chars.shape[0]):
-#         word = (word << letter_bits) | chars[i]
-#     return word
-#
-#
-# def get_chars(word, word_length, alphabet_size):
-#     chars = np.zeros(word_length, dtype=np.uint32)
-#     letter_bits = int(np.log2(alphabet_size))
-#     mask = (1 << letter_bits) - 1
-#     for i in range(word_length):
-#         # Extract the last bits
-#         char = word & mask
-#         chars[-i - 1] = char
-#
-#         # Right shift by to move to the next group of bits
-#         word >>= letter_bits
-#
-#     return chars
