6a86462d91cf41764f2aada8ed7766fec2383ae9
Tony Bagnall <ajb@uea.ac.uk>
2024-03-09 17:54:45 +0000

    meta data

--- aeon/utils/validation/_input.py
+++ aeon/utils/validation/_input.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 
-from aeon.utils.validation.collection import get_type, is_collection
+from aeon.utils.validation.collection import get_type, has_missing, is_collection
 from aeon.utils.validation.series import is_hierarchical, is_single_series
 
 
@@ -21,19 +21,27 @@ def validate_input(X):
         metadata["scitype"] = "Hierarchical"
         metadata["mtype"] = "pd_multiindex_hier"
         metadata["is_univariate"] = False
+        metadata["has_nans"] = X.isna().any().any()
     elif is_single_series(X):
         metadata["scitype"] = "Series"
         if isinstance(X, np.ndarray):
             metadata["mtype"] = "np.ndarray"
+            metadata["is_univariate"] = True
+            metadata["has_nans"] = np.isnan(X).any()
         elif isinstance(X, pd.Series):
             metadata["mtype"] = "pd.Series"
             metadata["is_univariate"] = True
+            metadata["has_nans"] = X.isna().any()
         else:
             metadata["mtype"] = "pd.DataFrame"
-            metadata["is_univariate"] = X.ncols == 1
+            metadata["is_univariate"] = X.shape[1] == 1
+            metadata["has_nans"] = X.isna().any().any()
     elif is_collection(X):
         metadata["scitype"] = "Panel"
         metadata["mtype"] = get_type(X)
+        #        metadata["is_univariate"] =
+        metadata["has_nans"] = has_missing(X)
+
     else:
         return False, None
     return True, metadata
