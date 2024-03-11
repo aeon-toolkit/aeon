Fixing broadcaster tags, add inverse transform

aeon/transformations/collection/_broadcaster.py
@@ -19,6 +19,10 @@ def _joblib_container_transform(transformer, X, y):
     return transformer.transform(X, y=y)
 
 
+def _joblib_container_inverse_transform(transformer, X, y):
+    return transformer.inverse_transform(X, y=y)
+
+
 class BroadcastTransformer(BaseCollectionTransformer):
     """Broadcasts a single series transformer over a collection.
 
@@ -53,6 +57,19 @@ class BroadcastTransformer(BaseCollectionTransformer):
     >>> transformer.fit_transform(X)
     """
 
+    _tags_to_inherit = [
+        "capability:unequal_length",
+        "capability:missing_values",
+        "univariate-only",
+        "capability:multivariate",
+        "capability:inverse_transform",
+        "requires_y",
+        "transform_labels",
+        "fit_is_empty",
+        "X-y-must-have-same-index",
+        "skip-inverse-transform",
+    ]
+
     def __init__(
         self,
         transformer: BaseSeriesTransformer,
@@ -62,9 +79,12 @@ class BroadcastTransformer(BaseCollectionTransformer):
         self.transformer = transformer
         self.n_jobs = n_jobs
         self.joblib_backend = joblib_backend
-        # Copy tags from transformer
-        self.set_tags(**transformer.get_tags())
         super().__init__()
+        # Setting tags before __init__() cause them to be overwriten.
+        _tags = {key: transformer.get_tags()[key] for key in self._tags_to_inherit}
+        if _tags["fit_is_empty"]:
+            transformer._is_fitted = True
+        self.set_tags(**_tags)
 
     def _check_n_jobs_broadcast(self, n_instances):
         """
@@ -177,6 +197,45 @@ class BroadcastTransformer(BaseCollectionTransformer):
         else:
             Xt = Parallel(n_jobs=n_jobs_joblib, backend=self.joblib_backend)(
                 delayed(_joblib_container_transform)(
+                    self.series_transformers[i], X[i], y[i]
+                )
+                for i in range(len(X))
+            )
+        return np.asarray(Xt)
+
+    def _inverse_transform(self, X, y=None):
+        """
+        Call the inverse_transform function of each transformer for each sample.
+
+        Parameters
+        ----------
+        X : 3D np.ndarray of shape = (n_instances, n_channels, n_timepoints)
+            The collection of time series to transform.
+        y : 1D np.ndarray of shape = (n_instances), optional
+            Class of the samples. The default is None, which means this parameter
+            is ignored.
+
+        Returns
+        -------
+        Xt : np.ndarray
+            The transformed collection of time series.
+
+        """
+        n_samples = len(X)
+        if y is None:
+            y = [None] * n_samples
+
+        n_jobs_joblib, _ = self._check_n_jobs_broadcast(n_samples)
+        if self.get_tag("fit_is_empty"):
+            Xt = Parallel(n_jobs=n_jobs_joblib, backend=self.joblib_backend)(
+                delayed(_joblib_container_inverse_transform)(
+                    self.transformer, X[i], y[i]
+                )
+                for i in range(len(X))
+            )
+        else:
+            Xt = Parallel(n_jobs=n_jobs_joblib, backend=self.joblib_backend)(
+                delayed(_joblib_container_inverse_transform)(
                     self.series_transformers[i], X[i], y[i]
                 )
                 for i in range(len(X))

aeon/transformations/collection/tests/test_broadcaster.py
@@ -1,6 +1,7 @@
 """Tests for broadcaster transformer"""
 
 import numpy as np
+import pytest
 
 from aeon.transformations.collection import BroadcastTransformer
 from aeon.transformations.series import (
@@ -8,31 +9,38 @@ from aeon.transformations.series import (
     DummySeriesTransformer_no_fit,
 )
 
+INPUT_SHAPES = [(2, 1, 10), (2, 2, 10)]
 
-def test_BroadcastTransformer_fit():
+
+@pytest.mark.parametrize("input_shape", INPUT_SHAPES)
+def test_BroadcastTransformer_fit(input_shape):
     constant = 1
     broadcaster = BroadcastTransformer(DummySeriesTransformer(constant=constant))
-    X = np.zeros((2, 1, 10))
+    X = np.zeros(input_shape)
     broadcaster.fit(X)
     Xt = broadcaster.transform(X)
-    assert hasattr(
-        broadcaster, "series_transformers"
-    ) and broadcaster.series_transformers == len(X)
-
+    Xit = broadcaster.inverse_transform(Xt)
+    assert hasattr(broadcaster, "series_transformers") and len(
+        broadcaster.series_transformers
+    ) == len(X)
     for i in range(len(X)):
         assert np.all(
             Xt[i] == constant + broadcaster.series_transformers[i].random_value_
         )
-    assert Xt.shape == (2, 1, 10)
+    np.testing.assert_array_equal(Xit, X)
+    assert Xt.shape == Xit.shape == X.shape
 
 
-def test_BroadcastTransformer_no_fit():
+@pytest.mark.parametrize("input_shape", INPUT_SHAPES)
+def test_BroadcastTransformer_no_fit(input_shape):
     constant = 1
     broadcaster = BroadcastTransformer(DummySeriesTransformer_no_fit(constant=constant))
-    X = np.zeros((2, 1, 10))
+    X = np.zeros(input_shape)
     broadcaster.fit(X)
     Xt = broadcaster.transform(X)
+    Xit = broadcaster.inverse_transform(Xt)
     assert not hasattr(broadcaster, "series_transformers")
     for i in range(len(X)):
         assert np.all(Xt[i] == constant)
-    assert Xt.shape == (2, 1, 10)
+    np.testing.assert_array_equal(Xit, X)
+    assert Xt.shape == Xit.shape == X.shape

aeon/transformations/series/_dummy.py
@@ -22,6 +22,7 @@ class DummySeriesTransformer(BaseSeriesTransformer):
 
     _tags = {
         "capability:multivariate": True,
+        "capability:inverse_transform": True,
     }
 
     def __init__(self, constant=0, axis=1, random_state=None):
@@ -60,7 +61,23 @@ class DummySeriesTransformer(BaseSeriesTransformer):
         np.ndarray
             2D transformed version of X
         """
-        return X + self.constant + self.random_value_
+        return X + (self.constant + self.random_value_)
+
+    def _inverse_transform(self, X, y=None):
+        """Inverse transform X by substracting the constant and random value.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            2D time series to be transformed
+        y : ignored argument for interface compatibility
+
+        Returns
+        -------
+        np.ndarray
+            2D inverse transformed version of X
+        """
+        return X - (self.constant + self.random_value_)
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
@@ -91,6 +108,7 @@ class DummySeriesTransformer_no_fit(BaseSeriesTransformer):
 
     _tags = {
         "capability:multivariate": True,
+        "capability:inverse_transform": True,
         "fit_is_empty": True,
     }
 
@@ -113,6 +131,22 @@ class DummySeriesTransformer_no_fit(BaseSeriesTransformer):
             2D transformed version of X
         """
         return X + self.constant
+
+    def _inverse_transform(self, X, y=None):
+        """Inverse transform X by substracting the constant.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            2D time series to be transformed
+        y : ignored argument for interface compatibility
+
+        Returns
+        -------
+        np.ndarray
+            2D inverse transformed version of X
+        """
+        return X - self.constant
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):

aeon/transformations/series/tests/test_dummy.py
@@ -1,23 +1,35 @@
 """Tests for dummy series transformer."""
 
 import numpy as np
+import pytest
 
-from aeon.transformations.series import DummySeriesTransformer
+from aeon.transformations.series import (
+    DummySeriesTransformer,
+    DummySeriesTransformer_no_fit,
+)
+
+INPUT_SHAPES = [(2, 5), (1, 5), (5)]
 
 
-def test_DummySeriesTransformer():
+@pytest.mark.parametrize("input_shape", INPUT_SHAPES)
+def test_DummySeriesTransformer(input_shape):
     constant = 1
-    X = np.zeros((2, 5))
+    X = np.zeros(input_shape)
     transformer = DummySeriesTransformer(constant=constant).fit(X)
     Xt = transformer.transform(X)
-    assert np.all(Xt == constant + transformer.random_value_) and Xt.shape == (2, 5)
+    assert (
+        np.all(Xt == constant + transformer.random_value_) and Xt.shape == input_shape
+    )
+    Xit = transformer.inverse_transform(Xt)
+    assert np.all(Xit == X) and Xit.shape == input_shape
 
-    X = np.zeros((1, 5))
-    transformer = DummySeriesTransformer(constant=constant).fit(X)
-    Xt = transformer.transform(X)
-    assert np.all(Xt == constant + transformer.random_value_) and Xt.shape == (1, 5)
 
-    X = np.zeros(5)
-    transformer = DummySeriesTransformer(constant=constant).fit(X)
+@pytest.mark.parametrize("input_shape", INPUT_SHAPES)
+def test_DummySeriesTransformer_no_fit(input_shape):
+    constant = 1
+    X = np.zeros(input_shape)
+    transformer = DummySeriesTransformer_no_fit(constant=constant).fit(X)
     Xt = transformer.transform(X)
-    assert np.all(Xt == constant + transformer.random_value_) and Xt.shape == (1, 5)
+    assert np.all(Xt == constant) and Xt.shape == input_shape
+    Xit = transformer.inverse_transform(Xt)
+    assert np.all(Xit == X) and Xit.shape == input_shape