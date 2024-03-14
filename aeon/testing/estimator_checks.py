56132d469457b2536919b4c67db57e13551f91bd
aadya940 <aadya@unify.ai>
2024-03-14 23:02:53 +0530

    Move estimator_checks to testing module as for maintainance

--- aeon/testing/estimator_checks.py
+++ aeon/testing/estimator_checks.py
@@ -61,7 +61,7 @@ def check_estimator(
     Examples
     --------
     >>> from aeon.transformations.exponent import ExponentTransformer
-    >>> from aeon.utils.estimator_checks import check_estimator
+    >>> from aeon.testing.estimator_checks import check_estimator
 
     Running all tests for ExponentTransformer class,
     this uses all instances from get_test_params and compatible scenarios
