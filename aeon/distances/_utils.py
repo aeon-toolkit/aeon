3acb57608a7fcada49c99f801586e913de7ea857
Sebastian Schmidl <CodeLionX@users.noreply.github.com>
2024-03-26 21:41:16 +0100

    [ENH] SBD and MSM: Support pairwise distance calculation for unequal length time series (#1287)
    * try numba-optimized implementation for unequal-length datasets
    
    * feat: move reshape helper to util class and allow x and y to have different types
    
    * fix: add tests for single TS reshape util function and fix edge cases
    
    * feat: support unequal-length TS in MSM distance
    
    * feat: add tests for unequal-length time series and fix msm distance
    
    * feat: improve input checks for sbd distance
    
    * chore: add remaining tests
    
    * refactor distance tests
    
    * uncomment
    
    * remove function
    
    * fixed pairwise minkowski
    
    * feat: switch to single implementation on list[np.ndarray]
    
    * feat: improve Numba implementation after benchmarking
    
    * feat: adapt tests to new variable-length code
    
    * fix: examples for msm and sbd
    
    * fix: documentation
    
    * chore: revert renaming from n_instances to n_cases
    
    * chore: revert renaming from m_instances to m_cases
    
    * chore: remove comments with old code
    
    ---------
    
    Co-authored-by: chrisholder <chrisholder987@hotmail.com>

--- aeon/distances/_utils.py
+++ aeon/distances/_utils.py
@@ -1,14 +1,15 @@
-from typing import Tuple
+from typing import List, Tuple, Union
 
 import numpy as np
 from numba import njit
+from numba.typed import List as NumbaList
 
 
 @njit(cache=True, fastmath=True)
 def reshape_pairwise_to_multiple(
     x: np.ndarray, y: np.ndarray
 ) -> Tuple[np.ndarray, np.ndarray]:
-    """Reshape two collection of time series for pairwise distance computation.
+    """Reshape two collections of time series for pairwise distance computation.
 
     Parameters
     ----------
@@ -46,10 +47,10 @@ def reshape_pairwise_to_multiple(
         raise ValueError("x and y must be 1D, 2D, or 3D arrays")
     else:
         if x.ndim == 3 and y.ndim == 2:
-            _y = y.reshape((1, y.shape[0], y.shape[1]))
+            _y = y.reshape((y.shape[0], 1, y.shape[1]))
             return x, _y
         if y.ndim == 3 and x.ndim == 2:
-            _x = x.reshape((1, x.shape[0], x.shape[1]))
+            _x = x.reshape((x.shape[0], 1, x.shape[1]))
             return _x, y
         if x.ndim == 3 and y.ndim == 1:
             _x = x
@@ -67,4 +68,58 @@ def reshape_pairwise_to_multiple(
             _x = x.reshape((1, 1, x.shape[0]))
             _y = y.reshape((y.shape[0], 1, y.shape[1]))
             return _x, _y
-        raise ValueError("x and y must be 2D or 3D arrays")
+        raise ValueError("x and y must be 1D, 2D, or 3D arrays")
+
+
+def _convert_to_list(
+    x: Union[np.ndarray, List[np.ndarray]], name: str = "X"
+) -> NumbaList[np.ndarray]:
+    """Convert input collections to a list of arrays for pairwise distance calculation.
+
+    Takes a single or multiple time series and converts them to a list of 2D arrays. If
+    the input is a single time series, it is reshaped to a 2D array as the sole element
+    of a list. If the input is a 2D array of shape (n_cases, n_timepoints), it is
+    reshaped to a list of n_cases 1D arrays with n_timepoints points. A 3D array is
+    converted to a list with n_cases 2D arrays of shape (n_channels, n_timepoints).
+    Lists of 1D arrays are converted to lists of 2D arrays.
+
+    Parameters
+    ----------
+    x : Union[np.ndarray, List[np.ndarray]]
+        One or more time series of shape (n_cases, n_channels, n_timepoints) or
+        (n_cases, n_timepoints) or (n_timepoints,).
+    name : str, optional
+        Name of the variable to be converted for error handling, by default "X".
+
+    Returns
+    -------
+    NumbaList[np.ndarray]
+        Numba typedList of 2D arrays with shape (n_channels, n_timepoints) of length
+        n_cases.
+
+    Raises
+    ------
+    ValueError
+        If x is not a 1D, 2D or 3D array or a list of 1D or 2D arrays.
+    """
+    if isinstance(x, np.ndarray):
+        if x.ndim == 3:
+            return NumbaList(x)
+        elif x.ndim == 2:
+            return NumbaList(x.reshape(x.shape[0], 1, x.shape[1]))
+        elif x.ndim == 1:
+            return NumbaList(x.reshape(1, 1, x.shape[0]))
+        else:
+            raise ValueError(f"{name} must be 1D, 2D or 3D")
+    elif isinstance(x, (List, NumbaList)):
+        x_new = NumbaList()
+        for i in range(len(x)):
+            if x[i].ndim == 2:
+                x_new.append(x[i])
+            elif x[i].ndim == 1:
+                x_new.append(x[i].reshape((1, x[i].shape[0])))
+            else:
+                raise ValueError(f"{name} must include only 1D or 2D arrays")
+        return x_new
+    else:
+        raise ValueError(f"{name} must be either np.ndarray or List[np.ndarray]")
