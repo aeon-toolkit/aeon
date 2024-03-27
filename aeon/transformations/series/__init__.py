f7fed85ff11553f1a2efc373ebf4ceedba957c3a
aadya940 <aadya@unify.ai>
2024-03-27 14:21:06 +0530

    Remove SpectrogramTransformer

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -3,9 +3,7 @@
 __all__ = [
     "BaseSeriesTransformer",
     "MatrixProfileSeriesTransformer",
-    "SpectrogramTransformer",
 ]
 
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
-from aeon.transformations.series._spectrogram import SpectrogramTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
