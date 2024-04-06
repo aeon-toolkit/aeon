576dcfeded5781074f5a4b9a56f1776540074ea4
Tony Bagnall <ajb@uea.ac.uk>
2024-04-06 11:37:41 +0100

    test base

--- aeon/transformations/series/tests/test_base.py
+++ aeon/transformations/series/tests/test_base.py
@@ -1,1 +1,10 @@
-""""Test the base series transformer."""
+"""Test the base series transformer."""
+
+
+def test_axis():
+    """Test axis conversion.
+
+     Test it works as expected for transformers with axis = 0 ,
+    axis =1 and both with and without multivariate capability
+    """
+    pass
