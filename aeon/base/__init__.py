fb1752b9bebda11e6daf22658e25af23667b98df
Tony Bagnall <ajb@uea.ac.uk>
2024-04-07 13:34:15 +0100

    remove _serialize

--- aeon/base/__init__.py
+++ aeon/base/__init__.py
@@ -6,11 +6,9 @@ __all__ = [
     "BaseCollectionEstimator",
     "BaseSeriesEstimator",
     "_HeterogenousMetaEstimator",
-    "load",
 ]
 
 from aeon.base._base import BaseEstimator, BaseObject
 from aeon.base._base_collection import BaseCollectionEstimator
 from aeon.base._base_series import BaseSeriesEstimator
 from aeon.base._meta import _HeterogenousMetaEstimator
-from aeon.base._serialize import load
