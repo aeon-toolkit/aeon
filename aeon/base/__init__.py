33dcd11b20bdced74df14e6849e15604f4820c8e
Tony Bagnall <ajb@uea.ac.uk>
2024-04-09 13:29:21 +0100

    revert load function in serialize

--- aeon/base/__init__.py
+++ aeon/base/__init__.py
@@ -6,9 +6,11 @@ __all__ = [
     "BaseCollectionEstimator",
     "BaseSeriesEstimator",
     "_HeterogenousMetaEstimator",
+    "load",
 ]
 
 from aeon.base._base import BaseEstimator, BaseObject
 from aeon.base._base_collection import BaseCollectionEstimator
 from aeon.base._base_series import BaseSeriesEstimator
 from aeon.base._meta import _HeterogenousMetaEstimator
+from aeon.base._serialize import load
