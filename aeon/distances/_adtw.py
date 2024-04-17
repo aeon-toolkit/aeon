bcaa43b3b5dce493c458f86e7a9fd5bad4be03ee
Chris Holder <chrisholder987@hotmail.com>
2024-04-17 12:22:05 +0200

    fixed final typo

--- aeon/distances/_adtw.py
+++ aeon/distances/_adtw.py
@@ -213,7 +213,7 @@ def adtw_pairwise_distance(
     y : np.ndarray or List of np.ndarray or None, default=None
         A single series or a collection of time series of shape ``(m_timepoints,)`` or
         ``(m_cases, m_timepoints)`` or ``(m_cases, m_channels, m_timepoints)``.
-        If None, then the msm pairwise distance between the instances of X is
+        If None, then the adtw pairwise distance between the instances of X is
         calculated.
     window : float or None, default=None
         The window to use for the bounding matrix. If None, no bounding matrix
