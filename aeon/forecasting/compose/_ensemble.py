05921ca1594b382737d21813a3c9f575b05006cd
_youteakay <backuputk@gmail.com>
2024-04-22 16:30:28 +0530

    Revert "wraps up forecasting"
    This reverts commit cd3bd6477c723ce48d4cbb4890080017bcbc748e.

--- aeon/forecasting/compose/_ensemble.py
+++ aeon/forecasting/compose/_ensemble.py
@@ -67,10 +67,6 @@ class AutoEnsembleForecaster(_HeterogenousEnsembleForecaster):
         trained ensemble models. If None, it will be set to 0.25.
     random_state : int, RandomState instance or None, default=None
         Used to set random_state of the default regressor.
-        If `int`, random_state is the seed used by the random number generator;
-        If `RandomState` instance, random_state is the random number generator;
-        If `None`, the random number generator is the `RandomState` instance used
-        by `np.random`.
     n_jobs : int or None, optional, default=None
         The number of jobs to run in parallel for fit. None means 1 unless
         in a joblib.parallel_backend context.
--- aeon/forecasting/structural.py
+++ aeon/forecasting/structural.py
@@ -163,12 +163,11 @@ class UnobservedComponents(_StatsModelsAdapter):
         not be available (including smoothed results and in-sample
         prediction), although out-of-sample forecasting is possible.
         Default is False.
-    random_state : int, RandomState instance or None, default=None
-        Used to set random_state of the default regressor.
-        If `int`, random_state is the seed used by the random number generator;
-        If `RandomState` instance, random_state is the random number generator;
-        If `None`, the random number generator is the `RandomState` instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None, optional ,
+        default=None – If int, random_state is the seed used by the random
+        number generator; If RandomState instance, random_state is the random
+        number generator; If None, the random number generator is the
+        RandomState instance used by np.random.
 
     See Also
     --------
