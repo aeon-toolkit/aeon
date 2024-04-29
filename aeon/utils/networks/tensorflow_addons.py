c29cc5d75ee70ecdf131ba2966c5b08550423d1e
hadifawaz1999 <hadifawaz2291999@gmail.com>
2024-04-29 18:03:28 +0200

    dep on utils

--- aeon/utils/networks/tensorflow_addons.py
+++ aeon/utils/networks/tensorflow_addons.py
@@ -14,49 +14,53 @@
 # ==============================================================================
 """Types for typing functions signatures."""
 
-import importlib
-from typing import Callable, List, Union
+from aeon.utils.validation._dependencies import _check_soft_dependencies
 
-import numpy as np
-import tensorflow as tf
-from tensorflow.python.keras.engine import keras_tensor
+if _check_soft_dependencies("tensorflow", severity="none"):
 
-Number = Union[
-    float,
-    int,
-    np.float16,
-    np.float32,
-    np.float64,
-    np.int8,
-    np.int16,
-    np.int32,
-    np.int64,
-    np.uint8,
-    np.uint16,
-    np.uint32,
-    np.uint64,
-]
+    import importlib
+    from typing import Callable, List, Union
 
-Initializer = Union[None, dict, str, Callable, tf.keras.initializers.Initializer]
-Regularizer = Union[None, dict, str, Callable, tf.keras.regularizers.Regularizer]
-Constraint = Union[None, dict, str, Callable, tf.keras.constraints.Constraint]
-Activation = Union[None, str, Callable]
-if importlib.util.find_spec("tensorflow.keras.optimizers.legacy") is not None:
-    Optimizer = Union[
-        tf.keras.optimizers.Optimizer, tf.keras.optimizers.legacy.Optimizer, str
+    import numpy as np
+    import tensorflow as tf
+    from tensorflow.python.keras.engine import keras_tensor
+
+    Number = Union[
+        float,
+        int,
+        np.float16,
+        np.float32,
+        np.float64,
+        np.int8,
+        np.int16,
+        np.int32,
+        np.int64,
+        np.uint8,
+        np.uint16,
+        np.uint32,
+        np.uint64,
     ]
-else:
-    Optimizer = Union[tf.keras.optimizers.Optimizer, str]
 
-TensorLike = Union[
-    List[Union[Number, list]],
-    tuple,
-    Number,
-    np.ndarray,
-    tf.Tensor,
-    tf.SparseTensor,
-    tf.Variable,
-    keras_tensor.KerasTensor,
-]
-FloatTensorLike = Union[tf.Tensor, float, np.float16, np.float32, np.float64]
-AcceptableDTypes = Union[tf.DType, np.dtype, type, int, str, None]
+    Initializer = Union[None, dict, str, Callable, tf.keras.initializers.Initializer]
+    Regularizer = Union[None, dict, str, Callable, tf.keras.regularizers.Regularizer]
+    Constraint = Union[None, dict, str, Callable, tf.keras.constraints.Constraint]
+    Activation = Union[None, str, Callable]
+    if importlib.util.find_spec("tensorflow.keras.optimizers.legacy") is not None:
+        Optimizer = Union[
+            tf.keras.optimizers.Optimizer, tf.keras.optimizers.legacy.Optimizer, str
+        ]
+    else:
+        Optimizer = Union[tf.keras.optimizers.Optimizer, str]
+
+    TensorLike = Union[
+        List[Union[Number, list]],
+        tuple,
+        Number,
+        np.ndarray,
+        tf.Tensor,
+        tf.SparseTensor,
+        tf.Variable,
+        keras_tensor.KerasTensor,
+    ]
+    FloatTensorLike = Union[tf.Tensor, float, np.float16, np.float32, np.float64]
+    AcceptableDTypes = Union[tf.DType, np.dtype, type, int, str, None]
