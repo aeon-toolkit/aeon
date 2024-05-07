40f8304de980817211baa6753cd9c15687dfd1a0
Tony Bagnall <ajb@uea.ac.uk>
2024-05-07 19:20:18 +0100

    exponent test

--- aeon/transformations/tests/test_exponent.py
+++ aeon/transformations/tests/test_exponent.py
@@ -9,27 +9,27 @@ from aeon.testing.utils.data_gen import make_series
 from aeon.transformations.exponent import ExponentTransformer
 
 
-@pytest.mark.parametrize("_offset", ["a", [1, 2.3]])
-def test_wrong_offset_type_raises_error(power_transformer, _offset):
+@pytest.mark.parametrize("offset", ["a", [1, 2.3]])
+def test_wrong_offset_type_raises_error(offset):
     """Test an error is raised for incorrect offset types."""
     y = make_series(n_timepoints=75)
 
     # Test input types
-    match = f"Expected `offset` to be int or float, but found {type(_offset)}."
+    match = f"Expected `offset` to be int or float, but found {type(offset)}."
     with pytest.raises(ValueError, match=match):
-        transformer = ExponentTransformer(offset=_offset)
+        transformer = ExponentTransformer(offset=offset)
         transformer.fit(y)
 
 
 # Test only applies to PowerTransformer b/c SqrtTransformer doesn't have power
 # hyperparameter
-@pytest.mark.parametrize("_power", ["a", [1, 2.3]])
-def test_wrong_power_type_raises_error(power_transformer, _power):
+@pytest.mark.parametrize("power", ["a", [1, 2.3]])
+def test_wrong_power_type_raises_error(power):
     """Test an error is raised for incorrect power types."""
     y = make_series(n_timepoints=75)
 
     # Test input types
-    match = f"Expected `power` to be int or float, but found {type(_power)}."
+    match = f"Expected `power` to be int or float, but found {type(power)}."
     with pytest.raises(ValueError, match=match):
-        transformer = ExponentTransformer(power=_power)
+        transformer = ExponentTransformer(power=power)
         transformer.fit(y)
