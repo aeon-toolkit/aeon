afd23bd6f1aa80950ac8d9c25a13d5c7138782c2
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-05-10 13:47:14 +0100

    fixes and pr open test

--- build_tools/issue_assign.py
+++ build_tools/issue_assign.py
@@ -1,8 +1,7 @@
-"""
-Script for the Self-Assign Bot.
+"""Script for the GitHub issue self-assign bot.
 
 It checks if a comment on an issue or PR includes the trigger
-phrase(as defined) and a mentioned user.
+phrase (as defined) and a mentioned user.
 If it does, it assigns the issue/PR to the mentioned user.
 """
 
@@ -13,25 +12,19 @@ import re
 from github import Github
 
 context_dict = json.loads(os.getenv("CONTEXT_GITHUB"))
-print(context_dict)  # noqa
 
-# Initialize a Github instance:
+repo = context_dict["repository"]
 g = Github(os.getenv("GITHUB_TOKEN"))
-
-# Get the repo from environment variables
-repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
-
-# Get the issue from the payload
-issue_number = int(os.getenv("ISSUE_NUMBER"))
+repo = g.get_repo(repo)
+issue_number = context_dict["event"]["issue"]["number"]
 issue = repo.get_issue(number=issue_number)
+comment_body = context_dict["event"]["comment"]["body"]
 
-# Get the comment from the payload
-comment_body = os.getenv("COMMENT_BODY")
+# Assign tagged used to the issue if the comment includes the trigger phrase
+# and starts with a mention to the bot
+if comment_body.startswith("@aeon-actions-bot") and "assign" in comment_body.lower():
+    mentioned_users = re.findall(r"@[a-zA-Z0-9_-]+", comment_body)
+    mentioned_users = [user[1:] for user in mentioned_users[1:]]
 
-# Check if the comment contains the word "assign"
-if "assign" in comment_body.lower():
-    # Extract the username mentioned in the comment
-    mentioned_users = re.findall(r"@([a-zA-Z0-9](?:-?[a-zA-Z0-9]){0,38})", comment_body)
-    for mentioned_user in mentioned_users:
-        # Assign the Issue/PR to the user
-        issue.add_to_assignees(mentioned_user)
+    for user in mentioned_users:
+        issue.add_to_assignees(user)
