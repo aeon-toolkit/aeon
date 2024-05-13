51219cb59ff3f8109a0bd96993dfae50c62411a6
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-05-13 23:31:48 +0100

    base class registry

--- aeon/registry/_base_classes.py
+++ aeon/registry/_base_classes.py
@@ -43,7 +43,6 @@ from aeon.classification.early_classification import BaseEarlyClassifier
 from aeon.clustering.base import BaseClusterer
 from aeon.forecasting.base import BaseForecaster
 from aeon.forecasting.model_selection._split import BaseSplitter
-from aeon.networks.base import BaseDeepNetwork
 from aeon.performance_metrics.base import BaseMetric
 from aeon.regression.base import BaseRegressor
 from aeon.segmentation.base import BaseSegmenter
@@ -62,7 +61,6 @@ BASE_CLASS_REGISTER = [
     ("early_classifier", BaseEarlyClassifier, "early time series classifier"),
     ("forecaster", BaseForecaster, "forecaster"),
     ("metric", BaseMetric, "performance metric"),
-    ("network", BaseDeepNetwork, "deep learning network"),
     ("regressor", BaseRegressor, "regressor"),
     ("segmenter", BaseSegmenter, "segmenter"),
     ("series-estimator", BaseSeriesEstimator, "single series estimator"),
