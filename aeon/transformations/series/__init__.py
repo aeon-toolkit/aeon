26b217c0222c7fe20f7c3e8e7270d51406b0e5b9
Tony Bagnall <ajb@uea.ac.uk>
2024-05-18 14:11:05 +0100

    dobin

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -1,6 +1,7 @@
 """Series transformations."""
 
-__all__ = ["BaseSeriesTransformer", "MatrixProfileSeriesTransformer"]
+__all__ = ["BaseSeriesTransformer", "MatrixProfileSeriesTransformer", "DOBIN"]
 
+from aeon.transformations.series._dobin import DOBIN
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
