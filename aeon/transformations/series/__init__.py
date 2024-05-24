e82c32a152a505db6d7f52deaad2978eec7093fa
Tony Bagnall <ajb@uea.ac.uk>
2024-05-24 15:26:08 +0100

    [ENH] Convert Theta transform to series transformer (#1550)
    * convert theta transformer
    
    * convert theta transformer
    
    * comments

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -3,10 +3,11 @@
 __all__ = [
     "AutoCorrelationSeriesTransformer",
     "BaseSeriesTransformer",
+    "Dobin",
     "MatrixProfileSeriesTransformer",
     "StatsModelsACF",
     "StatsModelsPACF",
-    "Dobin",
+    "ThetaTransformer",
 ]
 
 from aeon.transformations.series._acf import (
@@ -16,4 +17,5 @@ from aeon.transformations.series._acf import (
 )
 from aeon.transformations.series._dobin import Dobin
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
+from aeon.transformations.series._theta import ThetaTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
