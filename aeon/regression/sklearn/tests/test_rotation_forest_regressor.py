9dfc17e3817c8aa0293e382dc8c8ed3a25800a2e
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-05-28 01:45:25 +0100

    rotf testing

--- aeon/regression/sklearn/tests/test_rotation_forest_regressor.py
+++ aeon/regression/sklearn/tests/test_rotation_forest_regressor.py
@@ -7,8 +7,40 @@ from aeon.datasets import load_covid_3month
 from aeon.regression.sklearn import RotationForestRegressor
 
 
-def test_contracted_rotf():
+def test_rotf_output():
     """Test of RotF contracting and train estimate on test data."""
+    X_train, y_train = load_covid_3month(split="train", return_type="numpy2d")
+    X_test, y_test = load_covid_3month(split="test", return_type="numpy2d")
+
+    rotf = RotationForestRegressor(
+        n_estimators=10,
+        random_state=0,
+    )
+    rotf.fit(X_train, y_train)
+
+    expected = [
+        0.02942543,
+        0.02087061,
+        0.01973804,
+        0.06120395,
+        0.06659284,
+        0.01891775,
+        0.04199085,
+        0.01,
+        0.03088715,
+        0.05546291,
+        0.01987163,
+        0.03721117,
+        0.0172066,
+        0.02747714,
+        0.01639872,
+    ]
+
+    np.testing.assert_array_almost_equal(expected, rotf.predict(X_test[:15]), decimal=4)
+
+
+def test_contracted_rotf():
+    """Test of RotF contracting on testing data."""
     X_train, y_train = load_covid_3month(split="train", return_type="numpy2d")
     X_test, y_test = load_covid_3month(split="test", return_type="numpy2d")
 
@@ -30,7 +62,7 @@ def test_contracted_rotf():
 
 
 def test_rotf_fit_predict():
-    """Test of RotF fit_predict on test data."""
+    """Test of RotF fit_predict on testing data."""
     X_train, y_train = load_covid_3month(split="train", return_type="numpy2d")
 
     rotf = RotationForestRegressor(
