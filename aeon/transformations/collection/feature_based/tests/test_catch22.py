dca306a591efe743d8c39d7acb7aa2b0da97689e
Tony Bagnall <ajb@uea.ac.uk>
2024-06-05 21:34:22 +0100

    remove np.argsort from numba

--- aeon/transformations/collection/feature_based/tests/test_catch22.py
+++ aeon/transformations/collection/feature_based/tests/test_catch22.py
@@ -11,14 +11,7 @@ from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 def test_catch22_on_basic_motions():
     """Test of Catch22 on basic motions data."""
-    # the test currently fails when numba is disabled. See issue #622
-    # import os
-
-    # if os.environ.get("NUMBA_DISABLE_JIT") == "1":
-    #     return None
-    # load basic motions data
     X_train, _ = load_basic_motions(split="train")
-    indices = np.random.RandomState(4).choice(len(X_train), 5, replace=False)
     indices = [28, 39, 4, 15, 26]
     # fit Catch22 and assert transformed data is the same
     c22 = Catch22(replace_nans=True)
