76819d25f7671be2acfae558252b86a7f804a6c3
CodeLionX <sebastian.schmidl@t-online.de>
2024-06-05 11:50:15 +0200

    feat: implement kmeans AD

--- aeon/anomaly_detection/__init__.py
+++ aeon/anomaly_detection/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     "MERLIN",
     "STRAY",
+    "KMeansAD",
 ]
 
+from aeon.anomaly_detection._kmeans import KMeansAD
 from aeon.anomaly_detection._merlin import MERLIN
 from aeon.anomaly_detection._stray import STRAY
