ea144fb91b12dee9d647ba93fa8995f79d94a48a
Tony Bagnall <ajb@uea.ac.uk>
2024-06-05 23:10:27 +0100

    remove exclusion

--- aeon/testing/test_config.py
+++ aeon/testing/test_config.py
@@ -47,8 +47,6 @@ EXCLUDED_TESTS = {
     # test fails several variants of inversion, see
     # https://github.com/aeon-toolkit/aeon/issues/700
     "Differencer": ["test_transform_inverse_transform_equivalent"],
-    # Test fails, see https://github.com/aeon-toolkit/aeon/issues/1067
-    # "MockUnivariateForecasterLogger": ["test_non_state_changing_method_contract"],
     # has a keras fail, unknown reason, see #1387
     "LearningShapeletClassifier": ["test_fit_deterministic"],
 }
