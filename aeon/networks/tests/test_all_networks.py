2d2ac91fa783dc85a092dc977fc450aa66acb1f4
aadya940 <aadya@unify.ai>
2024-06-12 09:13:26 +0530

    refactor tests

--- aeon/networks/tests/test_all_networks.py
+++ aeon/networks/tests/test_all_networks.py
@@ -3,14 +3,14 @@
 import inspect
 
 from aeon import networks
+from aeon.utils.validation._dependencies import (
+    _check_python_version,
+    _check_soft_dependencies,
+)
 
 __maintainer__ = []
 
 
-@pytest.mark.skipif(
-    not _check_soft_dependencies(["tensorflow", "tensorflow_addons"], severity="none"),
-    reason="skip test if required soft dependency not available",
-)
 def test_all_networks_functionality():
     """Test the functionality of all networks."""
     network_classes = [
@@ -19,20 +19,32 @@ def test_all_networks_functionality():
     input_shape = (100, 2)
 
     for i in range(len(network_classes)):
-        if (
-            "BaseDeepNetwork" in str(network_classes[i])
-            or "BaseDeepLearningNetwork" in str(network_classes[i])
-            or "AEFCNNetwork" in str(network_classes[i])
-            or "EncoderNetwork" in str(network_classes[i])
-        ):
+
+        if "BaseDeepNetwork" in str(
+            network_classes[i]
+        ) or "BaseDeepLearningNetwork" in str(network_classes[i]):
             continue
 
         try:
             my_network = network_classes[i]()
         except ModuleNotFoundError:
-            continue
 
-        input_layer, output_layer = my_network.build_network(input_shape=input_shape)
+            if "AEFCNNetwork" in str(network_classes[i]) or "EncoderNetwork" in str(
+                network_classes[i]
+            ):
+                if _check_soft_dependencies(["tensorflow-addons"], severity=None):
+                    if _check_python_version(network_classes[i], severity=None):
+                        my_network = network_classes[i]()
+                    else:
+                        continue
 
-        assert input_layer is not None
-        assert output_layer is not None
+        if str(network_classes[i]).startswith("AE"):
+            encoder, decoder = my_network.build_network(input_shape=input_shape)
+            assert encoder.layers[-1].output_shape == (my_network.latent_space_dim,)
+            assert encoder.layers[0].input_shape == decoder.layers[-1].output_shape
+        else:
+            input_layer, output_layer = my_network.build_network(
+                input_shape=input_shape
+            )
+            assert input_layer is not None
+            assert output_layer is not None
