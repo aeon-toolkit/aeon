77e5d626d30c2d47ddff02571ae25d9cfb254e7b
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2024-06-17 11:28:20 +0200

    exclude test if ruptures is not installed

--- aeon/segmentation/tests/test_binseg.py
+++ aeon/segmentation/tests/test_binseg.py
@@ -3,10 +3,17 @@
 __maintainer__ = []
 __all__ = []
 
+import pytest
+
 from aeon.datasets import load_gun_point_segmentation
 from aeon.segmentation import BinSegSegmenter
+from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 
+@pytest.mark.skipif(
+    not _check_soft_dependencies(["ruptures"], severity="none"),
+    reason="skip test if required soft dependency not available",
+)
 def test_binseg_sparse():
     """Test BinSeg sparse segmentation.
 
