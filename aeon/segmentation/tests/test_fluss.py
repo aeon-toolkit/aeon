31796f6f3f61a70bbeffad642b50ef822b73462e
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2024-06-17 10:40:58 +0200

    Adds FLUSS segmenter

---
+++ aeon/segmentation/tests/test_fluss.py
@@ -0,0 +1,24 @@
+"""Simple FLUSS test."""
+
+__maintainer__ = []
+__all__ = []
+
+from aeon.datasets import load_gun_point_segmentation
+from aeon.segmentation import FLUSSSegmenter
+
+
+def test_fluss_sparse():
+    """Test FLUSS segmentation.
+
+    Check if the predicted change points match.
+    """
+    # load the test dataset
+    ts, period_size, cps = load_gun_point_segmentation()
+
+    # compute a FLUSS segmentation
+    fluss = FLUSSSegmenter(period_size, n_regimes=2)
+    found_cps = fluss.fit_predict(ts)
+    scores = fluss.predict_scores(ts)
+
+    assert len(found_cps) == 1 and found_cps[0] == 889
+    assert len(scores) == 1 and 0.53 > scores[0] > 0.52
