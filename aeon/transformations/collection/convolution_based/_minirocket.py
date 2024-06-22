e670cd43ad726c0d8b12ec1b9a1f2be3e3a65f58
Tony Bagnall <ajb@uea.ac.uk>
2024-06-22 16:28:06 +0100

    remove numba function

--- aeon/transformations/collection/convolution_based/_minirocket.py
+++ aeon/transformations/collection/convolution_based/_minirocket.py
@@ -4,12 +4,12 @@ __maintainer__ = []
 __all__ = ["MiniRocket"]
 
 import multiprocessing
+from itertools import combinations
 
 import numpy as np
 from numba import get_num_threads, njit, prange, set_num_threads, vectorize
 
 from aeon.transformations.collection import BaseCollectionTransformer
-from aeon.utils.numba.general import generate_combinations
 
 
 class MiniRocket(BaseCollectionTransformer):
@@ -63,6 +63,8 @@ class MiniRocket(BaseCollectionTransformer):
         "algorithm_type": "convolution",
         "capability:multivariate": True,
     }
+    # indices for the 84 kernels used by MiniRocket
+    _indices = np.array([_ for _ in combinations(np.arange(9), 3)], dtype=np.int32)
 
     def __init__(
         self,
@@ -147,67 +149,48 @@ class MiniRocket(BaseCollectionTransformer):
 
 
 @njit(
-    "float32[:](float32[:,:],int32[:],int32[:],float32[:],optional(int32))",
+    "float32[:](float32[:,:],int32[:],int32[:],float32[:],int32[:,:],optional(int32))",
     fastmath=True,
     parallel=False,
     cache=True,
 )
-def _fit_biases(X, dilations, num_features_per_dilation, quantiles, seed):
+def _fit_biases(X, dilations, num_features_per_dilation, quantiles, indices, seed):
     if seed is not None:
         np.random.seed(seed)
 
     n_cases, n_timepoints = X.shape
-
-    # equivalent to:
-    # >>> from itertools import combinations
-    # >>> indices = np.array([_ for _ in combinations(np.arange(9), 3)])
-    indices = generate_combinations(9, 3)
     num_kernels = len(indices)
     num_dilations = len(dilations)
-
     num_features = num_kernels * np.sum(num_features_per_dilation)
-
     biases = np.zeros(num_features, dtype=np.float32)
-
     feature_index_start = 0
 
     for dilation_index in range(num_dilations):
         dilation = dilations[dilation_index]
         padding = ((9 - 1) * dilation) // 2
-
         num_features_this_dilation = num_features_per_dilation[dilation_index]
 
         for kernel_index in range(num_kernels):
             feature_index_end = feature_index_start + num_features_this_dilation
-
             _X = X[np.random.randint(n_cases)]
-
             A = -_X  # A = alpha * X = -X
             G = _X + _X + _X  # G = gamma * X = 3X
-
             C_alpha = np.zeros(n_timepoints, dtype=np.float32)
             C_alpha[:] = A
-
             C_gamma = np.zeros((9, n_timepoints), dtype=np.float32)
             C_gamma[9 // 2] = G
-
             start = dilation
             end = n_timepoints - padding
-
             for gamma_index in range(9 // 2):
                 C_alpha[-end:] = C_alpha[-end:] + A[:end]
                 C_gamma[gamma_index, -end:] = G[:end]
-
                 end += dilation
-
             for gamma_index in range(9 // 2 + 1, 9):
                 C_alpha[:-start] = C_alpha[:-start] + A[start:]
                 C_gamma[gamma_index, :-start] = G[start:]
-
                 start += dilation
 
             index_0, index_1, index_2 = indices[kernel_index]
-
             C = C_alpha + C_gamma[index_0] + C_gamma[index_1] + C_gamma[index_2]
 
             biases[feature_index_start:feature_index_end] = np.quantile(
@@ -266,7 +249,9 @@ def _fit_uni(X, num_features=10_000, max_dilations_per_kernel=32, seed=None):
 
     quantiles = _quantiles(num_kernels * num_features_per_kernel)
 
-    biases = _fit_biases(X, dilations, num_features_per_dilation, quantiles, seed)
+    biases = _fit_biases(
+        X, dilations, num_features_per_dilation, quantiles, MiniRocket._indices, seed
+    )
 
     return dilations, num_features_per_dilation, biases
 
@@ -337,16 +322,15 @@ def _PPV(a, b):
 
 
 @njit(
-    "float32[:,:](float32[:,:],Tuple((int32[:],int32[:],float32[:])))",
+    "float32[:,:](float32[:,:],Tuple((int32[:],int32[:],float32[:]))),int32[:,:]",
     fastmath=True,
     parallel=True,
     cache=True,
 )
-def _transform_uni(X, parameters):
+def _transform_uni(X, parameters, indices):
     n_cases, n_timepoints = X.shape
 
     dilations, num_features_per_dilation, biases = parameters
-    indices = generate_combinations(9, 3)
     num_kernels = len(indices)
     num_dilations = len(dilations)
     num_features = num_kernels * np.sum(num_features_per_dilation)
@@ -415,12 +399,12 @@ def _transform_uni(X, parameters):
 
 
 @njit(
-    "float32[:,:](float32[:,:,:],Tuple((int32[:],int32[:],int32[:],int32[:],float32[:])))",  # noqa
+    "float32[:,:](float32[:,:,:],Tuple((int32[:],int32[:],int32[:],int32[:],float32[:]))),int32[:,:])",  # noqa
     fastmath=True,
     parallel=True,
     cache=True,
 )
-def _transform_multi(X, parameters):
+def _transform_multi(X, parameters, indices):
     n_cases, n_columns, n_timepoints = X.shape
 
     (
@@ -430,268 +414,6 @@ def _transform_multi(X, parameters):
         num_features_per_dilation,
         biases,
     ) = parameters
-
-    # equivalent to:
-    # >>> from itertools import combinations
-    # >>> indices = np.array([_ for _ in combinations(np.arange(9), 3)])
-    indices = np.array(
-        (
-            0,
-            1,
-            2,
-            0,
-            1,
-            3,
-            0,
-            1,
-            4,
-            0,
-            1,
-            5,
-            0,
-            1,
-            6,
-            0,
-            1,
-            7,
-            0,
-            1,
-            8,
-            0,
-            2,
-            3,
-            0,
-            2,
-            4,
-            0,
-            2,
-            5,
-            0,
-            2,
-            6,
-            0,
-            2,
-            7,
-            0,
-            2,
-            8,
-            0,
-            3,
-            4,
-            0,
-            3,
-            5,
-            0,
-            3,
-            6,
-            0,
-            3,
-            7,
-            0,
-            3,
-            8,
-            0,
-            4,
-            5,
-            0,
-            4,
-            6,
-            0,
-            4,
-            7,
-            0,
-            4,
-            8,
-            0,
-            5,
-            6,
-            0,
-            5,
-            7,
-            0,
-            5,
-            8,
-            0,
-            6,
-            7,
-            0,
-            6,
-            8,
-            0,
-            7,
-            8,
-            1,
-            2,
-            3,
-            1,
-            2,
-            4,
-            1,
-            2,
-            5,
-            1,
-            2,
-            6,
-            1,
-            2,
-            7,
-            1,
-            2,
-            8,
-            1,
-            3,
-            4,
-            1,
-            3,
-            5,
-            1,
-            3,
-            6,
-            1,
-            3,
-            7,
-            1,
-            3,
-            8,
-            1,
-            4,
-            5,
-            1,
-            4,
-            6,
-            1,
-            4,
-            7,
-            1,
-            4,
-            8,
-            1,
-            5,
-            6,
-            1,
-            5,
-            7,
-            1,
-            5,
-            8,
-            1,
-            6,
-            7,
-            1,
-            6,
-            8,
-            1,
-            7,
-            8,
-            2,
-            3,
-            4,
-            2,
-            3,
-            5,
-            2,
-            3,
-            6,
-            2,
-            3,
-            7,
-            2,
-            3,
-            8,
-            2,
-            4,
-            5,
-            2,
-            4,
-            6,
-            2,
-            4,
-            7,
-            2,
-            4,
-            8,
-            2,
-            5,
-            6,
-            2,
-            5,
-            7,
-            2,
-            5,
-            8,
-            2,
-            6,
-            7,
-            2,
-            6,
-            8,
-            2,
-            7,
-            8,
-            3,
-            4,
-            5,
-            3,
-            4,
-            6,
-            3,
-            4,
-            7,
-            3,
-            4,
-            8,
-            3,
-            5,
-            6,
-            3,
-            5,
-            7,
-            3,
-            5,
-            8,
-            3,
-            6,
-            7,
-            3,
-            6,
-            8,
-            3,
-            7,
-            8,
-            4,
-            5,
-            6,
-            4,
-            5,
-            7,
-            4,
-            5,
-            8,
-            4,
-            6,
-            7,
-            4,
-            6,
-            8,
-            4,
-            7,
-            8,
-            5,
-            6,
-            7,
-            5,
-            6,
-            8,
-            5,
-            7,
-            8,
-            6,
-            7,
-            8,
-        ),
-        dtype=np.int32,
-    ).reshape(84, 3)
-
     num_kernels = len(indices)
     num_dilations = len(dilations)
 
@@ -787,7 +509,7 @@ def _transform_multi(X, parameters):
 
 
 @njit(
-    "float32[:](float32[:,:,:],int32[:],int32[:],int32[:],int32[:],float32[:],optional(int32))",  # noqa
+    "float32[:](float32[:,:],int32[:],int32[:],float32[:],int32[:,:],optional(int32))",
     fastmath=True,
     parallel=False,
     cache=True,
@@ -799,17 +521,13 @@ def _fit_biases_multi(
     dilations,
     num_features_per_dilation,
     quantiles,
+    indices,
     seed,
 ):
     if seed is not None:
         np.random.seed(seed)
 
     n_cases, n_columns, n_timepoints = X.shape
-
-    # equivalent to:
-    # >>> from itertools import combinations
-    # >>> indices = np.array([_ for _ in combinations(np.arange(9), 3)])
-    indices = generate_combinations(9, 3)
     num_kernels = len(indices)
     num_dilations = len(dilations)
 
