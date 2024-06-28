696bc786b1796e0dfe2049eb690008d6dd15cd07
aadya940 <aadya@unify.ai>
2024-06-28 07:47:58 +0530

    Parametrize tests

--- aeon/networks/tests/test_all_networks.py
+++ aeon/networks/tests/test_all_networks.py
@@ -2,6 +2,8 @@
 
 import inspect
 
+import pytest
+
 from aeon import networks
 from aeon.utils.validation._dependencies import (
     _check_python_version,
@@ -10,50 +12,40 @@ from aeon.utils.validation._dependencies import (
 
 __maintainer__ = []
 
+_networks = network_classes = [
+    member[1] for member in inspect.getmembers(networks, inspect.isclass)
+]
 
-def test_network_config():
+
+@pytest.mark.parametrize("network", _networks)
+def test_network_config(network):
     """Tests if the config dictionary of classes is correctly configured."""
-    network_classes = [
-        member[1] for member in inspect.getmembers(networks, inspect.isclass)
-    ]
-
-    for i in range(len(network_classes)):
-
-        assert "python_dependencies" in network_classes[i]._config.keys()
-        assert "python_version" in network_classes[i]._config.keys()
-        assert "auto-encoder" in network_classes[i]._config.keys()
-        assert isinstance(network_classes[i]._config["python_dependencies"], str) and (
-            "tensorflow" in network_classes[i]._config["python_dependencies"]
-        )
-        assert isinstance(network_classes[i]._config["python_version"], str)
-        assert isinstance(network_classes[i]._config["auto-encoder"], bool)
+    assert "python_dependencies" in network._config.keys()
+    assert "python_version" in network._config.keys()
+    assert "auto-encoder" in network._config.keys()
+    assert isinstance(network._config["python_dependencies"], str) and (
+        "tensorflow" in network._config["python_dependencies"]
+    )
+    assert isinstance(network._config["python_version"], str)
+    assert isinstance(network._config["auto-encoder"], bool)
 
 
-def test_all_networks_functionality():
+@pytest.mark.parametrize("network", _networks)
+def test_all_networks_functionality(network):
     """Test the functionality of all networks."""
-    network_classes = [
-        member[1] for member in inspect.getmembers(networks, inspect.isclass)
-    ]
     input_shape = (100, 2)
 
-    for i in range(len(network_classes)):
-        if "BaseDeepLearningNetwork" in str(network_classes[i]):
-            continue
-
+    if "BaseDeepLearningNetwork" != str(network):
         if _check_soft_dependencies(
-            network_classes[i]._config["python_dependencies"], severity="none"
-        ) and _check_python_version(
-            network_classes[i]._config["python_version"], severity="none"
-        ):
-            if "EncoderNetwork" in str(network_classes[i]):
+            network._config["python_dependencies"], severity="none"
+        ) and _check_python_version(network._config["python_version"], severity="none"):
+            if "EncoderNetwork" == str(network):
                 if _check_soft_dependencies(["tensorflow-addons"], severity="none"):
-                    my_network = network_classes[i]()
+                    my_network = network()
             else:
-                my_network = network_classes[i]()
-        else:
-            continue
+                my_network = network()
 
-        if network_classes[i]._config["auto_encoder"]:
+        if network._config["auto_encoder"]:
             encoder, decoder = my_network.build_network(input_shape=input_shape)
             assert encoder.layers[-1].output_shape[1:] == (my_network.latent_space_dim,)
             assert encoder.layers[0].input_shape[0] == decoder.layers[-1].output_shape
@@ -64,7 +56,7 @@ def test_all_networks_functionality():
             assert input_layer is not None
             assert output_layer is not None
             if _check_soft_dependencies(["tensorflow"], severity="none"):
-                from tensorflow.keras import layers
+                import tensorflow as tf
 
-                assert isinstance(input_layer, layers.Layer)
-                assert isinstance(output_layer, layers.Layer)
+                assert isinstance(input_layer, tf.keras.layers.Layer)
+                assert isinstance(output_layer, tf.keras.layers.Layer)
