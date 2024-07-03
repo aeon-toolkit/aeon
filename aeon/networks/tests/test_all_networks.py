d4b7c427f2de7bbf6d05a015332c212b110125b8
hadifawaz1999 <hadifawaz2291999@gmail.com>
2024-07-03 13:57:29 +0200

    remove python version from test

--- aeon/networks/tests/test_all_networks.py
+++ aeon/networks/tests/test_all_networks.py
@@ -5,10 +5,7 @@ import inspect
 import pytest
 
 from aeon import networks
-from aeon.utils.validation._dependencies import (
-    _check_python_version,
-    _check_soft_dependencies,
-)
+from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 __maintainer__ = []
 
@@ -21,12 +18,10 @@ _networks = network_classes = [
 def test_network_config(network):
     """Tests if the config dictionary of classes is correctly configured."""
     assert "python_dependencies" in network._config.keys()
-    assert "python_version" in network._config.keys()
     assert "structure" in network._config.keys()
     assert isinstance(network._config["python_dependencies"], list) and (
         "tensorflow" in network._config["python_dependencies"]
     )
-    assert isinstance(network._config["python_version"], str)
     assert isinstance(network._config["structure"], str)
 
 
@@ -41,7 +36,7 @@ def test_all_networks_functionality(network):
     ):
         if _check_soft_dependencies(
             network._config["python_dependencies"], severity="none"
-        ) and _check_python_version(network._config["python_version"], severity="none"):
+        ):
             my_network = network()
 
             if network._config["structure"] == "auto-encoder":
