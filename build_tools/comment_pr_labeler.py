a5a01702812375c62a01d38194609a6cf42f3722
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2024-07-03 12:59:15 +0100

    CI fun

--- build_tools/comment_pr_labeler.py
+++ build_tools/comment_pr_labeler.py
@@ -17,18 +17,20 @@ comment_body = context_dict["event"]["comment"]["body"]
 comment_user = context_dict["event"]["comment"]["user"]["login"]
 labels = [label.name for label in issue.get_labels()]
 
-if comment_user != "aeon-actions-bot[bot]":
+if comment_user != "aeon-actions-bot[bot]" or issue.pull_request is None:
+    print("::set-output name=empty_commit::false")  # noqa: T201
     sys.exit(0)
+pr = issue.as_pull_request()
 
 
 def check_label_option(label, option):
     """Add or remove a label based on a checkbox in a comment."""
     if f"- [x] {option}" in comment_body:
         if label not in labels:
-            issue.add_to_labels(label)
+            pr.add_to_labels(label)
     elif f"- [ ] {option}" in comment_body:
         if label in labels:
-            issue.remove_from_labels(label)
+            pr.remove_from_labels(label)
 
 
 label_options = [
@@ -44,3 +46,8 @@ label_options = [
 
 for option in label_options:
     check_label_option(option[0], option[1])
+
+if "- [x] Push an empty commit to re-run CI checks" in comment_body:
+    print("::set-output name=empty_commit::true")  # noqa: T201
+else:
+    print("::set-output name=empty_commit::false")  # noqa: T201
