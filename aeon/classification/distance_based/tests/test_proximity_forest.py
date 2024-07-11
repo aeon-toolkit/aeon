c853e55bea9414aa343eda08686a179616a571b5
Divya Tiwari <gungun130903@gmail.com>
2024-07-11 22:51:57 +0530

    Using unit test dataset

--- aeon/classification/distance_based/tests/test_proximity_forest.py
+++ aeon/classification/distance_based/tests/test_proximity_forest.py
@@ -4,34 +4,24 @@ import pytest
 from sklearn.metrics import accuracy_score
 
 from aeon.classification.distance_based import ProximityForest
-from aeon.testing.data_generation import make_example_3d_numpy
+from aeon.datasets import load_unit_test
 
 
-@pytest.fixture
-def time_series_dataset():
-    """Generate time series dataset for testing."""
-    n_samples = 100  # Total number of samples (should be even)
-    n_timepoints = 24  # Length of each time series
-    n_channels = 1
-    data, labels = make_example_3d_numpy(n_samples, n_channels, n_timepoints)
-    return data, labels
-
-
-def test_univariate(time_series_dataset):
+def test_univariate():
     """Test that the function gives appropriate error message."""
-    X, y = time_series_dataset
-    X_multivariate = X.reshape((100, 2, 12))
+    X, y = load_unit_test()
+    X_multivariate = X.reshape((-1, 2, 12))
     clf = ProximityForest(n_trees=5, random_state=42, n_jobs=-1)
     with pytest.raises(ValueError):
         clf.fit(X_multivariate, y)
 
 
-def test_proximity_forest(time_series_dataset):
+def test_proximity_forest():
     """Test the fit method of ProximityTree."""
-    X, y = time_series_dataset
+    X_train, y_train = load_unit_test()
+    X_test, y_test = load_unit_test(split="test")
     clf = ProximityForest(n_trees=5, n_splitters=3, max_depth=4)
-    clf.fit(X, y)
-    X_test, y_test = time_series_dataset
+    clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     score = accuracy_score(y_test, y_pred)
     assert score >= 0.9
