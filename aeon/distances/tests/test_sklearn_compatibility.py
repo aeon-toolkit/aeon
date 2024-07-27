4ada27fd4a19c8330563fc5a6ba12ab6ef2349c1
David Guijo-Rubio <47889499+dguijo@users.noreply.github.com>
2024-07-27 17:28:56 +0200

    Tests for the regressors. May not work for EDR and LCSS without apparent reason :(

--- aeon/distances/tests/test_sklearn_compatibility.py
+++ aeon/distances/tests/test_sklearn_compatibility.py
@@ -2,15 +2,21 @@
 
 import numpy as np
 import pytest
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_allclose, assert_almost_equal
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.svm import SVR
 
 from aeon.classification.distance_based import KNeighborsTimeSeriesClassifier
-from aeon.datasets import load_basic_motions, load_unit_test
+from aeon.datasets import (
+    load_basic_motions,
+    load_cardano_sentiment,
+    load_covid_3month,
+    load_unit_test,
+)
 from aeon.distances._distance import DISTANCES
+from aeon.regression.distance_based import KNeighborsTimeSeriesRegressor
 from aeon.testing.data_generation import make_example_3d_numpy
 
 
@@ -63,105 +69,129 @@ def test_clusterer(dist):
 
 
 @pytest.mark.parametrize("dist", DISTANCES)
-@pytest.mark.parametrize("k", [1, 5])
-def test_classification_univariate(dist, k):
+@pytest.mark.parametrize("k", [1, 3])
+@pytest.mark.parametrize(
+    "task",
+    [
+        ["cls", load_unit_test, KNeighborsClassifier, KNeighborsTimeSeriesClassifier],
+        ["reg", load_covid_3month, KNeighborsRegressor, KNeighborsTimeSeriesRegressor],
+    ],
+)
+def test_univariate(dist, k, task):
     """Test all distances work with sklearn nearest neighbours."""
     # Test univariate with 2D format (compatible with sklearn)
+    problem_type, problem_loader, knn_sk_func, knn_aeon_func = task
 
     # Load the unit test dataset as a 2D numpy array
-    X_train, y_train = load_unit_test(split="train", return_type="numpy2D")
-    X_test, y_test = load_unit_test(split="test", return_type="numpy2D")
+    X_train, y_train = problem_loader(split="train", return_type="numpy2D")
+    X_test, y_test = problem_loader(split="test", return_type="numpy2D")
     indices = np.random.RandomState(0).choice(
-        min(len(y_test), len(y_train)), 10, replace=False
+        min(len(y_test), len(y_train)), 5, replace=False
     )
     # Compute the pairwise distance matrix for working with sklearn knn with
     # precomputed distances.
-    X_train_precomputed_distance = dist["pairwise_distance"](X_train[indices])
-    X_test_precomputed_distance = dist["pairwise_distance"](
+    X_train_precomp_distance = dist["pairwise_distance"](X_train[indices])
+    X_test_precomp_distance = dist["pairwise_distance"](
         X_test[indices], X_train[indices]
     )
 
-    knn_aeon = KNeighborsTimeSeriesClassifier(
-        distance=dist["name"], n_neighbors=k
-    )  # aeon
-    knn_sk = KNeighborsClassifier(metric=dist["distance"], n_neighbors=k)  # sklearn
-    knn_sk_precomputed = KNeighborsClassifier(
-        metric="precomputed", n_neighbors=k
-    )  # sklearn pre
+    knn_aeon = knn_aeon_func(distance=dist["name"], n_neighbors=k)  # aeon
+    knn_sk = knn_sk_func(metric=dist["distance"], n_neighbors=k)  # sklearn
+    knn_sk_precomp = knn_sk_func(metric="precomputed", n_neighbors=k)  # sklearn pre
 
     knn_aeon.fit(X_train[indices], y_train[indices])
     knn_sk.fit(X_train[indices], y_train[indices])
-    knn_sk_precomputed.fit(X_train_precomputed_distance, y_train[indices])
+    knn_sk_precomp.fit(X_train_precomp_distance, y_train[indices])
 
-    knn_aeon_probas = knn_aeon.predict_proba(X_test[indices])
-    knn_sk_probas = knn_sk.predict_proba(X_test[indices])
-    knn_sk_precomputed_probas = knn_sk_precomputed.predict_proba(
-        X_test_precomputed_distance
-    )
+    if problem_type == "cls":
+        knn_aeon_output = knn_aeon.predict_proba(X_test[indices])
+        knn_sk_output = knn_sk.predict_proba(X_test[indices])
+        knn_sk_precomp_output = knn_sk_precomp.predict_proba(X_test_precomp_distance)
+    elif problem_type == "reg":
+        knn_aeon_output = knn_aeon.predict(X_test[indices])
+        knn_sk_output = knn_sk.predict(X_test[indices])
+        knn_sk_precomp_output = knn_sk_precomp.predict(X_test_precomp_distance)
 
-    assert_almost_equal(knn_aeon_probas, knn_sk_probas)
-    assert_almost_equal(knn_aeon_probas, knn_sk_precomputed_probas)
+    assert_allclose(knn_aeon_output, knn_sk_output)
+    assert_allclose(knn_aeon_output, knn_sk_precomp_output)
 
 
 @pytest.mark.parametrize("dist", DISTANCES)
-@pytest.mark.parametrize("k", [1, 5])
-def test_classification_multivariate(dist, k):
+@pytest.mark.parametrize("k", [1, 3])
+@pytest.mark.parametrize(
+    "task",
+    [
+        [
+            "cls",
+            load_basic_motions,
+            KNeighborsClassifier,
+            KNeighborsTimeSeriesClassifier,
+        ],
+        [
+            "reg",
+            load_cardano_sentiment,
+            KNeighborsRegressor,
+            KNeighborsTimeSeriesRegressor,
+        ],
+    ],
+)
+def test_multivariate(dist, k, task):
     """Test all distances work with sklearn nearest neighbours."""
     # Test multivariate dataset in two ways: A) concatenating channels to be compatible
     # with sklearn, and B) precomputing distances.
 
+    problem_type, problem_loader, knn_sk_func, knn_aeon_func = task
+
     # Load the basic motions dataset as a 3D numpy array
-    X_train, y_train = load_basic_motions(split="train", return_type="numpy3D")
-    X_test, y_test = load_basic_motions(split="test", return_type="numpy3D")
+    X_train, y_train = problem_loader(split="train", return_type="numpy3D")
+    X_test, y_test = problem_loader(split="test", return_type="numpy3D")
 
     # Transform to 2D format concatenating channels.
     X_train_concat = X_train.reshape(X_train.shape[0], -1)
     X_test_concat = X_test.reshape(X_test.shape[0], -1)
 
-    indices = np.random.RandomState(4).choice(
-        min(len(y_test), len(y_train)), 10, replace=False
+    indices = np.random.RandomState(0).choice(
+        min(len(y_test), len(y_train)), 5, replace=False
     )
 
     # A) Test multivariate with 2D format (concatenates channels to be compatible with
     # sklearn)
-    knn_aeon = KNeighborsTimeSeriesClassifier(
-        distance=dist["name"], n_neighbors=k
-    )  # aeon concat
-    knn_sk = KNeighborsClassifier(
-        metric=dist["distance"], n_neighbors=k
-    )  # sklearn concat
+    knn_aeon = knn_aeon_func(distance=dist["name"], n_neighbors=k)  # aeon concat
+    knn_sk = knn_sk_func(metric=dist["distance"], n_neighbors=k)  # sklearn concat
 
     knn_aeon.fit(X_train_concat[indices], y_train[indices])
     knn_sk.fit(X_train_concat[indices], y_train[indices])
 
-    knn_aeon_probas = knn_aeon.predict_proba(X_test_concat[indices])
-    knn_sk_probas = knn_sk.predict_proba(X_test_concat[indices])
+    if problem_type == "cls":
+        knn_aeon_output = knn_aeon.predict_proba(X_test_concat[indices])
+        knn_sk_output = knn_sk.predict_proba(X_test_concat[indices])
+    elif problem_type == "reg":
+        knn_aeon_output = knn_aeon.predict(X_test_concat[indices])
+        knn_sk_output = knn_sk.predict(X_test_concat[indices])
 
-    assert_almost_equal(knn_aeon_probas, knn_sk_probas)
+    assert_allclose(knn_aeon_output, knn_sk_output)
 
     # ---------------------------------------------------------------------------------
     # B) Test multivariate with 3D format (compatible with sklearn if using precomputed
     # distances)
 
     # Compute the pairwise distance matrix
-    X_train_precomputed_distance = dist["pairwise_distance"](X_train[indices])
-    X_test_precomputed_distance = dist["pairwise_distance"](
+    X_train_precomp_distance = dist["pairwise_distance"](X_train[indices])
+    X_test_precomp_distance = dist["pairwise_distance"](
         X_test[indices], X_train[indices]
     )
 
-    knn_aeon_3D = KNeighborsTimeSeriesClassifier(
-        distance=dist["name"], n_neighbors=k
-    )  # aeon
-    knn_sk_precomputed = KNeighborsClassifier(
-        metric="precomputed", n_neighbors=k
-    )  # sklearn precomputed
+    knn_aeon_3D = knn_aeon_func(distance=dist["name"], n_neighbors=k)  # aeon
+    knn_sk_precomp = knn_sk_func(metric="precomputed", n_neighbors=k)  # sklearn precomp
 
     knn_aeon_3D.fit(X_train[indices], y_train[indices])
-    knn_sk_precomputed.fit(X_train_precomputed_distance, y_train[indices])
+    knn_sk_precomp.fit(X_train_precomp_distance, y_train[indices])
 
-    knn_aeon_3D_probas = knn_aeon_3D.predict_proba(X_test[indices])
-    knn_sk_precomputed_probas = knn_sk_precomputed.predict_proba(
-        X_test_precomputed_distance
-    )
+    if problem_type == "cls":
+        knn_aeon_3D_output = knn_aeon_3D.predict_proba(X_test[indices])
+        knn_sk_precomp_output = knn_sk_precomp.predict_proba(X_test_precomp_distance)
+    elif problem_type == "reg":
+        knn_aeon_3D_output = knn_aeon_3D.predict(X_test[indices])
+        knn_sk_precomp_output = knn_sk_precomp.predict(X_test_precomp_distance)
 
-    assert_almost_equal(knn_aeon_3D_probas, knn_sk_precomputed_probas)
+    assert_allclose(knn_aeon_3D_output, knn_sk_precomp_output)
