a80de2570a738e32f889014a20c9a0b5d1f4976a
Moonzyyy <Moonzyyy@users.noreply.github.com>
2024-08-01 15:53:34 +0000

    Automatic `pre-commit` fixes

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -29,8 +29,8 @@ from aeon.transformations.series._clasp import ClaSPTransformer
 from aeon.transformations.series._clear_sky import ClearSkyTransformer
 from aeon.transformations.series._dobin import Dobin
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
-from aeon.transformations.series._pla import PiecewiseLinearApproximation
 from aeon.transformations.series._pca import PCASeriesTransformer
+from aeon.transformations.series._pla import PiecewiseLinearApproximation
 from aeon.transformations.series._scaled_logit import ScaledLogitSeriesTransformer
 from aeon.transformations.series._theta import ThetaTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
