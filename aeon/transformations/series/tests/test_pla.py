1317027668aeddeb130462e126900fb02ddf9c5e
Moonzyyy <47296443+Moonzyyy@users.noreply.github.com>
2024-08-06 16:21:53 +0100

    changed pla tests

--- aeon/transformations/series/tests/test_pla.py
+++ aeon/transformations/series/tests/test_pla.py
@@ -41,33 +41,35 @@ def X():
 
 def test_piecewise_linear_approximation_sliding_window(X):
     """Test PLA transformer."""
-    pla_string = PiecewiseLinearApproximation(100, "sliding window")
+    pla_string = PiecewiseLinearApproximation(
+        max_error=100_000, transformer="sliding window"
+    )
     result_string = pla_string.fit_transform(X)
     expected = np.array(
         [
-            573.0,
-            375.0,
-            301.0,
-            212.0,
-            53.0,
-            38.0,
-            23.0,
-            33.0,
-            113.0,
-            143.0,
-            303.0,
-            615.0,
-            1226.0,
-            1281.0,
-            1221.0,
-            1081.0,
-            866.0,
-            1097.16666667,
-            1036.66666667,
-            976.16666667,
-            747.16666667,
-            578.66666667,
-            410.16666667,
+            430.95555556,
+            371.02222222,
+            311.08888889,
+            251.15555556,
+            191.22222222,
+            131.28888889,
+            71.35555556,
+            11.42222222,
+            -48.51111111,
+            73.8,
+            393.7,
+            713.6,
+            1033.5,
+            1353.4,
+            1195.83333333,
+            1125.77380952,
+            1055.71428571,
+            985.6547619,
+            915.5952381,
+            845.53571429,
+            775.47619048,
+            705.41666667,
+            409.0,
             182.0,
         ]
     )
@@ -76,34 +78,34 @@ def test_piecewise_linear_approximation_sliding_window(X):
 
 def test_piecewise_linear_approximation_top_down(X):
     """Test PLA transformer."""
-    pla_string = PiecewiseLinearApproximation(100, "top down")
+    pla_string = PiecewiseLinearApproximation(100_000, "top down")
     result_string = pla_string.fit_transform(X)
     expected = np.array(
         [
-            573.0,
-            375.0,
-            301.0,
-            212.0,
-            53.0,
-            38.0,
-            23.0,
-            33.0,
-            113.0,
-            143.0,
-            303.0,
-            615.0,
-            1226.0,
-            1281.0,
-            1221.0,
-            1081.0,
-            866.0,
-            1097.16666667,
-            1036.66666667,
-            976.16666667,
-            746.0,
-            581.0,
-            409.0,
-            182.0,
+            500.57142857,
+            408.71428571,
+            316.85714286,
+            225.0,
+            133.14285714,
+            41.28571429,
+            -50.57142857,
+            -29.4,
+            106.0,
+            241.4,
+            376.8,
+            512.2,
+            1319.0,
+            1227.0,
+            1135.0,
+            1043.0,
+            951.0,
+            1189.28571429,
+            1032.28571429,
+            875.28571429,
+            718.28571429,
+            561.28571429,
+            404.28571429,
+            247.28571429,
         ]
     )
     np.testing.assert_array_almost_equal(result_string, expected)
@@ -111,30 +113,32 @@ def test_piecewise_linear_approximation_top_down(X):
 
 def test_piecewise_linear_approximation_bottom_up(X):
     """Test PLA transformer."""
-    pla_string = PiecewiseLinearApproximation(5, "bottom up")
+    pla_string = PiecewiseLinearApproximation(
+        max_error=100_000, transformer="bottom up"
+    )
     result_string = pla_string.fit_transform(X)
     expected = np.array(
         [
-            538.8,
-            423.1,
-            307.4,
-            191.7,
-            48.0,
-            40.5,
-            33.0,
-            25.5,
+            471.33333333,
+            394.0952381,
+            316.85714286,
+            239.61904762,
+            162.38095238,
+            85.14285714,
+            7.9047619,
+            -69.33333333,
             43.6,
             210.2,
             376.8,
             543.4,
-            1276.5,
-            1227.0,
-            1177.5,
-            1128.0,
-            953.5,
-            980.5,
-            1007.5,
-            1034.5,
+            1246.33333333,
+            1203.98809524,
+            1161.64285714,
+            1119.29761905,
+            1076.95238095,
+            1034.60714286,
+            992.26190476,
+            949.91666667,
             759.1,
             572.7,
             386.3,
@@ -146,34 +150,36 @@ def test_piecewise_linear_approximation_bottom_up(X):
 
 def test_piecewise_linear_approximation_SWAB(X):
     """Test PLA transformer."""
-    pla_string = PiecewiseLinearApproximation(5, "swab")
+    pla_string = PiecewiseLinearApproximation(
+        max_error=100_000, transformer="swab", buffer_size=100
+    )
     result_string = pla_string.fit_transform(X)
     expected = np.array(
         [
-            538.8,
-            423.1,
-            307.4,
-            191.7,
-            48.0,
-            40.5,
-            33.0,
-            25.5,
-            43.6,
-            210.2,
-            376.8,
-            543.4,
-            1276.5,
-            1227.0,
-            1177.5,
-            1128.0,
-            953.5,
-            980.5,
-            1007.5,
-            1034.5,
-            759.1,
-            572.7,
-            386.3,
-            199.9,
+            471.33333333,
+            394.0952381,
+            316.85714286,
+            239.61904762,
+            162.38095238,
+            85.14285714,
+            7.9047619,
+            -69.33333333,
+            471.33333333,
+            394.0952381,
+            316.85714286,
+            239.61904762,
+            162.38095238,
+            85.14285714,
+            7.9047619,
+            -69.33333333,
+            471.33333333,
+            394.0952381,
+            316.85714286,
+            239.61904762,
+            162.38095238,
+            85.14285714,
+            7.9047619,
+            -69.33333333,
         ]
     )
     np.testing.assert_array_almost_equal(result_string, expected)
