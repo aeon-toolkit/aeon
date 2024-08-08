9e03f51206de727eb3afecbdbc05178d2efd3ac1
Tony Bagnall <ajb@uea.ac.uk>
2024-08-08 11:52:30 +0100

    docstring

--- aeon/transformations/collection/channel_selection/_random.py
+++ aeon/transformations/collection/channel_selection/_random.py
@@ -7,6 +7,7 @@ from sklearn.utils import check_random_state
 from aeon.transformations.collection.channel_selection.base import BaseChannelSelector
 
 __maintainer__ = ["TonyBagnall"]
+__all__ = ["RandomChannelSelector"]
 
 
 class RandomChannelSelector(BaseChannelSelector):
@@ -22,6 +23,16 @@ class RandomChannelSelector(BaseChannelSelector):
     ----------
     channels_selected_ : list[int]
         List of channels selected in fit.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from aeon.transformations.collection.channel_selection._random import RandomChannelSelector # noqa
+    >>> X = np.random.rand(10, 10, 100)
+    >>> selector = RandomChannelSelector(p=0.4)
+    >>> XNew = selector.fit_transform(X)
+    >>> XNew.shape
+    (10, 4, 100)
     """
 
     _tags = {
