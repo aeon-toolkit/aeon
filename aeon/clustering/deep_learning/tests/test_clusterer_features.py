869053e2db96b46b40d0986d5cddf3ecc9d8bcdb
aadya940 <aadya@unify.ai>
2024-08-09 18:57:18 +0530

    bug fix

--- aeon/clustering/deep_learning/tests/test_clusterer_features.py
+++ aeon/clustering/deep_learning/tests/test_clusterer_features.py
@@ -9,5 +9,5 @@ def test_multi_rec_fcn():
     """Tests whether multi-rec loss works fine or not."""
     X = np.random.random((100, 5, 2))
     clst = AEFCNClusterer(n_clusters=2, n_epochs=10, loss="multi_rec")
-    _history = clst.fit(X)
-    assert _history["loss"][0] > _history["loss"][1]  # Check if loss is decreasing.
+    clst.fit(X)
+    assert clst.history["loss"][0] > clst.history["loss"][1]  # Check if loss is decreasing.
