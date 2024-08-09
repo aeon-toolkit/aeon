65f150cf8b11140662cbb2ffe18d583ae665426a
aadya940 <aadya@unify.ai>
2024-08-09 13:12:19 +0530

    num_layers => n_layers

--- aeon/networks/tests/test_dcnn.py
+++ aeon/networks/tests/test_dcnn.py
@@ -13,7 +13,7 @@ from aeon.utils.validation._dependencies import _check_soft_dependencies
     reason="Tensorflow soft dependency unavailable.",
 )
 @pytest.mark.parametrize(
-    "latent_space_dim,num_layers",
+    "latent_space_dim,n_layers",
     [
         (32, 1),
         (128, 2),
@@ -21,13 +21,13 @@ from aeon.utils.validation._dependencies import _check_soft_dependencies
         (64, 4),
     ],
 )
-def test_dcnnnetwork_init(latent_space_dim, num_layers):
+def test_dcnnnetwork_init(latent_space_dim, n_layers):
     """Test whether DCNNNetwork initializes correctly for various parameters."""
     dcnnnet = DCNNNetwork(
         latent_space_dim=latent_space_dim,
-        num_layers=num_layers,
+        n_layers=n_layers,
         activation=random.choice(["relu", "tanh"]),
-        num_filters=[random.choice([50, 25, 100]) for _ in range(num_layers)],
+        num_filters=[random.choice([50, 25, 100]) for _ in range(n_layers)],
     )
     model = dcnnnet.build_network((1000, 5))
     assert model is not None
@@ -42,7 +42,7 @@ def test_dcnnnetwork_activations(activation):
     """Test whether DCNNNetwork initializes correctly with different activations."""
     dcnnnet = DCNNNetwork(
         latent_space_dim=64,
-        num_layers=2,
+        n_layers=2,
         activation=activation,
         num_filters=[50, 50],
     )
