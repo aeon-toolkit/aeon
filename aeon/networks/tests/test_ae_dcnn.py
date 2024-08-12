5cdb447f36a4f2bd28cb2d8bf8870ba6511a0f83
aadya940 <aadya@unify.ai>
2024-08-12 13:46:07 +0530

    fix tests

--- aeon/networks/tests/test_ae_dcnn.py
+++ aeon/networks/tests/test_ae_dcnn.py
@@ -16,10 +16,10 @@ def test_default_initialization():
     assert model.latent_space_dim == 128
     assert model.kernel_size_encoder == 3
     assert model.kernel_size_decoder is None
-    assert model.num_filters_encoder is None
-    assert model.num_filters_decoder is None
-    assert model.num_layers_encoder == 4
-    assert model.num_layers_decoder == 4
+    assert model.n_filters_decoder is None
+    assert model.n_filters_decoder is None
+    assert model.n_layers_encoder == 4
+    assert model.n_layers_decoder == 4
     assert model.dilation_rate_encoder is None
     assert model.dilation_rate_decoder is None
     assert model.activation_encoder == "relu"
@@ -36,25 +36,25 @@ def test_custom_initialization():
     model = AEDCNNNetwork(
         latent_space_dim=64,
         temporal_latent_space=True,
-        num_layers_encoder=3,
-        num_layers_decoder=5,
+        n_layers_encoder=3,
+        n_layers_decoder=5,
         kernel_size_encoder=5,
         kernel_size_decoder=7,
         activation_encoder="sigmoid",
         activation_decoder="tanh",
-        num_filters_encoder=[32, 64, 128],
-        num_filters_decoder=[128, 64, 32, 16, 8],
+        n_filters_decoder=[32, 64, 128],
+        n_filters_decoder=[128, 64, 32, 16, 8],
         dilation_rate_encoder=[1, 2, 4],
         dilation_rate_decoder=[4, 2, 1, 2, 4],
     )
     model.build_network((100, 5))
     assert model.latent_space_dim == 64
-    assert model._kernel_size_encoder == [5 for _ in range(model.num_layers_encoder)]
-    assert model._kernel_size_decoder == [7 for _ in range(model.num_layers_decoder)]
-    assert model.num_filters_encoder == [32, 64, 128]
-    assert model.num_filters_decoder == [128, 64, 32, 16, 8]
-    assert model.num_layers_encoder == 3
-    assert model.num_layers_decoder == 5
+    assert model._kernel_size_encoder == [5 for _ in range(model.n_layers_encoder)]
+    assert model._kernel_size_decoder == [7 for _ in range(model.n_layers_decoder)]
+    assert model.n_filters_decoder == [32, 64, 128]
+    assert model.n_filters_decoder == [128, 64, 32, 16, 8]
+    assert model.n_layers_encoder == 3
+    assert model.n_layers_decoder == 5
     assert model.dilation_rate_encoder == [1, 2, 4]
     assert model.dilation_rate_decoder == [4, 2, 1, 2, 4]
     assert model.activation_encoder == "sigmoid"
@@ -70,22 +70,22 @@ def test_edge_case_initialization():
     """Tests edge cases are correct or not."""
     model = AEDCNNNetwork(
         latent_space_dim=0,
-        num_layers_encoder=0,
-        num_layers_decoder=0,
+        n_layers_encoder=0,
+        n_layers_decoder=0,
         kernel_size_encoder=0,
         kernel_size_decoder=0,
-        num_filters_encoder=[],
-        num_filters_decoder=[],
+        n_filters_decoder=[],
+        n_filters_decoder=[],
         dilation_rate_encoder=[],
         dilation_rate_decoder=[],
     )
     assert model.latent_space_dim == 0
     assert model.kernel_size_encoder == 0
     assert model.kernel_size_decoder == 0
-    assert model.num_filters_encoder == []
-    assert model.num_filters_decoder == []
-    assert model.num_layers_encoder == 0
-    assert model.num_layers_decoder == 0
+    assert model.n_filters_decoder == []
+    assert model.n_filters_decoder == []
+    assert model.n_layers_encoder == 0
+    assert model.n_layers_decoder == 0
     assert model.dilation_rate_encoder == []
     assert model.dilation_rate_decoder == []
 
@@ -97,12 +97,12 @@ def test_edge_case_initialization():
 def test_invalid_initialization():
     """Test if the network raises valid exceptions or not."""
     with pytest.raises(AssertionError):
-        AEDCNNNetwork(num_filters_encoder=[32, 64], num_layers_encoder=3).build_network(
+        AEDCNNNetwork(n_filters_decoder=[32, 64], n_layers_encoder=3).build_network(
             (100, 10)
         )
 
     with pytest.raises(AssertionError):
-        AEDCNNNetwork(dilation_rate_encoder=[1, 2], num_layers_encoder=3).build_network(
+        AEDCNNNetwork(dilation_rate_encoder=[1, 2], n_layers_encoder=3).build_network(
             (100, 10)
         )
 
