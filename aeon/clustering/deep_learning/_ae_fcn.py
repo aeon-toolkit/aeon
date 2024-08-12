1dac0ea7b17a6bbc66e5e60591fd2885eff9eaee
neuron283 <neuron283@users.noreply.github.com>
2024-08-12 18:27:56 +0000

    Automatic `pre-commit` fixes

--- aeon/clustering/deep_learning/_ae_fcn.py
+++ aeon/clustering/deep_learning/_ae_fcn.py
@@ -211,7 +211,7 @@ class AEFCNClusterer(BaseDeepClusterer):
         """
         import numpy as np
         import tensorflow as tf
-        
+
         if self.metrics is None:
             self._metrics = ["mean_squared_error"]
         elif isinstance(self.metrics, list):
@@ -239,7 +239,11 @@ class AEFCNClusterer(BaseDeepClusterer):
             tf.keras.optimizers.Adam() if self.optimizer is None else self.optimizer
         )
 
-        model.compile(optimizer=self.optimizer_,loss=self.loss, metrics=self._metrics,)
+        model.compile(
+            optimizer=self.optimizer_,
+            loss=self.loss,
+            metrics=self._metrics,
+        )
         return model
 
     def _fit(self, X):
