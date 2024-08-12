5f775f68b3dc332ee61e3363cce17c7ab7eba90e
aadya940 <aadya@unify.ai>
2024-08-12 14:20:07 +0530

    Fix bugs

--- aeon/networks/tests/test_ae_dcnn.py
+++ aeon/networks/tests/test_ae_dcnn.py
@@ -37,7 +37,7 @@ def test_custom_initialization():
     )
     model.build_network((100, 5))
     assert model.latent_space_dim == 64
-    assert model._kernel_size == [5 for _ in range(model.n_layers)]
+    assert model._kernel_size_encoder == [5 for _ in range(model.n_layers)]
     assert model.n_layers == 3
     assert model.dilation_rate == [1, 2, 4]
     assert model.activation == "sigmoid"
@@ -69,42 +69,10 @@ def test_edge_case_initialization():
 def test_invalid_initialization():
     """Test if the network raises valid exceptions or not."""
     with pytest.raises(AssertionError):
-        AEDCNNNetwork(n_filters_decoder=[32, 64], n_layers=3).build_network((100, 10))
+        AEDCNNNetwork(n_filters=[32, 64], n_layers=3).build_network((100, 10))
 
     with pytest.raises(AssertionError):
         AEDCNNNetwork(dilation_rate=[1, 2], n_layers=3).build_network((100, 10))
-
-
-@pytest.mark.skipif(
-    not _check_soft_dependencies(["tensorflow"], severity="none"),
-    reason="skip test if required soft dependency not available",
-)
-def test_build_network():
-    """Test call to the build_network method."""
-    model = AEDCNNNetwork()
-    input_shape = (100, 10)  # Example input shape
-    encoder, decoder = model.build_network(input_shape)
-    assert encoder is not None
-    assert decoder is not None
-    assert encoder.input_shape == (None, 100, 10)
-    assert decoder.input_shape is not None
-
-
-@pytest.mark.skipif(
-    not _check_soft_dependencies(["tensorflow"], severity="none"),
-    reason="skip test if required soft dependency not available",
-)
-def test_invalid_initialization():
-    """Test if the network raises valid exceptions or not."""
-    with pytest.raises(AssertionError):
-        AEDCNNNetwork(n_filters_decoder=[32, 64], n_layers_encoder=3).build_network(
-            (100, 10)
-        )
-
-    with pytest.raises(AssertionError):
-        AEDCNNNetwork(dilation_rate_encoder=[1, 2], n_layers_encoder=3).build_network(
-            (100, 10)
-        )
 
 
 @pytest.mark.skipif(
