a95073d125795b1b7acd58f6866278475a7d4132
Moonzyyy <47296443+Moonzyyy@users.noreply.github.com>
2024-08-13 12:19:39 +0100

    Push and removal of all series transformer

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -7,7 +7,6 @@ __all__ = [
     "ClaSPTransformer",
     "Dobin",
     "MatrixProfileSeriesTransformer",
-    "PLASeriesTransformer",
     "StatsModelsACF",
     "StatsModelsPACF",
     "ThetaTransformer",
