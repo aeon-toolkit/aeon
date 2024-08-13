0779e070b937df4a6b0bbc7c7753b2fa223a8598
Moonzyyy <47296443+Moonzyyy@users.noreply.github.com>
2024-08-13 12:17:18 +0100

    Removal of PLA, forwhatever reason it was added

--- aeon/transformations/series/__init__.py
+++ aeon/transformations/series/__init__.py
@@ -30,7 +30,6 @@ from aeon.transformations.series._clear_sky import ClearSkyTransformer
 from aeon.transformations.series._dobin import Dobin
 from aeon.transformations.series._matrix_profile import MatrixProfileSeriesTransformer
 from aeon.transformations.series._pca import PCASeriesTransformer
-from aeon.transformations.series._pla import PLASeriesTransformer
 from aeon.transformations.series._scaled_logit import ScaledLogitSeriesTransformer
 from aeon.transformations.series._theta import ThetaTransformer
 from aeon.transformations.series.base import BaseSeriesTransformer
