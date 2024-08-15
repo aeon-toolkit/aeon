7f2b59cfa5a8db461cb1ec8ab19b3a0c1d7dc750
acquayefrank <acquayefrank@gmail.com>
2024-08-15 18:06:19 +0300

    completed estimation function

--- aeon/anomaly_detection/_madrid.py
+++ aeon/anomaly_detection/_madrid.py
@@ -59,27 +59,20 @@ class MADRID(BaseAnomalyDetector):
 
         X = X.squeeze()
         len_x = len(X)
-        bfs_seed = float("-inf")  # used for first time run of dump_topk
-        k = 1
-        time_bf = 0
 
         if estimate_time:
             self._estimate_processing_time(self, len_x)
 
-        num_rows = int(
-            np.ceil((self.max_length + 1 - self.min_length) / self.step_size)
-        )
-        num_cols = len(X)
-        multilength_discord_table = np.full((num_rows, num_cols), -np.inf)
-        bsf = np.zeros((num_rows, 1))
-        bsf_loc = np.full((num_rows, 1), np.nan)
-
-        m_set = np.arange(self.min_length, self.max_length + 1, self.step_size)
-        m_pointer = int(np.ceil(len(m_set) / 2))
-        m = m_set[m_pointer]
-
-        # anomalies = np.zeros(X.shape[0], dtype=bool)
-        # return anomalies
+        if factor:
+            if (self.max_length + factor - 1) // factor < 2:
+                raise ValueError(
+                    f"MADRID cannot be executed properly because {self.min_length}/{factor} < 2, please select an option again"
+                )
+            if (self.max_length + factor - 1) // factor < 2:
+                raise ValueError(
+                    f"MADRID cannot be executed properly because {self.max_length}/{factor} < 2, please select an option again"
+                )
+        self._compute_madrid()
 
     def _contains_constant_regions(self, X, sub_sequence_length):
         bool_vec = False  # in the origianl matlab code they use 0,1 but trus, false is a better representation
@@ -209,3 +202,92 @@ class MADRID(BaseAnomalyDetector):
                 )
             ),
         )
+
+        if predicted_execution_time_1 < 10:
+            factor = 1
+            self._compute_madrid()
+        else:
+            test_data = self._test_data_madrid()
+            factor = 16
+            actual_measurement_16 = self._compute_madrid()
+            scaling_factor = actual_measurement_16 / 0.65461
+            predicted_execution_time_16_scaled = (
+                predicted_execution_time_16 * scaling_factor
+            )
+            predicted_execution_time_8_scaled = (
+                predicted_execution_time_8 * scaling_factor
+            )
+            predicted_execution_time_4_scaled = (
+                predicted_execution_time_4 * scaling_factor
+            )
+            predicted_execution_time_2_scaled = (
+                predicted_execution_time_2 * scaling_factor
+            )
+            predicted_execution_time_1_scaled = (
+                predicted_execution_time_1 * scaling_factor
+            )
+
+            print(
+                f"Predicted execution time for MADRID 1 to 16: {predicted_execution_time_16_scaled:.1f} seconds, hence potential factor choise is 16"
+            )
+            print(
+                f"Predicted execution time for MADRID 1 to 8: {predicted_execution_time_8_scaled:.1f} seconds, hence potential factor choise is 8"
+            )
+            print(
+                f"Predicted execution time for MADRID 1 to 4: {predicted_execution_time_4_scaled:.1f} seconds, hence potential factor choise is 4"
+            )
+            print(
+                f"Predicted execution time for MADRID 1 to 2: {predicted_execution_time_2_scaled:.1f} seconds, hence potential factor choise is 2"
+            )
+            print(
+                f"Predicted execution time for MADRID 1 to 1: {predicted_execution_time_1_scaled:.1f} seconds, hence potential factor choise is 1"
+            )
+
+    def _compute_madrid(self):
+        bfs_seed = float("-inf")  # used for first time run of dump_topk
+        k = 1
+        time_bf = 0
+        num_rows = int(
+            np.ceil((self.max_length + 1 - self.min_length) / self.step_size)
+        )
+        num_cols = len(X)
+        multilength_discord_table = np.full((num_rows, num_cols), -np.inf)
+        bsf = np.zeros((num_rows, 1))
+        bsf_loc = np.full((num_rows, 1), np.nan)
+
+        m_set = np.arange(self.min_length, self.max_length + 1, self.step_size)
+        m_pointer = int(np.ceil(len(m_set) / 2))
+        m = m_set[m_pointer]
+
+        # anomalies = np.zeros(X.shape[0], dtype=bool)
+        # return anomalies
+
+    def _test_data_madrid(self):
+        np.random.seed(123456789)
+
+        Fs = 10000
+        t = np.arange(0, 10 + 1 / Fs, 1 / Fs)  # Time vector
+        f_in_start = 50
+        f_in_end = 60
+        f_in = np.linspace(f_in_start, f_in_end, len(t))  # Linearly spaced frequency
+        phase_in = np.cumsum(f_in / Fs)  # Cumulative phase
+
+        y = np.sin(2 * np.pi * phase_in)  # Sinusoidal signal
+        y += np.random.randn(len(y)) / 12  # Add noise
+
+        EndOfTrain = len(y) // 2  # End of training segment
+
+        # Add medium anomaly
+        anomaly_start_1 = EndOfTrain + 1200
+        y[anomaly_start_1 : anomaly_start_1 + 64] += np.random.randn(64) / 3
+
+        # Add another anomaly
+        anomaly_start_2 = EndOfTrain + 4180
+        y[anomaly_start_2 : anomaly_start_2 + 160] += np.random.randn(160) / 4
+
+        # Add long duration anomaly
+        anomaly_start_3 = EndOfTrain + 8200
+        anomaly_end_3 = EndOfTrain + 8390
+        y[anomaly_start_3:anomaly_end_3] *= 0.5
+
+        return y
