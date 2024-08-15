062864daa0dad4a0b29c8e0ffdd0b2cd623123be
Divya Tiwari <gungun130903@gmail.com>
2024-08-15 16:13:44 +0530

    Forest

--- aeon/classification/distance_based/_proximity_forest_2.py
+++ aeon/classification/distance_based/_proximity_forest_2.py
@@ -3,13 +3,158 @@
 from typing import Type, Union
 
 import numpy as np
+from joblib import Parallel, delayed
 from numba import njit
+from numba.typed import List as NumbaList
 from sklearn.utils import check_random_state
 
 from aeon.classification.base import BaseClassifier
 from aeon.classification.distance_based._distances import distance
 from aeon.distances import minkowski_distance
-from aeon.distances._ddtw import average_of_slope
+
+
+class ProximityForest2(BaseClassifier):
+    """Proximity Forest 2.0 Classifier.
+
+    The Proximity Forest 2.0 outperforms the Proximity Forest Classifier.
+    PF 2.0 incorporates three recent advances in time series similarity measures
+    (1) computationally e cient early abandoning and pruning to speedup elastic
+    similarity computations; (2) a new elastic similarity measure, Amerced Dynamic
+    Time Warping (ADTW); and (3) cost function tuning.
+
+    Parameters
+    ----------
+    n_trees: int, default = 100
+        The number of trees, by default an ensemble of 100 trees is formed.
+    n_splitters: int, default = 5
+        The number of candidate splitters to be evaluated at each node.
+    max_depth: int, default = None
+        The maximum depth of the tree. If None, then nodes are expanded until all
+        leaves are pure or until all leaves contain less than min_samples_split samples.
+    min_samples_split: int, default = 2
+        The minimum number of samples required to split an internal node.
+    random_state : int, RandomState instance or None, default=None
+        If `int`, random_state is the seed used by the random number generator;
+        If `RandomState` instance, random_state is the random number generator;
+        If `None`, the random number generator is the `RandomState` instance used
+        by `np.random`.
+    n_jobs : int, default = 1
+        The number of parallel jobs to run for neighbors search.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details. Parameter for compatibility purposes, still unimplemented.
+    parallel_backend : str, ParallelBackendBase instance or None, default=None
+        Specify the parallelisation backend implementation in joblib, if None a 'prefer'
+        value of "threads" is used by default.
+        Valid options are "loky", "multiprocessing", "threading" or a custom backend.
+        See the joblib Parallel documentation for more details.
+
+    Notes
+    -----
+    For the C++ version, see
+    `ProximityForest2.0
+    <https://github.com/MonashTS/ProximityForest2.0>`_.
+
+    Also refer to the Proximity Forest implementation,
+    see the :class:`~aeon.classification.distance_based.ProximityForest` API.
+
+    References
+    ----------
+    .. [1] Matthieu Herrmann, Chang Wei Tan, Mahsa Salehi, Geoffrey I. Webb.
+    Proximity Forest 2.0: A new e ective and scalable similarity-based classifier
+    for time series, https://doi.org/10.48550/arXiv.2304.05800.
+
+    Examples
+    --------
+    >>> from aeon.datasets import load_unit_test
+    >>> from aeon.classification.distance_based import ProximityForest
+    >>> X_train, y_train = load_unit_test(split="train")
+    >>> X_test, y_test = load_unit_test(split="test")
+    >>> classifier = ProximityForest2(n_trees = 10, n_splitters = 3)
+    >>> classifier.fit(X_train, y_train)
+    ProximityForest2(...)
+    >>> y_pred = classifier.predict(X_test)
+    """
+
+    _tags = {
+        "capability:multivariate": False,
+        "capability:unequal_length": False,
+        "capability:multithreading": True,
+        "algorithm_type": "distance",
+        "X_inner_type": ["numpy2D"],
+    }
+
+    def __init__(
+        self,
+        n_trees=100,
+        n_splitters: int = 5,
+        max_depth: int = None,
+        min_samples_split: int = 2,
+        random_state: Union[int, Type[np.random.RandomState], None] = None,
+        n_jobs: int = 1,
+        parallel_backend=None,
+    ):
+        self.n_trees = n_trees
+        self.n_splitters = n_splitters
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.random_state = random_state
+        self.n_jobs = n_jobs
+        self.parallel_backend = parallel_backend
+        super().__init__()
+
+    def _fit(self, X, y):
+        rng = check_random_state(self.random_state)
+        self.trees_ = Parallel(
+            n_jobs=self._n_jobs, backend=self.parallel_backend, prefer="threads"
+        )(
+            delayed(_fit_tree)(
+                X,
+                y,
+                self.n_splitters,
+                self.max_depth,
+                self.min_samples_split,
+                check_random_state(rng.randint(np.iinfo(np.int32).max)),
+            )
+            for _ in range(self.n_trees)
+        )
+
+    def _predict_proba(self, X):
+        classes = list(self.classes_)
+        preds = Parallel(
+            n_jobs=self._n_jobs, backend=self.parallel_backend, prefer="threads"
+        )(delayed(_predict_tree)(tree, X) for tree in self.trees_)
+        n_cases = X.shape[0]
+        votes = np.zeros((n_cases, self.n_classes_))
+        for i in range(len(preds)):
+            predictions = np.array(
+                [classes.index(class_label) for class_label in preds[i]]
+            )
+            for j in range(n_cases):
+                votes[j, predictions[j]] += 1
+        output_probas = votes / self.n_trees
+        return output_probas
+
+    def _predict(self, X):
+        probas = self._predict_proba(X)
+        idx = np.argmax(probas, axis=1)
+        preds = np.asarray([self.classes_[x] for x in idx])
+        return preds
+
+
+def _fit_tree(X, y, n_splitters, max_depth, min_samples_split, random_state):
+    clf = ProximityTree2(
+        n_splitters=n_splitters,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
+        random_state=random_state,
+    )
+    clf.fit(X, y)
+    return clf
+
+
+def _predict_tree(tree, X):
+    return tree.predict(X)
 
 
 class _Node:
@@ -51,19 +196,12 @@ class _Node:
 class ProximityTree2(BaseClassifier):
     """Proximity Tree classifier for PF2.0.
 
-    A Proximity Tree is a decision tree classifier where the splits based on the
-    similarity of instances to chosen time series exemplars. This tree is built
-    recursively, starting from the root and progressing down to the leaf nodes.
+    It leverages ADTW and tuning the cost functions for TSC to increase accuracy,
+    together with the computational e ciency of computing these distances provided by
+    EAP.
 
-    At each internal node, a pool of candidate splitters is evaluated. Each splitter
-    consists of a set of exemplar time series for each class and a parameterized
-    similarity measure, both chosen randomly. The optimal splitter is selected based on
-    its ability to maximize the reduction in Gini impurity, measured as the difference
-    between the Gini impurity of the parent node and the weighted sum of the Gini
-    impurity of the child nodes.
-
-    Proximity Trees are particularly useful as they are the building blocks of Proximity
-    Forest, the state-of-the art distance-based classifier.
+    PF 2.0 modifies the splitters to comprise three elements, a set of class exemplars,
+    a parameterised similarity measure and a time series transform.
 
     Parameters
     ----------
@@ -82,16 +220,18 @@ class ProximityTree2(BaseClassifier):
 
     Notes
     -----
-    For the Java version, see
-    `ProximityTree
-    <https://github.com/fpetitjean/ProximityForest/blob/master/src/trees/ProximityTree.java>`_.
+    For the C++ version, see
+    `ProximityForest2.0
+    <https://github.com/MonashTS/ProximityForest2.0>`_.
+
+    Also refer to the Proximity Tree implementation,
+    see the :class:`~aeon.classification.distance_based.ProximityTree` API.
 
     References
     ----------
-    .. [1] Lucas, B., Shifaz, A., Pelletier, C., O’Neill, L., Zaidi, N., Goethals, B.,
-    Petitjean, F. and Webb, G.I., 2019. Proximity forest: an effective and scalable
-    distance-based classifier for time series. Data Mining and Knowledge Discovery,
-    33(3), pp.607-635.
+    .. [1] Matthieu Herrmann, Chang Wei Tan, Mahsa Salehi, Geoffrey I. Webb.
+    Proximity Forest 2.0: A new e ective and scalable similarity-based classifier
+    for time series, https://doi.org/10.48550/arXiv.2304.05800.
 
     Examples
     --------
@@ -99,9 +239,9 @@ class ProximityTree2(BaseClassifier):
     >>> from aeon.classification.distance_based import ProximityTree
     >>> X_train, y_train = load_unit_test(split="train")
     >>> X_test, y_test = load_unit_test(split="test")
-    >>> classifier = ProximityTree(n_splitters = 3)
+    >>> classifier = ProximityTree2(n_splitters = 3)
     >>> classifier.fit(X_train, y_train)
-    ProximityTree(...)
+    ProximityTree2(...)
     >>> y_pred = classifier.predict(X_test)
     """
 
@@ -224,14 +364,20 @@ class ProximityTree2(BaseClassifier):
             measure = list(splitter[1].keys())[0]
             transform = splitter[2]
             if transform == "first_derivative":
-                X_trans = average_of_slope(X)
-                exemplars = np.array(list(splitter[0].values()))
+                X_trans = NumbaList()
+                for i in range(len(X)):
+                    X_trans.append(first_order_derivative(X[i]))
+                exemplars = NumbaList()
+                for i in range(len(labels)):
+                    exemplars.append(
+                        first_order_derivative(list(splitter[0].values())[i])
+                    )
             else:
                 X_trans = X
                 exemplars = np.array(list(splitter[0].values()))
 
             y_subs = [[] for _ in range(len(labels))]
-            for j in range(X_trans.shape[0]):
+            for j in range(len(X_trans)):
                 min_dist = float("inf")
                 sub = None
                 for k in range(len(labels)):
@@ -317,8 +463,12 @@ class ProximityTree2(BaseClassifier):
         measure = list(splitter[1].keys())[0]
         transform = splitter[2]
         if transform == "first_derivative":
-            X_trans = average_of_slope(X)
-            exemplars = np.array(list(splitter[0].values()))
+            X_trans = NumbaList()
+            for i in range(len(X)):
+                X_trans.append(first_order_derivative(X[i]))
+            exemplars = NumbaList()
+            for i in range(len(labels)):
+                exemplars.append(first_order_derivative(list(splitter[0].values())[i]))
         else:
             X_trans = X
             exemplars = np.array(list(splitter[0].values()))
@@ -409,13 +559,16 @@ class ProximityTree2(BaseClassifier):
         if treenode._is_leaf:
             return treenode
         else:
-            _x = x.reshape((1, x.shape[0]))
             measure = list(treenode.splitter[1].keys())[0]
             branches = list(treenode.splitter[0].keys())
             transform = treenode.splitter[2]
             if transform == "first_derivative":
-                x_trans = average_of_slope(_x).reshape((-1,))
-                exemplars = np.array(list(treenode.splitter[0].values()))
+                x_trans = first_order_derivative(x)
+                exemplars = NumbaList()
+                for i in range(len(list(treenode.splitter[0].values()))):
+                    exemplars.append(
+                        first_order_derivative(list(treenode.splitter[0].values())[i])
+                    )
             else:
                 x_trans = x
                 exemplars = np.array(list(treenode.splitter[0].values()))
@@ -526,3 +679,68 @@ def _global_warp_penalty(X):
     distances = np.array([minkowski_distance(pair[0], pair[1], 2) for pair in pairs])
     penalty = np.mean(distances)
     return penalty
+
+
+@njit(cache=True, fastmath=True)
+def first_order_derivative_1d(q: np.ndarray) -> np.ndarray:
+    """Compute the first-order derivative of a 1D time series."""
+    n = len(q)
+    result = np.zeros(n)
+
+    # First element
+    result[0] = (q[1] - q[0]) / 2.0
+
+    # Middle elements
+    for j in range(1, n - 1):
+        result[j] = ((q[j] - q[j - 1]) + (q[j + 1] - q[j - 1]) / 2.0) / 2.0
+
+    # Last element
+    result[-1] = (q[-1] - q[-2]) / 2.0
+
+    return result
+
+
+@njit(cache=True, fastmath=True)
+def first_order_derivative_2d(q: np.ndarray) -> np.ndarray:
+    """Compute the first-order derivative of a 2D time series."""
+    n = q.shape[1]
+    result = np.zeros((1, n))
+
+    # First element
+    result[0, 0] = (q[0, 1] - q[0, 0]) / 2.0
+
+    # Middle elements
+    for j in range(1, n - 1):
+        result[0, j] = (
+            (q[0, j] - q[0, j - 1]) + (q[0, j + 1] - q[0, j - 1]) / 2.0
+        ) / 2.0
+
+    # Last element
+    result[0, -1] = (q[0, -1] - q[0, -2]) / 2.0
+
+    return result
+
+
+@njit(cache=True, fastmath=True)
+def first_order_derivative(q: np.ndarray) -> np.ndarray:
+    """
+    Compute the first-order derivative of the time series.
+
+    Parameters
+    ----------
+    q : np.ndarray (n_timepoints,) or (1, n_timepoints)
+        Time series to take derivative of.
+
+    Returns
+    -------
+    np.ndarray
+        The first-order derivative of q with the same shape as the input.
+    """
+    if q.ndim == 1:
+        return first_order_derivative_1d(q)
+    elif q.ndim == 2 and q.shape[0] == 1:
+        return first_order_derivative_2d(q)
+    else:
+        raise ValueError(
+            "Time series must be either (n_timepoints,) or (1, n_timepoints)."
+        )
