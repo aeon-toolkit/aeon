02339ec2ff6899abf1f679a8f07403a266297694
datedote <datadote@gmail.com>
2024-08-22 13:21:55 -0700

    add window_size tests for <= 0 and window_size == 1

--- aeon/transformations/series/tests/test_moving_average.py
+++ aeon/transformations/series/tests/test_moving_average.py
@@ -3,6 +3,7 @@
 __maintainer__ = ["Datadote"]
 
 import numpy as np
+import pytest
 
 from aeon.transformations.series._moving_average import MovingAverageTransformer
 
@@ -13,9 +14,25 @@ EXPECTED_RESULTS = [
 ]
 
 
-def test_moving_average():
-    """Test Moving Average Transformer."""
+def test_window_size_greater_than_zero():
+    """Test window sizes > 0."""
+    ma = MovingAverageTransformer(window_size=1)
+    xt = ma.fit_transform(TEST_DATA[0])
+    np.testing.assert_array_almost_equal(xt, xt, decimal=2)
+
     ma = MovingAverageTransformer(window_size=2)
     for i in range(len(TEST_DATA)):
         xt = ma.fit_transform(TEST_DATA[i])
         np.testing.assert_array_almost_equal(xt, EXPECTED_RESULTS[i], decimal=2)
+
+
+def test_window_size_equal_zero():
+    """Test window size == 0."""
+    with pytest.raises(ValueError):
+        MovingAverageTransformer(window_size=0)
+
+
+def test_window_size_less_than_zero():
+    """Test window sizes < 0."""
+    with pytest.raises(ValueError):
+        MovingAverageTransformer(window_size=-1)
