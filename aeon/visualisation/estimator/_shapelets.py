extending correct feature weighting for multiclass

aeon/visualisation/estimator/_shapelets.py
@@ -698,7 +698,6 @@ class ShapeletClassifierVisualizer:
                     class_0_coefs[:, mask] = -class_0_coefs[:, mask]
                     class_1_coefs[:, ::3] = -class_1_coefs[:, ::3]
 
-                    # Append the two modified coefs arrays along axis 0
                     coefs = np.append(class_0_coefs, class_1_coefs, axis=0)
                     warnings.warn(
                         "Shapelet importance ranking may be unreliable "
@@ -711,8 +710,21 @@ class ShapeletClassifierVisualizer:
                     )
                 else:
                     coefs = np.append(coefs, -coefs, axis=0)
-            coefs = coefs[class_id]
-
+                    coefs = coefs[class_id]
+            elif isinstance(self.estimator, RDSTClassifier):
+                coefs = coefs[class_id]
+                coefs[::3] = -coefs[::3]
+                warnings.warn(
+                    "Shapelet importance ranking may be unreliable "
+                    "when using linear classifiers with RDST. "
+                    "This is due to the interaction between argmin "
+                    "and shapelet occurrence features, which can distort "
+                    "the rankings. Consider evaluating the results carefully "
+                    "or using an alternative method.",
+                    stacklevel=1,
+                )
+            else:
+                coefs = -coefs[class_id]
         elif isinstance(classifier, (BaseForest, BaseDecisionTree)):
             coefs = classifier.feature_importances_
 