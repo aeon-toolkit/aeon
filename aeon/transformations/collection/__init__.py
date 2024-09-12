c118201a60cad98a3b8ea33cf4a91155d6c0d519
Tony Bagnall <ajb@uea.ac.uk>
2024-09-12 09:26:56 +0100

    sort out imports

--- aeon/transformations/collection/__init__.py
+++ aeon/transformations/collection/__init__.py
@@ -6,6 +6,7 @@ __all__ = [
     # transformers
     "AutocorrelationFunctionTransformer",
     "ARCoefficientTransformer",
+    "DownsampleTransformer",
     "ElbowClassSum",
     "ElbowClassPairwise",
     "DWTTransformer",
@@ -22,7 +23,7 @@ __all__ = [
     "TimeSeriesScaler",
     "TruncationTransformer",
 ]
-
+from aeon.transformations.collection._downsample import DownsampleTransformer
 from aeon.transformations.collection.acf import AutocorrelationFunctionTransformer
 from aeon.transformations.collection.ar_coefficient import ARCoefficientTransformer
 from aeon.transformations.collection.base import BaseCollectionTransformer
