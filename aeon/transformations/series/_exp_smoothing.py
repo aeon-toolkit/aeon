e12a10d1341229e38d64b4f6623ed9f6dee614ba
datedote <datadote@gmail.com>
2024-09-15 14:00:41 -0700

    change super().init__(axis=0) to super().init__(axis=1)

--- aeon/transformations/series/_exp_smoothing.py
+++ aeon/transformations/series/_exp_smoothing.py
@@ -67,7 +67,7 @@ class ExpSmoothingSeriesTransformer(BaseSeriesTransformer):
             raise ValueError(f"alpha must be in range [0, 1], got {alpha}")
         if window_size is not None and window_size <= 0:
             raise ValueError(f"window_size must be > 0, got {window_size}")
-        super().__init__(axis=0)
+        super().__init__(axis=1)
         self.alpha = alpha if window_size is None else 2.0 / (window_size + 1)
         self.window_size = window_size
 
@@ -89,7 +89,7 @@ class ExpSmoothingSeriesTransformer(BaseSeriesTransformer):
             transformed version of X
         """
         Xt = np.zeros_like(X, dtype="float")
-        Xt[0, :] = X[0, :]
-        for i in range(1, Xt.shape[0]):
-            Xt[i, :] = self.alpha * X[i, :] + (1 - self.alpha) * Xt[i - 1, :]
+        Xt[:, 0] = X[:, 0]
+        for i in range(1, Xt.shape[1]):
+            Xt[:, i] = self.alpha * X[:, i] + (1 - self.alpha) * Xt[:, i - 1]
         return Xt
