b4a7c63537c4b467f760bec45fda1c870b768c5d
Aryan <aryanpola1603@gmail.com>
2024-09-17 15:44:01 +0530

    Optional Scoring_fiunction

--- aeon/transformations/collection/channel_selection/_channel_scorer.py
+++ aeon/transformations/collection/channel_selection/_channel_scorer.py
@@ -1,7 +1,7 @@
 import math
 from typing import Dict as TypingDict
 from typing import List as TypingList
-from typing import Union
+from typing import Union, Callable
 
 import numpy as np
 from sklearn.metrics import accuracy_score, mean_squared_error
@@ -36,6 +36,11 @@ class ChannelScorer(BaseChannelSelector):
         - If `y` is of type `int` or `str`, a `RocketClassifier` is used by default.
         - If `y` is of type `float`, a `RocketRegressor` is used by default.
 
+    scoring_function : Callable, optional (default=None)
+        Scoring function used to evaluate the performance of each channel. Defaults to:
+        - `accuracy_score` if using a classifier.
+        - `mean_squared_error` if using a regressor.
+
     proportion : float, default = 0.2
         Proportion of channels to keep, rounded up to nearest integer.
 
@@ -54,10 +59,12 @@ class ChannelScorer(BaseChannelSelector):
     def __init__(
         self,
         estimator: BaseEstimator = None,
+        scoring_function: Callable = None,
         proportion: float = 0.4,
     ):
         self.proportion = proportion
         self.estimator = estimator
+        self.scoring_function = scoring_function
         super().__init__()
 
     def _fit(self, X: np.ndarray, y: Union[np.ndarray, TypingList]):
@@ -87,24 +94,24 @@ class ChannelScorer(BaseChannelSelector):
                 self.estimator_ = RocketClassifier(
                     rocket_transform="minirocket", num_kernels=5000
                 )
-                scoring_function = accuracy_score
+                scoring_function = accuracy_score if self.scoring_function is None else self.scoring_function # noqa: E501
                 score_sign = 1  # Higher accuracy is better, hence positive sign
             elif np.issubdtype(np.array(y).dtype, np.float_):
                 # Default to a regressor if y is float
                 self.estimator_ = RocketRegressor(
                     rocket_transform="minirocket", num_kernels=5000
                 )
-                scoring_function = mean_squared_error
+                scoring_function = mean_squared_error if self.scoring_function is None else self.scoring_function # noqa: E501
                 score_sign = -1  # Lower MSE is better, hence negative sign
             else:
                 raise ValueError("y must be of type int, float, or str.")
         elif isinstance(self.estimator, BaseClassifier):
             self.estimator_ = self.estimator.clone()
-            scoring_function = accuracy_score
+            scoring_function = accuracy_score if self.scoring_function is None else self.scoring_function # noqa: E501
             score_sign = 1  # Higher accuracy is better, hence positive sign
         elif isinstance(self.estimator, BaseRegressor):
             self.estimator_ = self.estimator.clone()
-            scoring_function = mean_squared_error
+            scoring_function = mean_squared_error if self.scoring_function is None else self.scoring_function # noqa: E501
             score_sign = -1  # Lower MSE is better, hence negative sign
         else:
             raise ValueError(
