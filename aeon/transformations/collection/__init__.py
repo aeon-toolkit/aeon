f11a8e6dd01344c638b2ae77847617ce83d69b56
Tony Bagnall <ajb@uea.ac.uk>
2024-09-19 11:34:35 +0100

    import

--- aeon/transformations/collection/__init__.py
+++ aeon/transformations/collection/__init__.py
@@ -18,6 +18,7 @@ __all__ = [
     "Tabularizer",
     "IntervalSegmenter",
     "RandomIntervalSegmenter",
+    "Resizer",
     "SlidingWindowSegmenter",
     "SlopeTransformer",
     "TimeSeriesScaler",
