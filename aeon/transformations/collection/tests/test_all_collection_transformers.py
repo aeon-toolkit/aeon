ff4a7e08d1a353624c9acfeb9488d60e60385d62
Aryan <aryanpola1603@gmail.com>
2024-09-22 20:00:05 +0530

    changes in test_all_collection_transformers

--- aeon/transformations/collection/tests/test_all_collection_transformers.py
+++ aeon/transformations/collection/tests/test_all_collection_transformers.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pytest
 
+from aeon.classification import DummyClassifier
 from aeon.registry import all_estimators
 from aeon.testing.data_generation import make_example_3d_numpy
 from aeon.transformations.collection.channel_selection.base import BaseChannelSelector
@@ -26,11 +27,19 @@ def test_channel_selectors(trans):
         # Must select at least one channel
         X, _ = make_example_3d_numpy(n_cases=20, n_channels=6, n_timepoints=30)
         y = np.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
-        cs = trans()
+
+        # Handle initialization for transformers that need arguments like ChannelScorer
+        if trans.__name__ == "ChannelScorer":
+            estimator = DummyClassifier()  # Provide the required estimator
+            cs = trans(estimator=estimator)  # Initialize ChannelScorer with estimator
+        else:
+            cs = trans()
+
         assert not cs.get_tag("fit_is_empty")
         cs.fit(X, y)
         assert cs.channels_selected_ is not None
         assert len(cs.channels_selected_) > 0
+
         X2 = cs.transform(X)
         assert isinstance(X2, np.ndarray)
         assert X2.ndim == 3
