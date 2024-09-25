17826d95eaa81c29d29d1888d51578ae4064a0b8
ferewi <ferdinand.rewicki@gmail.com>
2024-09-25 10:20:13 +0200

    removed implementation of the streaming mode. Might be added again after a decision about the streaming API has been made.

--- aeon/anomaly_detection/_left_stampi.py
+++ aeon/anomaly_detection/_left_stampi.py
@@ -7,7 +7,6 @@ __all__ = ["LeftSTAMPi"]
 import numpy as np
 
 from aeon.anomaly_detection.base import BaseAnomalyDetector
-from aeon.utils.validation._dependencies import _check_soft_dependencies
 from aeon.utils.windowing import reverse_windowing
 
 
@@ -15,10 +14,11 @@ class LeftSTAMPi(BaseAnomalyDetector):
     """LeftSTAMPi anomaly detector.
 
     LeftSTAMPi [1]_ calculates the left matrix profile of a time series,
-    which is the distance to the nearest neighbor of each subsequence
-    in the time series, in an incremental manner. The matrix profile is then
-    used to calculate the anomaly score for each time point. The larger the distance to
-    the nearest neighbor, the more anomalous the time point is.
+    which is the distance to the nearest neighbor of all already observed
+    subsequences (i.e. all preceding subsequences) in the time series,
+    in an incremental manner. The matrix profile is then used to calculate
+    the anomaly score for each time point. The larger the distance to the
+    nearest neighbor, the more anomalous the time point is.
 
     LeftSTAMPi supports univariate time series only.
 
@@ -35,10 +35,11 @@ class LeftSTAMPi(BaseAnomalyDetector):
 
     Parameters
     ----------
-    window_size : int
-        Size of the sliding window.
-    n_init_train: int, defaul=None
+    window_size : int, default=3
+        Size of the sliding window. Defaults to the minimal possible value of 3.
+    n_init_train: int, default=3
         The number of points used to init the matrix profile.
+        n_init_train must not be smaller than window_size.
         The discord will not be found in this segment.
     normalize : bool, default=True
         Whether to normalize the windows before computing the distance.
@@ -49,32 +50,16 @@ class LeftSTAMPi(BaseAnomalyDetector):
 
     Examples
     --------
-    Batch Processing
-        Calculate the anomaly score for the complete time series at once.
-        Internally,this is applying the incremental approach outlined below.
+    Calculate the anomaly score for the complete time series at once.
+    Internally,this is applying the incremental approach outlined below.
 
-        >>> import numpy as np # doctest: +SKIP
-        >>> from aeon.anomaly_detection import LeftSTAMPi  # doctest: +SKIP
-        >>> X = np.random.default_rng(42).random((10))  # doctest: +SKIP
-        >>> detector = LeftSTAMPi(window_size=3, n_init_train=3)  # doctest: +SKIP
-        >>> detector.fit_predict(X)  # doctest: +SKIP
-        array([0.        , 0.        , 0.        , 0.07042306, 0.15989868,
-               0.68912499, 0.75398303, 0.89696118, 0.5516023 , 0.69736132])
-
-    Stream Processing
-        Calculate the anomaly score incrementally.
-
-        >>> import numpy as np # doctest: +SKIP
-        >>> from aeon.anomaly_detection import LeftSTAMPi  # doctest: +SKIP
-        >>> X = np.random.default_rng(42).random((10))  # doctest: +SKIP
-        >>> X_train, X_stream = X[:3], X[3:]  # doctest: +SKIP
-        >>> detector = LeftSTAMPi(window_size=3)  # doctest: +SKIP
-        >>> detector.fit(X_train)  # doctest: +SKIP
-        >>> score = np.array([]) # doctest: +SKIP
-        >>> for x in X_stream: # doctest: +SKIP
-        >>>     score = detector.predict(np.array([x]))  # doctest: +SKIP
-        array([0.        , 0.        , 0.        , 0.07042306, 0.15989868,
-               0.68912499, 0.75398303, 0.89696118, 0.5516023 , 0.69736132])
+    >>> import numpy as np # doctest: +SKIP
+    >>> from aeon.anomaly_detection import LeftSTAMPi  # doctest: +SKIP
+    >>> X = np.random.default_rng(42).random((10))  # doctest: +SKIP
+    >>> detector = LeftSTAMPi(window_size=3, n_init_train=3)  # doctest: +SKIP
+    >>> detector.fit_predict(X)  # doctest: +SKIP
+    array([0.        , 0.        , 0.        , 0.07042306, 0.15989868,
+           0.68912499, 0.75398303, 0.89696118, 0.5516023 , 0.69736132])
 
     References
     ----------
@@ -91,19 +76,19 @@ class LeftSTAMPi(BaseAnomalyDetector):
         "capability:univariate": True,
         "capability:multivariate": False,
         "capability:missing_values": False,
-        "fit_is_empty": False,
+        "fit_is_empty": True,
         "python_dependencies": ["stumpy"],
     }
 
     def __init__(
         self,
         window_size: int = 3,
-        n_init_train: int = None,
+        n_init_train: int = 3,
         normalize: bool = True,
         p: float = 2.0,
         k: int = 1,
     ):
-        self.mp: np.ndarray | None = None
+        self.mp_: np.ndarray | None = None
         self.window_size = window_size
         self.n_init_train = n_init_train
         self.normalize = normalize
@@ -130,29 +115,7 @@ class LeftSTAMPi(BaseAnomalyDetector):
                 "the time series minus the window size."
             )
 
-    def _fit(self, X, y=None):
-        self.n_init_train = len(X)
-        self._check_params(X)
-
-        if X.ndim > 1:
-            X = X.squeeze()
-        self._call_stumpi(X)
-
-        return self
-
     def _predict(self, X: np.ndarray) -> np.ndarray:
-        if X.ndim > 1:
-            X = X.squeeze()
-
-        self.mp.update(X)
-
-        lmp = self.mp._left_P
-        lmp[: self.n_init_train] = 0
-        point_anomaly_scores = reverse_windowing(lmp, self.window_size)
-
-        return point_anomaly_scores
-
-    def _fit_predict(self, X, y=None) -> np.ndarray:
         self._check_params(X)
 
         if X.ndim > 1:
@@ -161,9 +124,9 @@ class LeftSTAMPi(BaseAnomalyDetector):
         self._call_stumpi(X[: self.n_init_train])
 
         for x in X[self.n_init_train :]:
-            self.mp.update(x)
+            self.mp_.update(x)
 
-        lmp = self.mp._left_P
+        lmp = self.mp_._left_P
         lmp[: self.n_init_train] = 0
 
         point_anomaly_scores = reverse_windowing(lmp, self.window_size)
@@ -171,14 +134,13 @@ class LeftSTAMPi(BaseAnomalyDetector):
         return point_anomaly_scores
 
     def _call_stumpi(self, X):
-        if _check_soft_dependencies("stumpy", severity="error"):
-            import stumpy
+        import stumpy
 
-            self.mp = stumpy.stumpi(
-                X,
-                m=self.window_size,
-                egress=False,
-                normalize=self.normalize,
-                p=self.p,
-                k=self.k,
-            )
+        self.mp_ = stumpy.stumpi(
+            X,
+            m=self.window_size,
+            egress=False,
+            normalize=self.normalize,
+            p=self.p,
+            k=self.k,
+        )
--- aeon/anomaly_detection/tests/test_left_stampi.py
+++ aeon/anomaly_detection/tests/test_left_stampi.py
@@ -1,6 +1,8 @@
 """Tests for the LaftSTAMPi class."""
 
 __maintainer__ = ["ferewi"]
+from unittest.mock import ANY, call
+
 import numpy as np
 import pytest
 
@@ -25,30 +27,7 @@ class TestLeftSTAMPi:
         model = LeftSTAMPi(window_size=4, n_init_train=5)
 
         # when
-        pred = model.fit_predict(series)
-
-        # then
-        assert pred.shape == (20,)
-        assert pred.dtype == np.float_
-        assert np.argmax(pred) == 8
-
-    def test_functional_it_allows_stream_processing_by_incremental_updates(self):
-        """Functional testing the steam mode without mocking stumpy."""
-        # given
-        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
-        series[7:10] += 3
-        init_series = series[:5]
-        streaming_data = series[5:]
-
-        model = LeftSTAMPi(
-            window_size=4,
-        )
-
-        # when
-        model = model.fit(init_series)
-        pred = np.array([])
-        for x in streaming_data:
-            pred = model.predict(np.array([x]))
+        pred = model.predict(series)
 
         # then
         assert pred.shape == (20,)
@@ -89,58 +68,7 @@ class TestLeftSTAMPi:
         ad = LeftSTAMPi(window_size=4, n_init_train=5)
 
         # when
-        pred = ad.fit_predict(series)
-
-        # then
-        stumpi_mock.has_been_called_once_with(
-            series[:5], m=4, egress=False, normalize=True, p=2
-        )
-        assert stumpi_instance.update.call_count == 15
-        assert pred.shape == (20,)
-        assert pred.dtype == np.float_
-        assert np.argmax(pred) == 8
-
-    def test_it_allows_stream_processing_by_incremental_updates(self, mocker):
-        """Unit testing the stream mode."""
-        # given
-        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
-        series[7:10] += 3
-        init_series = series[:5]
-        streaming_data = series[5:]
-
-        stumpi_mock = mocker.patch("stumpy.stumpi")
-        stumpi_instance = mocker.Mock()
-        stumpi_mock.return_value = stumpi_instance
-        stumpi_instance._left_P = np.array(
-            [
-                5.92,
-                4.43,
-                3.07,
-                1.25,
-                3.07,
-                2.82,
-                1.19,
-                0.81,
-                0.58,
-                0.75,
-                0.70,
-                0.10,
-                0.35,
-                0.47,
-                0.77,
-                0.82,
-                0.62,
-            ]
-        )
-
-        ad = LeftSTAMPi(
-            window_size=4,
-        )
-
-        # when
-        ad = ad.fit(init_series)
-        for x in streaming_data:
-            pred = ad.predict(np.array([x]))
+        pred = ad.predict(series)
 
         # then
         stumpi_mock.has_been_called_once_with(
@@ -176,7 +104,7 @@ class TestLeftSTAMPi:
             match="The window size must be at least 3 and "
             "at most the length of the time series.",
         ):
-            _ = ad.fit(series)
+            _ = ad.predict(series)
 
         ad = LeftSTAMPi(window_size=81)
         # when
@@ -185,7 +113,7 @@ class TestLeftSTAMPi:
             match="The window size must be at least 3 and "
             "at most the length of the time series.",
         ):
-            _ = ad.fit_predict(series)
+            _ = ad.predict(series)
 
     def test_it_z_normalizes_the_subsequences_by_default(self, mocker):
         """Unit testing the normalization parameter."""
@@ -194,37 +122,25 @@ class TestLeftSTAMPi:
         series[50:58] += 2
         stumpi_stub = mocker.patch("stumpy.stumpi")
 
-        ad1 = LeftSTAMPi(
-            window_size=5,
-        )
-        ad2 = LeftSTAMPi(
+        ad = LeftSTAMPi(
             window_size=5,
             n_init_train=10,
         )
 
         # when
-        _ = ad1.fit(series[:10])
-        _ = ad2.fit_predict(series)
+        _ = ad.predict(series)
 
         # then
-        assert stumpi_stub.call_count == 2
+        assert stumpi_stub.call_count == 1
         stumpi_stub.assert_has_calls(
             [
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
                     normalize=True,
-                    p=mocker.ANY,
-                    k=mocker.ANY,
-                ),
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=True,
-                    p=mocker.ANY,
-                    k=mocker.ANY,
+                    p=ANY,
+                    k=ANY,
                 ),
             ]
         )
@@ -236,33 +152,23 @@ class TestLeftSTAMPi:
         series[50:58] += 2
         stumpi_stub = mocker.patch("stumpy.stumpi")
 
-        ad1 = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False)
-        ad2 = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False)
+        ad = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False)
 
         # when
-        _ = ad1.fit(series[:10])
-        _ = ad2.fit_predict(series)
+        _ = ad.predict(series)
 
         # then
-        assert stumpi_stub.call_count == 2
+        assert stumpi_stub.call_count == 1
         stumpi_stub.assert_has_calls(
             [
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
                     normalize=False,
-                    p=mocker.ANY,
-                    k=mocker.ANY,
-                ),
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=False,
-                    p=mocker.ANY,
-                    k=mocker.ANY,
-                ),
+                    p=ANY,
+                    k=ANY,
+                )
             ]
         )
 
@@ -277,30 +183,32 @@ class TestLeftSTAMPi:
         ad2 = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False, p=1)
 
         # when
-        _ = ad1.fit(series[:10])
-        _ = ad2.fit_predict(series)
+        _ = ad1.predict(series)
+        _ = ad2.predict(series)
 
         # then
         assert stumpi_stub.call_count == 2
+
         stumpi_stub.assert_has_calls(
             [
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=mocker.ANY,
-                    p=2,
-                    k=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
+                    normalize=False,
+                    p=2.0,
+                    k=ANY,
                 ),
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=mocker.ANY,
-                    p=1,
-                    k=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
+                    normalize=False,
+                    p=1.0,
+                    k=ANY,
                 ),
-            ]
+            ],
+            any_order=True,
         )
 
     def test_the_number_of_distances_k_defaults_to_1_and_can_be_changed(self, mocker):
@@ -317,44 +225,41 @@ class TestLeftSTAMPi:
         ad2 = LeftSTAMPi(window_size=5, n_init_train=10, k=2)
 
         # when
-        _ = ad1.fit(series[:10])
-        _ = ad2.fit_predict(series)
+        _ = ad1.predict(series)
+        _ = ad2.predict(series)
 
         # then
         assert stumpi_stub.call_count == 2
         stumpi_stub.assert_has_calls(
             [
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=mocker.ANY,
-                    p=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
+                    normalize=ANY,
+                    p=ANY,
                     k=1,
                 ),
-                mocker.call(
-                    mocker.ANY,
-                    m=mocker.ANY,
-                    egress=mocker.ANY,
-                    normalize=mocker.ANY,
-                    p=mocker.ANY,
+                call(
+                    ANY,
+                    m=ANY,
+                    egress=ANY,
+                    normalize=ANY,
+                    p=ANY,
                     k=2,
                 ),
-            ]
+            ],
+            any_order=True,
         )
 
     def test_it_checks_soft_dependencies(self, mocker):
         """Unit testing the dependency check."""
         # given
-        series = make_example_1d_numpy(n_timepoints=80, random_state=42)
-        series[50:58] += 2
-        ad = LeftSTAMPi(window_size=5, n_init_train=None)
         deps_checker_stub = mocker.patch(
-            "aeon.utils.validation._dependencies._check_soft_dependencies"
+            "aeon.base._base_series._check_estimator_deps", return_value=True
         )
-
-        # when
-        ad.fit(series[:10])
+        # deps_checker_stub.return_value = True
+        ad = LeftSTAMPi(window_size=5, n_init_train=10)
 
         # then
-        deps_checker_stub.assert_called_once_with("stumpy", severity="error", obj=ad)
+        deps_checker_stub.assert_called_once_with(ad)
