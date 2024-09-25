34a87fa9d10309657a7de2ee6f4b7bff15e73a5c
ferewi <ferdinand.rewicki@gmail.com>
2024-09-25 15:43:14 +0200

    mock stumpy to run unit tests if package not installed

--- aeon/anomaly_detection/tests/test_left_stampi.py
+++ aeon/anomaly_detection/tests/test_left_stampi.py
@@ -1,6 +1,8 @@
 """Tests for the LaftSTAMPi class."""
 
 __maintainer__ = ["ferewi"]
+
+from unittest import mock
 from unittest.mock import ANY, call
 
 import numpy as np
@@ -11,56 +13,26 @@ from aeon.testing.data_generation import make_example_1d_numpy
 from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 
-@pytest.mark.skipif(
-    not _check_soft_dependencies("stumpy", severity="none"),
-    reason="required soft dependency stumpy not available",
-)
-class TestLeftSTAMPi:
-    """Unit Tests for the LeftSTAMPi class."""
+class FakeStumpi:
+    """Fake stumpy.stumpi class."""
 
-    def test_functional_it_allows_batch_processing_two_step(self):
-        """Functional testing the batch mode without mocking stumpy."""
-        # given
-        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
-        series[7:10] += 3
+    def __init__(
+        self,
+        X,
+        m,
+        egress,
+        normalize,
+        p,
+        k,
+    ):
+        self.X = X
+        self.m = m
+        self.egress = egress
+        self.normalize = normalize
+        self.p = p
+        self.k = k
 
-        model = LeftSTAMPi(window_size=4, n_init_train=5)
-
-        # when
-        model = model.fit(series[:5])
-        pred = model.predict(series[5:])
-
-        # then
-        assert pred.shape == (20,)
-        assert pred.dtype == np.float_
-        assert np.argmax(pred) == 8
-
-    def test_functional_it_allows_batch_processing_via_fit_predict(self):
-        """Functional testing the batch mode without mocking stumpy."""
-        # given
-        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
-        series[7:10] += 3
-
-        model = LeftSTAMPi(window_size=4, n_init_train=5)
-
-        # when
-        pred = model.fit_predict(series)
-
-        # then
-        assert pred.shape == (20,)
-        assert pred.dtype == np.float_
-        assert np.argmax(pred) == 8
-
-    def test_it_allows_batch_processing(self, mocker):
-        """Unit testing the batch mode."""
-        # given
-        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
-        series[7:10] += 3
-
-        stumpi_mock = mocker.patch("stumpy.stumpi")
-        stumpi_instance = mocker.Mock()
-        stumpi_mock.return_value = stumpi_instance
-        stumpi_instance._left_P = np.array(
+        self._left_P = np.array(
             [
                 5.92,
                 4.43,
@@ -82,21 +54,91 @@ class TestLeftSTAMPi:
             ]
         )
 
+        self._update_count = 0
+
+    def update(self, X):
+        """Fake update method."""
+        self._update_count += 1
+
+
+@pytest.fixture
+def mock_stumpy_pkg():
+    """Mock stumpy package."""
+    mock_pkg = mock.MagicMock()
+
+    def fake_stumpi(X, m, egress, normalize, p, k):
+        return FakeStumpi(X, m, egress, normalize, p, k)
+
+    mock_pkg.stumpi.side_effect = fake_stumpi
+    with mock.patch.dict("sys.modules", {"stumpy": mock_pkg}):
+        yield mock_pkg
+
+
+class TestLeftSTAMPi:
+    """Unit Tests for the LeftSTAMPi class."""
+
+    @pytest.mark.skipif(
+        not _check_soft_dependencies("stumpy", severity="none"),
+        reason="required soft dependency stumpy not available",
+    )
+    def test_functional_it_allows_batch_processing_two_step(self):
+        """Functional testing the batch mode without mocking stumpy."""
+        # given
+        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
+        series[7:10] += 3
+
+        model = LeftSTAMPi(window_size=4, n_init_train=5)
+
+        # when
+        model = model.fit(series[:5])
+        pred = model.predict(series[5:])
+
+        # then
+        assert pred.shape == (20,)
+        assert pred.dtype == np.float_
+        assert np.argmax(pred) == 8
+
+    @pytest.mark.skipif(
+        not _check_soft_dependencies("stumpy", severity="none"),
+        reason="required soft dependency stumpy not available",
+    )
+    def test_functional_it_allows_batch_processing_via_fit_predict(self):
+        """Functional testing the batch mode without mocking stumpy."""
+        # given
+        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
+        series[7:10] += 3
+
+        model = LeftSTAMPi(window_size=4, n_init_train=5)
+
+        # when
+        pred = model.fit_predict(series)
+
+        # then
+        assert pred.shape == (20,)
+        assert pred.dtype == np.float_
+        assert np.argmax(pred) == 8
+
+    def test_it_allows_batch_processing(self, mock_stumpy_pkg):
+        """Unit testing the batch mode."""
+        # given
+        series = make_example_1d_numpy(n_timepoints=20, random_state=42)
+        series[7:10] += 3
+
         ad = LeftSTAMPi(window_size=4, n_init_train=5)
 
         # when
         pred = ad.fit_predict(series)
 
         # then
-        stumpi_mock.has_been_called_once_with(
+        mock_stumpy_pkg.stumpi.has_been_called_once_with(
             series[:5], m=4, egress=False, normalize=True, p=2
         )
-        assert stumpi_instance.update.call_count == 15
+        assert ad.mp_._update_count == 15
         assert pred.shape == (20,)
         assert pred.dtype == np.float_
         assert np.argmax(pred) == 8
 
-    def test_window_size_defaults_to_3(self):
+    def test_window_size_defaults_to_3(self, mock_stumpy_pkg):
         """Unit testing the default window size."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
@@ -108,7 +150,7 @@ class TestLeftSTAMPi:
         # then
         assert ad.window_size == 3
 
-    def test_it_validates_window_size(self):
+    def test_it_validates_window_size(self, mock_stumpy_pkg):
         """Unit testing the validation of window size."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
@@ -132,12 +174,11 @@ class TestLeftSTAMPi:
         ):
             _ = ad.fit_predict(series)
 
-    def test_it_z_normalizes_the_subsequences_by_default(self, mocker):
+    def test_it_z_normalizes_the_subsequences_by_default(self, mock_stumpy_pkg):
         """Unit testing the normalization parameter."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
         series[50:58] += 2
-        stumpi_stub = mocker.patch("stumpy.stumpi")
 
         ad = LeftSTAMPi(
             window_size=5,
@@ -148,8 +189,8 @@ class TestLeftSTAMPi:
         _ = ad.fit_predict(series)
 
         # then
-        assert stumpi_stub.call_count == 1
-        stumpi_stub.assert_has_calls(
+        assert mock_stumpy_pkg.stumpi.call_count == 1
+        mock_stumpy_pkg.stumpi.assert_has_calls(
             [
                 call(
                     ANY,
@@ -162,12 +203,11 @@ class TestLeftSTAMPi:
             ]
         )
 
-    def test_normalization_can_be_turned_off(self, mocker):
+    def test_normalization_can_be_turned_off(self, mock_stumpy_pkg):
         """Unit testing the normalization toggle."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
         series[50:58] += 2
-        stumpi_stub = mocker.patch("stumpy.stumpi")
 
         ad = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False)
 
@@ -175,8 +215,8 @@ class TestLeftSTAMPi:
         _ = ad.fit_predict(series)
 
         # then
-        assert stumpi_stub.call_count == 1
-        stumpi_stub.assert_has_calls(
+        assert mock_stumpy_pkg.stumpi.call_count == 1
+        mock_stumpy_pkg.stumpi.assert_has_calls(
             [
                 call(
                     ANY,
@@ -189,12 +229,13 @@ class TestLeftSTAMPi:
             ]
         )
 
-    def test_the_p_norm_can_be_changed_if_normalization_is_turned_off(self, mocker):
+    def test_the_p_norm_can_be_changed_if_normalization_is_turned_off(
+        self, mock_stumpy_pkg
+    ):
         """Unit testing the pnorm parameter."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
         series[50:58] += 2
-        stumpi_stub = mocker.patch("stumpy.stumpi")
 
         ad1 = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False)
         ad2 = LeftSTAMPi(window_size=5, n_init_train=10, normalize=False, p=1)
@@ -204,9 +245,8 @@ class TestLeftSTAMPi:
         _ = ad2.fit_predict(series)
 
         # then
-        assert stumpi_stub.call_count == 2
-
-        stumpi_stub.assert_has_calls(
+        assert mock_stumpy_pkg.stumpi.call_count == 2
+        mock_stumpy_pkg.stumpi.assert_has_calls(
             [
                 call(
                     ANY,
@@ -228,12 +268,13 @@ class TestLeftSTAMPi:
             any_order=True,
         )
 
-    def test_the_number_of_distances_k_defaults_to_1_and_can_be_changed(self, mocker):
+    def test_the_number_of_distances_k_defaults_to_1_and_can_be_changed(
+        self, mock_stumpy_pkg
+    ):
         """Unit testing the k parameter."""
         # given
         series = make_example_1d_numpy(n_timepoints=80, random_state=42)
         series[50:58] += 2
-        stumpi_stub = mocker.patch("stumpy.stumpi")
 
         ad1 = LeftSTAMPi(
             window_size=5,
@@ -246,8 +287,8 @@ class TestLeftSTAMPi:
         _ = ad2.fit_predict(series)
 
         # then
-        assert stumpi_stub.call_count == 2
-        stumpi_stub.assert_has_calls(
+        assert mock_stumpy_pkg.stumpi.call_count == 2
+        mock_stumpy_pkg.stumpi.assert_has_calls(
             [
                 call(
                     ANY,
