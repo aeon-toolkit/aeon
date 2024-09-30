828d2cdf467ab5cda2e1527ab679a210fb4b7288
aadya940 <aadya@unify.ai>
2024-09-30 20:50:53 +0530

    remove from __all__

--- aeon/utils/networks/__init__.py
+++ aeon/utils/networks/__init__.py
@@ -1,8 +1,1 @@
 """Utils for tensorflow_addons."""
-
-from aeon.utils.validation._dependencies import _check_soft_dependencies
-
-if _check_soft_dependencies(["tensorflow"]):
-    from aeon.utils.networks.weight_norm import WeightNormalization
-
-    __all__ = ["WeightNormalization"]
