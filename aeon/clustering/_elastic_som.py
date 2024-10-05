769eb055a2c77dd30b9c93628ec52fd8ee00417c
chrisholder <chrisholder987@hotmail.com>
2024-10-05 15:09:34 +0100

    added testing defaults

--- aeon/clustering/_elastic_som.py
+++ aeon/clustering/_elastic_som.py
@@ -381,6 +381,32 @@ class ElasticSOM(BaseClusterer):
     def _first_center_initializer(self, X: np.ndarray) -> np.ndarray:
         return X[list(range(self.n_clusters))]
 
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        return {
+            "n_clusters": 2,
+            "init": "random",
+            "random_state": 1,
+            "num_iterations": 10,
+        }
+
 
 # SOM decay functions
 def _asymptotic_decay(learning_rate, current_iteration, max_iter):
