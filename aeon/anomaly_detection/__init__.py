22693a1f0c454b288c295ae9fc25f1b768861fc7
Divya Tiwari <gungun130903@gmail.com>
2024-10-12 09:54:35 +0530

    LSTM_AD in init

--- aeon/anomaly_detection/__init__.py
+++ aeon/anomaly_detection/__init__.py
@@ -8,11 +8,13 @@ __all__ = [
     "PyODAdapter",
     "STOMP",
     "LeftSTAMPi",
+    "LSTM_AD",
 ]
 
 from aeon.anomaly_detection._dwt_mlead import DWT_MLEAD
 from aeon.anomaly_detection._kmeans import KMeansAD
 from aeon.anomaly_detection._left_stampi import LeftSTAMPi
+from aeon.anomaly_detection._lstm_ad import LSTM_AD
 from aeon.anomaly_detection._merlin import MERLIN
 from aeon.anomaly_detection._pyodadapter import PyODAdapter
 from aeon.anomaly_detection._stomp import STOMP
