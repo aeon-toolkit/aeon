1adabbd29eb201c178541804303909ebcb2de612
Tony Bagnall <ajb@uea.ac.uk>
2024-10-14 21:49:49 +0100

    test summary

--- aeon/transformations/collection/feature_based/tests/test_summary.py
+++ aeon/transformations/collection/feature_based/tests/test_summary.py
@@ -12,14 +12,17 @@ def test_summary_features():
     assert len(f) == 7
     assert callable(f[0])
     x = SevenNumberSummaryTransformer(summary_stats="percentiles")
-    assert len(x._get_functions()) == 7
-    assert isinstance(x[0], float)
-    assert x[1] == 0.887
+    f = x._get_functions()
+    assert len(f) == 7
+    assert isinstance(f[0], float)
+    assert f[1] == 0.887
     x = SevenNumberSummaryTransformer(summary_stats="bowley")
-    assert len(x._get_functions()) == 7
-    assert callable(x[0])
-    assert x[6] == 0.9
+    f = x._get_functions()
+    assert len(f) == 7
+    assert callable(f[0])
+    assert f[6] == 0.9
     x = SevenNumberSummaryTransformer(summary_stats="tukey")
     assert len(x._get_functions()) == 7
-    with pytest.raises(ValueError, match="Invalid summary_stats"):
+    with pytest.raises(ValueError, match="Summary function input invalid"):
         x = SevenNumberSummaryTransformer(summary_stats="invalid")
+        x._get_functions()
