930d7df34cdc39f6116e80f657470d7930b97682
Tony Bagnall <ajb@uea.ac.uk>
2024-10-14 14:04:27 +0100

    sfa tests

--- aeon/transformations/collection/dictionary_based/_sfa_fast.py
+++ aeon/transformations/collection/dictionary_based/_sfa_fast.py
@@ -711,10 +711,9 @@ class SFAFast(BaseCollectionTransformer):
         params = {
             "word_length": 4,
             "window_size": 4,
-            "return_sparse": True,
+            "return_sparse": False,
             "feature_selection": "chi2",
             "alphabet_size": 2,
-            "return_sparse": True,
         }
         return params
 
