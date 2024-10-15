a7afc15760e29e0a3259bc1f1885cd1202e13e3e
MatthewMiddlehurst <m.middlehurst@uea.ac.uk>
2024-10-15 17:52:24 +0100

    split up rocket

--- aeon/classification/convolution_based/_minirocket.py
+++ aeon/classification/convolution_based/_minirocket.py
@@ -1,10 +1,10 @@
-"""RandOm Convolutional KErnel Transform (Rocket).
+"""MiniRocket classifier.
 
-Pipeline classifier using the ROCKET transformer and an sklearn classifier.
+Pipeline classifier using the MiniRocket transformer and RidgeClassifierCV classifier.
 """
 
-__maintainer__ = []
-__all__ = ["RocketClassifier"]
+__maintainer__ = ["MatthewMiddlehurst"]
+__all__ = ["MiniRocketClassifier"]
 
 import numpy as np
 from sklearn.linear_model import RidgeClassifierCV
@@ -13,35 +13,24 @@ from sklearn.preprocessing import StandardScaler
 
 from aeon.base._base import _clone_estimator
 from aeon.classification import BaseClassifier
-from aeon.transformations.collection.convolution_based import (
-    MiniRocket,
-    MultiRocket,
-    Rocket,
-)
+from aeon.transformations.collection.convolution_based import MiniRocket
 
 
-class RocketClassifier(BaseClassifier):
+class MiniRocketClassifier(BaseClassifier):
     """
-    Classifier wrapped for the Rocket transformer using RidgeClassifierCV.
+    MiniRocket transformer using RidgeClassifierCV.
 
-    This classifier simply transforms the input data using a Rocket [1,2,3]_
-    transformer, performs a Standard scaling and fits a sklearn classifier,
-    using the transformed data (default classifier is RidgeClassifierCV).
-
-    The classifier can be configured to use Rocket [1]_, MiniRocket [2]_ or
-    MultiRocket [3]_.
+    This classifier transforms the input data using the MiniRocket [1]_ transformer
+    extracting features from randomly generated kernels, performs a Standard scaling
+    and fits a sklearn classifier using the transformed data (default classifier is
+    RidgeClassifierCV).
 
     Parameters
     ----------
     num_kernels : int, default=10,000
         The number of kernels for the Rocket transform.
-    rocket_transform : str, default="rocket"
-        The type of Rocket transformer to use.
-        Valid inputs = ["rocket", "minirocket", "multirocket"].
     max_dilations_per_kernel : int, default=32
-        MiniRocket and MultiRocket only. The maximum number of dilations per kernel.
-    n_features_per_kernel : int, default=4
-        MultiRocket only. The number of features per kernel.
+        The maximum number of dilations per kernel.
     estimator : sklearn compatible classifier or None, default=None
         The estimator used. If None, a RidgeClassifierCV(alphas=np.logspace(-3, 3, 10))
         is used.
@@ -73,24 +62,12 @@ class RocketClassifier(BaseClassifier):
     classes_ : list
         The classes labels.
 
-    See Also
-    --------
-    Rocket
-        Rocket transformers are in transformations/collection.
-    RocketRegressor
-
     References
     ----------
-    .. [1] Dempster, A., Petitjean, F. and Webb, G.I., 2020. ROCKET: exceptionally fast
-        and accurate time series classification using random convolutional kernels.
-        Data Mining and Knowledge Discovery, 34(5), pp.1454-1495.
-    .. [2] Dempster, A., Schmidt, D.F. and Webb, G.I., 2021, August. Minirocket: A very
+    .. [1] Dempster, A., Schmidt, D.F. and Webb, G.I., 2021, August. Minirocket: A very
         fast (almost) deterministic transform for time series classification. In
         Proceedings of the 27th ACM SIGKDD conference on knowledge discovery & data
         mining (pp. 248-257).
-    .. [3] Tan, C.W., Dempster, A., Bergmeir, C. and Webb, G.I., 2022. MultiRocket:
-        multiple pooling operators and transformations for fast and effective time
-        series classification. Data Mining and Knowledge Discovery, 36(5), pp.1623-1646.
 
     Examples
     --------
@@ -113,18 +90,14 @@ class RocketClassifier(BaseClassifier):
     def __init__(
         self,
         num_kernels=10000,
-        rocket_transform="rocket",
         max_dilations_per_kernel=32,
-        n_features_per_kernel=4,
         estimator=None,
         class_weight=None,
         n_jobs=1,
         random_state=None,
     ):
         self.num_kernels = num_kernels
-        self.rocket_transform = rocket_transform
         self.max_dilations_per_kernel = max_dilations_per_kernel
-        self.n_features_per_kernel = n_features_per_kernel
         self.estimator = estimator
 
         self.class_weight = class_weight
@@ -155,31 +128,12 @@ class RocketClassifier(BaseClassifier):
         """
         self.n_cases_, self.n_channels_, self.n_timepoints_ = X.shape
 
-        rocket_transform = self.rocket_transform.lower()
-        if rocket_transform == "rocket":
-            self._transformer = Rocket(
-                num_kernels=self.num_kernels,
-                n_jobs=self.n_jobs,
-                random_state=self.random_state,
-            )
-        elif rocket_transform == "minirocket":
-            self._transformer = MiniRocket(
-                num_kernels=self.num_kernels,
-                max_dilations_per_kernel=self.max_dilations_per_kernel,
-                n_jobs=self.n_jobs,
-                random_state=self.random_state,
-            )
-        elif rocket_transform == "multirocket":
-            self._transformer = MultiRocket(
-                num_kernels=self.num_kernels,
-                max_dilations_per_kernel=self.max_dilations_per_kernel,
-                n_features_per_kernel=self.n_features_per_kernel,
-                n_jobs=self.n_jobs,
-                random_state=self.random_state,
-            )
-        else:
-            raise ValueError(f"Invalid Rocket transformer: {self.rocket_transform}")
-
+        self._transformer = MiniRocket(
+            num_kernels=self.num_kernels,
+            max_dilations_per_kernel=self.max_dilations_per_kernel,
+            n_jobs=self.n_jobs,
+            random_state=self.random_state,
+        )
         self._scaler = StandardScaler(with_mean=False)
         self._estimator = _clone_estimator(
             (
