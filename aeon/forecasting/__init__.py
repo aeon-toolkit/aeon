e1540788975220c9e10582c10bc683165dbfe004
Tony Bagnall <ajb@uea.ac.uk>
2024-10-25 11:27:57 +0100

    forecasting tests

--- aeon/forecasting/__init__.py
+++ aeon/forecasting/__init__.py
@@ -1,5 +1,6 @@
 """Forecasters."""
 
-__all__ = ["DummyForecaster"]
+__all__ = ["DummyForecaster", "BaseForecaster"]
 
 from aeon.forecasting._dummy import DummyForecaster
+from aeon.forecasting.base import BaseForecaster
