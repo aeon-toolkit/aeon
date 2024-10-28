6aeb5d6a2cd8c9af1aa1b83d832dfa70f5445b9d
Aadya Chinubhai <77720426+aadya940@users.noreply.github.com>
2024-10-28 22:00:00 +0530

    [ENH] Add AEDCNNNetwork (#1702)
    * Add DCNN
    
    * remove triplet loss and move utils.py to utils/networks
    
    * Add docstring and minor changes dcnn network
    
    * minor fixes
    
    * Update DCNNEncoderNetwork
    
    * add activation kwarg
    
    * minor
    
    * minor
    
    * minor fixes
    
    * update class name
    
    * minor
    
    * minor
    
    * Add temporal_latent_space kwarg
    
    * minor
    
    * minor
    
    * Add test for DCNNNetwork
    
    * minor
    
    * refactor test
    
    * add AEDCNN Network
    
    * Add tag
    
    * bug fix
    
    * bug fix
    
    * bug fixes and add tests
    
    * add pytest.skipif
    
    * update base
    
    * Update _ae_dcnn.py
    
    * pre-commit
    
    * minor
    
    * minor
    
    * use flatten instead of GMP
    
    * minor fix
    
    * typo fix
    
    * Replace Conv1D with Conv1DTranspose in the decoder
    
    * num_ => n_
    
    * fix tests
    
    * Make autoencoder symmetric only
    
    * Fix bugs
    
    * Fix bugs
    
    * some fixes
    
    * Add handling on None in kernel_size
    
    * bug fixes
    
    * Parametrize padding
    
    * add padding parameters
    
    * fixes
    
    * minor fixes
    
    * Warn if dilation_rate > 1
    
    * fix tests
    
    * Add user warning

--- aeon/networks/__init__.py
+++ aeon/networks/__init__.py
@@ -13,6 +13,7 @@ __all__ = [
     "AEFCNNetwork",
     "AEResNetNetwork",
     "LITENetwork",
+    "AEDCNNNetwork",
     "AEAttentionBiGRUNetwork",
     "AEDRNNNetwork",
     "AEBiGRUNetwork",
@@ -20,6 +21,7 @@ __all__ = [
 
 from aeon.networks._ae_abgru import AEAttentionBiGRUNetwork
 from aeon.networks._ae_bgru import AEBiGRUNetwork
+from aeon.networks._ae_dcnn import AEDCNNNetwork
 from aeon.networks._ae_drnn import AEDRNNNetwork
 from aeon.networks._ae_fcn import AEFCNNetwork
 from aeon.networks._ae_resnet import AEResNetNetwork
