bbdec7e1fa0ca58b1739ac2004cc1625baf2a261
Tony Bagnall <ajb@uea.ac.uk>
2024-11-01 21:34:23 +0000

    normalise reconciliation

--- aeon/transformations/collection/_normalise.py
+++ aeon/transformations/collection/_normalise.py
@@ -1,7 +1,5 @@
 """Normalization like z-normalization, standardization and min-max scaling."""
 
-from typing import Optional
-
 import numpy as np
 
 from aeon.transformations.collection.base import BaseCollectionTransformer
@@ -10,45 +8,19 @@ from aeon.transformations.collection.base import BaseCollectionTransformer
 class Normalise(BaseCollectionTransformer):
     """Normaliser transformer for collections.
 
-    This transformer applies different normalization techniques to time series data,
-    ensuring that the data is scaled consistently across all samples. It supports
-    methods such as z-normalization, standardization, and min-max scaling, which are
+    This transformer applies z-normalization
     applied along the timepoints axis (the last axis).
-
-    This transformer converts all input data to 3D numpy arrays of shape
-    (n_cases, n_channels, n_timepoints). Normalization is always applied along the
-    timepoints axis. For input types that are not already 3D, the transformer handles
-    the conversion internally.
-
-    Parameters
-    ----------
-    method : str, optional (default="z_norm")
-        The normalization method to apply.
-        Supported methods: "z_norm", "standardize", "min_max".
-
-        z_norm: Subtracts the mean and divides by the standard deviation
-        along the specified axis. Used to center the data and standardize its variance,
-        making it dimensionless. This is useful when comparing datasets with different
-        units.
-
-        standardize: Subtracts the mean along the specified axis. Used to center data.
-
-        min_max: Useful when you need to normalize data to a bounded range, which is
-        important for algorithms that require or perform better with inputs within a
-        specific range.
     """
 
     _tags = {
-        "X_inner_type": "numpy3D",
+        "X_inner_type": ["numpy3D", "np-list"],
         "fit_is_empty": True,
-        "capability:multivariate": True,
     }
 
-    def __init__(self, method: str = "z_norm"):
-        self.method = method
+    def __init__(self):
         super().__init__()
 
-    def _transform(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
+    def _transform(self, X, y=None) -> np.ndarray:
         """
         Transform method to apply the seleted normalisation.
 
@@ -57,37 +29,112 @@ class Normalise(BaseCollectionTransformer):
 
         Parameters
         ----------
-        X: np.ndarray
-            The input samples to transform.
-        y: array-like or list, optional
-            The class values for X (not used in this method).
+        X : np.ndarray or list
+            Collection to transform. Either a list of 2D arrays with shape
+            ``(n_channels, n_timepoints_i)`` or a single 3D array of shape
+            ``(n_cases, n_channels, n_timepoints)``.
+        y : None
+            Ignored.
 
         Returns
         -------
-        X_transformed : np.ndarray
+        X_transformed : np.ndarray or list
             The normalized data.
+
+        Example
+        -------
+        >>> from aeon.transformations.collection import Normalise
+        >>> from aeon.datasets import load_unit_test
+        >>> X, y = load_unit_test()
+
         """
-        # Axis=-1 normalises across all the timepoints in the 3D array
-        if self.method in {"z_norm"}:
-            mean = np.mean(X, axis=-1, keepdims=True)
-            std = np.std(X, axis=-1, keepdims=True)
-
-            # Handle cases where std is 0
-            std_nonzero = np.where(std == 0, 1, std)
-            return (X - mean) / std_nonzero
-
-        if self.method in {"standardize"}:
-            mean = np.mean(X, axis=-1, keepdims=True)
-
-            return X - mean
-
-        elif self.method == "min_max":
-            min_val = np.min(X, axis=-1, keepdims=True)
-            max_val = np.max(X, axis=-1, keepdims=True)
-
-            # Prevent division by zero in case min_val == max_val
-            range_nonzero = np.where(max_val == min_val, 1, max_val - min_val)
-            return (X - min_val) / range_nonzero
+        if isinstance(X, np.ndarray):
+            # Case 1: X is a single 3D array
+            mean_val = np.mean(X, axis=-1, keepdims=True)
+            std_val = np.std(X, axis=-1, keepdims=True)
+            std_val = np.where(std_val == 0, 1, std_val)  # Prevent division by zero
+            X_standardized = (X - mean_val) / std_val
+            return X_standardized
 
         else:
-            raise ValueError(f"Unknown normalization method: {self.method}")
+            # Case 2: X is a list of 2D arrays
+            Xt = []
+            for x in X:
+                mean_val = np.mean(x, axis=-1, keepdims=True)
+                std_val = np.std(x, axis=-1, keepdims=True)
+                std_val = np.where(std_val == 0, 1, std_val)  # Prevent division by zero
+                x_standardized = (x - mean_val) / std_val
+                Xt.append(x_standardized)
+            return Xt
+
+
+class MinMax(BaseCollectionTransformer):
+    """MinMax transformer for collections.
+
+    This transformer scales a collection of time series data to a specified range
+    [min,max] along the time axis. The default is to [0,1]. For multivariate,
+    it scales each channel independently.
+
+
+    Parameters
+    ----------
+    min: float, default=0
+        Minumum value of the range to scale to.
+    max: float, default=1
+        Maximum value of the range to scale to.
+    """
+
+    _tags = {
+        "X_inner_type": ["numpy3D", "np-list"],
+        "fit_is_empty": True,
+    }
+
+    def __init__(self, min: float = 0, max: float = 1):
+        self.min = min
+        self.max = max
+        super().__init__()
+
+    def _transform(self, X, y=None) -> np.ndarray:
+        """
+        Transform method to apply the MinMax normalisation.
+
+        Parameters
+        ----------
+        X: np.ndarray or list
+            Collection to transform.
+        y: None
+           Ignored.
+
+        Returns
+        -------
+        X_transformed : np.ndarray or list
+            The data transformed onto [min,max] scale.
+        """
+        if self.min > self.max:
+            raise ValueError(
+                f"min value {self.min} should be less than max value {self.max}"
+            )
+
+        if isinstance(X, np.ndarray):
+            # Case 1: Equal length, X is a single 3D array
+            min_val = np.min(X, axis=-1, keepdims=True)
+            max_val = np.max(X, axis=-1, keepdims=True)
+            range_val = np.where(
+                max_val - min_val == 0, 1, max_val - min_val
+            )  # Prevent division by zero
+            X_scaled = (X - min_val) / range_val
+            X_scaled = self.min + X_scaled * (self.max - self.min)
+            return X_scaled
+        else:
+            # Case 2: X is a list of 2D arrays
+            Xt = []
+            for x in X:
+                min_val = np.min(x, axis=-1, keepdims=True)
+                max_val = np.max(x, axis=-1, keepdims=True)
+                range_val = np.where(
+                    max_val - min_val == 0, 1, max_val - min_val
+                )  # Prevent division by zero
+                x_scaled = (x - min_val) / range_val
+                x_scaled = self.min + x_scaled * (self.max - self.min)
+                Xt.append(x_scaled)
+            return Xt
