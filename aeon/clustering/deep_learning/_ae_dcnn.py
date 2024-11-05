711924902a92dff5a0acb90880c74e5065398e6c
Aadya Chinubhai <77720426+aadya940@users.noreply.github.com>
2024-11-06 00:00:47 +0530

    Update _ae_dcnn.py

--- aeon/clustering/deep_learning/_ae_dcnn.py
+++ aeon/clustering/deep_learning/_ae_dcnn.py
@@ -316,7 +316,7 @@ class AEDCNNClusterer(BaseDeepClusterer):
         return self._estimator.score(latent_space)
 
     @classmethod
-    def get_test_params(cls, parameter_set="default"):
+    def _get_test_params(cls, parameter_set="default"):
         """Return testing parameter settings for the estimator.
 
         Parameters
