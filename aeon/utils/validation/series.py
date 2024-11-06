ec2416a0c5a7226c5c17250553538568024b9772
Tony Bagnall <ajb@uea.ac.uk>
2024-11-06 17:19:45 +0000

    remove redundant functions

--- aeon/utils/validation/series.py
+++ aeon/utils/validation/series.py
@@ -3,20 +3,13 @@
 __all__ = [
     "is_single_series",
     "is_hierarchical",
+    "check_series",
+    "is_univariate_series",
 ]
 __maintainer__ = ["TonyBagnall"]
 
 import numpy as np
 import pandas as pd
-from pandas.api.types import is_integer_dtype
-
-# We currently support the following types for input data and time index types.
-VALID_DATA_TYPES = (pd.DataFrame, pd.Series, np.ndarray)
-VALID_INDEX_TYPES = (pd.RangeIndex, pd.PeriodIndex, pd.DatetimeIndex, pd.TimedeltaIndex)
-RELATIVE_INDEX_TYPES = (pd.RangeIndex, pd.TimedeltaIndex)
-ABSOLUTE_INDEX_TYPES = (pd.RangeIndex, pd.DatetimeIndex, pd.PeriodIndex)
-assert set(RELATIVE_INDEX_TYPES).issubset(VALID_INDEX_TYPES)
-assert set(ABSOLUTE_INDEX_TYPES).issubset(VALID_INDEX_TYPES)
 
 
 def is_single_series(y):
@@ -69,29 +62,6 @@ def is_hierarchical(y):
     return False
 
 
-def _check_pd_dataframe(y):
-    # check that columns are unique
-    if not y.columns.is_unique:
-        raise ValueError(
-            f"Series in a pd.DataFrame must have unique column indices " f"{y.columns}"
-        )
-    # check whether the time index is of valid type
-    if not _is_in_valid_index_types(y.index):
-        raise ValueError(
-            f"{type(y.index)} is not supported for series, use "
-            f"one of {VALID_INDEX_TYPES} or integer index instead."
-        )
-    # check that no dtype is object
-    if "object" in y.dtypes.values:
-        raise ValueError("y should not have column of 'object' dtype")
-    # Check time index is ordered in time
-    if not y.index.is_monotonic_increasing:
-        raise ValueError(
-            f"The (time) index of a series must be sorted monotonically increasing, "
-            f"but found: {y.index}"
-        )
-
-
 def is_univariate_series(y):
     """Check if series is univariate.
 
@@ -109,8 +79,7 @@ def is_univariate_series(y):
     if isinstance(y, pd.Series):
         return True
     if isinstance(y, pd.DataFrame):
-        nvars = y.shape[1]
-        if nvars > 1:
+        if y.shape[0] > 1 and y.shape[1] > 1:
             return False
         return True
     if isinstance(y, np.ndarray):
@@ -118,72 +87,6 @@ def is_univariate_series(y):
             return False
         return True
     return False
-
-
-def _is_integer_index(x) -> bool:
-    """Check that the input is an integer pd.Index."""
-    return isinstance(x, pd.Index) and is_integer_dtype(x)
-
-
-def _is_in_valid_index_types(x) -> bool:
-    """Check that the input type belongs to the valid index types."""
-    return isinstance(x, VALID_INDEX_TYPES) or _is_integer_index(x)
-
-
-def _is_in_valid_multiindex_types(x) -> bool:
-    """Check that the input type belongs to the valid multiindex types."""
-    return isinstance(x, (pd.RangeIndex, pd.Index)) or _is_integer_index(x)
-
-
-def is_pdmultiindex_hierarchical(y):
-    """Check if the input is a pd.DataFrame with MultiIndex.
-
-    Parameters
-    ----------
-    y : pd.DataFrame
-        Input data to be checked.
-
-    Returns
-    -------
-    bool
-        True if y is pd multindex hierarchical.
-
-    """
-    if not isinstance(y, pd.DataFrame) or not isinstance(y.index, pd.MultiIndex):
-        return False
-    if not y.columns.is_unique:
-        return False
-    # check that there are precisely two index levels
-    if y.index.nlevels < 3:
-        return False
-    # check that no dtype is object
-    if "object" in y.dtypes.values:
-        return False
-
-    # check whether the time index is of valid type
-    if not _is_in_valid_index_types(y.index.get_level_values(-1)):
-        return False
-    time_obj = y.reset_index(-1).drop(y.columns, axis=1)
-    time_grp = time_obj.groupby(level=0, group_keys=True, as_index=True)
-    inst_inds = time_obj.index.unique()
-
-    # check instance index being integer or range index
-    if not _is_in_valid_multiindex_types(inst_inds):
-        return False
-
-    if pd.__version__ < "1.5.0":
-        # Earlier versions of pandas are very slow for this type of operation.
-        montonic_list = [y.loc[i].index.is_monotonic for i in inst_inds]
-        time_is_monotonic = len([i for i in montonic_list if i is False]) == 0
-    else:
-        timedelta_by_grp = (
-            time_grp.diff().groupby(level=0, group_keys=True, as_index=True).nunique()
-        )
-        timedelta_unique = timedelta_by_grp.iloc[:, 0].unique()
-        time_is_monotonic = all(timedelta_unique >= 0)
-    if not time_is_monotonic:
-        return False
-    return True
 
 
 def check_series(y):
