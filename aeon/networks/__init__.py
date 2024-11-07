6badaecf36e0e305d342d6321b8c9cb7f042d376
hadifawaz1999 <hadifawaz2291999@gmail.com>
2024-11-07 19:53:36 +0100

    add to init

--- aeon/networks/__init__.py
+++ aeon/networks/__init__.py
@@ -18,6 +18,7 @@ __all__ = [
     "AEAttentionBiGRUNetwork",
     "AEDRNNNetwork",
     "AEBiGRUNetwork",
+    "DisjointCNNNetwork",
 ]
 from aeon.networks._ae_abgru import AEAttentionBiGRUNetwork
 from aeon.networks._ae_bgru import AEBiGRUNetwork
@@ -27,6 +28,7 @@ from aeon.networks._ae_fcn import AEFCNNetwork
 from aeon.networks._ae_resnet import AEResNetNetwork
 from aeon.networks._cnn import TimeCNNNetwork
 from aeon.networks._dcnn import DCNNNetwork
+from aeon.networks._disjoint_cnn import DisjointCNNNetwork
 from aeon.networks._encoder import EncoderNetwork
 from aeon.networks._fcn import FCNNetwork
 from aeon.networks._inception import InceptionNetwork
