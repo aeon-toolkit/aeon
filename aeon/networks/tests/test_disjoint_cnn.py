edd27e7589375811d6bed71a4f4d6c8c2039dec8
hadifawaz1999 <hadifawaz2291999@gmail.com>
2024-11-07 19:54:01 +0100

    test input list for kernel init

---
+++ aeon/networks/tests/test_disjoint_cnn.py
@@ -0,0 +1,20 @@
+"""Tests for the DisjointCNN Network."""
+
+import pytest
+
+from aeon.networks import DisjointCNNNetwork
+from aeon.utils.validation._dependencies import _check_soft_dependencies
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies(["tensorflow"], severity="none"),
+    reason="Tensorflow soft dependency unavailable.",
+)
+def test_disjoint_cnn_netowkr_kernel_initializer():
+    """Test DisjointCNN for different kernel_initializer per layer."""
+    input_layer, output_layer = DisjointCNNNetwork(
+        n_layers=2, kernel_initializer=["he_uniform", "glorot_uniform"]
+    )
+
+    assert len(output_layer.shape) == 3
+    assert len(input_layer.shape) == 3
