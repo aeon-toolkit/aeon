0bdd956aeb182dc8f4c2cdf3cfdab5282732aeb5
hadifawaz1999 <hadifawaz2291999@gmail.com>
2024-11-07 16:48:56 +0100

    precommit

--- aeon/clustering/deep_learning/tests/test_clusterer_features.py
+++ aeon/clustering/deep_learning/tests/test_clusterer_features.py
@@ -4,8 +4,8 @@ import numpy as np
 import pytest
 
 from aeon.clustering.deep_learning import AEFCNClusterer, AEResNetClusterer
-from aeon.utils.validation._dependencies import _check_soft_dependencies
 from aeon.clustering.dummy import DummyClusterer
+from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 
 @pytest.mark.skipif(
@@ -16,16 +16,16 @@ def test_multi_rec_fcn():
     """Tests whether multi-rec loss works fine or not."""
     X = np.random.random((100, 5, 2))
     clst = AEFCNClusterer(
-        n_epochs = 5,
-        batch_size = 4,
-        use_bias = False,
-        n_layers= 2,
-        n_filters = [2,2],
-        kernel_size = [2,2],
-        padding = "same",
-        strides = 1,
-        latent_space_dim = 4,
-        estimator= DummyClusterer(n_clusters=2),
+        n_epochs=5,
+        batch_size=4,
+        use_bias=False,
+        n_layers=2,
+        n_filters=[2, 2],
+        kernel_size=[2, 2],
+        padding="same",
+        strides=1,
+        latent_space_dim=4,
+        estimator=DummyClusterer(n_clusters=2),
         loss="multi_rec",
     )
     clst.fit(X)
@@ -33,14 +33,14 @@ def test_multi_rec_fcn():
         clst.history["loss"][0] > clst.history["loss"][4]
     )  # Check if loss is decreasing.
     clst = AEResNetClusterer(
-        n_epochs = 5,
-        batch_size = 4,
-        n_residual_blocks = 2,
-        n_conv_per_residual_block = 1,
-        n_filters = [2,2],
-        kernel_size = 2,
-        use_bias = False,
-        estimator = DummyClusterer(n_clusters=2),
+        n_epochs=5,
+        batch_size=4,
+        n_residual_blocks=2,
+        n_conv_per_residual_block=1,
+        n_filters=[2, 2],
+        kernel_size=2,
+        use_bias=False,
+        estimator=DummyClusterer(n_clusters=2),
         loss="multi_rec",
     )
     clst.fit(X)
