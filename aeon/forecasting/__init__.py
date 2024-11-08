e67fb620a584a11d464048bc4de230009480ba1f
Alex Banwell <arb1g19@soton.ac.uk>
2024-11-08 07:45:13 +0000

    Add fully functional ETS Forecaster. Modify base to not set default y in forecast. Update tests for ETS Forecaster. Add script to verify ETS Forecaster against statsforecast module using a large number of random parameter inputs.

--- aeon/forecasting/__init__.py
+++ aeon/forecasting/__init__.py
@@ -1,8 +1,14 @@
 """Forecasters."""
 
-__all__ = ["DummyForecaster", "BaseForecaster", "RegressionForecaster", "ETSForecaster"]
+__all__ = [
+    "DummyForecaster",
+    "BaseForecaster",
+    "RegressionForecaster",
+    "ETSForecaster",
+    "ModelType",
+]
 
 from aeon.forecasting._dummy import DummyForecaster
-from aeon.forecasting._ets import ETSForecaster
+from aeon.forecasting._ets import ETSForecaster, ModelType
 from aeon.forecasting._regression import RegressionForecaster
 from aeon.forecasting.base import BaseForecaster
--- aeon/forecasting/tests/test_ets.py
+++ aeon/forecasting/tests/test_ets.py
@@ -1,16 +1,80 @@
 """Test ETS."""
 
+__maintainer__ = []
+__all__ = []
+
 import numpy as np
 
-from aeon.forecasting import ETSForecaster
+from aeon.forecasting import ETSForecaster, ModelType
 
 
-def test_ets_forecaster():
+def test_ets_forecaster_additive():
     """TestETSForecaster."""
     data = np.array(
         [3, 10, 12, 13, 12, 10, 12, 3, 10, 12, 13, 12, 10, 12]
     )  # Sample seasonal data
-    forecaster = ETSForecaster(alpha=0.5, beta=0.3, gamma=0.4, season_length=4)
+    forecaster = ETSForecaster(
+        alpha=0.5,
+        beta=0.3,
+        gamma=0.4,
+        phi=1,
+        horizon=1,
+        model_type=ModelType(1, 1, 1, 4),
+    )
     forecaster.fit(data)
     p = forecaster.predict()
-    assert np.isclose(p, 15.85174501127)
+    assert np.isclose(p, 9.191190608800001)
+
+
+def test_ets_forecaster_mult_error():
+    """TestETSForecaster."""
+    data = np.array(
+        [3, 10, 12, 13, 12, 10, 12, 3, 10, 12, 13, 12, 10, 12]
+    )  # Sample seasonal data
+    forecaster = ETSForecaster(
+        alpha=0.7,
+        beta=0.6,
+        gamma=0.1,
+        phi=0.97,
+        horizon=1,
+        model_type=ModelType(2, 1, 1, 4),
+    )
+    forecaster.fit(data)
+    p = forecaster.predict()
+    assert np.isclose(p, 16.20176819429869)
+
+
+def test_ets_forecaster_mult_compnents():
+    """TestETSForecaster."""
+    data = np.array(
+        [3, 10, 12, 13, 12, 10, 12, 3, 10, 12, 13, 12, 10, 12]
+    )  # Sample seasonal data
+    forecaster = ETSForecaster(
+        alpha=0.4,
+        beta=0.2,
+        gamma=0.5,
+        phi=0.8,
+        horizon=1,
+        model_type=ModelType(1, 2, 2, 4),
+    )
+    forecaster.fit(data)
+    p = forecaster.predict()
+    assert np.isclose(p, 12.301259229712382)
+
+
+def test_ets_forecaster_multiplicative():
+    """TestETSForecaster."""
+    data = np.array(
+        [3, 10, 12, 13, 12, 10, 12, 3, 10, 12, 13, 12, 10, 12]
+    )  # Sample seasonal data
+    forecaster = ETSForecaster(
+        alpha=0.7,
+        beta=0.5,
+        gamma=0.2,
+        phi=0.85,
+        horizon=1,
+        model_type=ModelType(2, 2, 2, 4),
+    )
+    forecaster.fit(data)
+    p = forecaster.predict()
+    assert np.isclose(p, 16.811888294476528)
