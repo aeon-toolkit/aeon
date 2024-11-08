000cd590754458680b33034a6eea284069a1fb46
Tony Bagnall <ajb@uea.ac.uk>
2024-11-08 15:04:18 +0000

    imports

--- aeon/benchmarking/performance_metrics/__init__.py
+++ aeon/benchmarking/performance_metrics/__init__.py
@@ -1,1 +1,14 @@
 """Metrics for assessing model performance."""
+
+__all__ = [
+    "check_friedman",
+    "nemenyi_test",
+    "wilcoxon_test",
+    "clustering_accuracy_score",
+]
+from aeon.benchmarking.performance_metrics.clustering import clustering_accuracy_score
+from aeon.benchmarking.performance_metrics.stats import (
+    check_friedman,
+    nemenyi_test,
+    wilcoxon_test,
+)
