40618f77a9841008f46ac3e009e1eed1075f87c5
aadya940 <aadya940@users.noreply.github.com>
2024-11-09 13:35:13 +0000

    Automatic `pre-commit` fixes

--- aeon/clustering/deep_learning/__init__.py
+++ aeon/clustering/deep_learning/__init__.py
@@ -10,6 +10,5 @@ __all__ = [
 from aeon.clustering.deep_learning._ae_bgru import AEBiGRUClusterer
 from aeon.clustering.deep_learning._ae_drnn import AEDRNNClusterer
 from aeon.clustering.deep_learning._ae_fcn import AEFCNClusterer
-from aeon.clustering.deep_learning._ae_drnn import AEDRNNClusterer
 from aeon.clustering.deep_learning._ae_resnet import AEResNetClusterer
 from aeon.clustering.deep_learning.base import BaseDeepClusterer
