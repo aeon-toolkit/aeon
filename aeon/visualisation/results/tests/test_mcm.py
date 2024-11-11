5a362cd004b96bfc483568c2e9fc48aaf3d92212
Tony Bagnall <ajb@uea.ac.uk>
2024-11-11 21:55:46 +0000

    basic tests

--- aeon/visualisation/results/tests/test_mcm.py
+++ aeon/visualisation/results/tests/test_mcm.py
@@ -9,14 +9,15 @@ import pytest
 from aeon.utils.validation._dependencies import _check_soft_dependencies
 from aeon.visualisation.results._mcm import create_multi_comparison_matrix
 
-if not _check_soft_dependencies("matplotlib", severity="none"):
-    pytest.skip(allow_module_level=True)
-else:
-    import matplotlib.pyplot as plt
 
-
+@pytest.mark.skipif(
+    not _check_soft_dependencies("matplotlib", severity="none"),
+    reason="skip test if required soft dependency not available",
+)
 def test_mcm():
     """Test the multi-comparison-matrix visualisation."""
+    import matplotlib.pyplot as plt
+
     df = pd.DataFrame(
         np.random.rand(10, 3),  # 10 rows, 3 columns of random numbers
         columns=["Classifier1", "Classifier2", "Classifier3"],
@@ -25,8 +26,14 @@ def test_mcm():
     assert isinstance(fig, plt.Figure)
 
 
+@pytest.mark.skipif(
+    not _check_soft_dependencies("matplotlib", severity="none"),
+    reason="skip test if required soft dependency not available",
+)
 def test_mcm_file_save():
     """Test file save  in different formats."""
+    import matplotlib.pyplot as plt
+
     df = pd.DataFrame(
         np.random.rand(10, 3),  # 10 rows, 3 columns of random numbers
         columns=["Classifier1", "Classifier2", "Classifier3"],
