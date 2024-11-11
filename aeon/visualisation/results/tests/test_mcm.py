ad819a1fabd3ee2ec3703891721b112ef37353ba
Tony Bagnall <ajb@uea.ac.uk>
2024-11-11 19:45:24 +0000

    port in mcm

---
+++ aeon/visualisation/results/tests/test_mcm.py
@@ -0,0 +1,9 @@
+"""Test the multi-comparison-matrix visualisation."""
+
+from aeon.visualisation.results._mcm import create_multi_comparison_matrix
+
+
+def test_mcm():
+    """Test the multi-comparison-matrix visualisation."""
+    create_multi_comparison_matrix()
+    assert True
