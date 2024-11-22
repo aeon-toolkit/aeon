ef18e29aca4578cda32b7f581f82a8f4b0322b71
Ali El Hadi ISMAIL FAWAZ <54309336+hadifawaz1999@users.noreply.github.com>
2024-11-22 20:28:46 +0100

    [MNT] Deprecation in deep clustering module (#2380)
    * deprecate
    
    * rerun tests

--- aeon/clustering/deep_learning/_ae_abgru.py
+++ aeon/clustering/deep_learning/_ae_abgru.py
@@ -20,10 +20,6 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
 
     Parameters
     ----------
-    clustering_algorithm : str, default="deprecated"
-        Use 'estimator' parameter instead.
-    clustering_params : dict, default=None
-        Use 'estimator' parameter instead.
     estimator : aeon clusterer, default=None
         An aeon estimator to be built using the transformed data.
         Defaults to aeon TimeSeriesKMeans() with euclidean distance
@@ -105,8 +101,6 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
     def __init__(
         self,
         estimator=None,
-        clustering_algorithm="deprecated",
-        clustering_params=None,
         latent_space_dim=128,
         n_layers_encoder=2,
         n_layers_decoder=2,
@@ -150,8 +144,6 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
         self.random_state = random_state
 
         super().__init__(
-            clustering_algorithm=clustering_algorithm,
-            clustering_params=clustering_params,
             estimator=estimator,
             batch_size=batch_size,
             last_file_name=last_file_name,
