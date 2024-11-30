5aca568e03ccfc3e01ecd683b5057c14f098440f
dan <dr1n22@soton.ac.uk>
2024-11-30 00:09:36 +0000

    Added pseudocode functions to AutoPlaitSegmenter

--- aeon/segmentation/_autoplait.py
+++ aeon/segmentation/_autoplait.py
@@ -3,6 +3,8 @@
 __maintainer__ = []
 __all__ = ["AutoPlaitSegmenter"]
 
+import math
+
 import numpy as np
 
 from aeon.segmentation import BaseSegmenter
@@ -10,13 +12,90 @@ from aeon.segmentation import BaseSegmenter
 
 class AutoPlaitSegmenter(BaseSegmenter):
     """
-        DOCSTRING
-        """
+    DOCSTRING
+    """
 
-    _tags = {}
+    _tags = {
+        "returns_dense": True,
+    }
 
     def __init__(self):
         super().__init__(axis=0)
 
     def _predict(self, X) -> np.ndarray:
-        return
+        results = _autoplait(X)
+        self.n_segments = results[0]
+        cut_points = [e[1] for e in results[2]]
+        return np.array(cut_points)
+
+def _cut_point_search(X, regime1, regime2, transition_matrix):
+    m1, m2 = 0, 0
+    s1, s2 = [], []
+    l1, l2 = [], [] # Candidate cut point sets
+    for t in len(X):
+        # TODO: Compute likelihoods for all states of regime1 and regime2
+        # TODO: Update candidate cut point sets for both regimes
+        pass
+    l_best = [] # TODO: best_cut_point_set(l1, l2)
+    t_s = 1
+    for idx, l_i in enumerate(l_best):
+        s_i = (t_s, l_i)
+        if idx % 2 == 1:
+            s1.append(s_i)
+            m1 += 1
+        else:
+            s2.append(s_i)
+            m2 += 1
+        t_s = l_i
+    return m1, m2, s1, s2
+
+
+def _regime_split(X):
+    m1, m2, s1, s2 = 0, 0, [], [] # Initialise outputs
+    t1, t2, d = [], [], [] # TODO: Initialise models
+    current_cost = math.inf
+    while c := _cost(X, t1, t2, d) < current_cost:
+        current_cost = c
+        m1, m2, s1, s2 = _cut_point_search(X, t1, t2, d)
+        # TODO: Update model parameters t1, t2
+        # TODO: Update regime transitions d
+    return m1, m2, s1, s2, t1, t2, d
+
+def _cost(X, regime1, regime2=None, transition_matrix=None):
+    # TODO: Cost functions
+    if regime2 is not None:
+        # TODO: Cost of regime pair
+        pass
+    else:
+        # TODO: Cost of single regime
+        pass
+    return 0
+
+def _autoplait(X):
+    t_params, f = [], []
+    d = []
+
+    q = [] # Stack
+    s = [] # Output segment set
+    m = 0 # Output number of segments
+    r = 0 # Output number of regimes
+    s.append((1,len(X))) # 1 initial segment; the entire TS
+    m_0 = 1 # There is 1 initial segment
+
+    t0 = [] # TODO: Estimate t0 of s_0
+    q.append((m_0, s[0], t0)) # Push onto stack
+    while q: # While the stack is not empty
+        m_i, s_i, t_i = q.pop()
+        m1, m2, s1, s2, t1, t2, d = _regime_split(X)
+        if _cost(X, t1, t2, d) < _cost(X, t_i):
+            q.append((m1, s1, t1))
+            q.append((m2, s2, t2))
+        else:
+            s.append(s_i)
+            t_params.append(t_i)
+            r += 1
+            # TODO: Update regime transitions d
+            # TODO: segment membership f
+            m += m_i
+    t_params.append(d) # Add the regime transition matrix
+    return m, r, s, t_params, f
