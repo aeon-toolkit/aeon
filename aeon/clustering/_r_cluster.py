9bda6fb9b0cf03e7158799a016d8620d812ac59b
Ramana Raja <83065061+Ramana-Raja@users.noreply.github.com>
2024-12-01 02:27:17 +0530

    Update _r_cluster.py to fix none reproducibility

--- aeon/clustering/_r_cluster.py
+++ aeon/clustering/_r_cluster.py
@@ -360,6 +360,7 @@ class RCluster(BaseClusterer):
         )
 
     def _get_parameterised_data(self, X):
+        np.random.seed(42)
         _, n_channels, n_timepoints = X.shape
         X = X.astype(np.float32)
 
