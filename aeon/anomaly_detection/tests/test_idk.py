cb7992e579b8e9ce61bd970856a7dac13c6db53a
Ramana Raja <83065061+Ramana-Raja@users.noreply.github.com>
2024-12-31 03:12:02 +0400

    Updated test.py

--- aeon/anomaly_detection/tests/test_idk.py
+++ aeon/anomaly_detection/tests/test_idk.py
@@ -2,11 +2,9 @@
 
 import numpy as np
 import pytest
-from sklearn.utils import check_random_state
-
 from aeon.anomaly_detection import IDK
 from aeon.utils.validation._dependencies import _check_estimator_deps
-
+from sklearn.utils import check_random_state
 
 @pytest.mark.skipif(
     not _check_estimator_deps(IDK, severity="none"),
@@ -18,13 +16,11 @@ def test_idk_univariate():
     series = rng.normal(size=(100,))
     series[50:58] -= 10
 
-    ad = IDK(psi1=8, psi2=2, width=1, rng=rng)
+    ad = IDK(psi1=8, psi2=2, width=1, random_state=42)
     pred = ad.fit_predict(series)
     assert pred.shape == (100,)
     assert pred.dtype == np.float64
     assert 50 <= np.argmax(pred) <= 58
-
-
 @pytest.mark.skipif(
     not _check_estimator_deps(IDK, severity="none"),
     reason="skip test if required soft dependencies not available",
@@ -34,7 +30,7 @@ def test_idk_univariate_sliding():
     rng = check_random_state(seed=2)
     series = rng.normal(size=(100,))
     series[50:58] -= 10
-    ad = IDK(psi1=16, psi2=4, width=10, sliding=True, rng=rng)
+    ad = IDK(psi1=16, psi2=4, width=10, sliding=True, random_state=1)
     pred = ad.fit_predict(series)
     assert pred.shape == (91,)
     assert pred.dtype == np.float64
