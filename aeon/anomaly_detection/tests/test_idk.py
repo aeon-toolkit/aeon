a2f4bf0d6ff1355b2acada598b9d04b637ab294e
Ramana Raja <83065061+Ramana-Raja@users.noreply.github.com>
2024-12-31 21:17:29 +0400

    Updated test_idk.py to make sliding and non sliding into 1

--- aeon/anomaly_detection/tests/test_idk.py
+++ aeon/anomaly_detection/tests/test_idk.py
@@ -2,16 +2,12 @@
 
 import numpy as np
 import pytest
-from sklearn.utils import check_random_state
-
 from aeon.anomaly_detection import IDK
 from aeon.utils.validation._dependencies import _check_estimator_deps
-
+from sklearn.utils import check_random_state
 
 @pytest.mark.skipif(
-    not _check_estimator_deps(
-        IDK(psi1=8, psi2=2, width=1, random_state=42), severity="none"
-    ),
+    not _check_estimator_deps(IDK(psi1=8, psi2=2, width=1, random_state=42), severity="none"),
     reason="skip test if required soft dependencies not available",
 )
 def test_idk_univariate():
@@ -22,24 +18,11 @@ def test_idk_univariate():
 
     ad = IDK(psi1=8, psi2=2, width=1, random_state=42)
     pred = ad.fit_predict(series)
+    ad_sliding = IDK(psi1=16, psi2=4, width=10, sliding=True, random_state=1)
+    pred_sliding = ad_sliding.fit_predict(series)
     assert pred.shape == (100,)
     assert pred.dtype == np.float64
     assert 50 <= np.argmax(pred) <= 58
-
-
-@pytest.mark.skipif(
-    not _check_estimator_deps(
-        IDK(psi1=16, psi2=4, width=10, sliding=True, random_state=1), severity="none"
-    ),
-    reason="skip test if required soft dependencies not available",
-)
-def test_idk_univariate_sliding():
-    """Test IDK with sliding on univariate data."""
-    rng = check_random_state(seed=2)
-    series = rng.normal(size=(100,))
-    series[50:58] -= 10
-    ad = IDK(psi1=16, psi2=4, width=10, sliding=True, random_state=1)
-    pred = ad.fit_predict(series)
-    assert pred.shape == (91,)
-    assert pred.dtype == np.float64
-    assert 50 <= np.argmax(pred) <= 68
+    assert pred_sliding.shape == (91,)
+    assert pred_sliding.dtype == np.float64
+    assert 50 <= np.argmax(pred_sliding) <= 68
