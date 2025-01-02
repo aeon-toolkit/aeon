da2758c4fbfb1c9405b4aff1fa12fa14b94522ba
baraline <antoine.guillaume45@gmail.com>
2025-01-02 16:12:53 +0100

    more debug of subsequence tests

--- aeon/similarity_search/subsequence_search/base.py
+++ aeon/similarity_search/subsequence_search/base.py
@@ -357,6 +357,29 @@ class BaseSubsequenceSearch(BaseSimilaritySearch):
         exclusion_factor: Optional[float] = 2.0,
     ): ...
 
+    @classmethod
+    def _get_test_params(cls, parameter_set: str = "default") -> dict:
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            For classifiers, a "default" set of parameters should be provided for
+            general testing, and a "results_comparison" set for comparing against
+            previously recorded results if the general set does not produce suitable
+            probabilities to compare against.
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+        """
+        return {"length": 3}
+
 
 class BaseMatrixProfile(BaseSubsequenceSearch):
     """Base class for Matrix Profile methods using a length parameter."""
