3daa7a5a827786d8f8897487018330e679c21ae7
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2025-01-09 22:18:53 +0000

    Small refactor

--- aeon/segmentation/_autoplait.py
+++ aeon/segmentation/_autoplait.py
@@ -3,36 +3,20 @@
 __maintainer__ = []
 __all__ = ["AutoPlaitSegmenter"]
 
-import math
-
 import numpy as np
 
 from aeon.segmentation import BaseSegmenter
 
 
-class AutoPlaitSegmenter(BaseSegmenter):
-    """
-    DOCSTRING
-    """
+class _AutoPlait:
 
-    _tags = {
-        "returns_dense": True,
-    }
-
-    _X = None
-    _autoplait_results = None
+    _complete_parameters = {}
 
     def __init__(self):
-        super().__init__(axis=0)
+        pass
 
-    def _predict(self, X) -> np.ndarray:
-        self._autoplait(X)
-        cut_points = [e[1] for e in self._autoplait_results[2]]
-        return np.array(cut_points)
-
-    def complete_parameters(self):
-        if self._check_is_fitted():
-            return self._autoplait_results
+    def predict(self, X):
+        self._complete_parameters = self._autoplait(X)
 
     def _autoplait(self, X):
         t_params, f = [], []
@@ -61,10 +45,61 @@ class AutoPlaitSegmenter(BaseSegmenter):
                 # TODO: segment membership f
                 m += m_i
         t_params.append(d)  # Add the regime transition matrix
-        self._X = X
+        results = {
+            "num_segments":m,
+            "num_regimes":r,
+            "segment_set":s,
+            "regime_parameters":t_params,
+            "segment_memberships":f,
+        }
+        return results
+
+    def complete_parameters(self, as_list:bool = False):
+        if as_list:
+            return list(self._complete_parameters.values())
+        else:
+            return self._complete_parameters
+
+
+class AutoPlaitSegmenter(BaseSegmenter):
+    """
+    DOCSTRING
+    """
+
+    _tags = {
+        "returns_dense": True,
+        "fit_is_empty": True,
+    }
+
+    _X = None
+    _autoplait_results = None
+
+    def __init__(self):
+        self._autoplait = _AutoPlait()
+        super().__init__(axis=0)
+
+    def _predict(self, X) -> np.ndarray:
+        """Perform AutoPlait segmentation.
+
+        Parameters
+        ----------
+        X: np.ndarray
+            2D time series shape (n_timepoints, n_channels).
+
+        Returns
+        -------
+        y_pred : np.ndarray
+            DESC
+        """
+        self._autoplait.predict(X)
         self.is_fitted = True
-        self._autoplait_results = (m, r, s, t_params, f)
-        return self._autoplait_results
+        params = self._autoplait.complete_parameters()
+        cut_points = [e[1] for e in params["segment_set"]]
+        return np.array(cut_points)
+
+    def complete_parameters(self, as_list:bool = False):
+        self._check_is_fitted()
+        return self._autoplait.complete_parameters(as_list)
 
 def _cut_point_search(X, regime1, regime2, transition_matrix):
     m1, m2 = 0, 0
@@ -74,7 +109,7 @@ def _cut_point_search(X, regime1, regime2, transition_matrix):
         # TODO: Compute likelihoods for all states of regime1 and regime2
         # TODO: Update candidate cut point sets for both regimes
         pass
-    l_best = [] # TODO: best_cut_point_set(l1, l2)
+    l_best = best_cut_point_set(l1, l2)
     t_s = 1
     for idx, l_i in enumerate(l_best):
         s_i = (t_s, l_i)
@@ -87,11 +122,14 @@ def _cut_point_search(X, regime1, regime2, transition_matrix):
         t_s = l_i
     return m1, m2, s1, s2
 
+def best_cut_point_set(cut_set1, cut_set2):
+    # TODO: best_cut_point_set(l1, l2)
+    return cut_set1
 
 def _regime_split(X):
     m1, m2, s1, s2 = 0, 0, [], [] # Initialise outputs
     t1, t2, d = [], [], [] # TODO: Initialise models
-    current_cost = math.inf
+    current_cost = float('inf') # Current cost is infinity
     while c := _cost(X, t1, t2, d) < current_cost:
         current_cost = c
         m1, m2, s1, s2 = _cut_point_search(X, t1, t2, d)
@@ -100,13 +138,9 @@ def _regime_split(X):
     return m1, m2, s1, s2, t1, t2, d
 
 def _cost(X, regime1, regime2=None, transition_matrix=None):
-    # TODO: Cost functions
     if regime2 is not None:
         # TODO: Cost of regime pair
         pass
     else:
         # TODO: Cost of single regime
         pass
-    return 0
-
-
