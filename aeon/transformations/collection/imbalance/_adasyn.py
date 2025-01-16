e17586ac6374bd12ec4200449fcdab1048c33d22
Chuanhang Qiu <chuanhangqiu@outlook.com>
2025-01-16 20:04:12 +0000

    smote & adasyn in aeon.transformation.imbalance

--- aeon/transformations/collection/imbalance/_adasyn.py
+++ aeon/transformations/collection/imbalance/_adasyn.py
@@ -1,15 +1,15 @@
 """Wrapper for imblearn minority class rebalancer SMOTE."""
 
-from imblearn.over_sampling import SMOTE as smote
+from imblearn.over_sampling import ADASYN as adasyn
 import numpy as np
 from aeon.transformations.collection import BaseCollectionTransformer
 
 __maintainer__ = ["TonyBagnall"]
-__all__ = ["SMOTE"]
+__all__ = ["ADASYN"]
 
 
-class SMOTE(BaseCollectionTransformer):
-    """Wrapper for SMOTE transform."""
+class ADASYN(BaseCollectionTransformer):
+    """Wrapper for ADASYN transform."""
 
     _tags = {
         "capability:multivariate": True,
@@ -27,9 +27,8 @@ class SMOTE(BaseCollectionTransformer):
         return self
 
     def _fit_transform(self, X, y=None):
-
-        self.smote_ = smote(sampling_strategy=self.sampling_strategy,
-                            random_state=self.random_state, k_neighbors=self.k_neighbors)
+        self.smote_ = adasyn(sampling_strategy=self.sampling_strategy,
+                             random_state=self.random_state, n_neighbors=self.k_neighbors)
         res_X, res_y = self.smote_.fit_resample(np.squeeze(X), y)
         return res_X, res_y
 
@@ -37,11 +36,17 @@ class SMOTE(BaseCollectionTransformer):
 if __name__ == "__main__":
     # Example usage
     import numpy as np
+    n_samples = 100  # Total number of labels
+    imbalance_ratio = 0.9  # Proportion of majority class
 
-    X = np.random.rand(100, 1, 10)
-    y = np.random.randint(0, 2, 100)
+    X = np.random.rand(n_samples, 1, 10)
+    y = np.random.choice([0, 1], size=n_samples, p=[imbalance_ratio, 1 - imbalance_ratio])
 
-    transformer = SMOTE()
+    _, count = np.unique(y, return_counts=True)
+    print(count)
+    transformer = ADASYN()
     res_X, res_y = transformer.fit_transform(X, y)
     print(res_X.shape, res_y.shape)
     # Expected output: (200, 3, 10) (200,)
+    _, res_count = np.unique(res_y, return_counts=True)
+    print(res_count)
