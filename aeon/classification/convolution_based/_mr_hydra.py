fb966332e5d3a04b47dc0f2ecac558d75a20b05d
Akhil-Jasson <akhilinian@gmail.com>
2025-01-19 19:38:18 +0530

    Updated docstring to clarify class_weight parameter in MultiRocketHydraClassifier
    - Updated the class_weight parameter docstring in MultiRocketHydraClassifier to clarify supported values.
    
    - Explicitly mentioned that "balanced_subsample" is not supported as it is specific to ensemble models and not applicable to RidgeClassifierCV.

--- aeon/classification/convolution_based/_mr_hydra.py
+++ aeon/classification/convolution_based/_mr_hydra.py
@@ -29,17 +29,17 @@ class MultiRocketHydraClassifier(BaseClassifier):
         Number of kernels per group for the Hydra transform.
     n_groups : int, default=64
         Number of groups per dilation for the Hydra transform.
-    class_weight{“balanced”, “balanced_subsample”}, dict or list of dicts, default=None
+    class_weight{None, “balanced”}, dict or list of dicts, default=None
         From sklearn documentation:
-        If not given, all classes are supposed to have weight one.
+        If None, all classes are assigned equal weights.
         The “balanced” mode uses the values of y to automatically adjust weights
         inversely proportional to class frequencies in the input data as
         n_samples / (n_classes * np.bincount(y))
-        The “balanced_subsample” mode is the same as “balanced” except that weights
-        are computed based on the bootstrap sample for every tree grown.
         For multi-output, the weights of each column of y will be multiplied.
+        A dictionary can also be provided to specify weights for each class manually.
         Note that these weights will be multiplied with sample_weight (passed through
         the fit method) if sample_weight is specified.
+        Also Note that "balanced_subsample" is not supported as RidgeClassifierCV is not an ensemble model.
     n_jobs : int, default=1
         The number of jobs to run in parallel for both `fit` and `predict`.
         ``-1`` means using all processors.
