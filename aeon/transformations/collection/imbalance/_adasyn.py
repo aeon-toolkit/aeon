d0ec9c507cb99baff7d42bf72f4dbefe645cd984
Chuanhang Qiu <chuanhangqiu@outlook.com>
2025-01-20 19:42:17 +0000

    smote & adasyn in aeon.transformation.imbalance

--- aeon/transformations/collection/imbalance/_adasyn.py
+++ aeon/transformations/collection/imbalance/_adasyn.py
@@ -1,8 +1,18 @@
-"""Wrapper for imblearn minority class rebalancer SMOTE."""
+"""
+Wrapper for imblearn minority class rebalancer ADASYN.
+original authors:
+#          Guillaume Lemaitre <g.lemaitre58@gmail.com>
+#          Christos Aridas
+# License: MIT
+"""
 
 from imblearn.over_sampling import ADASYN as adasyn
 import numpy as np
 from aeon.transformations.collection import BaseCollectionTransformer
+from sklearn.neighbors import NearestNeighbors
+from sklearn.utils import check_random_state, _safe_indexing
+from scipy import sparse
+from collections import OrderedDict
 
 __maintainer__ = ["TonyBagnall, Chris Qiu"]
 __all__ = ["ADASYN"]
@@ -23,19 +33,100 @@ class ADASYN(BaseCollectionTransformer):
         self.k_neighbors = k_neighbors
         super().__init__()
 
-    def _transform(self, X, y=None):
+    def _fit(self, X, y=None):
+        # set the additional_neighbor=1
+        self.nn_ = NearestNeighbors(n_neighbors=self.k_neighbors + 1)
+
+        # generate sampling target by targeting all classes but not the majority
+        unique, counts = np.unique(y, return_counts=True)
+        target_stats = dict(zip(unique, counts))
+        n_sample_majority = max(target_stats.values())
+        class_majority = max(target_stats, key=target_stats.get)
+        sampling_strategy = {
+            key: n_sample_majority - value
+            for (key, value) in target_stats.items()
+            if key != class_majority
+        }
+        self.sampling_strategy_ = OrderedDict(
+            sorted(sampling_strategy.items())
+        )
         return self
 
-    def _fit_transform(self, X, y=None):
-        self.smote_ = adasyn(sampling_strategy=self.sampling_strategy,
-                             random_state=self.random_state, n_neighbors=self.k_neighbors)
-        res_X, res_y = self.smote_.fit_resample(np.squeeze(X), y)
-        return res_X, res_y
+    def _transform(self, X, y=None):
+        shape_recover = False  # use to recover the shape of X
+        if X.ndim == 3 and X.shape[1] == 1:
+            X = np.squeeze(X, axis=1)  # remove the middle dimension to be compatible with sklearn
+            shape_recover = True
+        random_state = check_random_state(self.random_state)
+        X_resampled = [X.copy()]
+        y_resampled = [y.copy()]
+
+        for class_sample, n_samples in self.sampling_strategy_.items():
+            if n_samples == 0:
+                continue
+            target_class_indices = np.flatnonzero(y == class_sample)
+            X_class = _safe_indexing(X, target_class_indices)
+
+            self.nn_.fit(X)
+            nns = self.nn_.kneighbors(X_class, return_distance=False)[:, 1:]
+            # The ratio is computed using a one-vs-rest manner. Using majority
+            # in multi-class would lead to slightly different results at the
+            # cost of introducing a new parameter.
+            n_neighbors = self.nn_.n_neighbors - 1
+            ratio_nn = np.sum(y[nns] != class_sample, axis=1) / n_neighbors
+            if not np.sum(ratio_nn):
+                raise RuntimeError(
+                    "Not any neigbours belong to the majority"
+                    " class. This case will induce a NaN case"
+                    " with a division by zero. ADASYN is not"
+                    " suited for this specific dataset."
+                    " Use SMOTE instead."
+                )
+            ratio_nn /= np.sum(ratio_nn)
+            n_samples_generate = np.rint(ratio_nn * n_samples).astype(int)
+            # rounding may cause new amount for n_samples
+            n_samples = np.sum(n_samples_generate)
+            if not n_samples:
+                raise ValueError(
+                    "No samples will be generated with the provided ratio settings."
+                )
+
+            # the nearest neighbors need to be fitted only on the current class
+            # to find the class NN to generate new samples
+            self.nn_.fit(X_class)
+            nns = self.nn_.kneighbors(X_class, return_distance=False)[:, 1:]
+
+            enumerated_class_indices = np.arange(len(target_class_indices))
+            rows = np.repeat(enumerated_class_indices, n_samples_generate)
+            cols = random_state.choice(n_neighbors, size=n_samples)
+            diffs = X_class[nns[rows, cols]] - X_class[rows]
+            steps = random_state.uniform(size=(n_samples, 1))
+
+            if sparse.issparse(X):
+                sparse_func = type(X).__name__
+                steps = getattr(sparse, sparse_func)(steps)
+                X_new = X_class[rows] + steps.multiply(diffs)
+            else:
+                X_new = X_class[rows] + steps * diffs
+
+            X_new = X_new.astype(X.dtype)
+            y_new = np.full(n_samples, fill_value=class_sample, dtype=y.dtype)
+            X_resampled.append(X_new)
+            y_resampled.append(y_new)
+
+        if sparse.issparse(X):
+            X_resampled = sparse.vstack(X_resampled, format=X.format)
+        else:
+            X_resampled = np.vstack(X_resampled)
+        y_resampled = np.hstack(y_resampled)
+
+        if shape_recover:
+            X_resampled = X_resampled[:, np.newaxis, :]
+        return X_resampled, y_resampled
 
 
 if __name__ == "__main__":
     # Example usage
-    import numpy as np
     n_samples = 100  # Total number of labels
     imbalance_ratio = 0.9  # Proportion of majority class
 
@@ -47,6 +138,6 @@ if __name__ == "__main__":
     transformer = ADASYN()
     res_X, res_y = transformer.fit_transform(X, y)
     print(res_X.shape, res_y.shape)
-    # Expected output: (200, 3, 10) (200,)
+
     _, res_count = np.unique(res_y, return_counts=True)
     print(res_count)
--- aeon/transformations/collection/imbalance/_smote.py
+++ aeon/transformations/collection/imbalance/_smote.py
@@ -1,8 +1,19 @@
-"""Wrapper for imblearn minority class rebalancer SMOTE."""
+"""
+Wrapper for imblearn minority class rebalancer SMOTE.
+original authors:
+#          Guillaume Lemaitre <g.lemaitre58@gmail.com>
+#          Fernando Nogueira
+#          Christos Aridas
+#          Dzianis Dudnik
+# License: MIT
+"""
 
-from imblearn.over_sampling import SMOTE as smote
 import numpy as np
 from aeon.transformations.collection import BaseCollectionTransformer
+from sklearn.neighbors import NearestNeighbors
+from sklearn.utils import check_random_state, _safe_indexing
+from scipy import sparse
+from collections import OrderedDict
 
 __maintainer__ = ["TonyBagnall"]
 __all__ = ["SMOTE"]
@@ -23,25 +34,191 @@ class SMOTE(BaseCollectionTransformer):
         self.k_neighbors = k_neighbors
         super().__init__()
 
-    def _transform(self, X, y=None):
+    def _fit(self, X, y=None):
+        # set the additional_neighbor=1
+        self.nn_ = NearestNeighbors(n_neighbors=self.k_neighbors + 1)
+
+        # generate sampling target by targeting all classes but not the majority
+        unique, counts = np.unique(y, return_counts=True)
+        target_stats = dict(zip(unique, counts))
+        n_sample_majority = max(target_stats.values())
+        class_majority = max(target_stats, key=target_stats.get)
+        sampling_strategy = {
+            key: n_sample_majority - value
+            for (key, value) in target_stats.items()
+            if key != class_majority
+        }
+        self.sampling_strategy_ = OrderedDict(
+            sorted(sampling_strategy.items())
+        )
         return self
 
-    def _fit_transform(self, X, y=None):
+    def _transform(self, X, y=None):
+        shape_recover = False   # use to recover the shape of X
+        if X.ndim == 3 and X.shape[1] == 1:
+            X = np.squeeze(X, axis=1)  # remove the middle dimension to be compatible with sklearn
+            shape_recover = True
+        X_resampled = [X.copy()]
+        y_resampled = [y.copy()]
+        for class_sample, n_samples in self.sampling_strategy_.items():
+            if n_samples == 0:
+                continue
+            target_class_indices = np.flatnonzero(y == class_sample)
+            # access to sklearn's _safe_indexing which needs to be modified
+            X_class = _safe_indexing(X, target_class_indices)
+            self.nn_.fit(X_class)
+            nns = self.nn_.kneighbors(X_class, return_distance=False)[:, 1:]
+            X_new, y_new = self._make_samples(
+                X_class, y.dtype, class_sample, X_class, nns, n_samples, 1.0
+            )
+            X_resampled.append(X_new)
+            y_resampled.append(y_new)
 
-        self.smote_ = smote(sampling_strategy=self.sampling_strategy,
-                            random_state=self.random_state, k_neighbors=self.k_neighbors)
-        res_X, res_y = self.smote_.fit_resample(np.squeeze(X), y)
-        return res_X, res_y
+        if sparse.issparse(X):
+            X_resampled = sparse.vstack(X_resampled, format=X.format)
+        else:
+            X_resampled = np.vstack(X_resampled)
+        y_resampled = np.hstack(y_resampled)
+        if shape_recover:
+            X_resampled = X_resampled[:, np.newaxis, :]
+        return X_resampled, y_resampled
+
+    def _make_samples(
+            self, X, y_dtype, y_type, nn_data, nn_num, n_samples, step_size=1.0, y=None
+    ):
+        """A support function that returns artificial samples constructed along
+        the line connecting nearest neighbours.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Points from which the points will be created.
+
+        y_dtype : dtype
+            The data type of the targets.
+
+        y_type : str or int
+            The minority target value, just so the function can return the
+            target values for the synthetic variables with correct length in
+            a clear format.
+
+        nn_data : ndarray of shape (n_samples_all, n_features)
+            Data set carrying all the neighbours to be used
+
+        nn_num : ndarray of shape (n_samples_all, k_nearest_neighbours)
+            The nearest neighbours of each sample in `nn_data`.
+
+        n_samples : int
+            The number of samples to generate.
+
+        step_size : float, default=1.0
+            The step size to create samples.
+
+        y : ndarray of shape (n_samples_all,), default=None
+            The true target associated with `nn_data`. Used by Borderline SMOTE-2 to
+            weight the distances in the sample generation process.
+
+        Returns
+        -------
+        X_new : {ndarray, sparse matrix} of shape (n_samples_new, n_features)
+            Synthetically generated samples.
+
+        y_new : ndarray of shape (n_samples_new,)
+            Target values for synthetic samples.
+        """
+        random_state = check_random_state(self.random_state)
+        samples_indices = random_state.randint(low=0, high=nn_num.size, size=n_samples)
+
+        # np.newaxis for backwards compatability with random_state
+        steps = step_size * random_state.uniform(size=n_samples)[:, np.newaxis]
+        rows = np.floor_divide(samples_indices, nn_num.shape[1])
+        cols = np.mod(samples_indices, nn_num.shape[1])
+
+        X_new = self._generate_samples(X, nn_data, nn_num, rows, cols, steps, y_type, y)
+        y_new = np.full(n_samples, fill_value=y_type, dtype=y_dtype)
+        return X_new, y_new
+
+    def _generate_samples(
+            self, X, nn_data, nn_num, rows, cols, steps, y_type=None, y=None
+    ):
+        r"""Generate a synthetic sample.
+
+        The rule for the generation is:
+
+        .. math::
+           \mathbf{s_{s}} = \mathbf{s_{i}} + \mathcal{u}(0, 1) \times
+           (\mathbf{s_{i}} - \mathbf{s_{nn}}) \,
+
+        where \mathbf{s_{s}} is the new synthetic samples, \mathbf{s_{i}} is
+        the current sample, \mathbf{s_{nn}} is a randomly selected neighbors of
+        \mathbf{s_{i}} and \mathcal{u}(0, 1) is a random number between [0, 1).
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Points from which the points will be created.
+
+        nn_data : ndarray of shape (n_samples_all, n_features)
+            Data set carrying all the neighbours to be used.
+
+        nn_num : ndarray of shape (n_samples_all, k_nearest_neighbours)
+            The nearest neighbours of each sample in `nn_data`.
+
+        rows : ndarray of shape (n_samples,), dtype=int
+            Indices pointing at feature vector in X which will be used
+            as a base for creating new samples.
+
+        cols : ndarray of shape (n_samples,), dtype=int
+            Indices pointing at which nearest neighbor of base feature vector
+            will be used when creating new samples.
+
+        steps : ndarray of shape (n_samples,), dtype=float
+            Step sizes for new samples.
+
+        y_type : str, int or None, default=None
+            Class label of the current target classes for which we want to generate
+            samples.
+
+        y : ndarray of shape (n_samples_all,), default=None
+            The true target associated with `nn_data`. Used by Borderline SMOTE-2 to
+            weight the distances in the sample generation process.
+
+        Returns
+        -------
+        X_new : {ndarray, sparse matrix} of shape (n_samples, n_features)
+            Synthetically generated samples.
+        """
+        diffs = nn_data[nn_num[rows, cols]] - X[rows]
+        if y is not None:  # only entering for BorderlineSMOTE-2
+            random_state = check_random_state(self.random_state)
+            mask_pair_samples = y[nn_num[rows, cols]] != y_type
+            diffs[mask_pair_samples] *= random_state.uniform(
+                low=0.0, high=0.5, size=(mask_pair_samples.sum(), 1)
+            )
+
+        if sparse.issparse(X):
+            sparse_func = type(X).__name__
+            steps = getattr(sparse, sparse_func)(steps)
+            X_new = X[rows] + steps.multiply(diffs)
+        else:
+            X_new = X[rows] + steps * diffs
+
+        return X_new.astype(X.dtype)
 
 
 if __name__ == "__main__":
     # Example usage
-    import numpy as np
-
-    X = np.random.rand(100, 1, 10)
-    y = np.random.randint(0, 2, 100)
+    n_samples = 100  # Total number of labels
+    imbalance_ratio = 0.9  # Proportion of majority class
+    X = np.random.rand(n_samples, 1, 10)
+    y = np.random.choice([0, 1], size=n_samples, p=[imbalance_ratio, 1 - imbalance_ratio])
+    _, count = np.unique(y, return_counts=True)
+    print(count)
 
     transformer = SMOTE()
-    res_X, res_y = transformer.fit_transform(X, y)
+    transformer.fit(X, y)
+    res_X, res_y = transformer.transform(X, y)
     print(res_X.shape, res_y.shape)
-    # Expected output: (200, 3, 10) (200,)
+
+    _, res_count = np.unique(res_y, return_counts=True)
+    print(res_count)
