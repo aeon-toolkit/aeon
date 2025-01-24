6e24ef0c5327eabd284fa988095b92ae979b0ffe
Tony Bagnall <ajb@uea.ac.uk>
2025-01-24 11:29:58 +0000

    format

--- aeon/transformations/collection/imbalance/__init__.py
+++ aeon/transformations/collection/imbalance/__init__.py
@@ -1,6 +1,6 @@
 """Supervised transformers to rebalance colelctions of time series."""
 
-__all__ = ["SMOTE", "ADASYN"]
+__all__ = ["ADASYN", "SMOTE"]
 
-from aeon.transformations.collection.imbalance._smote import SMOTE
 from aeon.transformations.collection.imbalance._adasyn import ADASYN
+from aeon.transformations.collection.imbalance._smote import SMOTE
