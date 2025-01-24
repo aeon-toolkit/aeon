a440a90a43e88d2149cf4a181875e4aad03360fb
Tony Bagnall <ajb@uea.ac.uk>
2025-01-24 11:18:34 +0000

    move tests

--- aeon/transformations/collection/imbalance/tests/test_adasyn.py
+++ aeon/transformations/collection/imbalance/tests/test_adasyn.py
@@ -8,6 +8,35 @@ from aeon.transformations.collection.imbalance import ADASYN
 from aeon.utils.validation._dependencies import _check_soft_dependencies
 
 
+def test_adasyn():
+    """Test the ADASYN class.
+
+    This function creates a 3D numpy array, applies
+    ADASYN using the ADASYN class, and asserts that the
+    transformed data has a balanced number of samples.
+    ADASYN is a variant of SMOTE that generates synthetic samples,
+    but it focuses on generating samples near the decision boundary.
+    Therefore, sometimes, it may generate more or less samples than SMOTE,
+    which is why we only check if the number of samples is nearly balanced.
+    """
+    n_samples = 100  # Total number of labels
+    majority_num = 90  # number of majority class
+    minority_num = n_samples - majority_num  # number of minority class
+
+    X = np.random.rand(n_samples, 1, 10)
+    y = np.array([0] * majority_num + [1] * minority_num)
+
+    transformer = ADASYN()
+    transformer.fit(X, y)
+    res_X, res_y = transformer.transform(X, y)
+    _, res_count = np.unique(res_y, return_counts=True)
+
+    assert np.abs(len(res_X) - 2 * majority_num) < minority_num
+    assert np.abs(len(res_y) - 2 * majority_num) < minority_num
+    assert res_count[0] == majority_num
+    assert np.abs(res_count[0] - res_count[1]) < minority_num
+
+
 @pytest.mark.skipif(
     not _check_soft_dependencies(
         "imbalanced-learn",
