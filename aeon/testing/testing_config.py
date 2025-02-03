f301acfc5426614c93c45f2d57f39f7cab7d4407
Tony Bagnall <ajb@uea.ac.uk>
2025-02-03 17:17:52 +0000

    CLASP

--- aeon/testing/testing_config.py
+++ aeon/testing/testing_config.py
@@ -51,7 +51,6 @@ EXCLUDED_TESTS = {
     "RSAST": ["check_fit_deterministic"],
     "SFA": ["check_persistence_via_pickle", "check_fit_deterministic"],
     # missed in legacy testing, changes state in predict/transform
-    "ClaSPSegmenter": ["check_non_state_changing_method"],
     "HMMSegmenter": ["check_non_state_changing_method"],
     "RSTSF": ["check_non_state_changing_method"],
     # Keeps length during predict to avoid recomputing means and std of data in fit
