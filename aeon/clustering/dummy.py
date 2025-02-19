7c27e02ff009e7ad2132a59f9ff18bece61e95c8
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-02-19 22:55:04 +0000

    dummy clusterer tags

--- aeon/clustering/dummy.py
+++ aeon/clustering/dummy.py
@@ -54,6 +54,13 @@ class DummyClusterer(BaseClusterer):
     array([0, 1, 0])
     """
 
+    _tags = {
+        "X_inner_type": ["np-list", "numpy3D"],
+        "capability:missing_values": True,
+        "capability:multivariate": True,
+        "capability:unequal_length": True,
+    }
+
     def __init__(self, strategy="uniform", n_clusters=3, random_state=None):
         self.strategy = strategy
         self.random_state = random_state
