c53c3a8615a61242f07f99e06f02b01cf059eb88
Ramana Raja <83065061+Ramana-Raja@users.noreply.github.com>
2025-03-06 21:03:47 +0530

    Updated _idk.py docs

--- aeon/anomaly_detection/_idk.py
+++ aeon/anomaly_detection/_idk.py
@@ -55,7 +55,7 @@ class IDK2(BaseAnomalyDetector):
          the data in fixed-width segments, offering faster computation at the
          cost of granularity.
     random_state : int, np.random.RandomState instance or None, default=None
-        Determines random number generation for random sample.
+        Determines random number for generation of random sample.
         If `int`, random_state is the seed used by the random number generator;
         If `np.random.RandomState` instance,
         random_state is the random number generator;
