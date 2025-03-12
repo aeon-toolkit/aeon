660c0a528c3962db6b5c7bca7f2e93e7a0a33aa3
tanishy7777 <23110328@iitgn.ac.in>
2025-03-12 15:32:05 +0530

    Minor changes

--- aeon/distances/elastic/_dtw_gi.py
+++ aeon/distances/elastic/_dtw_gi.py
@@ -14,16 +14,16 @@ from aeon.utils.validation.collection import _is_numpy_list_multivariate
 
 
 @njit(cache=True, fastmath=True)
-def _path2mat(path, x_ntimepoints, y_ntimepoints):
+def _path2mat(path, x_timepoints, y_timepoints):
     r"""Convert a warping alignment path to a binary warping matrix."""
-    w = np.zeros((x_ntimepoints, y_ntimepoints))
+    w = np.zeros((x_timepoints, y_timepoints))
     for i, j in path:
         w[i, j] = 1
     return w
 
 
 @njit(cache=True, fastmath=True)
-def dtw_gi(
+def _dtw_gi(
     x: np.ndarray,
     y: np.ndarray,
     window: Optional[float] = None,
@@ -51,7 +51,7 @@ def dtw_gi(
     itakura_max_slope : float, default=None
         Maximum slope as a proportion of the number of time points used to create
         Itakura parallelogram on the bounding matrix. Must be between 0. and 1.
-    init_p : array-like of shape (x_nchannels, y_nchannels), default=None
+    init_p : array-like of shape (x_channels, y_channels), default=None
         Initial linear transformation. If None, the identity matrix is used.
     max_iter : int, default=20
         Maximum number of iterations for the iterative optimization.
@@ -61,7 +61,7 @@ def dtw_gi(
     Returns
     -------
     - w_pi: binary warping matrix of shape (n0, n1)
-    - p: the final linear (Stiefel) matrix of shape (x_nchannels, y_nchannels)
+    - p: the final linear (Stiefel) matrix of shape (x_channels, y_channels)
     - cost: final DTW cost considering global invariances
 
     If use_bias is True, also returns:
@@ -69,53 +69,39 @@ def dtw_gi(
 
     """
     if x.ndim == 1 and y.ndim == 1:
-        _x = x.reshape((1, x.shape[0]))
-        _y = y.reshape((1, y.shape[0]))
-        return _dtw_gi(_x, _y, window, itakura_max_slope, init_p, max_iter, use_bias)
+        x_ = x.reshape((1, x.shape[0]))
+        y_ = y.reshape((1, y.shape[0]))
+    elif x.ndim == 2 and y.ndim == 2:
+        x_ = x
+        y_ = y
+    else:
+        raise ValueError("x and y must be 1D or 2D")
 
-    if x.ndim == 2 and y.ndim == 2:
-        return _dtw_gi(x, y, window, itakura_max_slope, init_p, max_iter, use_bias)
-    raise ValueError("x and y must be 1D or 2D")
-
-
-@njit(cache=True, fastmath=True)
-def _dtw_gi(
-    x: np.ndarray,
-    y: np.ndarray,
-    window: Optional[float] = None,
-    itakura_max_slope: Optional[float] = None,
-    init_p=None,
-    max_iter=20,
-    use_bias=False,
-):
-    x_ = x
-    y_ = y
-
-    x_nchannels, x_ntimepoints = x_.shape
-    y_nchannels, y_ntimepoints = y_.shape
+    x_channels, x_timepoints = x_.shape
+    y_channels, y_timepoints = y_.shape
 
     x_m = np.sum(x_, axis=1) / x_.shape[1]
     x_m = x_m.reshape((-1, 1))
     y_m = np.sum(y_, axis=1) / y_.shape[1]
     y_m = y_m.reshape((-1, 1))
 
-    w_pi = np.zeros((x_ntimepoints, y_ntimepoints))
+    w_pi = np.zeros((x_timepoints, y_timepoints))
     if init_p is None:
-        p = np.eye(x_nchannels, y_nchannels, dtype=np.float64)
+        p = np.eye(x_channels, y_channels, dtype=np.float64)
     else:
         p = init_p
 
     y_ = y_.astype(np.float64)
     x_ = x_.astype(np.float64)
 
-    bias = np.zeros((x_nchannels, 1))
+    bias = np.zeros((x_channels, 1))
 
     for _ in range(max_iter):
         w_pi_old = w_pi.copy()
         y_transformed = p.dot(y_) + bias
 
         path, cost = dtw_alignment_path(x_, y_transformed, window, itakura_max_slope)
-        w_pi = _path2mat(path, x_ntimepoints, y_ntimepoints)
+        w_pi = _path2mat(path, x_timepoints, y_timepoints)
 
         if np.allclose(w_pi, w_pi_old):
             break
@@ -136,7 +122,7 @@ def _dtw_gi(
     if use_bias:
         return w_pi, p, bias, cost, x, y_trans
     else:
-        dummy_bias = np.zeros((x_nchannels, 1), dtype=np.float64)
+        dummy_bias = np.zeros((x_channels, 1), dtype=np.float64)
         return w_pi, p, dummy_bias, cost, x, y_trans
 
 
@@ -152,14 +138,108 @@ def dtw_gi_distance(
 ) -> float:
     r"""Compute the DTW_GI distance between two time series.
 
-    TODO: Complete this
+    The DTW_gi distance between 2 timeseries x and y is the similarity
+    measure that estimates both temporal alignment and does feature space
+    transformation between time series simultaneously. This means that the
+    time series do not have to lie in the same ambient space.
+    A good background into DTW with global invariances can be found in [1]_.
+    This implementation is inspired by [2]_.
+
+    For example, if we have two time series x and y of lengths n and m
+    respectively, and we assume that the time series do not lie in the
+    same ambient space. Lets assume that features of x lie in :math:`\mathbb{R}^p`
+    and features of y lie in :math:`\mathbb{R}^q`. To compare the two time series,
+    we need to find an optimum mapping from the feature space of y to the feature space
+    where features of x lie. So think of it as optimizing on a family of functions F
+    that map features from y onto the feature space in which features of x
+    lie. (This is just one way to do it, the mapping can be
+    in the opposite direction as well. But this function assumes the former way).
+
+    More formally, we define Dynamic Time Warping with Global Invariances (DTW-GI)
+    as the solution of the following joint optimization problem:
+
+    :math:`\text{DTW-GI}(\mathbf{x}, \mathbf{y}) =
+    \min_{f \in \mathcal{F}, \pi \in \mathcal{A}(\mathbf{x}, \mathbf{y})}
+    \sqrt{\sum_{(i,j) \in \pi} d(x_i, f(y_j))^2},`
+
+    This similarity measure estimates temporal alignment
+    with feature space transformation between time series
+    allowing the alignment of time series that do not exist in the
+    same ambient space.
+
+    Note: The optimal warping path :math:`P^*` for a given cost matrix can be found
+    exactly through a dynamic programming formulation. This can be a time consuming
+    operation, and it is common to put a restriction on the amount of warping allowed.
+    This is implemented through the bounding_matrix structure, that supplies a mask for
+    allowable warpings. The most common bounding strategies include the
+    Sakoe-Chiba band [3]_. The width of the allowed warping is controlled through the
+     ``window`` parameter which sets the maximum proportion of warping allowed.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        First time series, either univariate, shape ``(n_timepoints,)``, or
+        multivariate, shape ``(n_channels, n_timepoints)``.
+    y : np.ndarray
+        Second time series, either univariate, shape ``(n_timepoints,)``, or
+        multivariate, shape ``(n_channels, n_timepoints)``.
+    window : float, default=None
+        The window to use for the bounding matrix. If None, no bounding matrix
+        is used. window is a percentage deviation, so if ``window = 0.1``,
+        10% of the series length is the max warping allowed.
+        is used.
+    itakura_max_slope : float, default=None
+        Maximum slope as a proportion of the number of time points used to create
+        Itakura parallelogram on the bounding matrix. Must be between 0. and 1.
+    init_p : array-like of shape (x_channels, y_channels), default=None
+        Initial linear transformation. If None, the identity matrix is used.
+    max_iter : int, default=20
+        Maximum number of iterations for the iterative optimization.
+    use_bias : bool, default=False
+        If True, the feature space map is affine (with a bias term).
+
+    Returns
+    -------
+    float
+        DTW_GI distance between x and y, minimum value 0.
+
+    Raises
+    ------
+    ValueError
+        If x and y are not 1D or 2D arrays.
+
+    References
+    ----------
+    .. [1] T. Vayer, R. Tavenard, L. Chapel, N. Courty, R. Flamary, and Y. Soullard,
+    “Time Series Alignment with Global Invariances,” arXiv.org, 2020.
+    https://arxiv.org/abs/2002.03848
+
+    .. [2] Romain Tavenard, “DTW with Global Invariances,” Github.io, Dec. 17, 2020.
+    https://rtavenar.github.io/hdr/parts/01/dtw/dtw_gi.html
+
+    .. [3] Sakoe H. and Chiba S.: Dynamic programming algorithm optimization for
+    spoken word recognition. IEEE Transactions on Acoustics, Speech, and Signal
+    Processing 26(1):43-49, 1978.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from aeon.distances import dtw_gi_distance
+    >>> x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> y = np.array([11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    >>> dtw_gi_distance(x, y) # 1D series
+    768.0
+    >>> x = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [0, 1, 0, 2, 0]])
+    >>> y = np.array([[11, 12, 13, 14],[7, 8, 9, 20],[1, 3, 4, 5]] )
+    >>> dtw_gi_distance(x, y) # 2D series with 3 channels, unequal length
+    359.203981741644
     """
     if x.ndim == 1 and y.ndim == 1:
         _x = x.reshape((1, x.shape[0]))
         _y = y.reshape((1, y.shape[0]))
-        return dtw_gi(_x, _y, window, itakura_max_slope, init_p, max_iter, use_bias)[3]
+        return _dtw_gi(_x, _y, window, itakura_max_slope, init_p, max_iter, use_bias)[3]
     if x.ndim == 2 and y.ndim == 2:
-        return dtw_gi(x, y, window, itakura_max_slope, init_p, max_iter, use_bias)[3]
+        return _dtw_gi(x, y, window, itakura_max_slope, init_p, max_iter, use_bias)[3]
     raise ValueError("x and y must be 1D or 2D")
 
 
@@ -169,6 +249,9 @@ def dtw_gi_cost_matrix(
     y: np.ndarray,
     window: Optional[float] = None,
     itakura_max_slope: Optional[float] = None,
+    init_p=None,
+    max_iter=20,
+    use_bias=False,
 ) -> np.ndarray:
     r"""Compute the DTW_GI cost matrix between two time series.
 
@@ -194,6 +277,12 @@ def dtw_gi_cost_matrix(
     itakura_max_slope : float, default=None
         Maximum slope as a proportion of the number of time points used to create
         Itakura parallelogram on the bounding matrix. Must be between 0. and 1.
+    init_p : array-like of shape (x_channels, y_channels), default=None
+        Initial linear transformation. If None, the identity matrix is used.
+    max_iter : int, default=20
+        Maximum number of iterations for the iterative optimization.
+    use_bias : bool, default=False
+        If True, the feature space map is affine (with a bias term).
 
     Returns
     -------
@@ -223,7 +312,9 @@ def dtw_gi_cost_matrix(
            [204., 140.,  91.,  55.,  30.,  14.,   5.,   1.,   0.,   1.],
            [285., 204., 140.,  91.,  55.,  30.,  14.,   5.,   1.,   0.]])
     """
-    _, _, _, _, xnew, y_trans = dtw_gi(x, y, window, itakura_max_slope)
+    _, _, _, _, xnew, y_trans = _dtw_gi(
+        x, y, window, itakura_max_slope, init_p, max_iter, use_bias
+    )
 
     return dtw_cost_matrix(xnew, y_trans, window, itakura_max_slope)
 
@@ -233,6 +324,9 @@ def dtw_gi_pairwise_distance(
     y: Optional[Union[np.ndarray, list[np.ndarray]]] = None,
     window: Optional[float] = None,
     itakura_max_slope: Optional[float] = None,
+    init_p=None,
+    max_iter=20,
+    use_bias=False,
 ) -> np.ndarray:
     r"""Compute the DTW_GI pairwise distance between a set of time series.
 
@@ -268,6 +362,12 @@ def dtw_gi_pairwise_distance(
     itakura_max_slope : float, default=None
         Maximum slope as a proportion of the number of time points used to create
         Itakura parallelogram on the bounding matrix. Must be between 0. and 1.
+    init_p : array-like of shape (x_channels, y_channels), default=None
+        Initial linear transformation. If None, the identity matrix is used.
+    max_iter : int, default=20
+        Maximum number of iterations for the iterative optimization.
+    use_bias : bool, default=False
+        If True, the feature space map is affine (with a bias term).
 
     Returns
     -------
@@ -337,6 +437,9 @@ def _dtw_gi_from_multiple_to_multiple_distance(
     window: Optional[float],
     itakura_max_slope: Optional[float],
     unequal_length: bool,
+    init_p=None,
+    max_iter=20,
+    use_bias=False,
 ) -> np.ndarray:
     n_cases = len(x)
     m_cases = len(y)
@@ -345,7 +448,9 @@ def _dtw_gi_from_multiple_to_multiple_distance(
     for i in range(n_cases):
         for j in range(m_cases):
             x1, y1 = x[i], y[j]
-            distances[i, j] = dtw_gi_distance(x1, y1, window, itakura_max_slope)
+            distances[i, j] = dtw_gi_distance(
+                x1, y1, window, itakura_max_slope, init_p, max_iter, use_bias
+            )
     return distances
 
 
@@ -355,6 +460,9 @@ def _dtw_gi_pairwise_distance(
     window: Optional[float],
     itakura_max_slope: Optional[float],
     unequal_length: bool,
+    init_p=None,
+    max_iter=20,
+    use_bias=False,
 ) -> np.ndarray:
     n_cases = len(X)
     distances = np.zeros((n_cases, n_cases))
@@ -362,7 +470,9 @@ def _dtw_gi_pairwise_distance(
     for i in range(n_cases):
         for j in range(i + 1, n_cases):
             x1, x2 = X[i], X[j]
-            distances[i, j] = dtw_gi_distance(x1, x2, window, itakura_max_slope)
+            distances[i, j] = dtw_gi_distance(
+                x1, x2, window, itakura_max_slope, init_p, max_iter, use_bias
+            )
             distances[j, i] = distances[i, j]
 
     return distances
@@ -374,8 +484,11 @@ def dtw_gi_alignment_path(
     y: np.ndarray,
     window: Optional[float] = None,
     itakura_max_slope: Optional[float] = None,
+    init_p=None,
+    max_iter=20,
+    use_bias=False,
 ) -> tuple[list[tuple[int, int]], float]:
-    """Compute the DTW alignment path between two time series.
+    """Compute the DTW_GI alignment path between two time series.
 
     Parameters
     ----------
@@ -389,6 +502,12 @@ def dtw_gi_alignment_path(
     itakura_max_slope : float, default=None
         Maximum slope as a proportion of the number of time points used to create
         Itakura parallelogram on the bounding matrix. Must be between 0. and 1.
+    init_p : array-like of shape (x_channels, y_channels), default=None
+        Initial linear transformation. If None, the identity matrix is used.
+    max_iter : int, default=20
+        Maximum number of iterations for the iterative optimization.
+    use_bias : bool, default=False
+        If True, the feature space map is affine (with a bias term).
 
     Returns
     -------
@@ -397,7 +516,7 @@ def dtw_gi_alignment_path(
         of the index in x and the index in y that have the best alignment according
         to the cost matrix.
     float
-        The DTW distance betweeen the two time series.
+        The DTW_GI distance betweeen the two time series.
 
     Raises
     ------
@@ -407,13 +526,15 @@ def dtw_gi_alignment_path(
     Examples
     --------
     >>> import numpy as np
-    >>> from aeon.distances import dtw_alignment_path
+    >>> from aeon.distances import dtw_gi_alignment_path
     >>> x = np.array([[1, 2, 3, 6]])
     >>> y = np.array([[1, 2, 3, 4]])
-    >>> dtw_alignment_path(x, y)
+    >>> dtw_gi_alignment_path(x, y)
     ([(0, 0), (1, 1), (2, 2), (3, 3)], 4.0)
     """
-    w_pi, _, _, cost, _, _ = dtw_gi(x, y, window, itakura_max_slope)
+    w_pi, _, _, cost, _, _ = _dtw_gi(
+        x, y, window, itakura_max_slope, init_p=None, max_iter=20, use_bias=False
+    )
     min_alignment_path = []
     for i in range(len(w_pi)):
         for j in range(len(w_pi[0])):
