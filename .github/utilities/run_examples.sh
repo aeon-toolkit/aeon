036912dde7a97a1bcec770367e609397e9222970
shinymack <akashkawle2995@gmail.com>
2025-03-23 00:34:49 +0530

    added signature_method.ipynb to excluded list

--- .github/utilities/run_examples.sh
+++ .github/utilities/run_examples.sh
@@ -23,6 +23,7 @@ if [ "$1" = true ]; then
     "examples/classification/shapelet_based.ipynb"
     "examples/classification/convolution_based.ipynb"
     "examples/similarity_search/code_speed.ipynb"
+    "examples/transformations/signature_method.ipynb"
 
   )
 fi
