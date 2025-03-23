e92a2a2af96f6a85ae51a8c6484727e704c947f5
Kaustbh <desalekaustubh3@gmail.com>
2025-03-23 13:57:48 +0530

    Fix Broken [Source] Link and Improve Documentation for suppress_output() Function

--- aeon/testing/utils/output_suppression.py
+++ aeon/testing/utils/output_suppression.py
@@ -11,7 +11,43 @@ from os import devnull
 
 @contextmanager
 def suppress_output(suppress_stdout=True, suppress_stderr=True):
-    """Redirects stdout and/or stderr to devnull."""
+    """
+    Context manager to suppress stdout and/or stderr output.
+
+    This function redirects standard output (stdout) and standard error (stderr)
+    to `devnull`, effectively silencing any print statements or error messages
+    within its context.
+
+    Parameters
+    ----------
+    suppress_stdout : bool, optional, default=True
+        If True, redirects stdout to null, suppressing print statements.
+    suppress_stderr : bool, optional, default=True
+        If True, redirects stderr to null, suppressing error messages.
+
+    Examples
+    --------
+    Suppressing both stdout and stderr:
+
+    >>> with suppress_output():
+    >>>     print("This will not be displayed")
+    >>>     import warnings
+    >>>     warnings.warn("This warning will be hidden", UserWarning)
+
+    Suppressing only stdout:
+
+    >>> with suppress_output(suppress_stdout=True, suppress_stderr=False):
+    >>>     print("This will not be shown")
+    >>>     import sys
+    >>>     sys.stderr.write("Error messages will still be visible")
+
+    Suppressing only stderr:
+
+    >>> with suppress_output(suppress_stdout=False, suppress_stderr=True):
+    >>>     print("This will be shown")
+    >>>     import sys
+    >>>     sys.stderr.write("This error message will be hidden")
+    """
     with open(devnull, "w") as null:
         stdout = sys.stdout
         stderr = sys.stderr
