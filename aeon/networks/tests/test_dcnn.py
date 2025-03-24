444a27e05beacd951a2c4e38b9b37ed9d99ba37b
Kavya Rambhia <kavyarambhia07@gmail.com>
2025-03-24 20:27:49 +0530

    Added Params in dcnn Test Suite

--- aeon/networks/tests/test_dcnn.py
+++ aeon/networks/tests/test_dcnn.py
@@ -30,7 +30,7 @@ def test_dcnnnetwork_init(latent_space_dim, n_layers):
         n_filters=[random.choice([50, 25, 100]) for _ in range(n_layers)],
     )
     model = dcnnnet.build_network((1000, 5))
-    assert model is not None
+    assert isinstance(model, object)
 
 
 @pytest.mark.skipif(
@@ -47,4 +47,50 @@ def test_dcnnnetwork_activations(activation):
         n_filters=[50, 50],
     )
     model = dcnnnet.build_network((150, 5))
-    assert model is not None
+    assert isinstance(model, object)
+
+
+@pytest.mark.parametrize(
+    "dilation_rate,kernel_size,activation,padding",
+    [
+        (None, None, None, None),
+        (2, 5, "relu", "causal"),
+        ([1, 2], [3, 5], ["relu", "tanh"], ["causal", "same"]),
+    ],
+)
+def test_dcnnnetwork_params(dilation_rate, kernel_size, activation, padding):
+    """Test DCNNNetwork initialization with different parameters."""
+    dcnnnet = DCNNNetwork(
+        latent_space_dim=64,
+        n_layers=2,
+        dilation_rate=dilation_rate,
+        kernel_size=kernel_size,
+        activation=activation,
+        padding=padding,
+        n_filters=[50, 50],
+    )
+    model = dcnnnet.build_network((150, 5))
+    assert isinstance(model, object)
+
+
+@pytest.mark.parametrize(
+    "dilation_rate,kernel_size,activation,padding",
+    [
+        (None, None, None, None),
+        (1, 3, "relu", "causal"),
+        ([1, 2, 1], [3, 5, 3], ["relu", "tanh", "relu"], ["causal", "same", "causal"]),
+    ],
+)
+def test_dcnnnetwork_varied_layers(dilation_rate, kernel_size, activation, padding):
+    """Test DCNNNetwork with varied layers and parameters."""
+    dcnnnet = DCNNNetwork(
+        latent_space_dim=128,
+        n_layers=3,
+        dilation_rate=dilation_rate,
+        kernel_size=kernel_size,
+        activation=activation,
+        padding=padding,
+        n_filters=[25, 50, 100],
+    )
+    model = dcnnnet.build_network((200, 10))
+    assert isinstance(model, object)
