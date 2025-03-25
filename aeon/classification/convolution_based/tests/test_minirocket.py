de0cc458c7ccaa45f06486ed1b37a776ad2b4e40
Ramana-Raja <ramanarajakesavaraja@gamil.com>
2025-03-25 23:00:34 +0530

    Added test cases for convo based classification

---
+++ aeon/classification/convolution_based/tests/test_minirocket.py
@@ -0,0 +1,39 @@
+"""MiniRocket classifier test code."""
+
+import numpy as np
+from aeon.datasets import load_unit_test
+from aeon.classification.convolution_based import MiniRocketClassifier
+from aeon.datasets import load_basic_motions
+
+def test_minirocket_univariate():
+    X_train, y_train = load_unit_test(split="train")
+    X_test, y_test = load_unit_test(split="test")
+
+    clf = MiniRocketClassifier(num_kernels=20, max_dilations_per_kernel=6)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_proba = clf.predict_proba(X_test)
+
+    assert clf.is_fitted
+    assert y_pred.shape == (X_test.shape[0],)
+    assert set(y_pred).issubset(set(y_train))
+    assert y_proba.shape == (X_test.shape[0], len(np.unique(y_train)))
+    assert np.all(y_proba >= 0) and np.all(y_proba <= 1)
+
+def test_minirocket_multivariate():
+    X_train, y_train = load_basic_motions(split="train")
+    X_test, y_test = load_basic_motions(split="test")
+
+    clf = MiniRocketClassifier(num_kernels=20, max_dilations_per_kernel=6)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_proba = clf.predict_proba(X_test)
+
+    assert clf.is_fitted
+    assert y_pred.shape == (X_test.shape[0],)
+    assert set(y_pred).issubset(set(y_train))
+    assert y_proba.shape == (X_test.shape[0], len(np.unique(y_train)))
+    assert np.all(y_proba >= 0) and np.all(y_proba <= 1)
+
---
+++ aeon/classification/convolution_based/tests/test_rocket.py
@@ -0,0 +1,38 @@
+"""RandOm Convolutional KErnel Transform (Rocket) classifier test code."""
+
+import numpy as np
+from aeon.classification.convolution_based import RocketClassifier
+from aeon.datasets import load_unit_test
+from aeon.datasets import load_basic_motions
+
+def test_rocket_univariate():
+    X_train, y_train = load_unit_test(split="train")
+    X_test, y_test = load_unit_test(split="test")
+
+    clf = RocketClassifier(random_state=0)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_proba = clf.predict_proba(X_test)
+
+    assert clf.is_fitted
+    assert y_pred.shape == (X_test.shape[0],)
+    assert set(y_pred).issubset(set(y_train))
+    assert y_proba.shape == (X_test.shape[0], len(np.unique(y_train)))
+    assert np.all(y_proba >= 0) and np.all(y_proba <= 1)
+
+def test_rocket_multivariate():
+    X_train, y_train = load_basic_motions(split="train")
+    X_test, y_test = load_basic_motions(split="test")
+
+    clf = RocketClassifier(random_state=0)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_proba = clf.predict_proba(X_test)
+
+    assert clf.is_fitted
+    assert y_pred.shape == (X_test.shape[0],)
+    assert set(y_pred).issubset(set(y_train))
+    assert y_proba.shape == (X_test.shape[0], len(np.unique(y_train)))
+    assert np.all(y_proba >= 0) and np.all(y_proba <= 1)
