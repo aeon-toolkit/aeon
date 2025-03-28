88cdbe336efdbcbafdb8d9f1f570a00e66220ad6
Aryan <aryanpola1603@gmail.com>
2025-03-28 20:23:15 +0530

    point 2 working

--- .github/utilities/issue_assign.py
+++ .github/utilities/issue_assign.py
@@ -3,6 +3,10 @@
 It checks if a comment on an issue or PR includes the trigger
 phrase (as defined) and a mentioned user.
 If it does, it assigns the issue/PR to the mentioned user.
+Users without write access can only have up to 2 open issues assigned.
+Users with write access (or admin) are exempt from this limit.
+If a non-write user already has 2 or more open issues, the bot
+comments on the issue with links to the currently assigned issues.
 """
 
 import json
@@ -28,4 +32,32 @@ if "@aeon-actions-bot" in body and "assign" in body:
     mentioned_users.remove("aeon-actions-bot")
 
     for user in mentioned_users:
-        issue.add_to_assignees(user)
+        user_obj = g.get_user(user)
+        permission = repo.get_collaborator_permission(user_obj)
+
+        if permission in ["admin", "write"]:
+            issue.add_to_assignees(user)
+        else:
+            # search for open issues
+            query = f"repo:{repo.full_name} is:issue is:open assignee:{user}"
+            issues_assigned_to_user = g.search_issues(query)
+            assigned_count = issues_assigned_to_user.totalCount
+
+            if assigned_count >= 2:
+                # link to issue
+                assigned_issues_list = [
+                    f"[#{assigned_issue.number}]({assigned_issue.html_url})"
+                    for assigned_issue in issues_assigned_to_user
+                ]
+
+                comment_message = (
+                    f"@{user}, you already have {assigned_count} open issues assigned. "
+                    "Please complete them before taking on a new issue.\n\n"
+                    "Here are the issues currently assigned to you:\n"
+                    + "\n".join(
+                        f"- {issue_link}" for issue_link in assigned_issues_list
+                    )
+                )
+                issue.create_comment(comment_message)
+            else:
+                issue.add_to_assignees(user)
