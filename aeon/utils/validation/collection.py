7240e98eac6d6bda79085cae35e0ed43f157f98d
Matthew Middlehurst <pfm15hbu@gmail.com>
2025-04-11 20:24:10 +0100

    [ENH] Collection conversion cleanup and `df-list` fix (#2654)
    * collection conversion cleanup
    
    * notebook
    
    * fixes
    
    ---------
    
    Co-authored-by: Tony Bagnall <a.j.bagnall@soton.ac.uk>

--- aeon/utils/validation/collection.py
+++ aeon/utils/validation/collection.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from numba.typed import List as NumbaList
 
-__maintainer__ = ["TonyBagnall"]
+__maintainer__ = ["TonyBagnall", "MatthewMiddlehurst"]
 
 
 def is_tabular(X):
@@ -14,23 +14,19 @@ def is_tabular(X):
 
     Parameters
     ----------
-    X : array-like
+    X : collection
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
     bool
         True if input is 2D, False otherwise.
     """
-    if isinstance(X, np.ndarray):
-        if X.ndim != 2:
-            return False
-        return True
-    if isinstance(X, pd.DataFrame):
-        return _is_pd_wide(X)
+    return get_type(X, raise_error=False) in ["numpy2D", "pd-wide"]
 
 
 def is_collection(X, include_2d=False):
-    """Check X is a valid collection data structure.
+    """Check X is a valid 3d collection data structure.
 
     Parameters
     ----------
@@ -44,40 +40,16 @@ def is_collection(X, include_2d=False):
     bool
         True if input is a collection, False otherwise.
     """
-    if isinstance(X, np.ndarray):
-        if X.ndim == 3:
-            return True
-        if include_2d and X.ndim == 2:
-            return True
-    if isinstance(X, pd.DataFrame):
-        if X.index.nlevels == 2:
-            return True
-        if include_2d and _is_pd_wide(X):
-            return True
-    if isinstance(X, list):
-        if isinstance(X[0], np.ndarray):
-            if X[0].ndim == 2:
-                return True
-        if isinstance(X[0], pd.DataFrame):
-            return True
-    return False
-
-
-def _is_pd_wide(X):
-    """Check whether the input DataFrame is "pd-wide" type."""
-    # only test is if all values are float.
-    if isinstance(X, pd.DataFrame) and not isinstance(X.index, pd.MultiIndex):
-        for col in X:
-            if not np.issubdtype(X[col].dtype, np.floating):
-                return False
-        return True
-    return False
+    valid = ["numpy3D", "np-list", "df-list", "pd-multiindex"]
+    if include_2d:
+        valid += ["numpy2D", "pd-wide"]
+    return get_type(X, raise_error=False) in valid
 
 
 def get_n_cases(X):
-    """Return the number of cases in a collectiom.
+    """Return the number of cases in a collection.
 
-    Handle the single exception of multi index DataFrame.
+    Returns len(X) except for "pd-multiindex".
 
     Parameters
     ----------
@@ -89,7 +61,8 @@ def get_n_cases(X):
     int
         Number of cases.
     """
-    if isinstance(X, pd.DataFrame) and isinstance(X.index, pd.MultiIndex):
+    t = get_type(X)
+    if t == "pd-multiindex":
         return len(X.index.get_level_values(0).unique())
     return len(X)
 
@@ -97,13 +70,13 @@ def get_n_cases(X):
 def get_n_timepoints(X):
     """Return the number of timepoints in the first element of a collection.
 
-    Handles the single exception of multi index DataFrames. If unequal length series,
-    returns the length of the first series.
+    If the collection contains unequal length series, returns the length of the first
+    series in the collection.
 
     Parameters
     ----------
     X : collection
-        See aeon.utils.COLLECTIONS_DATA_TYPES for details.
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
@@ -111,25 +84,21 @@ def get_n_timepoints(X):
         Number of time points in the first case.
     """
     t = get_type(X)
-    if t in ["numpy3D", "np-list"]:
+    if t in ["numpy3D", "np-list", "df-list"]:
         return X[0].shape[1]
-    if t in ["numpy2D", "df-list"]:
-        return X[0].shape[0]
+    if t in ["numpy2D", "pd-wide"]:
+        return X.shape[1]
     if t == "pd-multiindex":
-        return len(X.index.get_level_values(1).unique())
-    if t == "pd-wide":
-        return len(X.iloc[0])
+        return X.loc[X.index.get_level_values(0).unique()[0]].index.nunique()
 
 
 def get_n_channels(X):
-    """Return the number of channels in the first element of a collectiom.
-
-    Handle the single exception of multi index DataFrame.
+    """Return the number of channels in the first element of a collection.
 
     Parameters
     ----------
     X : collection
-        See aeon.utils.COLLECTIONS_DATA_TYPES for details.
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
@@ -139,13 +108,13 @@ def get_n_channels(X):
     Raises
     ------
     ValueError
-        X is list of 2D numpy but number of channels is not consistent.
-        X is list of 2D pd.DataFrames but number of channels is not consistent.
+        X is list of 2D numpy arrays or pd.DataFrames but number of channels is not
+        consistent.
     """
     t = get_type(X)
     if t == "numpy3D":
-        return X[0].shape[0]
-    if t == "np-list":
+        return X.shape[1]
+    if t in ["np-list", "df-list"]:
         if not all(arr.shape[0] == X[0].shape[0] for arr in X):
             raise ValueError(
                 f"ERROR: number of channels is not consistent. "
@@ -154,94 +123,20 @@ def get_n_channels(X):
         return X[0].shape[0]
     if t in ["numpy2D", "pd-wide"]:
         return 1
-    if t == "df-list":
-        if not all(arr.shape[1] == X[0].shape[1] for arr in X):
-            raise ValueError(
-                f"ERROR: number of channels is not consistent. "
-                f"Found values: {np.unique([arr.shape[1] for arr in X])}."
-            )
-        return X[0].shape[1]
     if t == "pd-multiindex":
-        return len(X.columns)
-
-
-def get_type(X):
-    """Get the string identifier associated with different data structures.
-
-    Parameters
-    ----------
-    X : collection
-        See aeon.utils.COLLECTIONS_DATA_TYPES for details.
-
-    Returns
-    -------
-    input_type : string
-        One of COLLECTIONS_DATA_TYPES.
-
-    Raises
-    ------
-    ValueError
-        X pd.ndarray but wrong dimension
-        X is list but not of np.ndarray or p.DataFrame.
-        X is a pd.DataFrame of non float primitives.
-
-    Examples
-    --------
-    >>> from aeon.utils.validation import get_type
-    >>> get_type( np.zeros(shape=(10, 3, 20)))
-    'numpy3D'
-    """
-    if isinstance(X, np.ndarray):  # "numpy3D" or numpy2D
-        if X.ndim == 3:
-            return "numpy3D"
-        elif X.ndim == 2:
-            return "numpy2D"
-        else:
-            raise ValueError(
-                f"ERROR np.ndarray must be 2D or 3D but found " f"{X.ndim}"
-            )
-    elif isinstance(X, list):  # np-list or df-list
-        if isinstance(X[0], np.ndarray):  # if one a numpy they must all be 2D numpy
-            for a in X:
-                if not (isinstance(a, np.ndarray) and a.ndim == 2):
-                    raise TypeError(
-                        f"ERROR nnp-list must contain 2D np.ndarray but found {a.ndim}"
-                    )
-            return "np-list"
-        elif isinstance(X[0], pd.DataFrame):
-            for a in X:
-                if not isinstance(a, pd.DataFrame):
-                    raise TypeError("ERROR df-list must only contain pd.DataFrame")
-            return "df-list"
-        else:
-            raise TypeError(
-                f"ERROR passed a list containing {type(X[0])}, "
-                f"lists should either 2D numpy arrays or pd.DataFrames."
-            )
-    elif isinstance(X, pd.DataFrame):  # Nested univariate, hierarchical or pd-wide
-        if isinstance(X.index, pd.MultiIndex):
-            return "pd-multiindex"
-        elif _is_pd_wide(X):
-            return "pd-wide"
-        raise TypeError(
-            "ERROR unknown pd.DataFrame, contains non float values, "
-            "not hierarchical nor is it nested pd.Series"
-        )
-    raise TypeError(
-        f"ERROR passed input of type {type(X)}, must be of type "
-        f"np.ndarray, pd.DataFrame or list of np.ndarray/pd.DataFrame"
-    )
+        return X.columns.nunique()
 
 
 def is_equal_length(X):
     """Test if X contains equal length time series.
 
-    Assumes input_type is a valid type (COLLECTIONS_DATA_TYPES).
+    Assumes input_type is a valid type
+    (See aeon.utils.data_types.COLLECTIONS_DATA_TYPES).
 
     Parameters
     ----------
     X : collection
-        See aeon.utils.COLLECTIONS_DATA_TYPES for details.
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
@@ -259,7 +154,22 @@ def is_equal_length(X):
     >>> is_equal_length( np.zeros(shape=(10, 3, 20)))
     True
     """
-    return _equal_length(X, get_type(X))
+    input_type = get_type(X)
+    if input_type in ["numpy3D", "numpy2D", "pd-wide"]:
+        return True
+
+    if input_type in ["np-list", "df-list"]:
+        for i in range(1, len(X)):
+            if X[i].shape[1] != X[0].shape[1]:
+                return False
+        return True
+    if input_type == "pd-multiindex":
+        cases = X.index.get_level_values(0).unique()
+        length = X.loc[cases[0]].index.nunique()
+        for case in cases:
+            if X.loc[case].index.nunique() != length:
+                return False
+        return True
 
 
 def has_missing(X):
@@ -268,8 +178,7 @@ def has_missing(X):
     Parameters
     ----------
     X : collection
-    input_type : string
-        One of COLLECTIONS_DATA_TYPES.
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
@@ -287,11 +196,11 @@ def has_missing(X):
     >>> m = has_missing( np.zeros(shape=(10, 3, 20)))
     """
     type = get_type(X)
-    if type == "numpy3D" or type == "numpy2D":
-        return np.any(np.isnan(np.min(X)))
+    if type in ["numpy3D", "numpy2D"]:
+        return np.any(np.isnan(X))
     if type == "np-list":
         for x in X:
-            if np.any(np.isnan(np.min(x))):
+            if np.any(np.isnan(x)):
                 return True
         return False
     if type == "df-list":
@@ -299,85 +208,151 @@ def has_missing(X):
             if x.isnull().any().any():
                 return True
         return False
-    if type == "pd-wide":
+    if type in ["pd-wide", "pd-multiindex"]:
         return X.isnull().any().any()
-    if type == "pd-multiindex":
-        if X.isna().values.any():
-            return True
-        return False
 
 
-def is_univariate(X, is_collection=True):
-    """Check if X is multivariate."""
-    type = get_type(X)
-    if type == "numpy2D" and is_collection:
-        return True
-    if type == "numpy2D" and not is_collection:
-        return X.shape[0] == 1
-    if type == "pd-wide":
-        return True
-    if type == "numpy3D":
-        return X.shape[1] == 1
-    # df list (n_timepoints, n_channels)
-    if type == "df-list":
-        return X[0].shape[1] == 1
-    # np list (n_channels, n_timepoints)
-    if type == "np-list":
-        return X[0].shape[0] == 1
-    if type == "pd-multiindex":
-        return X.columns.shape[0] == 1
-
-
-def _equal_length(X, input_type):
-    """Test if X contains equal length time series.
-
-    Assumes input_type is a valid type (COLLECTIONS_DATA_TYPES).
+def is_univariate(X):
+    """Check if X is multivariate.
 
     Parameters
     ----------
     X : collection
-    input_type : string
-        one of COLLECTIONS_DATA_TYPES
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
 
     Returns
     -------
-    boolean
-        True if all series in X are equal length, False otherwise
+    bool
+        True if series is univariate, else False.
 
     Raises
     ------
     ValueError
-        input_type not in COLLECTIONS_DATA_TYPES.
+        X is list of 2D numpy arrays or pd.DataFrames but number of channels is not
+        consistent.
+    """
+    return get_n_channels(X) == 1
+
+
+def get_type(X, raise_error=True):
+    """Get the string identifier associated with different data structures.
+
+    Parameters
+    ----------
+    X : collection
+        See aeon.utils.data_types.COLLECTIONS_DATA_TYPES for details.
+
+    Returns
+    -------
+    input_type : string
+        One of COLLECTIONS_DATA_TYPES.
+    raise_error : bool, default=True
+        If True, raise a ValueError if the input is not a valid type.
+        If False, returns None when an error would be raised.
+
+    Raises
+    ------
+    ValueError
+        X np.ndarray but does not have 2 or 3 dimensions.
+        X is a list but not of np.ndarray or pd.DataFrame or contained data has an
+        inconsistent number of channels.
+        X is a pd.DataFrame of non float primitives.
+        X is not a valid type.
 
     Examples
     --------
-    >>> _equal_length( np.zeros(shape=(10, 3, 20)), "numpy3D")
-    True
+    >>> from aeon.utils.validation import get_type
+    >>> get_type( np.zeros(shape=(10, 3, 20)))
+    'numpy3D'
     """
-    always_equal = {"numpy3D", "numpy2D", "pd-wide"}
-    if input_type in always_equal:
-        return True
-    # np-list are shape (n_channels, n_timepoints)
-    if input_type == "np-list":
-        first = X[0].shape[1]
-        for i in range(1, len(X)):
-            if X[i].shape[1] != first:
-                return False
-        return True
-    # df-list are shape (n_timepoints, n_channels)
-    if input_type == "df-list":
-        first = X[0].shape[0]
-        for i in range(1, len(X)):
-            if X[i].shape[0] != first:
-                return False
-        return True
-    if input_type == "pd-multiindex":  # multiindex dataframe
-        X = X.reset_index(-1).drop(X.columns, axis=1)
-        return (
-            X.groupby(level=0, group_keys=True, as_index=True).count().nunique().iloc[0]
-            == 1
+    msg = None
+    if isinstance(X, np.ndarray):  # "numpy3D" or numpy2D
+        if X.ndim == 3:
+            return "numpy3D"
+        elif X.ndim == 2:
+            return "numpy2D"
+        else:
+            msg = f"ERROR np.ndarray must be 2D or 3D but found " f"{X.ndim}"
+    elif isinstance(X, list):  # np-list or df-list
+        if isinstance(X[0], np.ndarray):
+            for a in X:
+                # if one a numpy they must all be 2D numpy
+                if not (isinstance(a, np.ndarray) and a.ndim == 2):
+                    msg = f"ERROR np-list must contain 2D np.ndarray but found {a.ndim}"
+                    break
+            if msg is None:
+                return "np-list"
+        elif isinstance(X[0], pd.DataFrame):
+            for a in X:
+                if not isinstance(a, pd.DataFrame):
+                    msg = "ERROR df-list must only contain pd.DataFrame"
+                    break
+                if not _is_pd_wide(a):
+                    msg = (
+                        "ERROR df-list must contain non-multiindex pd.DataFrame with"
+                        "numeric values"
+                    )
+                    break
+            if msg is None:
+                return "df-list"
+        else:
+            msg = (
+                f"ERROR passed a list containing {type(X[0])}, "
+                f"lists should either 2D numpy arrays or pd.DataFrames."
+            )
+    elif isinstance(X, pd.DataFrame):  # pd-multiindex or pd-wide
+        if _is_pd_multiindex(X):
+            return "pd-multiindex"
+        elif _is_pd_wide(X):
+            return "pd-wide"
+        else:
+            msg = (
+                "ERROR unknown pd.DataFrame, DataFrames must contain numeric values "
+                "only and meet pd-multiindex or pd-wide specification."
+            )
+    else:
+        msg = (
+            f"ERROR passed input of type {type(X)}, must be of type "
+            f"np.ndarray, pd.DataFrame or list of np.ndarray/pd.DataFrame."
+            f"See aeon.utils.data_types.COLLECTIONS_DATA_TYPES"
         )
-    raise ValueError(f" unknown input type {input_type}")
+
+    if raise_error and msg is not None:
+        raise TypeError(msg)
+    return None
+
+
+def _is_pd_multiindex(X):
+    """Check whether the input DataFrame is "pd-multiindex" type."""
+    if (
+        isinstance(X, pd.DataFrame)
+        and isinstance(X.index, pd.MultiIndex)
+        and not isinstance(X.columns, pd.MultiIndex)
+        and len(X.index.levels) == 2
+    ):
+        for col in X:
+            if not np.issubdtype(X[col].dtype, np.floating) and not np.issubdtype(
+                X[col].dtype, np.integer
+            ):
+                return False
+        return True
+    return False
+
+
+def _is_pd_wide(X):
+    """Check whether the input DataFrame is "pd-wide" type."""
+    if (
+        isinstance(X, pd.DataFrame)
+        and not isinstance(X.index, pd.MultiIndex)
+        and not isinstance(X.columns, pd.MultiIndex)
+    ):
+        for col in X:
+            if not np.issubdtype(X[col].dtype, np.floating) and not np.issubdtype(
+                X[col].dtype, np.integer
+            ):
+                return False
+        return True
+    return False
 
 
 def _is_numpy_list_multivariate(
