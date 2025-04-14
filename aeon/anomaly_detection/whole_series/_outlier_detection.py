1920ee49131f4bb0a8e5a4b261466cc3e32c137e
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-04-14 15:18:11 +0100

    wrappers

--- aeon/anomaly_detection/whole_series/_outlier_detection.py
+++ aeon/anomaly_detection/whole_series/_outlier_detection.py
@@ -1,33 +1,55 @@
-"""Basic outlier detection classifier."""
+"""Adapter to use outlier detection algorithms for collection anomaly detection."""
 
+__maintainer__ = []
+
+from sklearn.base import OutlierMixin
 from sklearn.ensemble import IsolationForest
 
 from aeon.anomaly_detection.whole_series.base import BaseCollectionAnomalyDetector
 from aeon.base._base import _clone_estimator
 
 
-class OutlierDetectionClassifier(BaseCollectionAnomalyDetector):
-    """Basic outlier detection classifier."""
+class OutlierDetectionAdapter(BaseCollectionAnomalyDetector):
+    """
+    Basic outlier detection adapter for collection anomaly detection.
+
+    This class wraps an sklearn outlier detection algorithm to be used as an anomaly
+    detector.
+
+    Parameters
+    ----------
+    detector : OutlierMixin
+        The outlier detection algorithm to be adapted.
+    random_state : int, RandomState instance or None, default=None
+        If `int`, random_state is the seed used by the random number generator;
+        If `RandomState` instance, random_state is the random number generator;
+        If `None`, the random number generator is the `RandomState` instance used
+        by `np.random`.
+    """
 
     _tags = {
         "X_inner_type": "numpy2D",
     }
 
-    def __init__(self, estimator, random_state=None):
-        self.estimator = estimator
+    def __init__(self, detector, random_state=None):
+        self.detector = detector
         self.random_state = random_state
 
         super().__init__()
 
     def _fit(self, X, y=None):
-        self.estimator_ = _clone_estimator(
-            self.estimator, random_state=self.random_state
-        )
-        self.estimator_.fit(X, y)
+        if not isinstance(self.detector, OutlierMixin):
+            raise ValueError(
+                "The estimator must be an outlier detection algorithm "
+                "that implements the OutlierMixin interface."
+            )
+
+        self.detector_ = _clone_estimator(self.detector, random_state=self.random_state)
+        self.detector_.fit(X, y)
         return self
 
     def _predict(self, X):
-        pred = self.estimator_.predict(X)
+        pred = self.detector_.predict(X)
         pred[pred == -1] = 0
         return pred
 
