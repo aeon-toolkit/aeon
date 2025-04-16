24881a8e1372294b48f7764f64c4efd62af365f3
tanishy7777 <tanishyelgoe777@gmail.com>
2025-04-17 00:28:04 +0530

    Adds numba support and fixes alignment patha and cost helper

--- aeon/distances/elastic/_dtw_arow.py
+++ aeon/distances/elastic/_dtw_arow.py
@@ -5,8 +5,7 @@ __maintainer__ = []
 from typing import Optional, Union
 
 import numpy as np
-
-# from numba import njit
+from numba import njit
 from numba.typed import List as NumbaList
 
 from aeon.distances.pointwise._squared import _univariate_squared_distance
@@ -14,6 +13,7 @@ from aeon.utils.conversion._convert_collection import _convert_collection_to_num
 from aeon.utils.validation.collection import _is_numpy_list_multivariate
 
 
+@njit(cache=True)
 def dtw_arow_distance(x: np.ndarray, y: np.ndarray) -> float:
     if x.ndim == 1 and y.ndim == 1:
         _x = x.reshape((1, x.shape[0]))
@@ -24,16 +24,18 @@ def dtw_arow_distance(x: np.ndarray, y: np.ndarray) -> float:
     raise ValueError("x and y must be 1D or 2D")
 
 
+@njit(cache=True)
 def dtw_arow_cost_matrix(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     if x.ndim == 1 and y.ndim == 1:
         _x = x.reshape((1, x.shape[0]))
         _y = y.reshape((1, y.shape[0]))
-        return _dtw_arow_cost_matrix(_x, _y)
+        return np.sqrt(_dtw_arow_cost_matrix(_x, _y))
     if x.ndim == 2 and y.ndim == 2:
-        return _dtw_arow_cost_matrix(x, y)
+        return np.sqrt(_dtw_arow_cost_matrix(x, y))
     raise ValueError("x and y must be 1D or 2D")
 
 
+@njit(cache=True)
 def _dtw_arow_distance(x: np.ndarray, y: np.ndarray) -> float:
     M1av = _total_non_nan(x)
     M2av = _total_non_nan(y)
@@ -41,23 +43,27 @@ def _dtw_arow_distance(x: np.ndarray, y: np.ndarray) -> float:
     return np.sqrt(gamma * _dtw_arow_cost_matrix(x, y)[x.shape[1], y.shape[1]])
 
 
+@njit(cache=True)
 def _check_nan(x: np.ndarray) -> bool:
-    return np.isnan(x).any()
+    return np.sum(np.isnan(x)) > 0
 
 
+@njit(cache=True, fastmath=True)
 def _cost_function(x: np.ndarray, y: np.ndarray) -> float:
     if _check_nan(x) or _check_nan(y):
         return 0
     return _univariate_squared_distance(x, y)
 
 
+@njit(cache=True)
 def _total_non_nan(x: np.ndarray) -> float:
     if x.shape[0] == 1:
         return np.sum(~np.isnan(x[0]))
     else:
-        return np.sum(~np.isnan(x).any(axis=0))
+        return np.sum(np.sum(np.isnan(x), axis=0) == 0)
 
 
+@njit(cache=True)
 def _dtw_arow_cost_path_helper(
     x: np.ndarray, y: np.ndarray
 ) -> tuple[np.ndarray, np.ndarray]:
@@ -66,62 +72,51 @@ def _dtw_arow_cost_path_helper(
     cost_matrix = np.full((n + 1, m + 1), np.inf)
     phi = np.full((n + 1, m + 1), -1)
 
-    # cost_matrix[0, :] = 0
-    # cost_matrix[:, 0] = 0
     cost_matrix[0, 0] = 0
-    for i in range(1, n + 1):  # i from 1 to N
-        for j in range(1, m + 1):  # j from 1 to M
-            if j > 1:
-                if (
-                    _check_nan(x[:, i - 1])  # Xi is NaN
-                    or _check_nan(y[:, j - 1])  # Yj is NaN
-                    or _check_nan(y[:, j - 2])  # Yj-1 is NaN
-                ):
-                    eh = np.inf
-                else:
-                    eh = 0
-            else:
-                if _check_nan(x[:, i - 1]) or _check_nan(y[:, j - 1]):
-                    # Xi is NaN or Yj is NaN
-                    eh = np.inf
-                else:
-                    eh = 0
 
+    x_avail = np.zeros(n, dtype=np.bool_)
+    for k in range(n):
+        x_avail[k] = not _check_nan(x[:, k])
+
+    y_avail = np.zeros(m, dtype=np.bool_)
+    for k in range(m):
+        y_avail[k] = not _check_nan(y[:, k])
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            ev = np.inf
             if i > 1:
-                if (
-                    _check_nan(x[:, i - 1])  # Xi is NaN
-                    or _check_nan(x[:, i - 2])  # Xi-1 is NaN
-                    or _check_nan(y[:, j - 1])  # Yj is NaN
-                ):
-                    ev = np.inf
-                else:
-                    ev = 0
-            else:
-                if _check_nan(x[:, i - 1]) or _check_nan(y[:, j - 1]):
-                    # Xi is NaN or Yj is NaN
-                    ev = np.inf
-                else:
+                if y_avail[j - 1] and x_avail[i - 1] and x_avail[i - 2]:
                     ev = 0
 
-            # costfunc(Xi, Yj)
-            current_cost = _cost_function(x[:, i - 1], y[:, j - 1])
-            cost_diag = cost_matrix[i - 1, j - 1]  # 1 indexed
-            cost_horiz = cost_matrix[i, j - 1] + eh
+            eh = np.inf
+            if j > 1:
+                if x_avail[i - 1] and y_avail[j - 1] and y_avail[j - 2]:
+                    eh = 0
+
+            cost_diag = cost_matrix[i - 1, j - 1]
             cost_vert = cost_matrix[i - 1, j] + ev
+            cost_horiz = cost_matrix[i, j - 1] + eh
 
-            best_prev = min(cost_diag, cost_horiz, cost_vert)
-            cost_matrix[i, j] = current_cost + best_prev
-            phi[i, j] = np.argmin([cost_diag, cost_horiz, cost_vert])
+            options = np.array([cost_diag, cost_vert, cost_horiz])
+            best_prev = np.min(options)
 
-    # print(cost_matrix, phi)
+            if np.isinf(best_prev):
+                cost_matrix[i, j] = np.inf
+            else:
+                current_cost = _cost_function(x[:, i - 1], y[:, j - 1])
+                cost_matrix[i, j] = current_cost + best_prev
+                phi[i, j] = np.argmin(options)
 
     return cost_matrix, phi
 
 
+@njit(cache=True)
 def _dtw_arow_cost_matrix(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     return _dtw_arow_cost_path_helper(x, y)[0]
 
 
+@njit(cache=True)
 def dtw_arow_pairwise_distance(
     X: Union[np.ndarray, list[np.ndarray]],
     y: Optional[Union[np.ndarray, list[np.ndarray]]] = None,
@@ -136,6 +131,7 @@ def dtw_arow_pairwise_distance(
     return _dtw_arow_from_multiple_to_multiple_distance(_X, _y)
 
 
+@njit(cache=True)
 def _dtw_arow_pairwise_distance(X: NumbaList[np.ndarray]) -> np.ndarray:
     n_cases = len(X)
     distances = np.zeros((n_cases, n_cases))
@@ -148,6 +144,7 @@ def _dtw_arow_pairwise_distance(X: NumbaList[np.ndarray]) -> np.ndarray:
     return distances
 
 
+@njit(cache=True)
 def _dtw_arow_from_multiple_to_multiple_distance(
     x: NumbaList[np.ndarray], y: NumbaList[np.ndarray]
 ) -> np.ndarray:
@@ -162,6 +159,7 @@ def _dtw_arow_from_multiple_to_multiple_distance(
     return distances
 
 
+@njit(cache=True)
 def dtw_arow_alignment_path(
     x: np.ndarray,
     y: np.ndarray,
@@ -175,23 +173,24 @@ def dtw_arow_alignment_path(
     raise ValueError("x and y must be 1D or 2D")
 
 
+@njit(cache=True)
 def _dtw_arow_alignment_path(
     x: np.ndarray, y: np.ndarray
 ) -> tuple[list[tuple[int, int]], float]:
     i, j = x.shape[1], y.shape[1]
-    path = [(x.shape[1], y.shape[1])]
+    path = []
     phi = _dtw_arow_cost_path_helper(x, y)[1]
     while i > 0 and j > 0:
+        path.append((i - 1, j - 1))
         step = phi[i, j]
         if step == 0:
             i, j = i - 1, j - 1
         elif step == 1:
-            j = j - 1
-        elif step == 2:
             i = i - 1
+        elif step == 2:
+            j = j - 1
         else:
             break
-        path.append((i, j))
     path.reverse()
 
     return path, dtw_arow_distance(x, y)
