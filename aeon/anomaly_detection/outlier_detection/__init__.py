9f7360833d7fa73bb472ed557db5046df894e683
Akhil-Jasson <114808672+Akhil-Jasson@users.noreply.github.com>
2025-05-01 18:36:05 +0000

    Automatic `pre-commit` fixes

--- aeon/anomaly_detection/outlier_detection/__init__.py
+++ aeon/anomaly_detection/outlier_detection/__init__.py
@@ -1,13 +1,8 @@
 """Time Series Outlier Detection."""
 
-__all__ = [
-    "IsolationForest",
-    "PyODAdapter",
-    "STRAY",
-    "EIF"
-]
+__all__ = ["IsolationForest", "PyODAdapter", "STRAY", "EIF"]
 
+from aeon.anomaly_detection.outlier_detection._eif import EIF
 from aeon.anomaly_detection.outlier_detection._iforest import IsolationForest
 from aeon.anomaly_detection.outlier_detection._pyodadapter import PyODAdapter
 from aeon.anomaly_detection.outlier_detection._stray import STRAY
-from aeon.anomaly_detection.outlier_detection._eif import EIF
