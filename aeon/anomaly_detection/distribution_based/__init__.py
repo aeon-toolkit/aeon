ac97f4d97579ea765c97a8a9876663db77ebc8cc
Ramana-Raja <ramanarajakesavaraja@gmail.com>
2025-05-10 01:32:07 +0530

    fixed conflicts

--- aeon/anomaly_detection/distribution_based/__init__.py
+++ aeon/anomaly_detection/distribution_based/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     "COPOD",
     "DWT_MLEAD",
+    "IDK2"
 ]
 
 from aeon.anomaly_detection.distribution_based._copod import COPOD
 from aeon.anomaly_detection.distribution_based._dwt_mlead import DWT_MLEAD
+from aeon.anomaly_detection.distribution_based._idk import IDK2
