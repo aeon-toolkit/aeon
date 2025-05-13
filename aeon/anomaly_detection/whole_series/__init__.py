ecf2d7659f2eb0b640685d2aa92452a617b5b7ec
pattplatt <pattplatt@gmail.com>
2025-05-13 13:17:10 +0200

    add whole-series ROCKAD to __init__.py

--- aeon/anomaly_detection/whole_series/__init__.py
+++ aeon/anomaly_detection/whole_series/__init__.py
@@ -4,10 +4,12 @@ __all__ = [
     "BaseCollectionAnomalyDetector",
     "ClassificationAdapter",
     "OutlierDetectionAdapter",
+    "ROCKAD",
 ]
 
 from aeon.anomaly_detection.whole_series._classification import ClassificationAdapter
 from aeon.anomaly_detection.whole_series._outlier_detection import (
     OutlierDetectionAdapter,
 )
+from aeon.anomaly_detection.whole_series._rockad import ROCKAD
 from aeon.anomaly_detection.whole_series.base import BaseCollectionAnomalyDetector
