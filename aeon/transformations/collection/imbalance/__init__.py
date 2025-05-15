2337f67c04c849834257160f20dc08e70451abb1
Chuanhang Qiu <80885865+LinGinQiu@users.noreply.github.com>
2025-05-15 20:25:13 +0100

    Ported OHIT (#2573)

--- aeon/transformations/collection/imbalance/__init__.py
+++ aeon/transformations/collection/imbalance/__init__.py
@@ -1,6 +1,7 @@
 """Supervised transformers to rebalance colelctions of time series."""
 
-__all__ = ["ADASYN", "SMOTE"]
+__all__ = ["ADASYN", "SMOTE", "OHIT"]
 
 from aeon.transformations.collection.imbalance._adasyn import ADASYN
 from aeon.transformations.collection.imbalance._smote import SMOTE
+from aeon.transformations.collection.imbalance._ohit import OHIT
