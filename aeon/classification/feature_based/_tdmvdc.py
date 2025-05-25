42a82919fdbaca2255a2187001455e67653a4882
lucifer4073 <lucifer4073@gmail.com>
2025-05-25 19:24:46 +0530

    changed

--- aeon/classification/feature_based/_tdmvdc.py
+++ aeon/classification/feature_based/_tdmvdc.py
@@ -1,12 +1,7 @@
 """Tracking Differentiator-based Multiview Dilated Characteristics (TDMVDC).
 
-Enhances the discriminative power of time series data by applying a
-combination of tracking differentiator-based transformations and multiview
-dilated feature extraction. Leverages advanced feature engineering techniques
-to capture both the original and differential characteristics of time series at
-multiple temporal resolutions. Integrates feature selection and ensemble
-learning to improve classification accuracy and robustness across diverse time
-series datasets.
+Ensemble classifier using TSFresh features and ANOVA,
+with RidgeClassifierCV and hard voting.
 """
 
 import numpy as np
@@ -17,12 +12,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from aeon.classification.base import BaseClassifier
-from aeon.transformations.collection.feature_based import (
-    TSFresh,
-    hard_voting,
-    series_set_dilation,
-    series_transform,
-)
+from aeon.transformations.collection.feature_based import TSFresh
 from aeon.utils.validation import check_n_jobs
 
 __all__ = ["TDMVDCClassifier"]
@@ -140,6 +130,188 @@ class TDMVDCClassifier(BaseClassifier):
         self.parallel_backend = parallel_backend
         super().__init__()
 
+    def _series_set_dilation(self, seriesX, d_rate=1):
+        """
+        Map each series of the time series set by dilation mapping.
+
+        Should have the same dilation rate.
+
+        Parameters
+        ----------
+        seriesX : 3D np.ndarray of shape = [n_cases, n_channels, n_timepoints]
+            The set of three dimensional time series set to be dilated.
+        d_rate : int, default=1
+            Dilation rate.
+
+        References
+        ----------
+        .. [1] P. Schaefer and U. Leser, "WEASEL 2.0: a random dilated dictionary
+        transform for fast, accurate and memory constrained time series classification"
+        Machine Learning, vol. 112, no. 12, pp. 4763–4788, Dec.(2024).
+        """
+        n_cases, n_channels, _ = seriesX.shape[:]
+        seriesXE = np.zeros_like(seriesX)  # Initializing the dilated time series set
+        for i in range(n_cases):
+            for j in range(n_channels):
+                series_ = []
+                for d in range(d_rate):
+                    series_.append(seriesX[i, j, d::d_rate])
+                seriesXE[i, j, :] = np.hstack(series_)
+        return seriesXE  # Return the dilated time series set
+
+    def _fhan(self, x1, x2, r, h0):
+        """
+        Calculate differential signal based on optimal control.
+
+        Parameters
+        ----------
+        x1 : float
+            State 1 of the observer.
+        x2 : float
+            State 2 of the observer.
+        r: float
+            Velocity factor used to control tracking speed.
+        h0 : float
+            Step size.
+
+        References
+        ----------
+        .. [1] J. Han, "From PID to active disturbance rejection control" IEEE Trans.
+        Ind. Electron., vol. 56, no. 3, pp. 900-906, Mar. (2009)..
+        """
+        d = r * h0
+        d0 = d * h0
+        y = x1 + h0 * x2  # Computing the differential signal
+        a0 = np.sqrt(d * d + 8 * r * np.abs(y))
+        if np.abs(y) > d0:
+            a = x2 + (a0 - d) / 2.0 * np.sign(y)
+        else:
+            a = x2 + y / h0
+        if np.abs(a) <= d:  # Computing the input u of observer
+            u = -r * a / d
+        else:
+            u = -r * np.sign(a)
+        return u, y  # Return input u of observer, and differential signal y
+
+    def _td(self, signal, r=100, k=3, h=1):
+        """
+        Compute a differential signal using the tracking differentiator.
+
+        with an adjustable filter factor.
+
+        Parameters
+        ----------
+        signal : 1D np.ndarray of shape = [n_timepoints]
+            Original time series
+        r : float
+            Velocity factor used to control tracking speed.
+        k: float
+            Filter factor.
+        h : float
+            Step size.
+
+        References
+        ----------
+        .. [1] J. Han, "From PID to active disturbance rejection control" IEEE Trans.
+        Ind. Electron., vol. 56, no. 3, pp. 900-906, Mar. (2009)..
+        """
+        x1 = signal[0]  # Initializing state 1
+        x2 = -(signal[1] - signal[0]) / h  # Initializing state 2
+        h0 = k * h
+        signalTD = np.zeros(len(signal))
+        dSignal = np.zeros(len(signal))
+        for i in range(len(signal)):
+            v = signal[i]
+            x1k = x1
+            x2k = x2
+            x1 = x1k + h * x2k  # Update state 1
+            u, y = self._fhan(
+                x1k - v, x2k, r, h0
+            )  # Update input u of observer and differential signal y
+            x2 = x2k + h * u  # Update state 2
+            dSignal[i] = y
+            signalTD[i] = x1
+        dSignal = -dSignal / h0  # Scale transform
+        return dSignal[1:]  # Return the differential signal
+
+    def _series_transform(self, seriesX, mode=1, k1=2, k2=2):
+        """
+        Transform each series of the time series set using a tracking differentiator.
+
+        with an adjustable filter factor.
+
+        Parameters
+        ----------
+        seriesX : 3D np.ndarray of shape = [n_cases, n_channels, n_timepoints]
+            The set of three dimensional time series set to be dilated.
+        mode : int, default=1
+            The flag bit of a first-order or second-order derivative is used.
+            Computing the first-order derivative when mode=1,
+            and computing the second-order derivative when mode=2
+        k1 : float, default=2
+            Filter factor 1 of the tracking differentiator 1.
+        k2 : float, default=2
+            Filter factor 2 of the tracking differentiator 2.
+            This parameter is invalid when mode=2.
+
+        References
+        ----------
+        .. [1] J. Han, "From PID to active disturbance rejection control" IEEE Trans.
+        Ind. Electron., vol. 56, no. 3, pp. 900-906, Mar. (2009)..
+        """
+        from sklearn.preprocessing import scale
+
+        n_cases, n_channels, n_timepoints = seriesX.shape[:]
+        if mode == 1:  # First-order derivative
+            seriesFX = np.zeros((n_cases, n_channels, n_timepoints - 1))
+            for i in range(n_cases):
+                for j in range(n_channels):
+                    seriesFX[i, j, :] = self._td(seriesX[i, j, :], k=k1)
+                    seriesFX[i, j, :] = scale(seriesFX[i, j, :])
+            return seriesFX  # Return the first-order differential time series set
+        if mode == 2:  # Second-order derivative
+            seriesSX = np.zeros((n_cases, n_channels, n_timepoints - 2))
+            for i in range(n_cases):
+                for j in range(n_channels):
+                    seriesF_ = self._td(seriesX[i, j, :], k=k1)
+                    seriesSX[i, j, :] = self._td(seriesF_, k=k2)
+                    seriesSX[i, j, :] = scale(seriesSX[i, j, :])
+            return seriesSX  # Return the second-order differential time series set
+
+    def _hard_voting(self, testYList):
+        """
+        Obtain the predicted labels by hard voting.
+
+        to process the labels matrix from multiple classifiers.
+
+        Parameters
+        ----------
+        testYList : 2D np.ndarray of shape = [n_classifierss, n_cases]
+        """
+        uniqueY = np.unique(testYList)  # Holds the label for each class
+        n_classes = len(uniqueY)  # Number of classes
+        n_classifiers, n_cases = testYList.shape[
+            :
+        ]  # Number of classifiers, Number of cases
+        testVY = np.zeros(
+            n_cases, int
+        )  # 1 * n_cases, Initializing the predicted labels
+        testWeightArray = np.zeros(
+            (n_classes, n_cases)
+        )  # n_classes * n_cases, Label weight matrix for samples
+        for i in range(n_cases):
+            for j in range(n_classifiers):
+                label_ = testYList[j, i]
+                index_ = np.arange(n_classes)[uniqueY == label_]
+                testWeightArray[
+                    index_, i
+                ] += 1  # The label weight for the sample is + 1
+        for i in range(n_cases):  # Predicting each sample label
+            testVY[i] = uniqueY[
+                np.argmax(testWeightArray[:, i])
+            ]  # The label is predicted to be the most weighted
+        return testVY  # return the predicted labels
+
     def _fit(self, X, y):
         """Fit a pipeline on cases (X, y).
 
@@ -163,8 +335,8 @@ class TDMVDCClassifier(BaseClassifier):
         self.dList_ = 2 ** np.arange(d_min, d_max + 1)
 
         # Differential transformations by tracking differentiator
-        X_F = series_transform(X, mode=1, k1=self.k1)
-        X_S = series_transform(X, mode=2, k1=self.k1, k2=self.k2)
+        X_F = self._series_transform(X, mode=1, k1=self.k1)
+        X_S = self._series_transform(X, mode=2, k1=self.k1, k2=self.k2)
 
         # Feature extraction
         self.tsFreshListR_ = []
@@ -240,9 +412,9 @@ class TDMVDCClassifier(BaseClassifier):
             Tuple containing feature extractors and extracted features.
         """
         # Dilation Mapping
-        RX_E = series_set_dilation(X, d_rate)
-        FX_E = series_set_dilation(X_F, d_rate)
-        SX_E = series_set_dilation(X_S, d_rate)
+        RX_E = self._series_set_dilation(X, d_rate)
+        FX_E = self._series_set_dilation(X_F, d_rate)
+        SX_E = self._series_set_dilation(X_S, d_rate)
 
         # Extracting the TSFresh features
         tsFreshR = TSFresh(
@@ -314,8 +486,8 @@ class TDMVDCClassifier(BaseClassifier):
         y : array-like, shape = [n_cases]
             Predicted class labels.
         """
-        X_F = series_transform(X, mode=1, k1=self.k1)
-        X_S = series_transform(X, mode=2, k1=self.k1, k2=self.k2)
+        X_F = self._series_transform(X, mode=1, k1=self.k1)
+        X_S = self._series_transform(X, mode=2, k1=self.k1, k2=self.k2)
 
         # Use parallel processing for feature extraction
         threads_to_use = check_n_jobs(self.n_jobs)
@@ -355,7 +527,7 @@ class TDMVDCClassifier(BaseClassifier):
         PYList = np.vstack(PYList)
 
         # Final prediction by hard voting
-        PYV = hard_voting(PYList)
+        PYV = self._hard_voting(PYList)
 
         return PYV
 
@@ -381,9 +553,9 @@ class TDMVDCClassifier(BaseClassifier):
             Tuple containing extracted features.
         """
         # Dilation Mapping
-        RX_E = series_set_dilation(X, d_rate)
-        FX_E = series_set_dilation(X_F, d_rate)
-        SX_E = series_set_dilation(X_S, d_rate)
+        RX_E = self._series_set_dilation(X, d_rate)
+        FX_E = self._series_set_dilation(X_F, d_rate)
+        SX_E = self._series_set_dilation(X_S, d_rate)
 
         # Extracting the TSFresh features
         tsFreshR = self.tsFreshListR_[i]
--- aeon/transformations/collection/feature_based/__init__.py
+++ aeon/transformations/collection/feature_based/__init__.py
@@ -14,13 +14,6 @@ __all__ = [
 
 from aeon.transformations.collection.feature_based._catch22 import Catch22
 from aeon.transformations.collection.feature_based._summary import SevenNumberSummary
-from aeon.transformations.collection.feature_based._tdmvdc_extractor import (
-    fhan,
-    hard_voting,
-    series_set_dilation,
-    series_transform,
-    td,
-)
 from aeon.transformations.collection.feature_based._tsfresh import (
     TSFresh,
     TSFreshRelevant,
