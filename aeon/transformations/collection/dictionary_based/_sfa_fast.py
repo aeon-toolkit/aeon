0ee4ec96ad1b4e831b3af775b6696edcb5694aa3
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2025-05-26 22:02:23 +0200

    bugfix

--- aeon/transformations/collection/dictionary_based/_sfa_fast.py
+++ aeon/transformations/collection/dictionary_based/_sfa_fast.py
@@ -1205,7 +1205,7 @@ def create_feature_names(sfa_words):
     return feature_names
 
 
-@njit(cache=True, fastmath=True, parallel=True)
+@njit(cache=True, fastmath=True)  # TODO does not work parallel=True ??
 def create_bag_none(
     X_index, breakpoints, n_cases, sfa_words, word_length, remove_repeat_words
 ):
