44b36a7b2d34c6b3452fdef97446a4ee83fe5789
Alex Banwell <arb1g19@soton.ac.uk>
2025-05-28 13:49:51 +0100

    Modularise SARIMA model

--- aeon/forecasting/_arima.py
+++ aeon/forecasting/_arima.py
@@ -9,10 +9,11 @@ __all__ = ["ARIMAForecaster"]
 from math import comb
 
 import numpy as np
-from numba import njit
 
 from aeon.forecasting.base import BaseForecaster
 from aeon.utils.forecasting._hypo_tests import kpss_test
+from aeon.utils.forecasting._seasonality import calc_seasonal_period
+from aeon.utils.optimisation._nelder_mead import nelder_mead
 
 NOGIL = False
 CACHE = True
@@ -83,11 +84,13 @@ class ARIMAForecaster(BaseForecaster):
         self.qs_ = 0
         self.seasonal_period_ = 0
         self.constant_term_ = 0
+        self.model_ = []
         self.c_ = 0
         self.phi_ = 0
         self.phi_s_ = 0
         self.theta_ = 0
         self.theta_s_ = 0
+        self.parameters_ = []
 
     def _fit(self, y, exog=None):
         """Fit AutoARIMA forecaster to series y.
@@ -109,32 +112,28 @@ class ARIMAForecaster(BaseForecaster):
         self.data_ = np.array(y.squeeze(), dtype=np.float64)
         (
             self.differenced_data_,
-            self.aic_,
-            self.p_,
             self.d_,
+            self.ds_,
+            self.model_,
+            self.parameters_,
+            self.aic_,
+        ) = _auto_arima(self.data_)
+        (
+            self.constant_term_,
+            self.p_,
             self.q_,
             self.ps_,
-            self.ds_,
             self.qs_,
             self.seasonal_period_,
-            self.constant_term_,
-            parameters,
-        ) = _auto_arima(self.data_)
+        ) = self.model_
         (self.c_, self.phi_, self.phi_s_, self.theta_, self.theta_s_) = _extract_params(
-            parameters, self.p_, self.q_, self.ps_, self.qs_, self.constant_term_
+            self.parameters_, self.model_
         )
         (
             self.aic_,
             self.residuals_,
-        ) = _arima_log_likelihood(
-            parameters,
-            self.differenced_data_,
-            self.p_,
-            self.q_,
-            self.ps_,
-            self.qs_,
-            self.seasonal_period_,
-            self.constant_term_,
+        ) = _arima_model(
+            self.parameters_, _calc_sarima, self.differenced_data_, self.model_
         )
         return self
 
@@ -156,19 +155,11 @@ class ARIMAForecaster(BaseForecaster):
             single prediction self.horizon steps ahead of y.
         """
         y = np.array(y, dtype=np.float64)
-        value = _calc_arima(
+        value = _calc_sarima(
             self.differenced_data_,
-            self.p_,
-            self.q_,
-            self.ps_,
-            self.qs_,
-            self.seasonal_period_,
+            self.model_,
             len(self.differenced_data_),
-            self.c_,
-            self.phi_,
-            self.phi_s_,
-            self.theta_,
-            self.theta_s_,
+            _extract_params(self.parameters_, self.model_),
             self.residuals_,
         )
         history = self.data_[::-1]
@@ -184,78 +175,86 @@ class ARIMAForecaster(BaseForecaster):
         return value
 
 
+def _aic(residuals, num_params):
+    """Calculate the log-likelihood of a model."""
+    variance = np.mean(residuals**2)
+    liklihood = len(residuals) * (np.log(2 * np.pi) + np.log(variance) + 1)
+    return liklihood + 2 * num_params
+
+
 # Define the ARIMA(p, d, q) likelihood function
-def _arima_log_likelihood(
-    params, data, p, q, ps, qs, seasonal_period, include_constant_term
-):
+def _arima_model(params, base_function, data, model):
     """Calculate the log-likelihood of an ARIMA model given the parameters."""
-    c, phi, phi_s, theta, theta_s = _extract_params(
-        params, p, q, ps, qs, include_constant_term
-    )  # Extract parameters
+    formatted_params = _extract_params(params, model)  # Extract parameters
 
     # Initialize residuals
     n = len(data)
     residuals = np.zeros(n)
     for t in range(n):
-        y_hat = _calc_arima(
+        y_hat = base_function(
             data,
-            p,
-            q,
-            ps,
-            qs,
-            seasonal_period,
+            model,
             t,
-            c,
-            phi,
-            phi_s,
-            theta,
-            theta_s,
+            formatted_params,
             residuals,
         )
         residuals[t] = data[t] - y_hat
-    # Calculate the log-likelihood
-    variance = np.mean(residuals**2)
-    liklihood = n * (np.log(2 * np.pi) + np.log(variance) + 1)
-    k = len(params)
-    aic = liklihood + 2 * k
-    return (
-        aic,
-        residuals,
-    )  # Return negative log-likelihood for minimization
+    return _aic(residuals, len(params)), residuals
 
 
-def _extract_params(params, p, q, ps, qs, include_constant_term):
+# Define the SARIMA(p, d, q)(P, D, Q) likelihood function
+
+
+def _extract_params(params, model):
     """Extract ARIMA parameters from the parameter vector."""
-    # Extract parameters
-    c = params[0] if include_constant_term else 0  # Constant term
-    # AR coefficients
-    phi = params[include_constant_term : p + include_constant_term]
-    # Seasonal AR coefficients
-    phi_s = params[include_constant_term + p : p + ps + include_constant_term]
-    # MA coefficients
-    theta = params[include_constant_term + p + ps : p + ps + q + include_constant_term]
-    # Seasonal MA coefficents
-    theta_s = params[
-        include_constant_term + p + ps + q : include_constant_term + p + ps + q + qs
-    ]
-    return c, phi, phi_s, theta, theta_s
+    if len(params) != np.sum(model):
+        previous_length = np.sum(model)
+        model = model[:-1]  # Remove the seasonal period
+        if len(params) != np.sum(model):
+            raise ValueError(
+                f"Expected {previous_length} parameters for a non-seasonal model or \
+                    {np.sum(model)} parameters for a seasonal model, got {len(params)}"
+            )
+    starts = np.cumsum([0] + model[:-1])
+    return [params[s : s + l].tolist() for s, l in zip(starts, model)]
 
 
-def _calc_arima(
-    data, p, q, ps, qs, seasonal_period, t, c, phi, phi_s, theta, theta_s, residuals
-):
+def _calc_arima(data, model, t, formatted_params, residuals):
     """Calculate the ARIMA forecast for time t."""
+    if len(model) != 3:
+        raise ValueError("Model must be of the form (c, p, q)")
     # AR part
+    p = model[1]
+    phi = formatted_params[1]
     ar_term = 0 if (t - p) < 0 else np.dot(phi, data[t - p : t][::-1])
+
+    # MA part
+    q = model[2]
+    theta = formatted_params[2]
+    ma_term = 0 if (t - q) < 0 else np.dot(theta, residuals[t - q : t][::-1])
+
+    c = formatted_params[0][0] if model[0] else 0
+    y_hat = c + ar_term + ma_term
+    return y_hat
+
+
+def _calc_sarima(data, model, t, formatted_params, residuals):
+    """Calculate the SARIMA forecast for time t."""
+    if len(model) != 6:
+        raise ValueError("Model must be of the form (c, p, q, ps, qs, seasonal_period)")
+    arima_forecast = _calc_arima(data, model[:3], t, formatted_params, residuals)
+    seasonal_period = model[5]
     # Seasonal AR part
+    ps = model[3]
+    phi_s = formatted_params[3]
     ars_term = (
         0
         if (t - seasonal_period * ps) < 0
         else np.dot(phi_s, data[t - seasonal_period * ps : t : seasonal_period][::-1])
     )
-    # MA part
-    ma_term = 0 if (t - q) < 0 else np.dot(theta, residuals[t - q : t][::-1])
     # Seasonal MA part
+    qs = model[4]
+    theta_s = formatted_params[4]
     mas_term = (
         0
         if (t - seasonal_period * qs) < 0
@@ -263,120 +262,20 @@ def _calc_arima(
             theta_s, residuals[t - seasonal_period * qs : t : seasonal_period][::-1]
         )
     )
-    y_hat = c + ar_term + ma_term + ars_term + mas_term
-    return y_hat
+    return arima_forecast + ars_term + mas_term
 
 
-def _nelder_mead(
-    data,
-    p,
-    q,
-    ps,
-    qs,
-    seasonal_period,
-    include_constant_term,
-    tol=1e-6,
-    max_iter=500,
-):
-    """Implement the nelder-mead optimisation algorithm."""
-    num_params = include_constant_term + p + ps + q + qs
-    points = np.full((num_params + 1, num_params), 0.5)
-    for i in range(num_params):
-        points[i + 1][i] = 0.6
-    values = np.array(
-        [
-            _arima_log_likelihood(
-                v, data, p, q, ps, qs, seasonal_period, include_constant_term
-            )[0]
-            for v in points
-        ]
-    )
-    for _iteration in range(max_iter):
-        # Order simplex by function values
-        order = np.argsort(values)
-        points = points[order]
-        values = values[order]
+def make_arima_llf(base_function, data, model):
+    """
+    Return a parameterized log-likelihood function for ARIMA.
 
-        # Centroid of the best n points
-        centre_point = points[:-1].sum(axis=0) / len(points[:-1])
+    This can then be used with an optimization algorithm.
+    """
 
-        # Reflection
-        # centre + distance between centre and largest value
-        reflected_point = centre_point + (centre_point - points[-1])
-        reflected_value = _arima_log_likelihood(
-            reflected_point,
-            data,
-            p,
-            q,
-            ps,
-            qs,
-            seasonal_period,
-            include_constant_term,
-        )[0]
-        # if between best and second best, use reflected value
-        if len(values) > 1 and values[0] <= reflected_value < values[-2]:
-            points[-1] = reflected_point
-            values[-1] = reflected_value
-            continue
-        # Expansion
-        # Otherwise if it is better than the best value
-        if reflected_value < values[0]:
-            expanded_point = centre_point + 2 * (reflected_point - centre_point)
-            expanded_value = _arima_log_likelihood(
-                expanded_point,
-                data,
-                p,
-                q,
-                ps,
-                qs,
-                seasonal_period,
-                include_constant_term,
-            )[0]
-            # if less than reflected value use expanded, otherwise go back to reflected
-            if expanded_value < reflected_value:
-                points[-1] = expanded_point
-                values[-1] = expanded_value
-            else:
-                points[-1] = reflected_point
-                values[-1] = reflected_value
-            continue
-        # Contraction
-        # Otherwise if reflection is worse than all current values
-        contracted_point = centre_point - 0.5 * (centre_point - points[-1])
-        contracted_value = _arima_log_likelihood(
-            contracted_point,
-            data,
-            p,
-            q,
-            ps,
-            qs,
-            seasonal_period,
-            include_constant_term,
-        )[0]
-        # If contraction is better use that otherwise move to shrinkage
-        if contracted_value < values[-1]:
-            points[-1] = contracted_point
-            values[-1] = contracted_value
-            continue
+    def loss_fn(v):
+        return _arima_model(v, base_function, data, model)[0]
 
-        # Shrinkage
-        for i in range(1, len(points)):
-            points[i] = points[0] - 0.5 * (points[0] - points[i])
-            values[i] = _arima_log_likelihood(
-                points[i],
-                data,
-                p,
-                q,
-                ps,
-                qs,
-                seasonal_period,
-                include_constant_term,
-            )[0]
-
-        # Convergence check
-        if np.max(np.abs(values - values[0])) < tol:
-            break
-    return points[0], values[0]
+    return loss_fn
 
 
 def _auto_arima(data):
@@ -385,7 +284,7 @@ def _auto_arima(data):
 
     For automatic ARIMA model selection.
     """
-    seasonal_period = _calc_seasonal_period(data)
+    seasonal_period = calc_seasonal_period(data)
     difference = 0
     while not kpss_test(data)[1]:
         data = np.diff(data, n=1)
@@ -395,98 +294,46 @@ def _auto_arima(data):
         data = data[seasonal_period:] - data[:-seasonal_period]
     include_c = 1 if difference == 0 else 0
     model_parameters = [
-        [2, 2, 0, 0, include_c],
-        [0, 0, 0, 0, include_c],
-        [1, 0, 0, 0, include_c],
-        [0, 1, 0, 0, include_c],
+        [include_c, 2, 2, 0, 0, seasonal_period],
+        [include_c, 0, 0, 0, 0, seasonal_period],
+        [include_c, 1, 0, 0, 0, seasonal_period],
+        [include_c, 0, 1, 0, 0, seasonal_period],
     ]
     model_points = []
+    model_scores = []
     for p in model_parameters:
-        points, aic = _nelder_mead(data, p[0], p[1], p[2], p[3], seasonal_period, p[4])
-        p.append(aic)
+        points, aic = nelder_mead(make_arima_llf(_calc_sarima, data, p), np.sum(p[:5]))
         model_points.append(points)
-    current_model = min(model_parameters, key=lambda item: item[5])
-    current_points = model_points[model_parameters.index(current_model)]
+        model_scores.append(aic)
+    best_score = min(model_scores)
+    best_index = model_scores.index(best_score)
+    current_model = model_parameters[best_index]
+    current_points = model_points[best_index]
     while True:
         better_model = False
-        for param_no in range(4):
+        for param_no in range(1, 5):
             for adjustment in [-1, 1]:
                 if (current_model[param_no] + adjustment) < 0:
                     continue
                 model = current_model.copy()
                 model[param_no] += adjustment
                 for constant_term in [0, 1]:
-                    points, aic = _nelder_mead(
-                        data,
-                        model[0],
-                        model[1],
-                        model[2],
-                        model[3],
-                        seasonal_period,
-                        constant_term,
+                    model[0] = constant_term
+                    points, aic = nelder_mead(
+                        make_arima_llf(_calc_sarima, data, model), np.sum(model[:5])
                     )
-                    if aic < current_model[5]:
-                        current_model = model
+                    if aic < best_score:
+                        current_model = model.copy()
                         current_points = points
-                        current_model[5] = aic
-                        current_model[4] = constant_term
+                        best_score = aic
                         better_model = True
         if not better_model:
             break
     return (
         data,
-        current_model[5],
-        current_model[0],
         difference,
-        current_model[1],
-        current_model[2],
         seasonal_difference,
-        current_model[3],
-        seasonal_period,
-        current_model[4],
+        current_model,
         current_points,
+        best_score,
     )
-
-
-@njit(cache=True, fastmath=True)
-def _acf(X, max_lag):
-    length = len(X)
-    X_t = np.zeros(max_lag, dtype=float)
-    for lag in range(1, max_lag + 1):
-        lag_length = length - lag
-        x1 = X[:-lag]
-        x2 = X[lag:]
-        s1 = np.sum(x1)
-        s2 = np.sum(x2)
-        m1 = s1 / lag_length
-        m2 = s2 / lag_length
-        ss1 = np.sum(x1 * x1)
-        ss2 = np.sum(x2 * x2)
-        v1 = ss1 - s1 * m1
-        v2 = ss2 - s2 * m2
-        v1_is_zero, v2_is_zero = v1 <= 1e-9, v2 <= 1e-9
-        if v1_is_zero and v2_is_zero:  # Both zero variance,
-            # so must be 100% correlated
-            X_t[lag - 1] = 1
-        elif v1_is_zero or v2_is_zero:  # One zero variance
-            # the other not
-            X_t[lag - 1] = 0
-        else:
-            X_t[lag - 1] = np.sum((x1 - m1) * (x2 - m2)) / np.sqrt(v1 * v2)
-    return X_t
-
-
-@njit(cache=True, fastmath=True)
-def _calc_seasonal_period(data):
-    """Estimate the seasonal period based on the autocorrelation of the series."""
-    lags = _acf(data, 24)
-    lags = np.concatenate((np.array([1.0]), lags))
-    peaks = []
-    mean_lags = np.mean(lags)
-    for i in range(1, len(lags) - 1):  # Skip the first (lag 0) and last elements
-        if lags[i] >= lags[i - 1] and lags[i] >= lags[i + 1] and lags[i] > mean_lags:
-            peaks.append(i)
-    if not peaks:
-        return 1
-    else:
-        return peaks[0]
---
+++ aeon/utils/optimisation/_nelder_mead.py
@@ -0,0 +1,106 @@
+"""Optimisation algorithms for automatic parameter tuning."""
+
+import numpy as np
+
+
+def nelder_mead(
+    loss_function,
+    num_params,
+    tol=1e-6,
+    max_iter=500,
+):
+    """
+    Perform optimisation using the Nelder–Mead simplex algorithm.
+
+    This function minimises a given loss (objective) function using the Nelder–Mead
+    algorithm, a derivative-free method that iteratively refines a simplex of candidate
+    solutions. The implementation supports unconstrained minimisation of functions
+    with a fixed number of parameters.
+
+    Parameters
+    ----------
+    loss_function : callable
+        The objective function to minimise. Should accept a 1D NumPy array of length
+        `num_params` and return a scalar value.
+    num_params : int
+        The number of parameters (dimensions) in the optimisation problem.
+    tol : float, optional (default=1e-6)
+        Tolerance for convergence. The algorithm stops when the maximum difference
+        between function values at simplex vertices is less than `tol`.
+    max_iter : int, optional (default=500)
+        Maximum number of iterations to perform.
+
+    Returns
+    -------
+    best_params : np.ndarray, shape (`num_params`,)
+        The parameter vector that minimises the loss function.
+    best_value : float
+        The value of the loss function at the optimal parameter vector.
+
+    Notes
+    -----
+    - The initial simplex is constructed by setting each parameter to 0.5,
+    with one additional point per dimension at 0.6 for that dimension.
+    - This implementation does not support constraints or bounds on the parameters.
+    - The algorithm does not guarantee finding a global minimum.
+
+    Examples
+    --------
+    >>> def sphere(x):
+    ...     return np.sum(x**2)
+    >>> x_opt, val = nelder_mead(sphere, num_params=2)
+    """
+    points = np.full((num_params + 1, num_params), 0.5)
+    for i in range(num_params):
+        points[i + 1][i] = 0.6
+    values = np.array([loss_function(v) for v in points])
+    for _iteration in range(max_iter):
+        # Order simplex by function values
+        order = np.argsort(values)
+        points = points[order]
+        values = values[order]
+
+        # Centroid of the best n points
+        centre_point = points[:-1].sum(axis=0) / len(points[:-1])
+
+        # Reflection
+        # centre + distance between centre and largest value
+        reflected_point = centre_point + (centre_point - points[-1])
+        reflected_value = loss_function(reflected_point)
+        # if between best and second best, use reflected value
+        if len(values) > 1 and values[0] <= reflected_value < values[-2]:
+            points[-1] = reflected_point
+            values[-1] = reflected_value
+            continue
+        # Expansion
+        # Otherwise if it is better than the best value
+        if reflected_value < values[0]:
+            expanded_point = centre_point + 2 * (reflected_point - centre_point)
+            expanded_value = loss_function(expanded_point)
+            # if less than reflected value use expanded, otherwise go back to reflected
+            if expanded_value < reflected_value:
+                points[-1] = expanded_point
+                values[-1] = expanded_value
+            else:
+                points[-1] = reflected_point
+                values[-1] = reflected_value
+            continue
+        # Contraction
+        # Otherwise if reflection is worse than all current values
+        contracted_point = centre_point - 0.5 * (centre_point - points[-1])
+        contracted_value = loss_function(contracted_point)
+        # If contraction is better use that otherwise move to shrinkage
+        if contracted_value < values[-1]:
+            points[-1] = contracted_point
+            values[-1] = contracted_value
+            continue
+
+        # Shrinkage
+        for i in range(1, len(points)):
+            points[i] = points[0] - 0.5 * (points[0] - points[i])
+            values[i] = loss_function(points[i])
+
+        # Convergence check
+        if np.max(np.abs(values - values[0])) < tol:
+            break
+    return points[0], values[0]
