79cff9a6a8dcd159a073471c41535d03ca0921b6
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-05-31 18:14:55 +0100

    test skip

--- aeon/testing/testing_config.py
+++ aeon/testing/testing_config.py
@@ -53,6 +53,7 @@ EXCLUDED_TESTS = {
     "RSAST": ["check_fit_deterministic"],
     "MatrixProfile": ["check_fit_deterministic", "check_persistence_via_pickle"],
     "LeftSTAMPi": ["check_anomaly_detector_output"],
+    "SignatureClassifier": ["check_classifier_against_expected_results"],
     # missed in legacy testing, changes state in predict/transform
     "FLUSSSegmenter": ["check_non_state_changing_method"],
     "ClaSPSegmenter": ["check_non_state_changing_method"],
