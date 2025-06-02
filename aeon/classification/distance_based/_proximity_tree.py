pf change fixes and derivative function merge

aeon/classification/distance_based/_proximity_tree.py
@@ -253,7 +253,7 @@ class ProximityTree(BaseClassifier):
 
         for _ in range(self.n_splitters):
             exemplars, dist_name, dist_params, X_std = self._get_candidate_splitter(
-                X, rng, cls_idx, X_std
+                X, der_X, rng, cls_idx, X_std
             )
             splits = [[] for _ in unique_classes]
 
@@ -343,10 +343,11 @@ class ProximityTree(BaseClassifier):
                 if dist < min_dist:
                     min_dist = dist
                     best_exemplar_label = label
+
             return self._traverse_tree(node.children[best_exemplar_label], x, der_x)
 
     @staticmethod
-    def _get_candidate_splitter(X, rng, cls_idx, X_std):
+    def _get_candidate_splitter(X, der_X, rng, cls_idx, X_std):
         """Generate candidate splitter.
 
         Takes a time series dataset and a set of parameterized
@@ -357,6 +358,8 @@ class ProximityTree(BaseClassifier):
         ----------
         X : np.ndarray shape (n_cases, n_channels, n_timepoints)
             The training input samples.
+        der_X : np.ndarray shape (n_cases, n_channels, n_timepoints)
+            The slope derivative of the training input samples, used for ddtw and wddtw.
         rng : np.random.RandomState
             Random number generator.
         cls_idx : dict
@@ -378,13 +381,8 @@ class ProximityTree(BaseClassifier):
             The standard deviation of the training set if calculated, otherwise same
             as input.
         """
-        exemplars = {}
-        for label in cls_idx.keys():
-            label_idx = cls_idx[label]
-            id = rng.choice(label_idx)
-            exemplars[label] = X[id]
-
         n = rng.randint(0, 11)
+        derivative = False
         if n == 0:
             dist_name = "euclidean"
             dist_params = {}
@@ -397,15 +395,18 @@ class ProximityTree(BaseClassifier):
         elif n == 3:
             dist_name = "ddtw"
             dist_params = {}
+            derivative = True
         elif n == 4:
             dist_name = "ddtw"
             dist_params = {"window": rng.uniform(0, 0.25)}
+            derivative = True
         elif n == 5:
             dist_name = "wdtw"
             dist_params = {"g": rng.uniform(0, 1)}
         elif n == 6:
             dist_name = "wddtw"
             dist_params = {"g": rng.uniform(0, 1)}
+            derivative = True
         elif n == 7:
             if X_std is None:
                 X_std = X.std()
@@ -433,6 +434,12 @@ class ProximityTree(BaseClassifier):
             }
         else:
             raise ValueError(f"Invalid distance index {n}. Must be in range [0, 10].")
+
+        exemplars = {}
+        for label in cls_idx.keys():
+            label_idx = cls_idx[label]
+            id = rng.choice(label_idx)
+            exemplars[label] = der_X[id] if derivative else X[id]
 
         return exemplars, dist_name, dist_params, X_std
 
@@ -488,15 +495,13 @@ def gini_gain(y, y_subs) -> float:
     # find gini for parent node
     score = gini(y)
 
-    weighted_child_gini = 0.0
     for child in y_subs:
         # ignore empty children
         if child.shape[0] > 0:
             # find gini score for this child and weight score by proportion of
             # instances at child compared to parent
-            weighted_child_gini += (child.shape[0] / y.shape[0]) * gini(child)
-
-    return score - weighted_child_gini
+            score -= (child.shape[0] / y.shape[0]) * gini(child)
+    return score
 
 
 msm_params = [

aeon/utils/numba/general.py
@@ -641,20 +641,37 @@ def combinations_1d(x: np.ndarray, y: np.ndarray) -> np.ndarray:
 
 @njit(fastmath=True, cache=True)
 def slope_derivative(X: np.ndarray) -> np.ndarray:
-    """Numba slope derivative transformation for a 1d numpy array.
+    r"""Compute the average of a slope between points.
 
-    Finds the derivative of the series, padding the first and last values so that the
-    length stays the same.
+    a.k.a. slope derivative.
+
+    Computes the average of the slope of the line through the point in question and
+    its left neighbour, and the slope of the line through the left neighbour and the
+    right neighbour. Proposed in [1] for use in distances i.e. ddtw and wddtw.
+    .. math::
+    q'_(i) = \frac{{}(q_{i} - q_{i-1} + ((q_{i+1} - q_{i-1}/2)}{2}
+    Where q is the original time series and q' is the derived time series.
 
     Parameters
     ----------
-    X : 1d numpy array
-        A 1d numpy array of values
+    X : np.ndarray (n_timepoints)
+        Time series to take derivative of.
 
     Returns
     -------
-    arr : 1d numpy array
-        The slope derivative of the series
+    np.ndarray (n_timepoints - 2)
+        Array containing the derivative of X.
+
+    Raises
+    ------
+    ValueError
+        If the time series length has less than 3 points.
+
+    References
+    ----------
+    .. [1] Keogh, Eamonn & Pazzani, Michael. (2002). Derivative Dynamic Time Warping.
+        First SIAM International Conference on Data Mining.
+        1. 10.1137/1.9781611972719.1.
 
     Examples
     --------
@@ -663,31 +680,47 @@ def slope_derivative(X: np.ndarray) -> np.ndarray:
     >>> X = np.array([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
     >>> X_der = slope_derivative(X)
     """
-    m = len(X)
-    arr = np.zeros(m)
-    for i in range(1, m - 1):
-        arr[i] = ((X[i] - X[i - 1]) + ((X[i + 1] - X[i - 1]) / 2.0)) / 2.0
-    arr[0] = arr[1]
-    arr[m - 1] = arr[m - 2]
-    return arr
+    if X.shape[0] < 3:
+        raise ValueError("Time series must have at least 3 points.")
+    result = np.zeros(X.shape[0] - 2)
+    for i in range(1, X.shape[0] - 1):
+        result[i - 1] = ((X[i] - X[i - 1]) + (X[i + 1] - X[i - 1]) / 2.0) / 2.0
+    return result
 
 
 @njit(fastmath=True, cache=True)
 def slope_derivative_2d(X: np.ndarray) -> np.ndarray:
-    """Numba slope derivative transformation for a 2d numpy array.
+    r"""Compute the average of a slope between points.
 
-    Finds the derivative of the series, padding the first and last values so that the
-    length stays the same.
+    a.k.a. slope derivative.
+
+    Computes the average of the slope of the line through the point in question and
+    its left neighbour, and the slope of the line through the left neighbour and the
+    right neighbour. Proposed in [1] for use in distances i.e. ddtw and wddtw.
+    .. math::
+    q'_(i) = \frac{{}(q_{i} - q_{i-1} + ((q_{i+1} - q_{i-1}/2)}{2}
+    Where q is the original time series and q' is the derived time series.
 
     Parameters
     ----------
-    X : 2d numpy array
-        A 2d numpy array of values
+    X : np.ndarray (n_channels, n_timepoints)
+        Time series to take derivative of.
 
     Returns
     -------
-    arr : 2d numpy array
-        The slope derivative of each series
+    np.ndarray (n_channels, n_timepoints - 2)
+        Array containing the derivative of X.
+
+    Raises
+    ------
+    ValueError
+        If the time series length has less than 3 points.
+
+    References
+    ----------
+    .. [1] Keogh, Eamonn & Pazzani, Michael. (2002). Derivative Dynamic Time Warping.
+        First SIAM International Conference on Data Mining.
+        1. 10.1137/1.9781611972719.1.
 
     Examples
     --------
@@ -696,7 +729,7 @@ def slope_derivative_2d(X: np.ndarray) -> np.ndarray:
     >>> X = np.array([[1, 2, 2, 3, 3, 3, 4, 4, 4, 4], [5, 6, 6, 7, 7, 7, 8, 8, 8, 8]])
     >>> X_der = slope_derivative_2d(X)
     """
-    arr = np.zeros(X.shape)
+    arr = np.zeros((X.shape[0], X.shape[2] - 2))
     for i in range(X.shape[0]):
         arr[i] = slope_derivative(X[i])
     return arr
@@ -704,20 +737,37 @@ def slope_derivative_2d(X: np.ndarray) -> np.ndarray:
 
 @njit(fastmath=True, cache=True)
 def slope_derivative_3d(X: np.ndarray) -> np.ndarray:
-    """Numba slope derivative transformation for a 3d numpy array.
+    r"""Compute the average of a slope between points.
 
-    Finds the derivative of the series, padding the first and last values so that the
-    length stays the same.
+    a.k.a. slope derivative.
+
+    Computes the average of the slope of the line through the point in question and
+    its left neighbour, and the slope of the line through the left neighbour and the
+    right neighbour. Proposed in [1] for use in distances i.e. ddtw and wddtw.
+    .. math::
+    q'_(i) = \frac{{}(q_{i} - q_{i-1} + ((q_{i+1} - q_{i-1}/2)}{2}
+    Where q is the original time series and q' is the derived time series.
 
     Parameters
     ----------
-    X : 3d numpy array
-        A 3d numpy array of values
+    X : np.ndarray (n_cases, n_channels, n_timepoints)
+        Time series to take derivative of.
 
     Returns
     -------
-    arr : 3d numpy array
-        The slope derivative of each series
+    np.ndarray (n_cases, n_channels, n_timepoints - 2)
+        Array containing the derivative of X.
+
+    Raises
+    ------
+    ValueError
+        If the time series length has less than 3 points.
+
+    References
+    ----------
+    .. [1] Keogh, Eamonn & Pazzani, Michael. (2002). Derivative Dynamic Time Warping.
+        First SIAM International Conference on Data Mining.
+        1. 10.1137/1.9781611972719.1.
 
     Examples
     --------
@@ -729,7 +779,7 @@ def slope_derivative_3d(X: np.ndarray) -> np.ndarray:
     ... ])
     >>> X_der = slope_derivative_3d(X)
     """
-    arr = np.zeros(X.shape)
+    arr = np.zeros((X.shape[0], X.shape[1], X.shape[2] - 2))
     for i in range(X.shape[0]):
         arr[i] = slope_derivative_2d(X[i])
     return arr

aeon/utils/numba/tests/test_general.py
@@ -1,7 +1,5 @@
 """Tests for numba functions."""
 
-__maintainer__ = []
-
 import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal, assert_array_equal
@@ -13,11 +11,23 @@ from aeon.utils.numba.general import (
     get_subsequence_with_mean_std,
     normalise_subsequences,
     sliding_mean_std_one_series,
+    unique_count,
     z_normalise_series,
     z_normalise_series_with_mean_std,
 )
 
 DATATYPES = ["int32", "int64", "float32", "float64"]
+
+
+@pytest.mark.parametrize("type", DATATYPES)
+def test_unique_count(type):
+    """Test numba unique count."""
+    a = np.array([2, 0, 2, 2, 1, 1, 0, 2, 2, 1], dtype=type)
+    unique_expected = [0, 1, 2]
+    count_expected = [2, 3, 5]
+    a_result = unique_count(a)
+    assert_array_equal(a_result[0], unique_expected)
+    assert_array_equal(a_result[1], count_expected)
 
 
 @pytest.mark.parametrize("type", DATATYPES)