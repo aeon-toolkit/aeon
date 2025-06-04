64777dd173f9356219fc8dc2f67ba2a703e0c2bc
tina <jintina48@gmail.com>
2025-06-04 19:48:01 +0800

    few modifications according to reviews

--- aeon/forecasting/_naive.py
+++ aeon/forecasting/_naive.py
@@ -9,39 +9,34 @@ class NaiveForecaster(BaseForecaster):
     """
     Naive forecaster with multiple strategies and flexible horizon.
 
-    Strategies:
-    - "last": predicts the last seen value in training for all horizon steps.
-
-    - "mean": predicts the mean of the training series for all horizon steps.
-
-    - "seasonal_last": predict the last season value seen in the training series.
-                       Returns np.nan if the effective seasonal data is empty.
+    Parameters
+    ----------
+    strategy : str, default="last"
+        The forecasting strategy to use.
+        Options: "last", "mean", "seasonal_last".
+            - "last" predicts the last seen value in training for all horizon steps.
+            - "mean": predicts the mean of the training series for all horizon steps.
+            - "seasonal_last": predicts the last season value in the training series.
+            Returns np.nan if the effective seasonal data is empty.
+    seasonal_period : int, default=1
+        The seasonal period to use for the "seasonal_last" strategy.
+        E.g., 12 for monthly data with annual seasonality.
+    horizon : int, default=1
+        The number of time steps ahead to forecast.
     """
 
     def __init__(self, strategy="last", seasonal_period=1, horizon=1):
-        """
-        Initialize NaiveForecaster.
-
-        Parameters
-        ----------
-        strategy : str, default="last"
-            The forecasting strategy to use.
-            Options: "last", "mean", "seasonal_last".
-        seasonal_period : int, default=1
-            The seasonal period to use for the "seasonal_last" strategy.
-            E.g., 12 for monthly data with annual seasonality.
-        horizon : int, default=1
-            The number of time steps ahead to forecast.
-        """
+        """Initialize NaiveForecaster."""
         self.strategy = strategy
         self.seasonal_period = seasonal_period
-        self._fitted_scalar_value_ = None  # For 'last' and 'mean' strategies
-        self._fitted_last_season_ = np.array([])  # For 'seasonal_last', init as empty
 
         super().__init__(horizon=horizon, axis=1)
 
     def _fit(self, y, exog=None):
         """Fit Naive forecaster to training data `y`."""
+        self._fitted_scalar_value_ = None  # For 'last' and 'mean' strategies
+        self._fitted_last_season_ = np.array([])  # For 'seasonal_last', init as empty
+
         y_squeezed = y.squeeze()
 
         if self.strategy == "last":
