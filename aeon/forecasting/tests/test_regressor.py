0f21953c54b4b013e3e44875e8826badc5ccd172
Tony Bagnall <ajb@uea.ac.uk>
2025-06-06 17:06:36 +0100

    stop returning an array

--- aeon/forecasting/tests/test_regressor.py
+++ aeon/forecasting/tests/test_regressor.py
@@ -1,14 +1,16 @@
 """Test the regression forecaster."""
 
+import numpy as np
+import pytest
 from sklearn.linear_model import LinearRegression
 
-from aeon.datasets import load_airline
 from aeon.forecasting import RegressionForecaster
+from aeon.regression import DummyRegressor
 
 
 def test_regression_forecaster():
     """Test the regression forecaster."""
-    y = load_airline()
+    y = np.random.rand(100)
     f = RegressionForecaster(window=10)
     f.fit(y)
     p = f.predict()
@@ -20,3 +22,13 @@ def test_regression_forecaster():
     f2.fit(y)
     p2 = f2.predict()
     assert p == p2
+    f2 = RegressionForecaster(regressor=DummyRegressor, window=10)
+    f2.fit(y)
+    f2.predict()
+
+    with pytest.raises(ValueError):
+        f = RegressionForecaster(window=-1)
+        f.fit(y)
+    with pytest.raises(ValueError):
+        f = RegressionForecaster(window=101)
+        f.fit(y)
