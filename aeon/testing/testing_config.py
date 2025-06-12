daf56f29b7fb0e767b1d1765f8fb4e06a9c34851
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-06-12 18:46:04 +0100

    skip test

--- aeon/testing/testing_config.py
+++ aeon/testing/testing_config.py
@@ -55,6 +55,7 @@ EXCLUDED_TESTS = {
     "LeftSTAMPi": ["check_series_anomaly_detector_output"],
     "SignatureClassifier": ["check_classifier_against_expected_results"],
     "SeriesToCollectionBroadcaster": ["check_transform_inverse_transform_equivalent"],
+    "CollectionToSeriesWrapper": ["check_transform_inverse_transform_equivalent"],
     # missed in legacy testing, changes state in predict/transform
     "FLUSSSegmenter": ["check_non_state_changing_method"],
     "ClaSPSegmenter": ["check_non_state_changing_method"],
