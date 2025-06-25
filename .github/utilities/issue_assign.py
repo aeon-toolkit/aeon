184994b479ed9e5bc8f804a5b4a88618c25247c2
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-06-25 22:39:17 +0530

    [MNT] Prevents assignment on Restricted Labels (#2706)
    * restricted labels
    
    * set
    
    * resolve conflicts
    
    * merge assign PRs and small change
    
    ---------
    
    Co-authored-by: MatthewMiddlehurst <pfm15hbu@gmail.com>

--- .github/utilities/issue_assign.py
+++ .github/utilities/issue_assign.py
@@ -3,6 +3,7 @@
 It checks if a comment on an issue or PR includes the trigger
 phrase (as defined) and a mentioned user.
 If it does, it assigns the issue to the mentioned user.
+
 Users without write access can only have up to 2 open issues assigned.
 Users with write access (or admin) are exempt from this limit.
 If a non-write user already has 2 or more open issues, the bot
@@ -22,20 +23,32 @@ g = Github(os.getenv("GITHUB_TOKEN"))
 repo = g.get_repo(repo)
 issue_number = context_dict["event"]["issue"]["number"]
 issue = repo.get_issue(number=issue_number)
-comment_body = context_dict["event"]["comment"]["body"]
+issue_labels = {label.name.lower() for label in issue.labels}
 pr = context_dict["event"]["issue"].get("pull_request")
+comment_body = context_dict["event"]["comment"]["body"]
 commenter = context_dict["event"]["comment"]["user"]["login"]
 
+restricted_labels = {"meta-issue"}
+
+# Assign tagged used to the issue if the comment includes the trigger phrase
 body = comment_body.lower()
-if "@aeon-actions-bot" in body and not pr:
-    # Assign commenter if comment includes "assign me"
-    if "assign me" in body:
-        issue.add_to_assignees(commenter)
-    # Assign tagged used to the issue if the comment includes the trigger phrase
-    elif "assign" in body:
+if "@aeon-actions-bot" in body and "assign" in body and not pr:
+    # Check if the issue has any restricted labels for auto assignment
+    label_intersect = issue_labels & restricted_labels
+    if len(label_intersect) > 0:
+        issue.create_comment(
+            f"This issue contains the following restricted label(s): "
+            f"{', '.join(label_intersect)}. Cannot assign to users."
+        )
+    else:
+        # collect any mentioned (@username) users
         mentioned_users = re.findall(r"@[a-zA-Z0-9_-]+", comment_body)
         mentioned_users = [user[1:] for user in mentioned_users]
         mentioned_users.remove("aeon-actions-bot")
+        # Assign commenter if comment includes "assign me"
+        if "assign me" in body:
+            mentioned_users.append(commenter)
+        mentioned_users = set(mentioned_users)
 
         for user in mentioned_users:
             user_obj = g.get_user(user)
@@ -53,7 +66,7 @@ if "@aeon-actions-bot" in body and not pr:
                 issues_assigned_to_user = g.search_issues(query)
                 assigned_count = issues_assigned_to_user.totalCount
 
-                if assigned_count >= 2:
+                if assigned_count >= 3:
                     # link to issue
                     assigned_issues_list = [
                         f"[#{assigned_issue.number}]({assigned_issue.html_url})"
@@ -61,11 +74,10 @@ if "@aeon-actions-bot" in body and not pr:
                     ]
 
                     comment_message = (
-                        f"@{user}, you already have {assigned_count} "
-                        f"open issues assigned."
-                        "Users without write access are limited to self-assigning two"
-                        "issues.\n\n"
-                        "Here are the open issues assigned to you:\n"
+                        f"@{user}, already has {assigned_count} open issues assigned."
+                        "Users without write access are limited to self-assigning "
+                        "three issues.\n\n"
+                        "Here are the open issues assigned:\n"
                         + "\n".join(
                             f"- {issue_link}" for issue_link in assigned_issues_list
                         )
