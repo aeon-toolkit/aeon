fc3e1886f32a19388f51c04488e231c3313abee3
Tony Bagnall <ajb@uea.ac.uk>
2025-07-03 17:18:13 +0100

    docstring

--- aeon/forecasting/_tvp.py
+++ aeon/forecasting/_tvp.py
@@ -1,29 +1,35 @@
 """Time-Varying Parameter (TVP) Forecaster using Kalman filter."""
+
 import numpy as np
-from scipy.stats import betanbinom
 
 from aeon.forecasting.base import BaseForecaster
 
+
 class TVPForecaster(BaseForecaster):
-    """
-    Time-Varying Parameter (TVP) Forecaster using Kalman filter as described in [1].
+    r"""Time-Varying Parameter (TVP) Forecaster using Kalman filter as described in [1].
 
     This forecaster models the target series using a time-varying linear autoregression:
-        \[ \hat{y}_t = \beta_1,t * y_{t-1} + ... + \beta_k,t * y_{t-k} \]
-    where the coefficients $\beta_t$ evolve based on observations $y_t$. At each step, a weight
-    vector is calculated based in the latest residual
 
-    Related to stochastic gradient descent (SGD) regression, with the update weight the dynamically
-    calculated Kalman gain based on the covariance of the parameters rather than a fixed learning rate.
+    .. math::
+
+        \\hat{y}_t = \beta_1,t * y_{t-1} + ... + \beta_k,t * y_{t-k}
+
+    where the coefficients $\beta_t$ evolve based on observations $y_t$. At each
+    step, a weight vector is calculated based in the latest residual
+
+    Related to stochastic gradient descent (SGD) regression, with the update weight
+    the dynamically calculated Kalman gain based on the covariance of the parameters
+    rather than a fixed learning rate.
 
     Parameters
     ----------
     window : int
         Number of autoregressive lags to use.
     var : float, default=0.01
-        Observation noise variance. ``var`` controls the influence of recency in the update. A small R (e.g. 0.01) means
-        the parameters will be more affected by recent values. A large R (e.g., 1.0 or more)
-        means the observations are noisy, so the filter will adjust the parameters less to match recent values.
+        Observation noise variance. ``var`` controls the influence of recency in the
+        update. A small R (e.g. 0.01) means the parameters will be more affected by
+        recent values. A large R (e.g., 1.0 or more) means the observations are
+        noisy, so the filter will adjust the parameters less to match recent values.
     coeff_var : float, default=0.01
         State evolution noise variance, applied to all coefficients at each step. Small
         ``coeff_var`` leads to slowly evolving parameters.
@@ -34,7 +40,7 @@ class TVPForecaster(BaseForecaster):
     Oxford University Press, 2nd Edition, 2012
     """
 
-    def __init__(self, window, horizon = 1, var=0.01, beta_var=0.01):
+    def __init__(self, window, horizon=1, var=0.01, beta_var=0.01):
         self.window = window
         self.var = var
         self.beta_var = beta_var
@@ -46,7 +52,6 @@ class TVPForecaster(BaseForecaster):
         # Create autoregressive design matrix
         X = np.lib.stride_tricks.sliding_window_view(y, window_shape=self.window)
         X = X[: -self.horizon]
-
 
         y_train = y[self.window + self.horizon - 1 :]
 
@@ -67,16 +72,18 @@ class TVPForecaster(BaseForecaster):
 
             # Update beta parameters with kalman weights times error.
             beta = beta + kalman_weight * error_t
-            beta_covariance = beta_covariance - np.outer(kalman_weight, x_t) @ beta_covariance
+            beta_covariance = (
+                beta_covariance - np.outer(kalman_weight, x_t) @ beta_covariance
+            )
 
         self._beta = beta
-        self._last_window = y[-self.window:]
+        self._last_window = y[-self.window :]
         self.forecast_ = self._last_window @ self._beta  # store forecast y_{t+1}
         return self
 
-    def _predict(self, y = None, exog = None):
-        if y is None: return self.forecast_
-        x_t = y[-self.window:]
+    def _predict(self, y=None, exog=None):
+        if y is None:
+            return self.forecast_
+        x_t = y[-self.window :]
         y_hat = x_t @ self._beta
         return y_hat
-
