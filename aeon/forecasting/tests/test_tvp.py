0dd0006442e5cd64afa842a4b327ffbfeb3d1ddc
Tony Bagnall <ajb@uea.ac.uk>
2025-07-05 18:06:54 +0100

    dummy test

--- aeon/forecasting/tests/test_tvp.py
+++ aeon/forecasting/tests/test_tvp.py
@@ -8,9 +8,7 @@ from aeon.forecasting._tvp import TVPForecaster
 def test_expected_results():
     """Test aeon TVP Forecaster equivalent to statsmodels."""
     expected = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
-    tvp = TVPForecaster(window=5, horizon=5, var=0.01, beta_var=0.01)
+    tvp = TVPForecaster(window=5, horizon=1, var=0.01, beta_var=0.01)
     p = tvp.forecast(expected)
     p2 = tvp.direct_forecast(expected, prediction_horizon=5)
-    p3 = tvp.iterative_forecast(expected, prediction_horizon=5)
     assert p == p2[0]
-    assert p == p3[0]
