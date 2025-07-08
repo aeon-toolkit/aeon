f6447b180c4b725d889dbed5b80fe948945bbd6c
lucifer4073 <lucifer4073@gmail.com>
2025-07-08 13:34:47 +0530

    tcn changed

--- aeon/networks/tests/test_tcn.py
+++ aeon/networks/tests/test_tcn.py
@@ -16,10 +16,10 @@ def test_tcn_network_basic():
 
     input_shape = (100, 5)
     num_inputs = 5
-    num_channels = [32, 64]
+    n_filters = [32, 64]
 
     tcn_network = TemporalConvolutionalNetwork(
-        num_inputs=num_inputs, num_channels=num_channels
+        num_inputs=num_inputs, n_filters=n_filters
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
 
@@ -38,8 +38,8 @@ def test_tcn_network_basic():
     not _check_soft_dependencies(["tensorflow"], severity="none"),
     reason="Tensorflow soft dependency unavailable.",
 )
-@pytest.mark.parametrize("num_channels", [[32], [32, 64], [16, 32, 64], [64, 32, 16]])
-def test_tcn_network_different_channels(num_channels):
+@pytest.mark.parametrize("n_filters", [[32], [32, 64], [16, 32, 64], [64, 32, 16]])
+def test_tcn_network_different_channels(n_filters):
     """Test TCN network with different channel configurations."""
     import tensorflow as tf
 
@@ -47,7 +47,7 @@ def test_tcn_network_different_channels(num_channels):
     num_inputs = 3
 
     tcn_network = TemporalConvolutionalNetwork(
-        num_inputs=num_inputs, num_channels=num_channels
+        num_inputs=num_inputs, n_filters=n_filters
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
 
@@ -74,11 +74,11 @@ def test_tcn_network_kernel_sizes(kernel_size):
 
     input_shape = (80, 4)
     num_inputs = 4
-    num_channels = [32, 64]
+    n_filters = [32, 64]
 
     tcn_network = TemporalConvolutionalNetwork(
         num_inputs=num_inputs,
-        num_channels=num_channels,
+        n_filters=n_filters,
         kernel_size=kernel_size,
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
@@ -99,10 +99,10 @@ def test_tcn_network_dropout_rates(dropout):
 
     input_shape = (60, 2)
     num_inputs = 2
-    num_channels = [16, 32]
+    n_filters = [16, 32]
 
     tcn_network = TemporalConvolutionalNetwork(
-        num_inputs=num_inputs, num_channels=num_channels, dropout=dropout
+        num_inputs=num_inputs, n_filters=n_filters, dropout=dropout
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
 
@@ -123,10 +123,10 @@ def test_tcn_network_output_shape():
     input_shape = (40, 6)
     batch_size = 16
     num_inputs = 6
-    num_channels = [32, 64]
+    n_filters = [32, 64]
 
     tcn_network = TemporalConvolutionalNetwork(
-        num_inputs=num_inputs, num_channels=num_channels
+        num_inputs=num_inputs, n_filters=n_filters
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
@@ -148,7 +148,7 @@ def test_tcn_network_output_shape():
 )
 def test_tcn_network_config():
     """Test TCN network configuration attributes."""
-    tcn_network = TemporalConvolutionalNetwork(num_inputs=3, num_channels=[16, 32])
+    tcn_network = TemporalConvolutionalNetwork(num_inputs=3, n_filters=[16, 32])
 
     # Check _config attributes
     assert "python_dependencies" in tcn_network._config
@@ -165,20 +165,20 @@ def test_tcn_network_config():
 def test_tcn_network_parameter_initialization():
     """Test TCN network parameter initialization."""
     num_inputs = 4
-    num_channels = [32, 64, 128]
+    n_filters = [32, 64, 128]
     kernel_size = 3
     dropout = 0.2
 
     tcn_network = TemporalConvolutionalNetwork(
         num_inputs=num_inputs,
-        num_channels=num_channels,
+        n_filters=n_filters,
         kernel_size=kernel_size,
         dropout=dropout,
     )
 
     # Check that parameters are set correctly
     assert tcn_network.num_inputs == num_inputs
-    assert tcn_network.num_channels == num_channels
+    assert tcn_network.n_filters == n_filters
     assert tcn_network.kernel_size == kernel_size
     assert tcn_network.dropout == dropout
 
@@ -193,10 +193,10 @@ def test_tcn_network_single_layer():
 
     input_shape = (30, 2)
     num_inputs = 2
-    num_channels = [16]  # Single layer
+    n_filters = [16]  # Single layer
 
     tcn_network = TemporalConvolutionalNetwork(
-        num_inputs=num_inputs, num_channels=num_channels
+        num_inputs=num_inputs, n_filters=n_filters
     )
     input_layer, output_layer = tcn_network.build_network(input_shape)
 
