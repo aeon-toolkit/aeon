35e5b1c1a12bfd1fd992cea91c91f8d3baf293f0
Tony Bagnall <ajb@uea.ac.uk>
2025-07-19 12:17:05 +0100

    numba optimise

--- aeon/forecasting/_loss_functions.py
+++ aeon/forecasting/_loss_functions.py
@@ -3,6 +3,8 @@
 import numpy as np
 from numba import njit
 
+LOG_2PI = 1.8378770664093453
+
 
 @njit(cache=True, fastmath=True)
 def _arima_fit(params, data, model):
@@ -16,31 +18,43 @@ def _arima_fit(params, data, model):
     p = model[1]
     q = model[2]
     for t in range(n):
-        ar_term = 0
+        ar_term = 0.0
         max_ar = min(p, t)
         for j in range(max_ar):
             ar_term += formatted_params[1, j] * data[t - j - 1]
-        ma_term = 0
+        ma_term = 0.0
         max_ma = min(q, t)
         for j in range(max_ma):
             ma_term += formatted_params[2, j] * residuals[t - j - 1]
         y_hat = c + ar_term + ma_term
         residuals[t] = data[t] - y_hat
-    variance = np.mean(residuals**2)
-    LOG_2PI = 1.8378770664093453
+    sse = 0.0
+    for i in range(n):
+        sse += residuals[i] ** 2
+    variance = sse / n
     likelihood = n * (LOG_2PI + np.log(variance) + 1.0)
-    return likelihood + 2 * len(params)
+    k = len(params)
+    return likelihood + 2 * k
 
 
 @njit(cache=True, fastmath=True)
 def _extract_params(params, model):
     """Extract ARIMA parameters from the parameter vector."""
-    starts = np.cumsum(np.concatenate((np.zeros(1, dtype=np.int32), model[:-1])))
-    n = len(starts)
+    n_parts = len(model)
+    starts = np.zeros(n_parts, dtype=np.int32)
+    for i in range(1, n_parts):
+        starts[i] = starts[i - 1] + model[i - 1]
+
     max_len = np.max(model)
-    result = np.full((n, max_len), np.nan, dtype=params.dtype)
-    for i in range(n):
+    result = np.empty((n_parts, max_len), dtype=params.dtype)
+    for i in range(n_parts):
+        for j in range(max_len):
+            result[i, j] = np.nan
+
+    for i in range(n_parts):
         length = model[i]
         start = starts[i]
-        result[i, :length] = params[start : start + length]
+        for j in range(length):
+            result[i, j] = params[start + j]
+
     return result
