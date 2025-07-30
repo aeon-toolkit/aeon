f757c0ff0adf7e18ef4b915ac4386dcb4969012f
chrisholder <chrisholder987@hotmail.com>
2025-07-30 19:52:51 +0100

    debug

--- aeon/utils/_threading.py
+++ aeon/utils/_threading.py
@@ -57,13 +57,14 @@ def threaded(func: Callable) -> Callable:
             result = func(*args, **kwargs)
             return result
         finally:
-            if original_thread_count is None:
-                original_thread_count = 1
-            if not isinstance(original_thread_count, int):
-                raise TypeError(
-                    f"Expected original_thread_count to be an int, got type: "
-                    f"{type(original_thread_count)} value: {original_thread_count}"
+            try:
+                set_num_threads(original_thread_count)
+            except Exception:
+                raise ValueError(
+                    f"Failed to restore original thread count: {original_thread_count} "
+                    f"type {type(original_thread_count)} \n\n "
+                    f"Other debug info: {inspect.getsource(func)} \n\n Args: {args} "
+                    f"\n\n Kwargs: {kwargs}"
                 )
-            set_num_threads(original_thread_count)
 
     return wrapper
