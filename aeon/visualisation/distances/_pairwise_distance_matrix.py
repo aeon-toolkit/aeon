ec53645899a7bc9c0c23c8c8ea1c4c0dd91a1cb8
Kaustbh <desalekaustubh3@gmail.com>
2025-08-01 21:42:44 +0530

    adding support for 1D arrays

--- aeon/visualisation/distances/_pairwise_distance_matrix.py
+++ aeon/visualisation/distances/_pairwise_distance_matrix.py
@@ -39,7 +39,10 @@ def plot_pairwise_distance_matrix(
     import seaborn as sns
 
     plt.figure(1, figsize=(10, 10))
-    a_size, ts_length = a.shape
+    if len(a.shape) == 1:
+        a_size = ts_length = len(a)
+    else:
+        a_size, ts_length = a.shape
 
     left, bottom = 0.01, 0.1
     w_ts = h_ts = 0.2
@@ -82,9 +85,12 @@ def plot_pairwise_distance_matrix(
 
     ax_gram.axis("off")
 
-    # Plot the orange time series b.
+    if b.ndim == 1:
+        b = b[np.newaxis, :]  # reshape (1, len(b))
 
+    # Plot the orange time series b.
     for i in range(b.shape[0]):
+
         ax_s_x.plot(
             np.linspace(0.5, ts_length - 1.5, ts_length),
             b[i],
@@ -96,6 +102,9 @@ def plot_pairwise_distance_matrix(
     ax_s_x.set_xlim((0, a_size - 1))
     ax_s_x.set_title(r"$\mathbf{b}$", fontsize=15)
     ax_s_x.axis("off")
+
+    if a.ndim == 1:
+        a = a[np.newaxis, :]
 
     # Plot the blue time series a.
     for i in range(a.shape[0]):
