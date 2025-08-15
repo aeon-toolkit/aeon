3a16ad964eef9554cc6c08424e246586cb9c5c2a
hadifawaz1999 <hadifawaz2291999@gmail.com>
2025-08-15 16:11:52 +0200

    fix bug

--- aeon/datasets/__init__.py
+++ aeon/datasets/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
     "download_all_regression",
     "load_time_series_segmentation_benchmark",
     "load_human_activity_segmentation_datasets",
-    # rehabpile
+    # rehab_pile
     "load_rehab_pile_dataset",
     "load_rehab_pile_classification_datasets",
     "load_rehab_pile_regression_datasets",
@@ -92,12 +92,12 @@ from aeon.datasets._tsad_data_loaders import (
     load_from_timeeval_csv_file,
     load_kdd_tsad_135,
 )
-from aeon.datasets.rehabpile_loader.py import (
-    load_rehab_pile_dataset,
-    load_rehab_pile_classification_datasets,
-    load_rehab_pile_regression_datasets,
-)
 from aeon.datasets._tss_data_loaders import (
     load_human_activity_segmentation_datasets,
     load_time_series_segmentation_benchmark,
+)
+from aeon.datasets.rehabpile_loader import (
+    load_rehab_pile_classification_datasets,
+    load_rehab_pile_dataset,
+    load_rehab_pile_regression_datasets,
 )
