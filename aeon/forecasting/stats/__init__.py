fce5143bcc62c43779f96fbd75d94b21c885029f
Alex Banwell <arb1g19@soton.ac.uk>
2025-08-19 23:18:29 +0100

    Merge arima and autoarima files, also add parameter limits

--- aeon/forecasting/stats/__init__.py
+++ aeon/forecasting/stats/__init__.py
@@ -8,8 +8,7 @@ __all__ = [
     "TVPForecaster",
 ]
 
-from aeon.forecasting.stats._arima import ARIMA
-from aeon.forecasting.stats._auto_arima import AutoARIMA
+from aeon.forecasting.stats._arima import ARIMA, AutoARIMA
 from aeon.forecasting.stats._ets import ETS
 from aeon.forecasting.stats._theta import Theta
 from aeon.forecasting.stats._tvp import TVPForecaster
