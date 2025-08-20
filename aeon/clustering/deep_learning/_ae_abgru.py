bfa5b1fd3e68e0d0303683041ce9580a9ba5322c
Pablo Flores <pablo.flores@helsinki.fi>
2025-08-20 12:43:10 +0300

    [ENH] Validation loss for deep learning clustering #3039

--- aeon/clustering/deep_learning/_ae_abgru.py
+++ aeon/clustering/deep_learning/_ae_abgru.py
@@ -41,6 +41,8 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
         The number of epochs to train the model.
     batch_size : int, default = 16
         The number of samples per gradient update.
+    validation_split: float, default = 0
+        Fraction of the training data to be used as validation data.
     use_mini_batch_size : bool, default = True,
         Whether or not to use the mini batch size formula.
     random_state : int, RandomState instance or None, default=None
@@ -129,6 +131,7 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
         activation_decoder="relu",
         n_epochs=2000,
         batch_size=32,
+        validation_split=0,
         use_mini_batch_size=False,
         random_state=None,
         verbose=False,
@@ -157,6 +160,7 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
         self.callbacks = callbacks
         self.file_path = file_path
         self.n_epochs = n_epochs
+        self.validation_split = validation_split
         self.save_best_model = save_best_model
         self.save_last_model = save_last_model
         self.save_init_model = save_init_model
@@ -270,7 +274,7 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
                 ),
                 tf.keras.callbacks.ModelCheckpoint(
                     filepath=self.file_path + self.file_name_ + ".keras",
-                    monitor="loss",
+                    monitor="val_loss" if self.validation_split else "loss",
                     save_best_only=True,
                 ),
             ]
@@ -288,6 +292,7 @@ class AEAttentionBiGRUClusterer(BaseDeepClusterer):
             epochs=self.n_epochs,
             verbose=self.verbose,
             callbacks=self.callbacks_,
+            validation_split=self.validation_split,
         )
 
         try:
