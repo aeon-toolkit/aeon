1512ac40b51432c5dbb86992fbe2ac8efa2e609e
Balgopal Moharana <99070111+lucifer4073@users.noreply.github.com>
2025-09-23 22:48:52 +0530

    [ENH] Added `DeepARForecaster` to aeon/toolkit (#3051)
    * Basedeep forecaster added
    
    * init for basedlf added
    
    * test file and axis added for basedeepforecaster
    
    * test locally
    
    * dlf corrected
    
    * tf soft dep added
    
    * base fst changed
    
    * test file corrected
    
    * basedelf updated
    
    * test base chanegd
    
    * deepar netowork added with tests
    
    * deepar api ref added
    
    * current basedlf
    
    * base changed
    
    * save best model changed
    
    * conversations resolved
    
    * conversations resolved
    
    * Basedlf corrected
    
    * deepar forecaster made
    
    * gaussian loss added. use_prob arg added
    
    * gaussian loss corrected
    
    * gaussian_loss corrected
    
    ---------
    
    Co-authored-by: Tony Bagnall <a.j.bagnall@soton.ac.uk>

--- aeon/forecasting/deep_learning/__init__.py
+++ aeon/forecasting/deep_learning/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     "BaseDeepForecaster",
     "TCNForecaster",
+    "DeepARForecaster",
 ]
 
+from aeon.forecasting.deep_learning._deepar import DeepARForecaster
 from aeon.forecasting.deep_learning._tcn import TCNForecaster
 from aeon.forecasting.deep_learning.base import BaseDeepForecaster
