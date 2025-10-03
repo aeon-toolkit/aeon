de4180dab62bbd26f88a617ab8f43e1f53738f22
Pablo Flores <40040230+PipaFlores@users.noreply.github.com>
2025-10-03 12:39:43 +0300

    [ENH] Validation loss for deep learning clustering #3039 (#3046)
    * [ENH] Validation loss for deep learning clustering #3039
    
    * implemented validation on missing modules, validation>0, feature test #3039
    
    * Add missing docstrings

--- aeon/clustering/deep_learning/_ae_drnn.py
+++ aeon/clustering/deep_learning/_ae_drnn.py
@@ -55,6 +55,8 @@ class AEDRNNClusterer(BaseDeepClusterer):
         The number of epochs to train the model.
     batch_size : int, default = 16
         The number of samples per gradient update.
+    validation_split: float, default = 0
+        Fraction of the training data to be used as validation data.
     use_mini_batch_size : bool, default = True,
         Whether or not to use the mini batch size formula.
     random_state : int, RandomState instance or None, default=None
@@ -137,6 +139,7 @@ class AEDRNNClusterer(BaseDeepClusterer):
         activation_decoder="relu",
         n_epochs=2000,
         batch_size=32,
+        validation_split=0,
         use_mini_batch_size=False,
         random_state=None,
         verbose=False,
@@ -170,6 +173,7 @@ class AEDRNNClusterer(BaseDeepClusterer):
         self.callbacks = callbacks
         self.file_path = file_path
         self.n_epochs = n_epochs
+        self.validation_split = validation_split
         self.save_best_model = save_best_model
         self.save_last_model = save_last_model
         self.save_init_model = save_init_model
@@ -288,7 +292,7 @@ class AEDRNNClusterer(BaseDeepClusterer):
                 ),
                 tf.keras.callbacks.ModelCheckpoint(
                     filepath=self.file_path + self.file_name_ + ".keras",
-                    monitor="loss",
+                    monitor="val_loss" if self.validation_split > 0 else "loss",
                     save_best_only=True,
                 ),
             ]
@@ -304,6 +308,7 @@ class AEDRNNClusterer(BaseDeepClusterer):
             X,
             batch_size=mini_batch_size,
             epochs=self.n_epochs,
+            validation_split=self.validation_split,
             verbose=self.verbose,
             callbacks=self.callbacks_,
         )
