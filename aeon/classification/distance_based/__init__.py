378573025826d30b92ec8455ec83d95306dc94c3
chrisholder <chrisholder987@hotmail.com>
2025-12-03 04:15:25 +0000

    Nearest centroid added.

--- aeon/classification/distance_based/__init__.py
+++ aeon/classification/distance_based/__init__.py
@@ -5,9 +5,11 @@ __all__ = [
     "KNeighborsTimeSeriesClassifier",
     "ProximityTree",
     "ProximityForest",
+    "NearestCentroid",
 ]
 
 from aeon.classification.distance_based._elastic_ensemble import ElasticEnsemble
+from aeon.classification.distance_based._nearest_centroid import NearestCentroid
 from aeon.classification.distance_based._proximity_forest import ProximityForest
 from aeon.classification.distance_based._proximity_tree import ProximityTree
 from aeon.classification.distance_based._time_series_neighbors import (
