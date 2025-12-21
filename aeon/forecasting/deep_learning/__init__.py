7f6f153dcb8f4ed37257354777158793c5faf3a0
Aditya Kushwaha <adiksh006@gmail.com>
2025-12-21 17:29:28 +0530

    Remove accidentally committed DLinear implementation
    - Removed aeon/forecasting/deep_learning/_dlinear.py
    - Removed aeon/forecasting/deep_learning/tests/test_dlinear.py
    - Removed DLinearForecaster from __init__.py exports
    
    These files were part of a separate feature branch and should not
    have been committed to this branch.

--- aeon/forecasting/deep_learning/__init__.py
+++ aeon/forecasting/deep_learning/__init__.py
@@ -4,10 +4,8 @@ __all__ = [
     "BaseDeepForecaster",
     "TCNForecaster",
     "DeepARForecaster",
-    "DLinearForecaster",
 ]
 
 from aeon.forecasting.deep_learning._deepar import DeepARForecaster
-from aeon.forecasting.deep_learning._dlinear import DLinearForecaster
 from aeon.forecasting.deep_learning._tcn import TCNForecaster
 from aeon.forecasting.deep_learning.base import BaseDeepForecaster
