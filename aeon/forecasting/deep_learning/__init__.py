d6e7e465773839096b4611ce478c1984df1c9f36
Arpit Rawat <arpitrawat0404@gmail.com>
2026-01-05 16:50:55 +0530

    resolve error

--- aeon/forecasting/deep_learning/__init__.py
+++ aeon/forecasting/deep_learning/__init__.py
@@ -1,6 +1,10 @@
 """Initialization for aeon forecasting deep learning module."""
 
-__all__ = ["BaseDeepForecaster", "TCNForecaster", "DeepARForecaster"]
+__all__ = [
+    "BaseDeepForecaster",
+    "TCNForecaster",
+    "DeepARForecaster",
+]
 
 from aeon.forecasting.deep_learning._deepar import DeepARForecaster
 from aeon.forecasting.deep_learning._tcn import TCNForecaster
