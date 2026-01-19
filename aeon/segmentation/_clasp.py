5ba595c9e81d30711c04696c4148ba972e6363cc
vicky sharma <raghunathsharma296@gmail.com>
2026-01-19 21:55:10 +0530

    ENH: add primitive type hints to segmentation module

--- aeon/segmentation/_clasp.py
+++ aeon/segmentation/_clasp.py
@@ -15,7 +15,7 @@ from aeon.transformations.series import ClaSPTransformer
 from aeon.utils.validation import check_n_jobs
 
 
-def find_dominant_window_sizes(X, offset=0.05):
+def find_dominant_window_sizes(X, offset: float = 0.05):
     """Determine the Window-Size using dominant FFT-frequencies.
 
     Parameters
@@ -55,7 +55,7 @@ def find_dominant_window_sizes(X, offset=0.05):
     )
 
 
-def _is_trivial_match(candidate, change_points, n_timepoints, exclusion_radius=0.05):
+def _is_trivial_match(candidate: int, change_points, n_timepoints: int, exclusion_radius: float = 0.05):
     """Check if a candidate change point is in close proximity to other change points.
 
     Parameters
@@ -87,7 +87,7 @@ def _is_trivial_match(candidate, change_points, n_timepoints, exclusion_radius=0
     return False
 
 
-def _segmentation(X, clasp, n_change_points=None, exclusion_radius=0.05):
+def _segmentation(X, clasp, n_change_points = None, exclusion_radius: float = 0.05):
     """Segments the time series by extracting change points.
 
     Parameters
@@ -209,7 +209,7 @@ class ClaSPSegmenter(BaseSegmenter):
 
     _tags = {"capability:multithreading": True, "fit_is_empty": True}
 
-    def __init__(self, period_length=10, n_cps=1, exclusion_radius=0.05, n_jobs=1):
+    def __init__(self, period_length: int = 10, n_cps: int = 1, exclusion_radius: float = 0.05, n_jobs: int =1):
         self.period_length = int(period_length)
         self.n_cps = n_cps
         self.exclusion_radius = exclusion_radius
@@ -304,7 +304,7 @@ class ClaSPSegmenter(BaseSegmenter):
         return pd.IntervalIndex.from_arrays(start, end)
 
     @classmethod
-    def _get_test_params(cls, parameter_set="default"):
+    def _get_test_params(cls, parameter_set :str = "default"):
         """Return testing parameter settings for the estimator.
 
         Parameters
