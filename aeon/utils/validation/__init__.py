fec5d7322e4ce1255d6dd8d06b80e17ded068b2b
MatthewMiddlehurst <pfm15hbu@gmail.com>
2026-02-21 23:16:35 +0000

    keep njobs

--- aeon/utils/validation/__init__.py
+++ aeon/utils/validation/__init__.py
@@ -1,1 +1,36 @@
 """Validation and checking functions for time series."""
+
+__all__ = [
+    "check_n_jobs",
+]
+
+import os
+
+
+def check_n_jobs(n_jobs: int) -> int:
+    """Check `n_jobs` parameter according to the scikit-learn convention.
+
+    https://scikit-learn.org/stable/glossary.html#term-n_jobs
+
+    Parameters
+    ----------
+    n_jobs : int or None
+        The number of jobs for parallelization.
+        If None or 0, 1 is used.
+        If negative, (n_cpus + 1 + n_jobs) is used. In such a case, -1 would use all
+        available CPUs and -2 would use all but one. If the number of CPUs used would
+        fall under 1, 1 is returned instead.
+
+    Returns
+    -------
+    n_jobs : int
+        The number of threads to be used.
+    """
+    if n_jobs is None or n_jobs == 0:
+        return 1
+    elif not isinstance(n_jobs, int):
+        raise ValueError(f"`n_jobs` must be None or an integer, but found: {n_jobs}")
+    elif n_jobs < 0:
+        return max(1, os.cpu_count() + 1 + n_jobs)
+    else:
+        return n_jobs
