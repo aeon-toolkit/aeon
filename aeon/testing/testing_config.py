8c6be4c67462139021c111ca79d83da71952e7bb
MatthewMiddlehurst <pfm15hbu@gmail.com>
2026-02-22 16:43:22 +0000

    remove cif also

--- aeon/testing/testing_config.py
+++ aeon/testing/testing_config.py
@@ -57,8 +57,6 @@ EXCLUDED_TESTS = {
     # Unknown issue not producing the same results
     "RDSTRegressor": ["check_regressor_against_expected_results"],
     "RISTRegressor": ["check_regressor_against_expected_results"],
-    # Affected by threading changes in distance module
-    "CanonicalIntervalForestRegressor": ["check_regressor_against_expected_results"],
     # Requires y to be passed in inverse_transform,
     # but this is not currently enabled/supported
     "DifferenceTransformer": ["check_transform_inverse_transform_equivalent"],
