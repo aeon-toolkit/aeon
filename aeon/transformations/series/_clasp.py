d100bb19ecd7a1ba09fb432f8ca9136c4acc93a7
Nandinisingh07 <nandini.singhaiml2023@indoreinstitute.com>
2026-03-25 22:14:22 +0530

    ENH: add primitive type hints to ClaSPTransformer - relates to #1454

--- aeon/transformations/series/_clasp.py
+++ aeon/transformations/series/_clasp.py
@@ -1,5 +1,15 @@
 """
 ClaSP (Classification Score Profile) Transformer implementation.
+
+Notes
+-----
+As described in
+@inproceedings{clasp2021,
+  title={ClaSP - Time Series Segmentation},
+  author={Sch"afer, Patrick and Ermshaus, Arik and Leser, Ulf},
+  booktitle={CIKM},
+  year={2021}
+}
 """
 
 __maintainer__ = []
@@ -17,6 +27,20 @@ from aeon.utils.validation import check_n_jobs
 
 
 def _sliding_window(X, m: int):
+    """Return the sliding windows for a time series and a window size.
+
+    Parameters
+    ----------
+    X : array-like, shape = [n]
+        A single univariate time series of length n
+    m : int
+        The window size to generate sliding windows
+
+    Returns
+    -------
+    windows : array of shape [n-m+1, m]
+        The sliding windows of length over the time series of length n
+    """
     shape = X.shape[:-1] + (X.shape[-1] - m + 1, m)
     strides = X.strides + (X.strides[-1],)
     return np.lib.stride_tricks.as_strided(X, shape=shape, strides=strides)
@@ -38,7 +62,9 @@ def _sliding_dot_product(query, time_series):
         q_add = 1
 
     query = query[::-1]
+
     query = np.concatenate((query, np.zeros(n - m + time_series_add - q_add)))
+
     trim = m - 1 + time_series_add
 
     with objmode(dot_product="float64[:]"):
@@ -49,24 +75,58 @@ def _sliding_dot_product(query, time_series):
 
 @njit(fastmath=True, cache=True)
 def _sliding_mean_std(X, m: int):
+    """Return the sliding mean and std for a time series and a window size.
+
+    Parameters
+    ----------
+    X : array-like, shape [n]
+        A single univariate time series of length n
+    m : int
+        The window size to generate sliding windows
+
+    Returns
+    -------
+    Tuple (float, float)
+        The moving mean and moving std
+    """
     s = np.concatenate((np.zeros(1, dtype=np.float64), np.cumsum(X)))
     sSq = np.concatenate((np.zeros(1, dtype=np.float64), np.cumsum(X**2)))
     segSum = s[m:] - s[:-m]
     segSumSq = sSq[m:] - sSq[:-m]
     movmean = segSum / m
+
+    # avoid dividing by too small std, like 0
     movstd = np.sqrt(np.clip(segSumSq / m - (segSum / m) ** 2, 0, None))
     movstd = np.where(np.abs(movstd) < 0.001, 1, movstd)
     return [movmean, movstd]
 
 
 @njit(fastmath=True, cache=True, parallel=True)
-def _compute_distances_iterative(
-    X,
-    m: int,
-    k: int,
-    n_jobs: int = 1,
-    slack: float = 0.5,
-):
+def _compute_distances_iterative(X, m: int, k: int, n_jobs: int = 1, slack: float = 0.5):
+    """Compute kNN indices with dot-product.
+
+    No-loops implementation for a time series, given
+    a window size and k neighbours.
+
+    Parameters
+    ----------
+    X : array-like, shape [n]
+        A single univariate time series of length n
+    m : int
+        The window size to generate sliding windows
+    k : int
+        The number of nearest neighbors
+    n_jobs : int, default=1
+        Number of jobs to be used.
+    slack: float
+        Defines an exclusion zone around each subsequence to avoid trivial matches.
+        Defined as percentage of m. E.g. 0.5 is equal to half the window length.
+
+    Returns
+    -------
+    knns : array-like, shape = [n-m+1, k], dtype=int
+        The knns (offsets!) for each subsequence in X
+    """
     n = np.int32(X.shape[0] - m + 1)
     halve_m = int(m * slack)
 
@@ -83,8 +143,10 @@ def _compute_distances_iterative(
         dot_prev = None
         for order in np.arange(start, end):
             if order == start:
+                # first iteration O(n log n)
                 dot_rolled = _sliding_dot_product(X[start : start + m], X)
             else:
+                # constant time O(1) operations
                 dot_rolled = (
                     np.roll(dot_prev, 1)
                     + X[order + m - 1] * X[m - 1 : n + m]
@@ -97,6 +159,7 @@ def _compute_distances_iterative(
 
             dist = 2 * m * (1 - (dot_rolled - m * means * x_mean) / (m * stds * x_std))
 
+            # self-join: exclusion zone
             trivialMatchRange = (
                 int(max(0, order - halve_m)),
                 int(min(order + halve_m + 1, n)),
@@ -114,8 +177,25 @@ def _compute_distances_iterative(
 
 @njit(fastmath=True, cache=True)
 def _calc_knn_labels(knn_mask, split_idx: int, m: int):
+    """Compute kNN indices relabeling at a given split index.
+
+    Parameters
+    ----------
+    knn_mask : array-like, shape = [k, n-m+1], dtype=int
+        The knn indices for each subsequence
+    split_idx : int
+        The split index to use
+    m : int
+        The window size to generate sliding windows
+
+    Returns
+    -------
+    Tuple (array-like of shape=[n-m+1], array-like of shape=[n-m+1]):
+        True labels and predicted labels
+    """
     k_neighbours, n_timepoints = knn_mask.shape
 
+    # create labels for given potential split
     y_true = np.concatenate(
         (
             np.zeros(split_idx, dtype=np.int64),
@@ -125,15 +205,19 @@ def _calc_knn_labels(knn_mask, split_idx: int, m: int):
 
     knn_mask_labels = np.zeros(shape=(k_neighbours, n_timepoints), dtype=np.int64)
 
+    # relabel the kNN indices
     for i_neighbor in range(k_neighbours):
         neighbours = knn_mask[i_neighbor]
         knn_mask_labels[i_neighbor] = y_true[neighbours]
 
+    # compute kNN prediction
     ones = np.sum(knn_mask_labels, axis=0)
     zeros = k_neighbours - ones
     y_pred = np.asarray(ones > zeros, dtype=np.int64)
 
+    # apply exclusion zone at split point
     exclusion_zone = np.arange(split_idx - m, split_idx)
+    # exclusion_zone[exclusion_zone < 0] = 0
     y_pred[exclusion_zone] = np.ones(m, dtype=np.int64)
 
     return y_true, y_pred
@@ -141,6 +225,20 @@ def _calc_knn_labels(knn_mask, split_idx: int, m: int):
 
 @njit(fastmath=True, cache=False)
 def _binary_f1_score(y_true, y_pred):
+    """Compute f1-score.
+
+    Parameters
+    ----------
+    y_true : array-like, shape=[n-m+1], dtype = int
+        True integer labels for each subsequence
+    y_pred : array-like, shape=[n-m+1], dtype = int
+        Predicted integer labels for each subsequence
+
+    Returns
+    -------
+    F1 : float
+        F1-score
+    """
     f1_scores = np.zeros(shape=2, dtype=np.float64)
 
     for label in (0, 1):
@@ -150,6 +248,7 @@ def _binary_f1_score(y_true, y_pred):
 
         pr = tp / (tp + fp)
         re = tp / (tp + fn)
+
         f1 = 2 * (pr * re) / (pr + re)
         f1_scores[label] = f1
 
@@ -158,17 +257,38 @@ def _binary_f1_score(y_true, y_pred):
 
 @njit(fastmath=True, cache=True)
 def _roc_auc_score(y_score, y_true):
+    """Compute roc-auc score.
+
+    Parameters
+    ----------
+    y_true : array-like, shape=[n-m+1], dtype = int
+        True integer labels for each subsequence
+    y_pred : array-like, shape=[n-m+1], dtype = int
+        Predicted integer labels for each subsequence
+
+    Returns
+    -------
+    F1 : float
+        ROC-AUC-score
+    """
+    # make y_true a boolean vector
     y_true = y_true == 1
+
+    # sort scores and corresponding truth values (y_true is sorted by design)
     desc_score_indices = np.arange(y_score.shape[0])[::-1]
 
     y_score = y_score[desc_score_indices]
     y_true = y_true[desc_score_indices]
 
+    # y_score typically has many tied values. Here we extract
+    # the indices associated with the distinct values. We also
+    # concatenate a value for the end of the curve.
     distinct_value_indices = np.where(np.diff(y_score))[0]
     threshold_idxs = np.concatenate(
         (distinct_value_indices, np.array([y_true.size - 1]))
     )
 
+    # accumulate the true positives with decreasing threshold
     tps = np.cumsum(y_true)[threshold_idxs]
     fps = 1 + threshold_idxs - tps
 
@@ -184,13 +304,39 @@ def _roc_auc_score(y_score, y_true):
     if fpr.shape[0] < 2:
         return np.nan
 
+    direction = 1
     dx = np.diff(fpr)
-    direction = 1 if not np.any(dx < 0) else -1
-    return direction * np.trapz(tpr, fpr)
+
+    if np.any(dx < 0):
+        if np.all(dx <= 0):
+            direction = -1
+        else:
+            return np.nan
+
+    area = direction * np.trapz(tpr, fpr)
+    return area
 
 
 @njit(fastmath=True)
 def _calc_profile(m: int, knn_mask, score, exclusion_zone: int):
+    """Calculate ClaSP profile for the kNN indices and a score.
+
+    Parameters
+    ----------
+    m : int
+        The window size to generate sliding windows
+    knn_mask : array-like, shape = [k, n-m+1], dtype=int
+        The knn indices
+    score : function
+        Scoring method used
+    exclusion_zone : int
+        Exclusion zone
+
+    Returns
+    -------
+    profile : array-like, shape=[n-m+1], dtype = float
+        The ClaSP
+    """
     n_timepoints = knn_mask.shape[1]
     profile = np.full(shape=n_timepoints, fill_value=np.nan, dtype=np.float64)
 
@@ -210,6 +356,30 @@ def clasp(
     exclusion_radius: float = 0.05,
     n_jobs: int = 1,
 ):
+    """Calculate ClaSP for a time series and a window size.
+
+    Parameters
+    ----------
+    X : array-like, shape = [n]
+        A single univariate time series of length n
+    m : int
+        The window size to generate sliding windows
+    k_neighbours : int
+        The number of knn to use
+    score : function
+        Scoring method used
+    interpolate : bool
+        Interpolate the profile
+    exclusion_radius : float
+        Blind spot of the profile to the corners
+    n_jobs : int
+        Number of jobs to be used.
+
+    Returns
+    -------
+    Tuple (array-like of shape [n], array-like of shape [k_neighbours, n])
+        The ClaSP and the knn_mask
+    """
     knn_mask = _compute_distances_iterative(X, m, k_neighbours, n_jobs=n_jobs).T
 
     n_timepoints = knn_mask.shape[1]
@@ -223,6 +393,54 @@ def clasp(
 
 
 class ClaSPTransformer(BaseSeriesTransformer):
+    """ClaSP (Classification Score Profile) Transformer.
+
+    Implementation of the Classification Score Profile of a time series.
+    ClaSP hierarchically splits a TS into two parts, where each split point is
+    determined by training a binary TS classifier for each possible split point and
+    selecting the one with highest accuracy, i.e., the one that is best at identifying
+    subsequences to be from either of the partitions.
+
+    Parameters
+    ----------
+    window_length : int, default = 10
+        size of window for sliding.
+    scoring_metric : str, default = "ROC_AUC"
+        the scoring metric to use in ClaSP - choose from ROC_AUC or F1
+    exclusion_radius : float, default = 0.05
+        Exclusion Radius for change points to be non-trivial matches
+    n_jobs : int, default = 1
+        Number of jobs to be used.
+
+    Notes
+    -----
+    As described in
+    @inproceedings{clasp2021,
+      title={ClaSP - Time Series Segmentation},
+      author={Sch"afer, Patrick and Ermshaus, Arik and Leser, Ulf},
+      booktitle={CIKM},
+      year={2021}
+    }
+
+    Examples
+    --------
+    >>> from aeon.transformations.series import ClaSPTransformer
+    >>> from aeon.segmentation import find_dominant_window_sizes
+    >>> from aeon.datasets import load_electric_devices_segmentation
+    >>> X, true_period_size, true_cps = load_electric_devices_segmentation()
+    >>> dominant_period_size = find_dominant_window_sizes(X)
+    >>> clasp = ClaSPTransformer(window_length=dominant_period_size).fit(X)
+    >>> profile = clasp.transform(X)
+    """
+
+    _tags = {
+        "input_data_type": "Series",
+        "output_data_type": "Series",
+        "X_inner_type": "np.ndarray",
+        "fit_is_empty": True,
+        "requires_y": False,
+        "capability:multithreading": True,
+    }
 
     def __init__(
         self,
@@ -238,15 +456,42 @@ class ClaSPTransformer(BaseSeriesTransformer):
         super().__init__(axis=0)
 
     def _transform(self, X, y=None):
+        """Compute ClaSP.
+
+        Takes as input a single time series dataset and returns the
+        Classification Score profile for that single time series.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            A univariate time series
+        y : ignored argument for interface compatibility
+            Additional data, e.g., labels for transformation
+
+        Returns
+        -------
+        Xt : 1D numpy.ndarray
+            transformed version of X
+            ClaSP of the single time series as output
+            with length as (n-window_length+1)
+        """
         n_jobs = check_n_jobs(self.n_jobs)
 
         if len(X) - self.window_length < 2 * self.exclusion_radius * len(X):
-            warnings.warn("Period-Length is larger than size of the time series")
+            warnings.warn(
+                "Period-Length is larger than size of the time series", stacklevel=1
+            )
 
         if X.dtype != np.float64:
-            warnings.warn(f"dtype is {X.dtype}, converting to float64")
+            warnings.warn(
+                f"dtype is {X.dtype} but should be {np.float64}. "
+                f"Will apply conversion to float64 now",
+                stacklevel=1,
+            )
 
         scoring_metric_call = self._check_scoring_metric(self.scoring_metric)
+
+        # The input has to be of type float64
         X = X.flatten().astype(np.float64)
 
         Xt, _ = clasp(
@@ -260,9 +505,25 @@ class ClaSPTransformer(BaseSeriesTransformer):
         return Xt
 
     def _check_scoring_metric(self, scoring_metric: str):
+        """Check which scoring metric to use.
+
+        Parameters
+        ----------
+        scoring_metric : str
+            Choose from "ROC_AUC" or "F1"
+
+        Returns
+        -------
+        scoring_metric_call : a callable, keyed by the `scoring_metric` input
+            _roc_auc_score, if scoring_metric = "ROC_AUC"
+            _binary_f1_score, if scoring_metric = "F1"
+        """
+        valid_scores = ("ROC_AUC", "F1")
+
+        if scoring_metric not in valid_scores:
+            raise ValueError(f"invalid input, please use one of {valid_scores}")
+
         if scoring_metric == "ROC_AUC":
             return _roc_auc_score
         elif scoring_metric == "F1":
             return _binary_f1_score
-        else:
-            raise ValueError("Invalid scoring metric")
