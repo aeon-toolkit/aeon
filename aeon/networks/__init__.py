91ce6cf3e08aa96ce28ef36ee224259b3fc0dcdd
hadifawaz1999 <hadifawaz2291999@gmail.com>
2026-06-18 23:51:14 +0200

    precommit fix

--- aeon/networks/__init__.py
+++ aeon/networks/__init__.py
@@ -30,7 +30,6 @@ from aeon.networks.auto_encoder._ae_dcnn import AEDCNNNetwork
 from aeon.networks.auto_encoder._ae_drnn import AEDRNNNetwork
 from aeon.networks.auto_encoder._ae_fcn import AEFCNNetwork
 from aeon.networks.auto_encoder._ae_resnet import AEResNetNetwork
-from aeon.networks.encoder._nbeats import NBeatsNetwork
 from aeon.networks.base import BaseDeepLearningNetwork
 from aeon.networks.encoder._cnn import TimeCNNNetwork
 from aeon.networks.encoder._dcnn import DCNNNetwork
@@ -40,6 +39,7 @@ from aeon.networks.encoder._fcn import FCNNetwork
 from aeon.networks.encoder._inception import InceptionNetwork
 from aeon.networks.encoder._lite import LITENetwork
 from aeon.networks.encoder._mlp import MLPNetwork
+from aeon.networks.encoder._nbeats import NBeatsNetwork
 from aeon.networks.encoder._resnet import ResNetNetwork
 from aeon.networks.encoder._rnn import RecurrentNetwork
 from aeon.networks.encoder._tcn import TCNNetwork
