fix: bypass variance checks on short intervals and fix ETS predict logic

aeon/base/_base_series.py
@@ -79,6 +79,7 @@ class BaseSeriesEstimator(BaseAeonEstimator):
         "capability:univariate": True,
         "capability:multivariate": False,
         "X_inner_type": "np.ndarray",  # one of VALID_SERIES_INNER_TYPES
+        "capability:small_scale": False,
     }
 
     @abstractmethod
@@ -203,6 +204,9 @@ class BaseSeriesEstimator(BaseAeonEstimator):
         allow_multivariate = self.get_tag("capability:multivariate")
         allow_univariate = self.get_tag("capability:univariate")
         allow_missing = self.get_tag("capability:missing_values")
+        allow_small_scale = self.get_tag(
+            "capability:small_scale", raise_error=False, tag_value_default=False
+        )
         if metadata["missing_values"] and not allow_missing:
             raise ValueError(
                 f"Missing values not supported by {self.__class__.__name__}"
@@ -216,7 +220,8 @@ class BaseSeriesEstimator(BaseAeonEstimator):
                 f"Univariate data not supported by {self.__class__.__name__}"
             )
 
-        check_series_variance(X, axis=axis)
+        if not allow_small_scale:
+            check_series_variance(X, axis=axis)
 
         return metadata
 

aeon/forecasting/stats/_ets.py
@@ -249,7 +249,31 @@ class ETS(BaseForecaster, IterativeForecastingMixin):
         float
             single prediction self.horizon steps ahead of y.
         """
-        return self.forecast_
+        data = np.asarray(y.squeeze(), dtype=np.float64)
+        (
+            _,
+            level,
+            trend,
+            seasonality,
+            n_timepoints,
+            _,
+            _,
+            _,
+            _,
+            _,
+        ) = _ets_fit(self.parameters_, data, self._model)
+        forecast = _numba_predict(
+            self._trend_type,
+            self._seasonality_type,
+            level,
+            trend,
+            seasonality,
+            self.phi_,
+            self.horizon,
+            n_timepoints,
+            self._seasonal_period,
+        )
+        return forecast
 
     def _initialise(self, data):
         """