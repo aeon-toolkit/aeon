8b6dd0bd5b01fe5bb8200dfce499c4af1be8a892
GiGiKoneti <gigikoneti@gmail.com>
2026-07-02 00:53:04 +0530

    [DOC] Round CES/AutoCES docstring predictions and revert workflow changes

--- aeon/forecasting/stats/_ces.py
+++ aeon/forecasting/stats/_ces.py
@@ -209,8 +209,8 @@ class CES(BaseForecaster, IterativeForecastingMixin):
     >>> from aeon.forecasting.stats import CES
     >>> y = np.array([2.1, 2.4, 2.8, 3.0, 3.6, 4.1, 4.4, 4.9, 5.3, 5.9])
     >>> f = CES()
-    >>> f.iterative_forecast(y, prediction_horizon=2)
-    array([6.38295867, 7.05058121])
+    >>> f.iterative_forecast(y, prediction_horizon=2).round(4)
+    array([6.383 , 7.0506])
     """
 
     _tags = {
@@ -616,8 +616,8 @@ class AutoCES(BaseForecaster, IterativeForecastingMixin):
     >>> from aeon.forecasting.stats import AutoCES
     >>> y = np.array([2.1, 2.4, 2.8, 3.0, 3.6, 4.1, 4.4, 4.9, 5.3, 5.9])
     >>> f = AutoCES()
-    >>> f.iterative_forecast(y, prediction_horizon=2)
-    array([6.38295867, 7.05058121])
+    >>> f.iterative_forecast(y, prediction_horizon=2).round(4)
+    array([6.383 , 7.0506])
     """
 
     _tags = {
