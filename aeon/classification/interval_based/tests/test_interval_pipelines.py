8c9903b530a870a5908064ea4ea417febe25c686
aashoday <aashoday.ss@gmail.com>
2026-08-13 13:35:30 +0530

    [MNT] add lifecycle tests for interval classifier attributes

--- aeon/classification/interval_based/tests/test_interval_pipelines.py
+++ aeon/classification/interval_based/tests/test_interval_pipelines.py
@@ -28,3 +28,64 @@ def test_interval_pipeline_classifiers(cls):
     clf.fit(X_train, y_train)
     prob = clf.predict_proba(X_test)
     _assert_predict_probabilities(prob, X_test, n_classes=2)
+
+
+def test_interval_classifier_estimator_attribute_lifecycle():
+    """Test estimator and transformer attributes are created only on fit."""
+    from aeon.classification.interval_based import (
+        QUANTClassifier,
+        RandomIntervalClassifier,
+        SupervisedIntervalClassifier,
+    )
+    from aeon.testing.data_generation import make_example_3d_numpy
+
+    X, y = make_example_3d_numpy(n_cases=10, n_channels=1, n_timepoints=20)
+
+    for cls in [
+        QUANTClassifier,
+        RandomIntervalClassifier,
+        SupervisedIntervalClassifier,
+    ]:
+        params = cls._get_test_params()
+        if isinstance(params, list):
+            params = params[0]
+
+        clf = cls(**params)
+
+        assert not hasattr(clf, "estimator_")
+        assert not hasattr(clf, "_estimator")
+        assert not hasattr(clf, "transformer_")
+        assert not hasattr(clf, "_transformer")
+
+        clf.fit(X, y)
+
+        assert hasattr(clf, "estimator_")
+        assert not hasattr(clf, "_estimator")
+        assert hasattr(clf, "transformer_")
+        assert not hasattr(clf, "_transformer")
+
+
+def test_rstsf_transformers_attribute_lifecycle():
+    """Test transformers and series_transformers attributes are created only on fit."""
+    from aeon.classification.interval_based import RSTSF
+    from aeon.testing.data_generation import make_example_3d_numpy
+
+    X, y = make_example_3d_numpy(n_cases=10, n_channels=1, n_timepoints=20)
+
+    params = RSTSF._get_test_params()
+    if isinstance(params, list):
+        params = params[0]
+
+    clf = RSTSF(**params)
+
+    assert not hasattr(clf, "transformers_")
+    assert not hasattr(clf, "_transformers")
+    assert not hasattr(clf, "series_transformers_")
+    assert not hasattr(clf, "_series_transformers")
+
+    clf.fit(X, y)
+
+    assert hasattr(clf, "transformers_")
+    assert not hasattr(clf, "_transformers")
+    assert hasattr(clf, "series_transformers_")
+    assert not hasattr(clf, "_series_transformers")
