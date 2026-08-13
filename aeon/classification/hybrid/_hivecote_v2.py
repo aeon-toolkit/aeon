5d3b55dbc3ba8200c3aa98d9443051af47ddc4df
aashoday <aashoday.ss@gmail.com>
2026-08-13 10:41:03 +0530

    [MNT] make estimators_ attribute public in HIVECOTEV2

--- aeon/classification/hybrid/_hivecote_v2.py
+++ aeon/classification/hybrid/_hivecote_v2.py
@@ -84,6 +84,8 @@ class HIVECOTEV2(_BaseHIVECOTE):
     component_probas : dict
         Only used if save_component_probas is true. Saved probability predictions for
         each HIVE-COTEV2 component.
+    estimators_ : list of tuples
+        The fitted estimators for the ensemble.
 
     See Also
     --------
@@ -202,9 +204,9 @@ class HIVECOTEV2(_BaseHIVECOTE):
             self._arsenal_params["time_limit_in_minutes"] = ct
             self._tde_params["time_limit_in_minutes"] = ct
 
-        # Build component estimators (stored in _estimators to avoid mutating
+        # Build component estimators (stored in estimators_ to avoid mutating
         # the self.estimators init parameter, for scikit-learn compatibility)
-        self._estimators = [
+        self.estimators_ = [
             ("STC", ShapeletTransformClassifier(**self._stc_params)),
             ("DrCIF", DrCIFClassifier(**self._drcif_params)),
             ("Arsenal", Arsenal(**self._arsenal_params)),
