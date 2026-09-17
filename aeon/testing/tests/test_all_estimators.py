d1ef067d24c3e873e186b945c8b840d8a53c0f0b
MatthewMiddlehurst <pfm15hbu@gmail.com>
2026-09-17 16:12:17 +0100

    Map PR matrix Python versions 3.12-3.14 to distinct subsample indices

--- aeon/testing/tests/test_all_estimators.py
+++ aeon/testing/tests/test_all_estimators.py
@@ -13,16 +13,18 @@ from aeon.utils.discovery import all_estimators
 ALL_TEST_ESTIMATORS = all_estimators(return_names=False, include_sklearn=False)
 
 # subsample estimators by OS & python version
-# this ensures that only a 1/3 of estimators are tested for a given combination
-# but all are tested on every OS at least once, and on every python version once
+# this ensures that only 1/3 of estimators are tested for a given combination
+# but across the PR pytest matrix all are tested on every OS and python version once
 if PR_TESTING:
-    # only use 3 Python versions in PR
+    # map the 3 Python versions used in the PR pytest matrix to distinct indices.
+    # other versions keep their minor version, i.e. 3.11, which only runs with PR
+    # testing in the soft dependency skip job and is not part of the matrix rotation
     i = sys.version_info.minor
-    if i == 10:
+    if i == 12:
         i = 0
-    elif i == 12:
-        i = 1
     elif i == 13:
+        i = 1
+    elif i == 14:
         i = 2
 
     os_str = platform.system()
