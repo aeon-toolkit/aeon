Fixed dtype normalization, added int16 tests

aeon/transformations/collection/_slope.py
@@ -61,7 +61,8 @@ class SlopeTransformer(BaseCollectionTransformer):
         n_cases, n_channels, n_timepoints = X.shape
         self._check_parameters(n_timepoints)
 
-        X = X / 1
+        dtype = np.float32 if X.dtype == np.float32 else np.float64
+        X = X.astype(dtype, copy=False)
 
         Xt = np.empty(
             (n_cases, n_channels, self.n_intervals),

aeon/transformations/collection/interval_based/_supervised_intervals.py
@@ -241,7 +241,8 @@ class SupervisedIntervals(BaseCollectionTransformer):
         return self
 
     def _transform(self, X, y=None):
-        X = X / 1
+        dtype = np.float32 if X.dtype == np.float32 else np.float64
+        X = X.astype(dtype, copy=False)
 
         transform = Parallel(
             n_jobs=self._n_jobs, backend=self.parallel_backend, prefer="threads"
@@ -263,7 +264,8 @@ class SupervisedIntervals(BaseCollectionTransformer):
         return Xt
 
     def _fit_setup(self, X, y):
-        X = X / 1
+        dtype = np.float32 if X.dtype == np.float32 else np.float64
+        X = X.astype(dtype, copy=False)
 
         self.intervals_ = []
 

aeon/transformations/series/_pla.py
@@ -88,8 +88,8 @@ class PLASeriesTransformer(BaseSeriesTransformer):
         if not (self.buffer_size is None or isinstance(self.buffer_size, (int, float))):
             raise ValueError("Invalid buffer_size: use a number only or keep empty.")
         results = None
-        X = X / 1
-        X = np.concatenate(X)
+        dtype = np.float32 if X.dtype == np.float32 else np.float64
+        X = np.concatenate(X).astype(dtype, copy=False)
         if isinstance(self.transformer, (str)):
             if self.transformer.lower() == "sliding window":
                 results = self._sliding_window(X)